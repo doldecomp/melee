5109ed6400b6bd875878ce3b08a178b9ec1454c1
Will C <wcarter312@gmail.com>
2020-11-20 12:15:04 -0600

    Fix alloc_data typing in tev

--- src/sysdolphin/tev.c
+++ src/sysdolphin/tev.c
@@ -1,12 +1,10 @@
 #include "tev.h"
 
-#include "objalloc.h"
-
 extern void* lbl_80405B98;
 
-extern u8 lbl_804C07F8[0x1C]; // render_alloc_data
-extern u8 lbl_804C0824[0x14]; // tevreg_alloc_data
-extern u8 lbl_804C0850[0x30]; // chan_alloc_data
+extern HSD_ObjAllocData lbl_804C07F8; // render_alloc_data
+extern HSD_ObjAllocData lbl_804C0824; // tevreg_alloc_data
+extern HSD_ObjAllocData lbl_804C0850; // chan_alloc_data
 
 extern s32 lbl_804D7600; // state_num_chans
 extern void* lbl_804D7604; // chan_chan
@@ -49,17 +47,17 @@ asm void HSD_RenderInitAllocData(void)
 }
 #endif
 
-void* HSD_RenderGetAllocData(void)
+HSD_ObjAllocData* HSD_RenderGetAllocData(void)
 {
     return &lbl_804C07F8;
 }
 
-void* HSD_TevRegGetAllocData(void)
+HSD_ObjAllocData* HSD_TevRegGetAllocData(void)
 {
     return &lbl_804C0824;
 }
 
-void* HSD_ChanGetAllocData(void)
+HSD_ObjAllocData* HSD_ChanGetAllocData(void)
 {
     return &lbl_804C0850;
 }
