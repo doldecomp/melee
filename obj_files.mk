d406d36be2472d08e8b3f1e0128b7d428b4b2e56
Will C <wcarter312@gmail.com>
2021-08-25 14:42:42 -0500

    Split ef section of code from text_3.s

--- obj_files.mk
+++ obj_files.mk
@@ -26,6 +26,8 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/melee/text_2.o 		 	            \
 	$(BUILD_DIR)/asm/melee/text_3.o 		 	            \
 															\
+	$(BUILD_DIR)/asm/melee/ef/code_8005B4B8.o				\
+															\
 	$(BUILD_DIR)/asm/melee/ft/code_800679B0.o 		 	\
 	$(BUILD_DIR)/asm/melee/ft/ftdonkey_1.o 		    \
 	$(BUILD_DIR)/asm/melee/ft/ftmario.o 			            \
