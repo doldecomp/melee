1b1eed45b574c2a44f5e0db0a55a80b6f89da297
Will C <wcarter312@gmail.com>
2021-10-02 21:38:59 -0500

    Second pass on cleanup

--- asm/data.s
+++ asm/data.s
@@ -12604,7 +12604,101 @@ lbl_803C710C:
 .global lbl_803C7120
 lbl_803C7120:
     .4byte 0xFFFFFFFF
-    .incbin "baserom.dol", 0x3C4124, 0x17C
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0xFFFFFFFF
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000127
+    .4byte 0x00340111
+    .4byte 0x12000000
+    .4byte func_800E0E18
+    .4byte func_800E0E54
+    .4byte func_800E0E84
+    .4byte func_800E0EA4
+    .4byte func_800761C8
+    .4byte 0x00000128
+    .4byte 0x00340511
+    .4byte 0x12000000
+    .4byte func_800E10B0
+    .4byte func_800E10EC
+    .4byte func_800E111C
+    .4byte func_800E113C
+    .4byte func_800761C8
+    .4byte 0x00000129
+    .4byte 0x00341012
+    .4byte 0x13000000
+    .4byte func_800E1550
+    .4byte func_800E15C8
+    .4byte func_800E15D0
+    .4byte func_800E1840
+    .4byte func_800761C8
+    .4byte 0x0000012A
+    .4byte 0x00341012
+    .4byte 0x13000000
+    .4byte func_800E158C
+    .4byte func_800E15CC
+    .4byte func_800E16E0
+    .4byte func_800E187C
+    .4byte func_800761C8
+    .4byte 0x0000012B
+    .4byte 0x00340213
+    .4byte 0x14000000
+    .4byte func_800E1B24
+    .4byte func_800E1BE4
+    .4byte func_800E1E74
+    .4byte func_800E1F70
+    .4byte func_800761C8
+    .4byte 0x0000012C
+    .4byte 0x00340613
+    .4byte 0x14000000
+    .4byte func_800E1B84
+    .4byte func_800E1D2C
+    .4byte func_800E1EAC
+    .4byte func_800E1FE0
+    .4byte func_800761C8
+    .4byte 0x0000012D
+    .4byte 0x00340214
+    .4byte 0x15000000
+    .4byte func_800E22BC
+    .4byte func_800E23DC
+    .4byte func_800E23E4
+    .4byte func_800E25C4
+    .4byte func_800761C8
+    .4byte 0x0000012E
+    .4byte 0x00340614
+    .4byte 0x15000000
+    .4byte func_800E2308
+    .4byte func_800E23E0
+    .4byte func_800E2508
+    .4byte func_800E2778
+    .4byte func_800761C8
+    .4byte 0x0000000E
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C7158
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x0000000F
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C7200
+    .4byte 0x00000000
+    .4byte 0x00000000
 .global lbl_803C72A0
 lbl_803C72A0:
     .asciz "A@"
@@ -12661,13 +12755,210 @@ lbl_803C9DD0:
 	.incbin "baserom.dol", 0x3C6DD0, 0x84
 .global lbl_803C9E54
 lbl_803C9E54:
-	.incbin "baserom.dol", 0x3C6E54, 0x174
+    .4byte 0x800F93CC
+    .4byte 0x800FE240
+    .4byte 0x800F9A54
+    .4byte 0x800FFA10
+    .4byte 0x800F6070
+    .4byte 0x800FA958
+    .4byte 0x800FBA00
+    .4byte 0x801061E4
+    .4byte 0x800FEDD0
+    .4byte 0x8010C560
+    .4byte 0x80108E14
+    .4byte 0x00000000
+    .4byte 0x800FA064
+    .4byte 0x800FD020
+    .4byte 0x801094FC
+    .4byte 0x80101618
+    .4byte 0x80107638
+    .4byte 0x800F9684
+    .4byte 0x8010B4A0
+    .4byte 0x80105BA8
+    .4byte 0x800FBA00
+    .4byte 0x800F93CC
+    .4byte 0x800FE240
+    .4byte 0x800FA064
+    .4byte 0x8010D204
+    .4byte 0x800F9A54
+    .4byte 0x8010B4A0
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x800FA958
+    .4byte 0x00000000
+.global lbl_803C9ED8
+lbl_803C9ED8:
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+.global lbl_803C9F08
+lbl_803C9F08:
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+.global lbl_803C9F38
+lbl_803C9F38:
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+.global lbl_803C9F68
+lbl_803C9F68:
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+.global lbl_803C9F98
+lbl_803C9F98:
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
 .global lbl_803C9FC8
 lbl_803C9FC8:
-	.incbin "baserom.dol", 0x3C6FC8, 0x104
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte lbl_803C9ED8
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte lbl_803C9F08
+    .4byte lbl_803C9F38
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte lbl_803C9F68
+    .4byte 0x00000000
+    .4byte lbl_803C9F98
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x0000000E
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C737C
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x0000000F
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C737C
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000010
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C737C
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte 0x00000011
+    .4byte 0x00000000
+    .4byte 0x01000000
+    .4byte 0x00000000
+    .4byte 0x00000000
+    .4byte func_800C7414
+    .4byte 0x00000000
+    .4byte 0x00000000
 .global jtbl_803CA0CC
 jtbl_803CA0CC:
-	.incbin "baserom.dol", 0x3C70CC, 0x80
+    .4byte lbl_800EE854
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE8EC
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE874
+    .4byte lbl_800EE904
+    .4byte lbl_800EE904
+    .4byte lbl_800EE8B0
+    .4byte 0x66696768
+    .4byte 0x74657220
+    .4byte 0x70617274
+    .4byte 0x73206D6F
+    .4byte 0x64656C20
+    .4byte 0x646F626A
+    .4byte 0x206E756D
+    .4byte 0x206F7665
+    .4byte 0x72210A00
+    .4byte 0x66746B69
+    .4byte 0x7262792E
+    .4byte 0x63000000
+    .4byte 0x66696768
+    .4byte 0x74657220
+    .4byte 0x646F626A
+    .4byte 0x206E756D
+    .4byte 0x206F7665
+    .4byte 0x72210A00
 .global jtbl_803CA14C
 jtbl_803CA14C:
 	.incbin "baserom.dol", 0x3C714C, 0x80
