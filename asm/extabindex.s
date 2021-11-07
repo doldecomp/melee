8ce78dd73839cfea8f29877a0ea9fc216daca926
Will C <wcarter312@gmail.com>
2021-11-06 19:46:30 -0500

    Relabel using intns's tool

--- asm/extabindex.s
+++ asm/extabindex.s
@@ -83,73 +83,73 @@ lbl_800056C0:
     .4byte func_8039F6CC
     .4byte 0x000000C0
     .4byte 0x800055F0
-    .4byte 0x8039F78C
+	.4byte func_8039F78C
     .4byte 0x00000110
     .4byte 0x800055F8
-    .4byte 0x8039F89C
+	.4byte func_8039F89C
     .4byte 0x0000018C
     .4byte 0x80005600
-    .4byte 0x8039FC70
+	.4byte func_8039FC70
     .4byte 0x00000418
     .4byte 0x80005608
-    .4byte 0x803A0088
+	.4byte func_803A0088
     .4byte 0x00003898
     .4byte 0x80005610
-    .4byte 0x803A3920
+	.4byte func_803A3920
     .4byte 0x00000130
     .4byte 0x80005618
-    .4byte 0x803A3A5C
+	.4byte func_803A3A5C
     .4byte 0x000006DC
     .4byte 0x80005620
-    .4byte 0x803A4138
+	.4byte func_803A4138
     .4byte 0x00000040
     .4byte 0x80005628
-    .4byte 0x803A4178
+	.4byte func_803A4178
     .4byte 0x000000E4
     .4byte 0x80005630
-    .4byte 0x803A425C
+	.4byte func_803A425C
     .4byte 0x00000038
     .4byte 0x80005638
-    .4byte 0x803A4294
+	.4byte func_803A4294
     .4byte 0x00000038
     .4byte 0x80005640
-    .4byte 0x803A4344
+	.4byte func_803A4344
     .4byte 0x000000A4
     .4byte 0x80005648
-    .4byte 0x803A43E8
+	.4byte func_803A43E8
     .4byte 0x000000BC
     .4byte 0x80005650
-    .4byte 0x803B3344
+	.4byte func_803B3344
     .4byte 0x00000054
     .4byte 0x80005658
-    .4byte 0x803B3398
+	.4byte func_803B3398
     .4byte 0x00000070
     .4byte 0x80005660
-    .4byte 0x803B3CD8
+	.4byte func_803B3CD8
     .4byte 0x000009FC
     .4byte 0x80005668
-    .4byte 0x803B46D4
+	.4byte func_803B46D4
     .4byte 0x00000358
     .4byte 0x80005670
-    .4byte 0x803B4A2C
+	.4byte func_803B4A2C
     .4byte 0x00000338
     .4byte 0x80005678
-    .4byte 0x803B4D64
+	.4byte func_803B4D64
     .4byte 0x00000464
     .4byte 0x80005680
-    .4byte 0x803B51C8
+	.4byte func_803B51C8
     .4byte 0x00000A64
     .4byte 0x80005688
-    .4byte 0x803B5C4C
+	.4byte func_803B5C4C
     .4byte 0x00000124
     .4byte 0x80005690
-    .4byte 0x803B5D70
+	.4byte func_803B5D70
     .4byte 0x00000130
     .4byte 0x80005698
-    .4byte 0x803B5EA0
+	.4byte func_803B5EA0
     .4byte 0x00000314
     .4byte 0x800056A0
-    .4byte 0x803B6BE4
+	.4byte func_803B6BE4
     .4byte 0x0000064C
     .4byte 0x800056A8
 
