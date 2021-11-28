167c28348073576c4787f26f7d1b29999cd62123
Will C <wcarter312@gmail.com>
2021-11-28 17:03:12 -0600

    Match HSD_TExpRef

--- src/sysdolphin/baselib/texp.c
+++ src/sysdolphin/baselib/texp.c
@@ -12,3 +12,39 @@ HSD_TExpType HSD_TExpGetType(HSD_TExp* texp) {
     }
     return texp->type;
 }
+
+inline HSD_TExpType HSD_TExpGetType_inline(HSD_TExp* texp) {
+    if (texp == NULL) {
+        return HSD_TE_ZERO;
+    }
+    if ((u32)texp == -1) {
+        return HSD_TE_TEX;
+    }
+    if ((u32)texp == -2) {
+        return HSD_TE_RAS;
+    }
+    return texp->type;
+}
+
+void HSD_TExpRef(HSD_TExp* texp, u8 sel)
+{
+    HSD_TExpType type = HSD_TExpGetType_inline(texp);
+    if (type != HSD_TE_CNST) {
+        if (type >= HSD_TE_CNST){
+            return;
+        }
+        if (type != HSD_TE_TEV)
+        {
+            return;
+        }
+        if (sel == TRUE) {
+            texp->tev.c_ref += 1;
+            return;
+        } else {
+            texp->tev.a_ref += 1;
+            return;
+        }
+    }
+    
+    texp->cnst.ref += 1;
+}
