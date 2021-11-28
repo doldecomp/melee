85cbe5ad66ec5e1754a3178672d41ef1dce3cbe8
Will C <wcarter312@gmail.com>
2021-11-28 17:36:21 -0600

    Match HSD_TExpUnref

--- src/sysdolphin/baselib/texp.c
+++ src/sysdolphin/baselib/texp.c
@@ -48,3 +48,35 @@ void HSD_TExpRef(HSD_TExp* texp, u8 sel)
     
     texp->cnst.ref += 1;
 }
+
+void HSD_TExpUnref(HSD_TExp* texp, u8 sel)
+{
+    s32 i;
+    HSD_TExpType type = HSD_TExpGetType_inline(texp);
+
+    if (type != HSD_TE_CNST) {
+        if (type >= HSD_TE_CNST || type != HSD_TE_TEV) {
+            return;
+        }
+        if (sel == TRUE) {
+            if (texp->tev.c_ref != 0) {
+                texp->tev.c_ref -= 1;
+            }
+        } else if (texp->tev.a_ref != 0) {
+            texp->tev.a_ref -= 1;
+        }
+
+        if (texp->tev.c_ref == 0 && texp->tev.a_ref == 0) {
+            for (i = 0; i < 4; i++) {
+                HSD_TExpUnref(texp->tev.c_in[i].exp, texp->tev.c_in[i].sel);
+                HSD_TExpUnref(texp->tev.a_in[i].exp, texp->tev.a_in[i].sel);
+            }
+        }
+        return;
+    }
+
+    if (texp->cnst.ref != 0) {
+        texp->cnst.ref -= 1;
+        return;
+    }
+}
