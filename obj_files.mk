4b8fe0f3290ba402cf7859f7a6294815c4e36fe8
Will C <wcarter312@gmail.com>
2021-12-02 10:53:41 -0600

    Correct some data sections to be in the correct file, split lb further

--- obj_files.mk
+++ obj_files.mk
@@ -15,8 +15,9 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/src/melee/lb/lbcommand.o			\
 	$(BUILD_DIR)/asm/melee/lb/lbcommand.o  		    \
 	$(BUILD_DIR)/asm/melee/lb/lbcollision.o  	    \
-	$(BUILD_DIR)/asm/melee/lb/code_8000AD8C.o  	    \
-	$(BUILD_DIR)/asm/melee/lb/code_8000B1CC.o  	    \
+	$(BUILD_DIR)/asm/melee/lb/lblanguage.o  	    \
+	$(BUILD_DIR)/asm/melee/lb/lbtime.o  	        \
+	$(BUILD_DIR)/asm/melee/lb/code_8000B074.o  	    \
 	$(BUILD_DIR)/asm/melee/lb/code_8000CE50.o  	    \
 	$(BUILD_DIR)/asm/melee/lb/lbvector.o		 	\
 	$(BUILD_DIR)/asm/melee/lb/code_8000E9F0.o 		\
