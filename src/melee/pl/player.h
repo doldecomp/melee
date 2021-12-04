50a4712e5f977f37bceadf40e34de1a2863a725d
Will C <wcarter312@gmail.com>
2021-12-04 17:36:31 -0600

    Continue moving player.s to player.c

--- src/melee/pl/player.h
+++ src/melee/pl/player.h
@@ -14,5 +14,10 @@ StaticPlayer* Player_GetPtrForSlot(s32 slot);
 void Player_80031790(s32 slot);
 void Player_80031848(s32 slot);
 void Player_80031900(s32 slot);
+BOOL Player_800319C4(s32 slot, BOOL param_2);
+void Player_80031AD0(s32 slot);
+void Player_80031CB0(s32 id, s32 slot);
+void Player_80031D2C(s32 id, s32 slot);
+void Player_80031DA8(s32 param_1, s32 param_2);
 
 #endif
