ab480b1864d4f699fef1c77a96e22c4332fa2c52
Will C <wcarter312@gmail.com>
2021-12-10 17:10:31 -0600

    Match HSD_Randi

--- src/sysdolphin/baselib/random.c
+++ src/sysdolphin/baselib/random.c
@@ -11,13 +11,16 @@ s32 HSD_Rand(void)
     return *lbl_804D5F94 >> 0x10;
 }
 
-#ifdef NON_MATCHING
-f32 HSD_Randf(void)
+inline s32 HSD_Rand_inline(void)
 {
-    s32 temp;
     *lbl_804D5F94 = *lbl_804D5F94 * 214013 + 2531011;
-    temp = (f32)(*lbl_804D5F94 >> 0x10);
-    return (f32)(*lbl_804D5F94 >> 0x10) / lbl_804DE798;
+    return *lbl_804D5F94 >> 0x10;
+}
+
+#ifdef NON_MATCHING
+f32 HSD_Randf(void) {
+    *lbl_804D5F94 = *lbl_804D5F94 * 214013 + 2531011;
+    return (f32)(*lbl_804D5F94 >> 0x10) / lbl_804DE790;
 }
 #else
 asm f32 HSD_Randf(void)
@@ -48,39 +51,12 @@ asm f32 HSD_Randf(void)
 }
 #endif
 
-#ifdef NON_MATCHING
-s32 HSD_Randi(s32 max_val)
-{
-    s32 temp;
-    *lbl_804D5F94 = *lbl_804D5F94 * 214013 + 2531011;
-    temp = max_val * (*lbl_804D5F94 >> 0x10);
-    return temp / lbl_804DE798;
-}
-#else
-asm s32 HSD_Randi(s32 max_val)
-{
-    nofralloc
-/* 80380580 0037D160  80 AD A8 F4 */	lwz r5, lbl_804D5F94(r13)
-/* 80380584 0037D164  3C 80 00 03 */	lis r4, 0x000343FD@ha
-/* 80380588 0037D168  38 04 43 FD */	addi r0, r4, 0x000343FD@l
-/* 8038058C 0037D16C  80 85 00 00 */	lwz r4, 0(r5)
-/* 80380590 0037D170  7C 84 01 D6 */	mullw r4, r4, r0
-/* 80380594 0037D174  3C 84 00 27 */	addis r4, r4, 0x27
-/* 80380598 0037D178  38 04 9E C3 */	addi r0, r4, -24893
-/* 8038059C 0037D17C  90 05 00 00 */	stw r0, 0(r5)
-/* 803805A0 0037D180  80 8D A8 F4 */	lwz r4, lbl_804D5F94(r13)
-/* 803805A4 0037D184  80 04 00 00 */	lwz r0, 0(r4)
-/* 803805A8 0037D188  54 00 84 3E */	srwi r0, r0, 0x10
-/* 803805AC 0037D18C  7C 03 01 D6 */	mullw r0, r3, r0
-/* 803805B0 0037D190  7C 03 86 70 */	srawi r3, r0, 0x10
-/* 803805B4 0037D194  7C 63 01 94 */	addze r3, r3
-/* 803805B8 0037D198  4E 80 00 20 */	blr 
-}
-#endif
-
 #pragma push 
-#pragma force_active on
 #pragma peephole on
+s32 HSD_Randi(s32 max_val) {
+    return (max_val * HSD_Rand_inline()) / 0x10000;
+}
+
 void _HSD_RandForgetMemory(u32* low, u32* high)
 {
     if (low <= lbl_804D5F94 && lbl_804D5F94 < high){
