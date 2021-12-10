7ba3d418e4096b44551a5a93bfc4e6c03d4912e5
William Carter <wcarter312@gmail.com>
2021-12-10 10:29:16 -0600

    Add header and main struct for HSD_Shadow (#159)

---
+++ src/sysdolphin/baselib/shadow.h
@@ -0,0 +1,29 @@
+#ifndef _shadow_h_
+#define _shadow_h_
+
+#include <global.h>
+
+#include <dolphin/types.h>
+
+#include "sysdolphin/baselib/cobj.h"
+#include "sysdolphin/baselib/tobj.h"
+#include "sysdolphin/baselib/list.h"
+
+typedef struct _HSD_Shadow {
+    struct _HSD_SList* objects; // 0x0
+    struct _HSD_CObj* camera; // 0x4
+    struct _HSD_TObj* texture; //0x8
+    f32 scaleS; // 0xC
+    f32 scaleT; // 0x10
+    f32 transS; // 0x14
+    f32 transT; // 0x18
+    BOOL active; // 0x1C
+    u8 intensity; // 0x20
+    void* user_data; // 0x24
+} HSD_Shadow;
+
+/*typedef struct _HSD_ShadowDesc {
+
+} HSD_ShadowDesc;*/
+
+#endif
