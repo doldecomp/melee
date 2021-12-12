273af980ba3d17a17945dc1b678956d72356b836
Snuffy <mattsasa@me.com>
2021-12-12 03:55:59 -0600

    match some more easy stage getter functions

--- src/melee/gr/stage.c
+++ src/melee/gr/stage.c
@@ -38,3 +38,43 @@ f32 func_80224AD8()
 {
     return stage_info.cam_info.cam_zoom_rate;
 }
+
+f32 func_80224AE8() {
+    return stage_info.cam_info.x20;
+}
+
+f32 func_80224AF8() {
+    return stage_info.cam_info.x24;
+}
+
+f32 func_80224B08() {
+    return stage_info.cam_info.cam_fixed_zoom;
+}
+
+f32 func_80224B18() {
+    return stage_info.cam_info.cam_track_ratio;
+}
+
+f32 func_80224B28() {
+    return stage_info.cam_info.cam_track_smooth;
+}
+
+f32 func_80224B38() {
+    return stage_info.blast_zone.right + stage_info.cam_info.cam_x_offset;
+}
+
+f32 func_80224B50() {
+    return stage_info.blast_zone.left + stage_info.cam_info.cam_x_offset;
+}
+
+f32 func_80224B68(void) {
+    return stage_info.blast_zone.top + stage_info.cam_info.cam_y_offset;
+}
+
+f32 func_80224B80(void) {
+   return stage_info.blast_zone.bottom + stage_info.cam_info.cam_y_offset;
+}
+
+
+
+
