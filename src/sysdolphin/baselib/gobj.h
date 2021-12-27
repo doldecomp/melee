2d0592ead7e70010cea3835b9528597c58f7fb6a
Will C <wcarter312@gmail.com>
2021-12-27 13:44:08 -0600

    Match GObj_SetupGXLink

--- src/sysdolphin/baselib/gobj.h
+++ src/sysdolphin/baselib/gobj.h
@@ -3,6 +3,8 @@
 
 #include <dolphin/types.h>
 
+#include "sysdolphin/baselib/objalloc.h"
+
 typedef struct _HSD_GObj {
     u16 classifier;
     u8 p_link;
@@ -16,7 +18,7 @@ typedef struct _HSD_GObj {
     struct _HSD_GObj* next_gx; //0x10
     struct _HSD_GObj* prev_gx; //0x14
     struct _HSD_GObjProc* proc; //0x18
-    void (*render_cb)(struct _HSD_GObj* gobj, int code); //0x1C
+    void (*render_cb)(struct _HSD_GObj* gobj, s32 code); //0x1C
     u64 gxlink_prios;
     void* hsd_obj;
     void* user_data;
@@ -35,6 +37,16 @@ typedef struct _HSD_GObjProc {
     struct _HSD_GObj* gobj; //0x10
     void (*callback)(struct _HSD_GObj*); //0x14
 } HSD_GObjProc;
+
+typedef struct _HSD_GObjLibInitData {
+    u8 p_link_max; //804CE380
+    u8 gx_link_max; //804CE381
+    u8 gproc_pri_max; //804CE382
+    u32 unk_1; //804CE384
+    u32* unk_2; //804CE388
+    HSD_ObjAllocData gobj_def; //804CE38C
+    HSD_ObjAllocData gobj_proc_def; //804CE3B8
+} HSD_GObjLibInitData;
 
 #endif
 
--- src/sysdolphin/baselib/gobjgxlink.c
+++ src/sysdolphin/baselib/gobjgxlink.c
@@ -3,6 +3,11 @@
 extern HSD_GObj** lbl_804D7820;
 extern HSD_GObj** lbl_804D7824;
 
+extern HSD_GObjLibInitData lbl_804CE380;
+
+extern char lbl_804084F0[13]; //"gobjgxlink.c"
+extern char lbl_80408500[43]; //"gx_link <= HSD_GObjLibInitData.gx_link_max"
+
 void GObj_GXReorder(HSD_GObj* gobj, HSD_GObj* hiprio_gobj)
 {
     u32 link = gobj->gx_link;
@@ -21,4 +26,23 @@ void GObj_GXReorder(HSD_GObj* gobj, HSD_GObj* hiprio_gobj)
     } else {
         lbl_804D7820[gobj->gx_link] = gobj;
     }
+}
+
+void GObj_SetupGXLink(HSD_GObj* gobj, void (*render_cb)(HSD_GObj*, s32), u8 gx_link, u32 priority)
+{
+    HSD_GObj* i;
+    HSD_GObj* prev;
+
+    if (gx_link > lbl_804CE380.gx_link_max) {
+        __assert(lbl_804084F0, 167, lbl_80408500);
+    }
+    gobj->render_cb = render_cb;
+    gobj->gx_link = gx_link;
+    gobj->render_priority = priority;
+
+    for (i = lbl_804D7820[gobj->gx_link]; i != NULL && (i->render_priority > gobj->render_priority); i = prev) 
+    {
+        prev = i->prev_gx;
+    }
+    GObj_GXReorder(gobj, i);
 }
