6082c51634043f16ba0b1084be400ef307fffd0a
Will C <wcarter312@gmail.com>
2022-01-04 11:22:49 -0600

    More splitting up stage 2

--- obj_files.mk
+++ obj_files.mk
@@ -58,7 +58,9 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/melee/ft/ftcommon.o 			\
 	$(BUILD_DIR)/asm/melee/ft/code_800804EC.o 		\
 	$(BUILD_DIR)/asm/melee/ft/ftcliffcommon.o 		\
-	$(BUILD_DIR)/asm/melee/ft/code_8008169C.o 		\
+	$(BUILD_DIR)/asm/melee/ft/ftwalljump.o 			\
+	$(BUILD_DIR)/asm/melee/ft/code_80081938.o 		\
+	$(BUILD_DIR)/asm/melee/ft/code_80081B38.o 		\
 	$(BUILD_DIR)/asm/melee/ft/code_8008521C.o 		\
 	$(BUILD_DIR)/asm/melee/ft/chara/ftdonkey_1.o 	\
 	$(BUILD_DIR)/asm/melee/ft/chara/ftmario.o 		\
