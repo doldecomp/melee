07c57871d51dbb15025ea5ea3db26b09c43ae361
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-01-16 10:20:51 -0700

    ft: adjustments

--- src/melee/ft/chara/ftness.c
+++ src/melee/ft/chara/ftness.c
@@ -3,11 +3,11 @@ void ftNess_Reset(struct _HSD_GObj* arg0, s32 arg1) // formerly func_801147C0
 {
     struct Fighter* temp = (struct Fighter*)arg0->data;
     func_80074A4C(arg0, 0, 0);
-    temp->unk2240 = 0;
-    temp->unk2244 = 0;
-    temp->unk2248 = 0;
-    temp->unk222C = 0;
-    temp->unk224C = 0;
+    temp->x2240 = 0;
+    temp->x2244 = 0;
+    temp->x2248 = 0;
+    temp->x222C = 0;
+    temp->x224C = 0;
 }
 void func_8011493C(s32 arg) {
     func_8011B0F8(arg);
@@ -16,12 +16,12 @@ f32 func_8011495C(struct Fighter* arg0) {
     return ((struct ftAttributes2D4* )arg0->unk2D4)->unk94;
 }
 void func_80114A48(struct _HSD_GObj* arg0) {
-    if (func_8026B2B4(((struct Fighter*)arg0->data)->unk1974) == 0) {
+    if (func_8026B2B4(((struct Fighter*)arg0->data)->x1974) == 0) {
         func_80070CC4(arg0, 1);
     }
 }
 void func_80114A90(struct _HSD_GObj* arg0) {
-    if (func_8026B2B4(((struct Fighter*)arg0->data)->unk1974) == 0) {
+    if (func_8026B2B4(((struct Fighter*)arg0->data)->x1974) == 0) {
         func_80070C48(arg0, 1);
     }
 }
--- src/melee/ft/fighter.h
+++ src/melee/ft/fighter.h
@@ -2,24 +2,27 @@
 #define _fighter_h_
 #include <global.h>
 #include <dolphin/types.h>
-struct Fighter {
-    u8 unkData0[0x2D4];
-    u8* unk2D4;
-    u8 unkData2D8[0x169C];
-    s32 unk1974;
-    u8 unkData1978[0x8B8];
-    u32 unk222C;
-    u8 unkData2230[0x10];
-    u32 unk2240;
-    u32 unk2244;
-    u32 unk2248;
-    u32 unk224C;
-    u32 unkData2250[0x19c];
-};
-struct ftAttributes2D4 {
-    u8 unkData[0x94];
+typedef struct _ftAttributes2D4 {
+    u8 data[0x94];
     f32 unk94;
-};
+} ftAttributes2D4;
+typedef struct _Fighter {
+    u8 data0[0x2D4];
+    ftAttributes2D4* unk2D4;
+    u8 data2D8[0x169C];
+    s32 x1974;
+    u8 data1978[0x8B8];
+    u32 x222C;
+    u32 x2230;
+    u32 x2234;
+    u32 x2238;
+    u32 x223C;
+    u32 x2240;
+    u32 x2244;
+    u32 x2248;
+    u32 x224C;
+    u32 data2250[0x19c];
+} Fighter;
 void func_800704F0(struct _HSD_GObj* arg0, s32 arg1, f32 arg2);
 void func_80070CC4(struct _HSD_GObj* arg0, int arg1);
 void func_80070FB4(struct _HSD_GObj* arg0, s32 arg1, s32 arg2);
