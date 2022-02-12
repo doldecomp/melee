e33111caed94aafcdbc67fbf081c94a7aace1308
Johnny Urosevic <TheMagicalCake@gmail.com>
2022-02-12 11:53:14 -0800

    Change onload to memcopy to match

--- src/melee/ft/chara/ftsandbag.c
+++ src/melee/ft/chara/ftsandbag.c
@@ -2,8 +2,7 @@
 
 typedef struct _PairStruct
 {
-    u32 x0_first;
-    u32 x4_second;
+    u32 x0_pair[2];
     u8 padding[0xB8];
 } PairStruct;
 
@@ -20,18 +19,11 @@ void func_8014F9CC() {}
 
 void ftSandbag_OnLoad(HSD_GObj* gobj)
 {
-    Fighter* fighter = gobj->user_data;
-    ftData* fighterDataInfo = fighter->x10C_ftData;
+    Fighter* fighter/*r3*/ = gobj->user_data;
+    ftData* fighterDataInfo/*r5*/ = fighter->x10C_ftData;
+    PairStruct* special_attr_2/*r7*/ = fighter->x2D8_specialAttributes2;
 
-    PairStruct* extAttrs = (PairStruct *) fighterDataInfo->ext_attr;
-
-    u32 ext_attr_first = extAttrs->x0_first;
-    u32 ext_attr_second = extAttrs->x4_second;
-
-    PairStruct* special_attr_2 = fighter->x2D8_specialAttributes2;
-
-    special_attr_2->x0_first = ext_attr_first;
-    special_attr_2->x4_second = ext_attr_second;
+    __memcpy(special_attr_2->x0_pair, ((PairStruct *) fighterDataInfo->ext_attr)->x0_pair, 8);
 
     fighter->x2D4_specialAttributes = special_attr_2;
 
@@ -81,11 +73,11 @@ u32 func_8014FB78(HSD_GObj* gobj)
 
     PairStruct* ext_attr = (PairStruct *)fighter_data->ext_attr;
 
-    s32 ext_attr_first = ext_attr->x0_first;
-    s32 ext_attr_second = ext_attr->x4_second;
+    s32 ext_attr_first = ext_attr->x0_pair[0];
+    s32 ext_attr_second = ext_attr->x0_pair[1];
 
-    special_attr->x0_first = ext_attr_first;
-    special_attr->x4_second = ext_attr_second;
+    special_attr->x0_pair[0] = ext_attr_first;
+    special_attr->x0_pair[1] = ext_attr_second;
 
     return ext_attr_first;
 }
