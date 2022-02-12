e44156fdcb1d4babba12570d383b6b40a5e7dac4
Johnny Urosevic <TheMagicalCake@gmail.com>
2022-02-11 22:17:10 -0800

    add back ftSandbag_OnLoad

--- src/melee/ft/chara/ftsandbag.c
+++ src/melee/ft/chara/ftsandbag.c
@@ -15,28 +15,27 @@ struct UnkFighterStruct
 
 extern struct UnkFighterStruct* lbl_804D6554;
 
+void ftSandbag_OnLoad(HSD_GObj* gobj) {
+    Fighter* fighter = gobj->user_data;
+    ftData* fighterDataInfo = fighter->x10C_ftData;
 
-// void ftSandbag_OnLoad(HSD_GObj* gobj) {
-//     Fighter* fighter = gobj->user_data;
-//     ftData* fighterDataInfo = fighter->x10C_ftData;
+    PairStruct* extAttrs = (PairStruct *) fighterDataInfo->ext_attr;
 
-//     PairStruct* extAttrs = (PairStruct *) fighterDataInfo->ext_attr;
+    u32 ext_attr_first = extAttrs->x0_first;
+    u32 ext_attr_second = extAttrs->x4_second;
 
-//     u32 ext_attr_first = extAttrs->x0_first;
-//     u32 ext_attr_second = extAttrs->x4_second;
+    PairStruct* special_attr_2 = fighter->x2D8_specialAttributes2;
 
-//     PairStruct* special_attr_2 = fighter->x2D8_specialAttributes2;
+    special_attr_2->x0_first = ext_attr_first;
+    special_attr_2->x4_second = ext_attr_second;
 
-//     special_attr_2->x0_first = ext_attr_first;
-//     special_attr_2->x4_second = ext_attr_second;
+    fighter->x2D4_specialAttributes = special_attr_2;
 
-//     fighter->x2D4_specialAttributes = special_attr_2;
+    fighter->x2228_flag.bits.b2 = 1;
+    fighter->x2226_flag.bits.b0 = 1;
 
-//     fighter->x2228_flag.bits.b2 = 1;
-//     fighter->x2226_flag.bits.b0 = 1;
-
-//     func_8014FA30(fighter);
-// }
+    func_8014FA30(fighter);
+}
 
 u32 func_8014FB78(HSD_GObj* gobj) {
     Fighter* fighter = gobj->user_data;
