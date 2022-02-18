54e7af23fd9487c133e1115350a5d422a9a6dcd8
Kalua <manuelk89@gmx.net>
2022-02-18 02:54:46 +0100

    Drafts; won't compile, contains individual decompiled functions

---
+++ src/drafts.c
@@ -0,0 +1,14 @@
+// Contains drafts for individual functions.
+// This file is not supposed to compile, it just contains a list of decompiled functions
+// that you can pick from when you decompile the module they belong to.
+
+// https://decomp.me/scratch/VBrFf
+// melee/ft/code_8008521C.s
+#include "melee/ft/fighter.h"
+void func_800C88A0(Fighter* f) {
+    f->x2226_5 = 0;
+    f->x2226_6 = 0;
+    f->x2134 = 0;
+    f->x2030 = 0;
+    f->x2226_8 = 0;
+}
