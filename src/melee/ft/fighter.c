0c2195f6681b2bbfc0aa0d0c363c7a8dd88243b6
Kalua <manuelk89@gmx.net>
2022-02-22 15:19:31 +0100

    move fpclassify from fighter.c to math.h

--- src/melee/ft/fighter.c
+++ src/melee/ft/fighter.c
@@ -13,37 +13,6 @@
 // uncomment to compile the C functions instead of the inline asm functions
 // #define FIGHTER_DONE
 
-// ==== fpclassify -- should go into some math file ====
-// =====================================================
-enum FloatType { FP_NAN=1, FP_INFINITE=2, FP_ZERO=3, FP_NORMAL=4, FP_SUBNORMAL=5 };
-
-// https://github.com/projectPiki/pikmin2/blob/main/include/Dolphin/math.h
-inline s32 __fpclassifyf(float x)
-{
-	const s32 exp_mask      = 0b01111111100000000000000000000000; // = 0x7F800000
-	const s32 mantissa_mask = 0b00000000011111111111111111111111; // = 0x007fffff
-	switch ((*(s32*)&x) & exp_mask)
-	{
-		case exp_mask: return ((*(s32*)&x) & mantissa_mask) ? FP_NAN : FP_INFINITE;
-		case 0:        return ((*(s32*)&x) & mantissa_mask) ? FP_SUBNORMAL : FP_ZERO;
-		default:       return FP_NORMAL;
-	}
-}
-
-inline s32 __fpclassifyd(double x)
-{
-	switch (__HI(x) & 0x7ff00000)
-	{
-		case 0x7ff00000: return ((__HI(x) & 0x000fffff) || (__LO(x) & 0xffffffff)) ? FP_NAN : FP_INFINITE;
-		case 0:          return ((__HI(x) & 0x000fffff) || (__LO(x) & 0xffffffff)) ? FP_SUBNORMAL : FP_ZERO;
-		default: return FP_NORMAL;
-	}
-}
-
-#define fpclassify(x) \
-	((sizeof(x) == sizeof(float)) ? __fpclassifyf((float)(x)) \
-	                              : __fpclassifyd((double)(x)))
-
 // ==== external functions ====
 // ============================
 // TODO: check which of these can be #included instead.
