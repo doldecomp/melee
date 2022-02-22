7401bfad283168c0b52e1203d31e707319249455
William Carter <wcarter312@gmail.com>
2022-02-22 16:31:30 -0600

    Get completely rid of dobj.s (#307)
    * Get completely rid of dobj.s
    
    * Remove static function from header

--- src/sysdolphin/baselib/dobj.c
+++ src/sysdolphin/baselib/dobj.c
@@ -3,6 +3,7 @@
 #include "sysdolphin/baselib/aobj.h"
 #include "sysdolphin/baselib/pobj.h"
 
+static void DObjInfoInit(void);
 HSD_DObjInfo hsdDObj = { DObjInfoInit };
 
 static HSD_DObjInfo* default_class = NULL;
@@ -10,12 +11,6 @@ static HSD_DObjInfo* default_class = NULL;
 static HSD_DObj* current_dobj = NULL;
 
 static char lbl_804D5C78[7] = "dobj.c\0";
-static char lbl_804D5C80[1] = "\0";
-static char lbl_804D5C84[5] = "dobj\0";
-
-extern char lbl_80405494[]; // "mobj has unexpected blending flags (0x%x)."
-extern char lbl_80405548[]; // "sysdolphin_base_library"
-extern char lbl_80405560[]; // "hsd_dobj"
 
 void HSD_DObjSetCurrent(HSD_DObj* dobj)
 {
@@ -180,12 +175,14 @@ static int DObjLoad(HSD_DObj* dobj, HSD_DObjDesc* desc)
                 HSD_DObjModifyFlags(dobj, 4, 0xE);
                 break;
             default:
-                OSReport(lbl_80405494, dobj->mobj->rendermode);
-                HSD_Panic(lbl_804D5C78, 312, lbl_804D5C80);
+                OSReport("mobj has unexpected blending flags (0x%x).", dobj->mobj->rendermode);
+                HSD_Panic(lbl_804D5C78, 312, "\0");
         }
     }
     return 0;
 }
+
+static char lbl_804D5C84[5] = "dobj\0";
 
 HSD_DObj* HSD_DObjLoadDesc(HSD_DObjDesc* desc)
 {
@@ -229,6 +226,16 @@ void HSD_DObjRemoveAll(HSD_DObj* dobj)
         next = dobj->next;
         HSD_DObjRemove(dobj);
     }
+}
+
+void HSD_DObjSetDefaultClass(HSD_DObjInfo* info)
+{
+    if (info) {
+        if (!hsdIsDescendantOf(info, &hsdDObj)) {
+            __assert(lbl_804D5C78, 498, "hsdIsDescendantOf(info, &hsdDObj)"); // The line number here is totally made up, this function is removed in practice but the string isn't
+        }
+    }
+    default_class = info;
 }
 
 // This really does match, Frank is just breaking it.
@@ -290,6 +297,22 @@ void HSD_DObjResolveRefsAll(HSD_DObj* dobj, HSD_DObjDesc* desc)
     }
 }
 
+void forceStringAllocation(HSD_DObj* dobj, HSD_MObj* mobj) // This function exists for the sole purpose of causing strings to end up in data by the compiler despite not being used
+{
+    if (dobj->pobj == NULL) 
+    {
+        __assert(lbl_804D5C78, 700, "can not find specified pobj in link.\n");
+    }
+    if (dobj->pobj == NULL) 
+    {
+        __assert(lbl_804D5C78, 702, "can not find specified pobj in link.");
+    }
+    if (dobj->mobj != mobj) 
+    {
+        __assert(lbl_804D5C78, 704, "dobj->mobj == mobj");
+    }
+}
+
 void HSD_DObjDisp(HSD_DObj* dobj, Mtx vmtx, Mtx pmtx, u32 rendermode)
 {
     HSD_PObj* p;
@@ -325,53 +348,14 @@ static void DObjAmnesia(HSD_ClassInfo* info)
     }
     HSD_PARENT_INFO(&hsdDObj)->amnesia(info);
 }
-#pragma pop
 
-// Non-matching because a mess of externs
-#ifdef NON_MATCHING
 static void DObjInfoInit(void)
 {
-    hsdInitClassInfo(HSD_CLASS_INFO(&hsdDObj), HSD_CLASS_INFO(&hsdClass), lbl_80405548, lbl_80405560, sizeof(HSD_DObjInfo), sizeof(HSD_DObj));
+    hsdInitClassInfo(HSD_CLASS_INFO(&hsdDObj), HSD_CLASS_INFO(&hsdClass), "sysdolphin_base_library", "hsd_dobj", sizeof(HSD_DObjInfo), sizeof(HSD_DObj));
 
-    HSD_PARENT_INFO(&hsdDObj)->release = DObjRelease;
-    HSD_PARENT_INFO(&hsdDObj)->amnesia = DObjAmnesia;
+    HSD_CLASS_INFO(&hsdDObj)->release = DObjRelease;
+    HSD_CLASS_INFO(&hsdDObj)->amnesia = DObjAmnesia;
     HSD_DOBJ_INFO(&hsdDObj)->disp = HSD_DObjDisp;
     HSD_DOBJ_INFO(&hsdDObj)->load = DObjLoad;
 }
-#else
-asm void DObjInfoInit(void)
-{
-    nofralloc
-/* 8035E4E4 0035B0C4  7C 08 02 A6 */	mflr r0
-/* 8035E4E8 0035B0C8  3C 60 80 40 */	lis r3, hsdDObj@ha
-/* 8035E4EC 0035B0CC  90 01 00 04 */	stw r0, 4(r1)
-/* 8035E4F0 0035B0D0  38 E0 00 44 */	li r7, 0x44
-/* 8035E4F4 0035B0D4  39 00 00 18 */	li r8, 0x18
-/* 8035E4F8 0035B0D8  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 8035E4FC 0035B0DC  93 E1 00 0C */	stw r31, 0xc(r1)
-/* 8035E500 0035B0E0  3B E3 54 50 */	addi r31, r3, hsdDObj@l
-/* 8035E504 0035B0E4  3C 60 80 40 */	lis r3, hsdClass@ha
-/* 8035E508 0035B0E8  38 83 75 90 */	addi r4, r3, hsdClass@l
-/* 8035E50C 0035B0EC  38 7F 00 00 */	addi r3, r31, 0
-/* 8035E510 0035B0F0  38 BF 00 F8 */	addi r5, r31, 0xf8
-/* 8035E514 0035B0F4  38 DF 01 10 */	addi r6, r31, 0x110
-/* 8035E518 0035B0F8  48 02 37 01 */	bl hsdInitClassInfo
-/* 8035E51C 0035B0FC  3C 60 80 36 */	lis r3, DObjRelease@ha
-/* 8035E520 0035B100  38 03 E4 40 */	addi r0, r3, DObjRelease@l
-/* 8035E524 0035B104  3C 60 80 36 */	lis r3, DObjAmnesia@ha
-/* 8035E528 0035B108  90 1F 00 30 */	stw r0, 0x30(r31)
-/* 8035E52C 0035B10C  38 03 E4 9C */	addi r0, r3, DObjAmnesia@l
-/* 8035E530 0035B110  3C 60 80 36 */	lis r3, HSD_DObjDisp@ha
-/* 8035E534 0035B114  90 1F 00 38 */	stw r0, 0x38(r31)
-/* 8035E538 0035B118  38 03 E3 88 */	addi r0, r3, HSD_DObjDisp@l
-/* 8035E53C 0035B11C  3C 60 80 36 */	lis r3, DObjLoad@ha
-/* 8035E540 0035B120  90 1F 00 3C */	stw r0, 0x3c(r31)
-/* 8035E544 0035B124  38 03 E0 A4 */	addi r0, r3, DObjLoad@l
-/* 8035E548 0035B128  90 1F 00 40 */	stw r0, 0x40(r31)
-/* 8035E54C 0035B12C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 8035E550 0035B130  83 E1 00 0C */	lwz r31, 0xc(r1)
-/* 8035E554 0035B134  38 21 00 10 */	addi r1, r1, 0x10
-/* 8035E558 0035B138  7C 08 03 A6 */	mtlr r0
-/* 8035E55C 0035B13C  4E 80 00 20 */	blr 
-}
-#endif
+#pragma pop
