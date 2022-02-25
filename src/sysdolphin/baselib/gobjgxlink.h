698ab493bbd51a91d7d3339efa2234b713385a5e
William Carter <wcarter312@gmail.com>
2022-02-25 10:09:48 -0600

    gobjgxlink matches (#312)
    * Match func_8039075C
    
    * Match func_803907C8
    
    * Add function declarations

--- src/sysdolphin/baselib/gobjgxlink.h
+++ src/sysdolphin/baselib/gobjgxlink.h
@@ -7,5 +7,7 @@
 
 void GObj_GXReorder(HSD_GObj* gobj, HSD_GObj* hiprio_gobj);
 void GObj_SetupGXLink(HSD_GObj* gobj, void (*render_cb)(HSD_GObj*, s32), u8 gx_link, u32 priority);
+void GObj_SetupGXLinkMax(HSD_GObj* gobj, void (*render_cb)(HSD_GObj*, s32), u32 priority);
+void GObj_SetupGXLinkMaxSorted(HSD_GObj* gobj, void (*render_cb)(HSD_GObj*, s32), u32 priority);
 
 #endif
