824f314bba0a2b2bd54a251b915d0cb35990b435
Ryan Burns <rtburns@protonmail.com>
2022-02-26 15:22:56 -0800

    Add all grstory function headers

--- src/melee/gr/grstory.h
+++ src/melee/gr/grstory.h
@@ -5,9 +5,28 @@
 
 #include <sysdolphin/baselib/gobj.h>
 
+void func_801E302C(void);
+void func_801E3030(void);
+void lbl_801E30A8(void);
+void lbl_801E30AC(void);
+s32 lbl_801E30D0(void);
 HSD_GObj* func_801E30D8(s32);
+void lbl_801E31C0(HSD_GObj*);
+s32 lbl_801E3224(void);
+void lbl_801E322C(void);
+void lbl_801E3230(void);
+void lbl_801E3234(HSD_GObj*);
+s32 lbl_801E332C(void);
+void lbl_801E3334(HSD_GObj*);
+void lbl_801E336C();
+void lbl_801E3370(HSD_GObj*);
+s32 lbl_801E33D8(void);
+void lbl_801E33E0(HSD_GObj*);
+void lbl_801E3414(void);
 void func_801E3418(HSD_GObj*);
 void func_801E366C(HSD_GObj*);
+s32 lbl_801E36D0(void);
+BOOL lbl_801E36D8(Vec3*, u32, u8*);
 
 #define ITEM_SHYGUY 0xD2
 
