70f2fd8b1a48e87a4abf4502df81be489b31e541
Ryan Burns <rtburns@protonmail.com>
2022-03-27 13:14:18 -0700

    Add first parameter type to ef_Spawn
    Ellipses cannot be the only parameter

--- include/functions.h
+++ include/functions.h
@@ -9130,6 +9130,6 @@ void func_80390A70();
 void efLib_PauseAll(struct _HSD_GObj*);
 void efLib_ResumeAll(struct _HSD_GObj*);
 s32 func_80096CC8(struct _HSD_GObj*);
-void ef_Spawn(...);
+void ef_Spawn(s32, ...);
 
 #endif
