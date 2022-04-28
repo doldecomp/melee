e5cb02e730504fbd71b1cc1c1947d8ba9d42b83c
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2022-04-28 00:44:50 -0400

    starting link map visualizer

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ ifneq ($(findstring MSYS,$(shell uname)),)
   WINDOWS := 1
 endif
 
-GENERATE_MAP ?= 0
+GENERATE_MAP ?= 1
 NON_MATCHING ?= 0
 
 VERBOSE ?= 0
---
+++ tools/linkmapviz.py
@@ -0,0 +1,5 @@
+# Link Map Visualizer
+# Regex to match symbol found in file: "(\w)\] ([_a-zA-Z0-9\-.]+) \((\w+),(\w+)\) found in ([_a-zA-Z0-9\-.]+)"
+# Regex to match symbol found as a linker generated symbol: "(\w)\] ([_a-zA-Z0-9\-.]+) found as linker generated symbol"
+
+# TODO work this
