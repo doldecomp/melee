68f6561788d2a521af678ccfbb3c87c634a0c812
VetriTheRetri <jupiter9764@gmail.com>
2022-05-27 10:20:10 +0300

    ftNess_7.c complete - shifted at 0x80200000

--- src/melee/ft/chara/ftPikachu/ftpikachu4.c
+++ src/melee/ft/chara/ftPikachu/ftpikachu4.c
@@ -314,7 +314,7 @@ void func_801267C8(HSD_GObj* fighterObj) {
     if (!func_80081298(fighterObj)) {
 
         if (collData->x134_envFlags & 0x6000) {
-            f32 angle = lbvector_AngleXY(&collData->x190, &fighter->x80_self_vel);
+            f32 angle = lbvector_AngleXY(&collData->x190_vec, &fighter->x80_self_vel);
             if (angle > (0.017453292f * (90.0f + pika_attr->xA0))) {
                 func_80127534_file_end(fighterObj);
             }
