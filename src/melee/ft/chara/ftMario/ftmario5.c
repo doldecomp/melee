356b4c8897b750fe071c00f576b45d290e001fd2
Nicolas Merz <nicolasmerz19@gmail.com>
2022-06-07 23:47:42 -0400

    Bunch of inlining for func_800E207C and func_800E2194

--- src/melee/ft/chara/ftMario/ftmario5.c
+++ src/melee/ft/chara/ftMario/ftmario5.c
@@ -21,99 +21,98 @@ extern s32 lbl_803C5A20[];
 //https://decomp.me/scratch/8zo4V
 void func_800E2050(HSD_GObj* gobj) {
     Fighter* ft;
-    ft = gobj->user_data;
+    ft = getFighter(gobj);
 
     func_8007592C(ft, 0, 0.0f);
 }
 
-//https://decomp.me/scratch/7yi8z
+void _ftMario_call_efspawn_and_set_cb(HSD_GObj* gobj) {
+    void* hsd_obj_ptr;
+    Fighter* ft;
+    ft = getFighter(gobj);
+    hsd_obj_ptr = gobj->hsd_obj;
+
+    ef_Spawn(0x47C, gobj, hsd_obj_ptr);
+    ft->x2219_flag.bits.b0 = 1;
+    ft->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
+    ft->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
+}
+
+void _ftMario_set_cb_to_800E2050(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    ft->cb.x21DC_callback_OnTakeDamage = &func_800E2050;
+    ft->cb.x21E4_callback_OnDeath2 = &func_800E2050;
+}
+
+void _ftMario_800E207C_800E2194_helper(HSD_GObj* gobj) {
+    Fighter* ft;
+    ftMarioAttributes* sa;
+    void* hsd_obj_ptr;
+    ft = getFighter(gobj);
+    sa = (ftMarioAttributes*)ft->x2D4_specialAttributes;
+    ft->x2200_ftcmd_var0 = 0;
+    ft->x2204_ftcmd_var1 = 0;
+    ft->x2340_f32 = (f32) 0.0f;
+    ft->x2344_stateVar2_s32 = (s32) (sa->x50 + 1);
+    ft->x234C_stateVar4 = 0;
+    _ftMario_set_cb_to_800E2050(gobj);
+    _ftMario_call_efspawn_and_set_cb(gobj);
+}
+
+void _ftMario_set_ftcmd_var2(HSD_GObj* gobj) {
+    Fighter* ft = gobj->user_data;
+    ft->x2208_ftcmd_var2 = 0;
+}
+
+//https://decomp.me/scratch/4saR2
 void func_800E207C(HSD_GObj* gobj) {
-    f32 temp_f1;
 
     Fighter* ft;
-    Fighter* ft_2;
-    Fighter* ft_3;
-    Fighter* ft_4;
-    Fighter* ft_5;
-    ftMarioAttributes* sa;
-    ftMarioAttributes* sa_2;
-    void* hsd_obj_ptr;
-    u8 padding[40];
 
-    ft_2 = ft = gobj->user_data;
-    sa = (ftMarioAttributes*)ft_2->x2D4_specialAttributes;
-    ft->x2208_ftcmd_var2 = 0;
-    temp_f1 = 0.0f;
-    Fighter_ActionStateChange_800693AC(gobj, 0x15E, 0, NULL, temp_f1, 1.0f, temp_f1);
+    ftMarioAttributes* sa;
+
+    void* hsd_obj_ptr;
+    u8 padding[24];
+    
+    ft = gobj->user_data;
+    
+    sa = (ftMarioAttributes*)ft->x2D4_specialAttributes;
+    _ftMario_set_ftcmd_var2(gobj);
+    Fighter_ActionStateChange_800693AC(gobj, 0x15E, 0, NULL, 0.0f, 1.0f, 0.0f);
     func_8006EBA4(gobj);
-    ft_2->x80_self_vel.y = (f32) (sa->x38 - sa->x54);
-    func_8007D440(ft_2, sa->x40);
-    ft_3 = gobj->user_data;
-    sa_2 = (ftMarioAttributes*)ft_3->x2D4_specialAttributes;
-    ft_3->x2200_ftcmd_var0 = 0;
-    ft_3->x2204_ftcmd_var1 = 0;
-    ft_3->x2340_f32 = (f32) 0.0f;
-    ft_3->x2344_stateVar2_s32 = (s32) (sa_2->x50 + 1);
-    ft_3->x234C_stateVar4 = 0;
-    ft_4 = gobj->user_data;
-    ft_4->cb.x21DC_callback_OnTakeDamage = &func_800E2050;
-    ft_4->cb.x21E4_callback_OnDeath2 = &func_800E2050;
-    ft_5 = gobj->user_data;
-    hsd_obj_ptr = gobj->hsd_obj;
-    ef_Spawn(0x47C, gobj, hsd_obj_ptr, ft_3);
-    ft_5->x2219_flag.bits.b0 = 1;
-    ft_5->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
-    ft_5->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
-    ft_2->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
-    ft_2->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
+    ft->x80_self_vel.y = (f32) (sa->x38 - sa->x54);
+    func_8007D440(ft, sa->x40);
+    _ftMario_800E207C_800E2194_helper(gobj);
+    ft->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
+    ft->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
 }
 
 //https://decomp.me/scratch/nQT5V
 void func_800E2194(HSD_GObj* gobj) {
-    f32 temp_f1;
     f32 sub_val;
-
     Fighter* ft;
-    Fighter* ft_2;
-    Fighter* ft_3;
-    Fighter* ft_4;
-    Fighter* ft_5;
-    ftMarioAttributes* sa;
-    ftMarioAttributes* sa_2;
-    void* hsd_obj_ptr;
-    u8 padding[40];
 
-    ft_2 = ft = gobj->user_data;
-    sa = (ftMarioAttributes*)ft_2->x2D4_specialAttributes;
-    ft->x2208_ftcmd_var2 = 0;
-    temp_f1 = 0.0f;
-    Fighter_ActionStateChange_800693AC(gobj, 0x15E, 0, NULL, temp_f1, 1.0f, temp_f1);
+    ftMarioAttributes* sa;
+
+    void* hsd_obj_ptr;
+    u8 padding[24];
+    
+    ft = gobj->user_data;
+    
+    sa = (ftMarioAttributes*)ft->x2D4_specialAttributes;
+    _ftMario_set_ftcmd_var2(gobj);
+   Fighter_ActionStateChange_800693AC(gobj, 0x15E, 0, NULL, 0.0f, 1.0f, 0.0f);
     func_8006EBA4(gobj);
-    if ((s32) ft_2->sa.mario.x2234 != 0) {
+    if ((s32) ft->sa.mario.x2234 != 0) {
         sub_val = 0.0f;
     } else {
         sub_val = sa->x54;
     }
-    ft_2->x80_self_vel.y = (f32) (sa->x38 - sub_val);
-    func_8007D440(ft_2, sa->x40);
-    ft_3 = gobj->user_data;
-    sa_2 = (ftMarioAttributes*)ft_3->x2D4_specialAttributes;
-    ft_3->x2200_ftcmd_var0 = 0;
-    ft_3->x2204_ftcmd_var1 = 0;
-    ft_3->x2340_f32 = (f32) 0.0f;
-    ft_3->x2344_stateVar2_s32 = (s32) (sa_2->x50 + 1);
-    ft_3->x234C_stateVar4 = 0;
-    ft_4 = gobj->user_data;
-    ft_4->cb.x21DC_callback_OnTakeDamage = &func_800E2050;
-    ft_4->cb.x21E4_callback_OnDeath2 = &func_800E2050;
-    ft_5 = gobj->user_data;
-    hsd_obj_ptr = gobj->hsd_obj;
-    ef_Spawn(0x47C, gobj, hsd_obj_ptr);
-    ft_5->x2219_flag.bits.b0 = 1;
-    ft_5->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
-    ft_5->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
-    ft_2->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
-    ft_2->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
+    ft->x80_self_vel.y = (f32) (sa->x38 - sub_val);
+    func_8007D440(ft, sa->x40);
+    _ftMario_800E207C_800E2194_helper(gobj);
+    ft->cb.x21D4_callback_EnterHitlag = &efLib_PauseAll;
+    ft->cb.x21D8_callback_ExitHitlag = &efLib_ResumeAll;
 }
 
 //https://decomp.me/scratch/FT3Fl
