ded8650219ccd23e8534d312009ad310f40867a8
nicolasrmerz <57548231+nicolasrmerz@users.noreply.github.com>
2022-06-15 22:47:04 -0400

    ftMario Complete and Matching (#393)
    * Working to func_800E1B84
    
    * decomp.me match up to func_800E1F40
    
    * Add functions with problematic labels to NON_MATCHING ifdef
    
    * Working to func_800E207C
    
    * func_800E2308 sort of matching
    
    * Working to func_800E23E4
    
    * Working to func_800E25C4
    
    * Finish matching functions - need to convert float labels to immediates and cleanup
    
    * All matched except pi/3 issue
    
    * Use more getFighter, inline for func_800E0EE0 and func_800E25C4, Windows -> Unix EOL and formatting fixes
    
    * Found remaining float causing extra sdata label, now matching
    
    * More getFighter, more inlining and cleanup
    
    * ftmario3 cleanup done
    
    * ftmario4 cleanup done
    
    * Bunch of inlining for func_800E207C and func_800E2194
    
    * Cleanup done except func_800E2778
    
    * ftmario5 cleanup done
    
    * Complete ftmario.h, remove unnecessary externs, add ftMario prefix to functions

--- asm/melee/ft/chara/ftdrmario.s
+++ asm/melee/ft/chara/ftdrmario.s
@@ -23,66 +23,66 @@ lbl_803D1498:
     .4byte 0x00000127
     .4byte 0x00340111
     .4byte 0x12000000
-    .4byte func_800E0E18
-    .4byte func_800E0E54
-    .4byte func_800E0E84
-    .4byte func_800E0EA4
+    .4byte ftMario_func_800E0E18
+    .4byte ftMario_func_800E0E54
+    .4byte ftMario_func_800E0E84
+    .4byte ftMario_func_800E0EA4
     .4byte func_800761C8
     .4byte 0x00000128
     .4byte 0x00340511
     .4byte 0x12000000
-    .4byte func_800E10B0
-    .4byte func_800E10EC
-    .4byte func_800E111C
-    .4byte func_800E113C
+    .4byte ftMario_func_800E10B0
+    .4byte ftMario_func_800E10EC
+    .4byte ftMario_func_800E111C
+    .4byte ftMario_func_800E113C
     .4byte func_800761C8
     .4byte 0x00000129
     .4byte 0x00341012
     .4byte 0x13000000
-    .4byte func_800E1550
-    .4byte func_800E15C8
-    .4byte func_800E15D0
-    .4byte func_800E1840
+    .4byte ftMario_func_800E1550
+    .4byte ftMario_func_800E15C8
+    .4byte ftMario_func_800E15D0
+    .4byte ftMario_func_800E1840
     .4byte func_800761C8
     .4byte 0x0000012A
     .4byte 0x00341012
     .4byte 0x13000000
-    .4byte func_800E158C
-    .4byte func_800E15CC
-    .4byte func_800E16E0
-    .4byte func_800E187C
+    .4byte ftMario_func_800E158C
+    .4byte ftMario_func_800E15CC
+    .4byte ftMario_func_800E16E0
+    .4byte ftMario_func_800E187C
     .4byte func_800761C8
     .4byte 0x0000012B
     .4byte 0x00340213
     .4byte 0x14000000
-    .4byte func_800E1B24
-    .4byte func_800E1BE4
-    .4byte func_800E1E74
-    .4byte func_800E1F70
+    .4byte ftMario_func_800E1B24
+    .4byte ftMario_func_800E1BE4
+    .4byte ftMario_func_800E1E74
+    .4byte ftMario_func_800E1F70
     .4byte func_800761C8
     .4byte 0x0000012C
     .4byte 0x00340613
     .4byte 0x14000000
-    .4byte func_800E1B84
-    .4byte func_800E1D2C
-    .4byte func_800E1EAC
-    .4byte func_800E1FE0
+    .4byte ftMario_func_800E1B84
+    .4byte ftMario_func_800E1D2C
+    .4byte ftMario_func_800E1EAC
+    .4byte ftMario_func_800E1FE0
     .4byte func_800761C8
     .4byte 0x0000012D
     .4byte 0x00340214
     .4byte 0x15000000
-    .4byte func_800E22BC
-    .4byte func_800E23DC
-    .4byte func_800E23E4
-    .4byte func_800E25C4
+    .4byte ftMario_func_800E22BC
+    .4byte ftMario_func_800E23DC
+    .4byte ftMario_func_800E23E4
+    .4byte ftMario_func_800E25C4
     .4byte func_800761C8
     .4byte 0x0000012E
     .4byte 0x00340614
     .4byte 0x15000000
-    .4byte func_800E2308
-    .4byte func_800E23E0
-    .4byte func_800E2508
-    .4byte func_800E2778
+    .4byte ftMario_func_800E2308
+    .4byte ftMario_func_800E23E0
+    .4byte ftMario_func_800E2508
+    .4byte ftMario_func_800E2778
     .4byte func_800761C8
 .global lbl_803D15D8
 lbl_803D15D8:
---
+++ src/melee/ft/chara/ftMario/ftmario2.c
@@ -0,0 +1,145 @@
+#include "ftmario.h"
+#include <sysdolphin/baselib/random.h>
+
+#include <melee/it/id.h>
+
+int _func_800E0D1C_helper(Fighter* ft, int* arr, int outpos) {
+    int r3;
+    r3 = (int)arr[HSD_Randi(outpos)];
+    ft->sa.mario.x2230 = ft->sa.mario.x222C;
+    ft->sa.mario.x222C = r3;
+    return r3;
+}
+
+//https://decomp.me/scratch/od8nq
+int ftMario_func_800E0D1C(HSD_GObj* gobj) {
+    Fighter* ft;
+    int arr[9];
+    int r3,i;
+
+    ft = gobj->user_data;
+
+    for (i = r3 = 0; i < 9; i++) {
+        if (i != (int)ft->sa.mario.x222C && i != (int)ft->sa.mario.x2230) {
+            arr[r3] = i;
+            r3++;
+        }
+    }
+
+    r3 = _func_800E0D1C_helper(ft, arr, r3);
+
+    return r3;
+}
+
+void ftMario_func_800E0DA8(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    ft->x2200_ftcmd_var0 = 0;
+    ft->x2210_ThrowFlags.flags = 0;
+    Fighter_ActionStateChange_800693AC(gobj,0x157,0,NULL,0.0f,1.0f,0.0f);
+    func_8006EBA4(gobj);
+    ft->cb.x21BC_callback_Accessory4 = ftMario_func_800E0EE0;
+}
+
+void ftMario_func_800E0E18(HSD_GObj* gobj) {
+    if (func_8006F238(gobj) == 0) {
+        func_8008A2BC(gobj);
+    }
+}
+
+void ftMario_func_800E0E54(HSD_GObj* gobj) {
+    Fighter* ft;
+    ft = getFighter(gobj);
+    if(ft->x2200_ftcmd_var0 != 0) {
+        func_8008A4D4(gobj);
+    }
+}
+
+void ftMario_func_800E0E84(HSD_GObj* gobj) {
+    func_80084F3C(gobj);
+}
+
+void ftMario_func_800E0EA4(HSD_GObj* gobj) {
+    if (func_80082708(gobj) == 0) {
+        ftMario_func_800E1178(gobj);
+    }
+}
+
+//https://decomp.me/scratch/Of8qP
+void ftMario_func_800E0EE0(HSD_GObj* gobj) {
+    Vec3 coords;
+    Fighter* ft;
+
+    s32 flag_res;
+
+    int rand_val_800E0D1C;
+
+    u8 padding[4];
+
+    ft = gobj->user_data;
+
+    if (ft->x2210_ThrowFlags.b0 != 0) {
+        ft->x2210_ThrowFlags.b0 = 0;
+        flag_res = 1;
+    } else {
+        flag_res = 0;
+    }
+
+    if (flag_res != 0) {
+        func_8000B1CC(ft->x5E8_fighterBones[func_8007500C(ft, 0x17)].x0_jobj,NULL,&coords);
+        if(ft->x4_fighterKind == 0) {
+            func_8029B6F8(ft->x2C_facing_direction, gobj,&coords,0x30);
+            ef_Spawn(0x47a,gobj,ft->x5E8_fighterBones[func_8007500C(ft, 0x17)].x0_jobj,&ft->x2C_facing_direction);
+        } else {
+            rand_val_800E0D1C = ftMario_func_800E0D1C(gobj);
+            func_802C0510(gobj, &coords, rand_val_800E0D1C, 0x31, ft->x2C_facing_direction);
+        }
+    }
+}
+
+void ftMario_func_800E1040(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    ft->x2200_ftcmd_var0 = 0;
+    ft->x2210_ThrowFlags.flags = 0;
+    Fighter_ActionStateChange_800693AC(gobj,0x158,0,NULL,0.0f,1.0f,0.0f);
+    func_8006EBA4(gobj);
+    ft->cb.x21BC_callback_Accessory4 = ftMario_func_800E0EE0;
+}
+
+void ftMario_func_800E10B0(HSD_GObj* gobj) {
+    if (func_8006F238(gobj) == 0) {
+        func_800CC730(gobj);
+    }
+}
+
+void ftMario_func_800E10EC(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    if (ft->x2200_ftcmd_var0 != 0) {
+        func_800CCAAC(gobj);
+    }
+}
+
+void ftMario_func_800E111C(void) {
+    func_80084DB0();
+}
+
+void ftMario_func_800E113C(HSD_GObj* gobj) {
+    if (func_80081D0C(gobj) != 0) {
+        ftMario_func_800E11E0(gobj);
+    }
+}
+
+void ftMario_func_800E1178(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    func_8007D5D4(ft);
+    Fighter_ActionStateChange_800693AC(gobj,0x158,0x5000,NULL,ft->x894,1.0f,0.0f);
+
+    ft->cb.x21BC_callback_Accessory4 = ftMario_func_800E0EE0;
+}
+
+void ftMario_func_800E11E0(HSD_GObj* gobj) {
+    Fighter* ft = getFighter(gobj);
+    func_8007D7FC(ft);
+    Fighter_ActionStateChange_800693AC(gobj,0x157,0x5000,NULL,ft->x894,1.0f,0.0f);
+
+    ft->cb.x21BC_callback_Accessory4 = ftMario_func_800E0EE0;
+}
