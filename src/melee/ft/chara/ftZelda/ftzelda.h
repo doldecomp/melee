b99e02198b0ecee0359524a6029d03645dcc574d
Durgan <72322229+DRGN-DRC@users.noreply.github.com>
2022-08-01 20:41:11 -0700

    Original functions commented out. Adding to header.

--- src/melee/ft/chara/ftZelda/ftzelda.h
+++ src/melee/ft/chara/ftZelda/ftzelda.h
@@ -43,4 +43,77 @@ typedef struct _ftZeldaAttributes {
     u8 data_filler_1[0x18];
 } ftZeldaAttributes;
 
+// ftzelda
+void ftZelda_OnDeath(HSD_GObj* fighterObj);
+void ftZelda_OnLoad(HSD_GObj* fighterObj);
 void ftZelda_801393AC(HSD_GObj* fighterObj);
+void ftZelda_OnItemPickup(HSD_GObj* fighterObj, BOOL bool);
+void ftZelda_OnItemInvisible(HSD_GObj *fighterObj);
+void ftZelda_OnItemVisible(HSD_GObj *fighterObj);
+void ftZelda_OnItemDrop(HSD_GObj* fighterObj, BOOL bool1);
+void ftZelda_LoadSpecialAttrs(HSD_GObj* gobj);
+void ftZelda_801395C8(HSD_GObj* fighterObj);
+void ftZelda_OnKnockbackEnter(HSD_GObj* fighterObj);
+void ftZelda_OnKnockbackExit(HSD_GObj* fighterObj);
+void ftZelda_801396AC(HSD_GObj* fighterObj);
+void func_801396E0(HSD_GObj* fighterObj);
+void ftZelda_8013979C(HSD_GObj* fighterObj);
+void ftZelda_SpecialHi_StartAction(HSD_GObj* fighterObj);
+void ftZelda_SpecialAirHi_StartAction(HSD_GObj* fighterObj);
+void ftZelda_801399B4(HSD_GObj* arg0);
+void ftZelda_801399F0(HSD_GObj* arg0);
+void ftZelda_80139A2C(HSD_GObj* arg0);
+void ftZelda_80139A30(HSD_GObj* arg0);
+void ftZelda_80139A34(HSD_GObj* arg0);
+void ftZelda_80139A54(HSD_GObj* fighterObj);
+void ftZelda_80139A98(HSD_GObj* arg0);
+void ftZelda_80139AD4(HSD_GObj* fighterObj);
+void ftZelda_80139B44(HSD_GObj* fighterObj);
+void ftZelda_80139BB0(HSD_GObj* fighterObj);
+void ftZelda_80139C1C(HSD_GObj* fighterObj);
+void ftZelda_80139C58(HSD_GObj* fighterObj);
+void ftZelda_80139C94(HSD_GObj* arg0);
+void ftZelda_80139C98(HSD_GObj* arg0);
+void ftZelda_80139C9C(HSD_GObj* arg0);
+void ftZelda_80139CBC(HSD_GObj* arg0);
+void ftZelda_80139CC0(HSD_GObj* fighterObj);
+void ftZelda_80139D60(HSD_GObj* fighterObj);
+void ftZelda_80139F6C(HSD_GObj* fighterObj);
+void ftZelda_80139FE8(HSD_GObj* fighterObj);
+void ftZelda_8013A058(HSD_GObj* fighterObj);
+void ftZelda_8013A244(HSD_GObj* fighterObj);
+void ftZelda_8013A448(HSD_GObj* fighterObj);
+void ftZelda_8013A484(HSD_GObj* fighterObj);
+void ftZelda_8013A4E4(HSD_GObj* arg0);
+void ftZelda_8013A4E8(HSD_GObj* arg0);
+void ftZelda_8013A4EC(HSD_GObj* arg0);
+void ftZelda_8013A50C(HSD_GObj* fighterObj);
+void ftZelda_8013A588(HSD_GObj* arg0);
+void ftZelda_8013A5C4(HSD_GObj* fighterObj);
+void ftZelda_8013A648(HSD_GObj* fighterObj);
+void ftZelda_8013A6A8(HSD_GObj* fighterObj);
+void ftZelda_8013A764(HSD_GObj* fighterObj);
+
+// ftzelda 2
+void ftZelda_8013A830(HSD_GObj* fighterObj);
+void ftZelda_8013A8AC(HSD_GObj* fighterObj);
+void ftZelda_SpecialN_StartAction(HSD_GObj* fighterObj);
+void ftZelda_SpecialAirN_StartAction(HSD_GObj* fighterObj);
+void ftZelda_8013AA38(HSD_GObj* fighterObj);
+void ftZelda_8013AACC(HSD_GObj* fighterObj);
+void ftZelda_8013AB60(HSD_GObj* fighterObj);
+void ftZelda_8013AB64(HSD_GObj* fighterObj);
+void ftZelda_8013AB68(HSD_GObj* arg0);
+void ftZelda_8013AB9C(HSD_GObj* fighterObj);
+void ftZelda_8013AC10(HSD_GObj* fighterObj);
+void ftZelda_8013AC4C(HSD_GObj* fighterObj);
+void ftZelda_8013AC88(HSD_GObj* fighterObj);
+void ftZelda_8013AD1C(HSD_GObj* fighterObj);
+void ftZelda_8013ADB0(HSD_GObj* fighterObj);
+
+// ftzelda 3
+void ftZelda_8013ADB4(HSD_GObj* fighterObj);
+void ftZelda_8013AE30(HSD_GObj* fighterObj);
+
+// ftzelda 4
+
