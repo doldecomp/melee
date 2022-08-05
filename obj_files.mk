c448f0c9410a0f8c68bf6f52eb5ec63264d9eb73
Robin Avery <robin@ribbanya.dev>
2022-08-05 09:13:29 -0400

    Rename part 3 based on string

--- obj_files.mk
+++ obj_files.mk
@@ -35,7 +35,7 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/melee/lb/lbsnap.s.o				\
 	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_1.s.o		\
 	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_2.s.o		\
-	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_3.s.o		\
+	$(BUILD_DIR)/asm/melee/lb/lbmthp.s.o		\
 	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_4.s.o		\
 	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_5.s.o		\
 	$(BUILD_DIR)/asm/melee/lb/code_8001E2F8_6.s.o		\
