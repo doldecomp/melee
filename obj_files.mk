2eab4f49f7c7fb2d10305893d537eaac81025ed9
Robin Avery <robin@ribbanya.dev>
2022-08-16 03:25:07 -0400

    Clean up and rename `code_800877F8.s` to `ft_unknown_006.s`

--- obj_files.mk
+++ obj_files.mk
@@ -87,7 +87,7 @@ TEXT_O_FILES +=\
 	$(BUILD_DIR)/asm/melee/ft/ftdata.s.o\
 	$(BUILD_DIR)/src/melee/ft/ftlib.c.o\
 	$(BUILD_DIR)/asm/melee/ft/ftlib.s.o\
-	$(BUILD_DIR)/asm/melee/ft/code_800877F8.s.o\
+	$(BUILD_DIR)/asm/melee/ft/ft_unknown_006.s.o\
 
 TEXT_O_FILES +=\
 	$(BUILD_DIR)/src/melee/ft/chara/ftDonkey/ftdonkey1.c.o\
--- tools/convert_data.py
+++ tools/convert_data.py
@@ -7,7 +7,7 @@ from typing import TextIO, Match, Callable, Any, Tuple, Union, List
 root = Path(__file__).parent.parent
 
 options = {
-    'input_glob': r"asm\melee\ft\*_unknown_*.s"
+    'input_glob': r"asm\melee\ft\*_unknown_006.s"
 }
 
 default_options = {
