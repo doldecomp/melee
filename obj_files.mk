d5bd6adb4da3b5294dc0c435ce728a1f6d620f92
Robin Avery <robin@ribbanya.dev>
2022-08-20 10:26:55 -0400

    Move `GXInit.s` and `GXAttr.s`

--- obj_files.mk
+++ obj_files.mk
@@ -553,9 +553,9 @@ TEXT_O_FILES +=\
 	$(BUILD_DIR)/src/dolphin/dvd/fstload.c.o\
 
 TEXT_O_FILES +=\
-	$(BUILD_DIR)/asm/dolphin/gx/GXInit/GXInit_001.s.o\
+	$(BUILD_DIR)/asm/dolphin/gx/GXInit.s.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXFifo.c.o\
-	$(BUILD_DIR)/asm/dolphin/gx/GXAttr/GXAttr_001.s.o\
+	$(BUILD_DIR)/asm/dolphin/gx/GXAttr.s.o\
 	$(BUILD_DIR)/asm/dolphin/gx/GXMisc/__GXMisc.s.o\
 	$(BUILD_DIR)/asm/dolphin/gx/GXMisc/GXMisc_001.s.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXMisc/GXMisc_002.c.o\
