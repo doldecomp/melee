9db94b6063c792fd8da7ea02eb7fcfaa93a7b7d6
Robin Avery <robin@ribbanya.dev>
2022-08-22 06:30:55 -0400

    Take a pattern in `-r` option

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 HERE=$(dirname "$(readlink -f $0)")
 clean=false
-rebuild=false
+rebuild_pattern=
 expected=false
 generate_map=0
 non_matching=0
 frank=0
 clear=false
 
-usage="$(basename "$0") [-cremnfxh]
+usage="$(basename "$0") [-cemnfxh] [-r pattern]
 
 where
     -h  help: show this message
     -c  clean: ""make clean"" before running make
-    -r  rebuild: delete *.o files; -c takes priority
+    -r  rebuild: delete *.o files matching the given pattern; -c takes priority
     -e  expected: after a successful make run, sync ./build to ./expected/build
                   (requires ""pacman -S rsync"")
     -f  frank: pass EPILOGUE_PROCESS=1 to make
@@ -20,18 +20,22 @@ where
     -n  non-matching: pass NON_MATCHING=1 to make
     -x  clear: clear the console before executing this script"
 
-while getopts ":cremnfxh" arg; do
+while getopts ":cr:emnfxh" arg; do
     case $arg in
     h) echo "$usage"
        exit
        ;;
     c) clean=true ;;
-    r) rebuild=true ;;
+    r) rebuild_pattern=$OPTARG ;;
     e) expected=true ;;
     m) generate_map=1 ;;
     n) non_matching=1 ;;
     f) frank=1 ;;
     x) clear=true ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
     \?) printf "illegal option: -%s\n" "$OPTARG" >&2
         echo "$usage" >&2
         exit 1
@@ -47,9 +51,10 @@ pushd $HERE
 if [ "$clean" = true ]; then
     echo "Cleaning."
     make clean
-elif [ "$rebuild" = true ]; then
-    echo "Deleting object files."
-    find build/ -name '*.o' -delete
+elif [ "$rebuild_pattern" ]; then
+    rebuild_pattern="*/${rebuild_pattern}*.o"
+    echo "Deleting files matching pattern $rebuild_pattern"
+    find ./build -iwholename $rebuild_pattern -delete
 fi
 
 echo "Running make with NON_MATCHING=$non_matching GENERATE_MAP=$generate_map EPILOGUE_PROCESS=$frank"
