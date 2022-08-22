4bec5a9962312aaeec75fd8964a2f374c13d1f1b
Robin Avery <robin@ribbanya.dev>
2022-08-22 05:11:40 -0400

    Support toggling `frank` in `Makefile` and `build.sh`
    `build.sh` now requires that you pass `-f` to apply `frank`.
    
    `Makefile` now defaults to `EPILOGUE_PROCESS=1` but no longer overwrites
    user arguments.

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ rebuild=false
 expected=false
 generate_map=0
 non_matching=0
+frank=0
 clear=false
 
-usage="$(basename "$0") [-cremnxh]
+usage="$(basename "$0") [-cremnfxh]
 
 where
     -h  help: show this message
@@ -14,11 +15,12 @@ where
     -r  rebuild: delete *.o files; -c takes priority
     -e  expected: after a successful make run, sync ./build to ./expected/build
                   (requires ""pacman -S rsync"")
+    -f  frank: pass EPILOGUE_PROCESS=1 to make
     -m  map: pass GENERATE_MAP=1 to make
     -n  non-matching: pass NON_MATCHING=1 to make
     -x  clear: clear the console before executing this script"
 
-while getopts ":cremnxh" arg; do
+while getopts ":cremnfxh" arg; do
     case $arg in
     h) echo "$usage"
        exit
@@ -28,6 +30,7 @@ while getopts ":cremnxh" arg; do
     e) expected=true ;;
     m) generate_map=1 ;;
     n) non_matching=1 ;;
+    f) frank=1 ;;
     x) clear=true ;;
     \?) printf "illegal option: -%s\n" "$OPTARG" >&2
         echo "$usage" >&2
@@ -51,6 +54,7 @@ fi
 
 echo "Running make with NON_MATCHING=$non_matching and GENERATE_MAP=$generate_map"
 make NON_MATCHING=$non_matching GENERATE_MAP=$generate_map
+make NON_MATCHING=$non_matching GENERATE_MAP=$generate_map EPILOGUE_PROCESS=$frank
 result=$?
 
 if [ "$expected" = true ]; then
