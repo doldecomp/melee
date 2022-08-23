03f4ea2c0ea73fd6d6be50ee51672056882e9d45
Robin Avery <robin@ribbanya.dev>
2022-08-23 17:46:36 -0400

    Clean up `__start`

--- include/dolphin/os/init/__start.h
+++ include/dolphin/os/init/__start.h
@@ -1,12 +1,11 @@
-#pragma once
+#ifndef DOLPHIN_OS_INIT___START_H
+#define DOLPHIN_OS_INIT___START_H
 
 #include <dolphin/types.h>
 #include <dolphin/os/OSReset.h>
 
 #define PAD3_BUTTON_ADDR 0x800030E4
 #define OS_RESET_RESTART 0
-#define FALSE 0
-#define TRUE 1
 #define EXCEPTIONMASK_ADDR 0x80000044
 #define BOOTINFO2_ADDR 0x800000F4
 #define OS_BI2_DEBUGFLAG_OFFSET 0xC
@@ -29,56 +28,55 @@ static u8 Debug_BBA = 0;
 extern void memset(void *, int, int);
 extern int main(int argc, char *argv[]);
 extern void exit(int);
-extern void __init_user(void);
-extern void OSInit(void);
-extern void DBInit(void);
-extern void __OSCacheInit(void);
-extern void __OSPSInit(void);
+extern void __init_user();
+extern void OSInit();
+extern void DBInit();
+extern void __OSCacheInit();
+extern void __OSPSInit();
 
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __check_pad3();
 
-	extern void __check_pad3(void);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __start();
 
-	extern void __start(void);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __init_registers();
 
-	extern void __init_registers(void);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __init_data();
 
-	extern void __init_data(void);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __init_hardware();
 
-	extern void __init_hardware(void);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
+	void __flush_cache(void *address, size_t size);
 
-	extern void __flush_cache(void *address, unsigned int size);
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
-
 	extern char _stack_addr[];
+
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
-
 	extern char _SDA_BASE_[];
+
 #ifndef M2CTX
 __declspec(section ".init")
 #endif
-
 	extern char _SDA2_BASE_[];
 
 typedef struct __rom_copy_info
@@ -103,3 +101,5 @@ typedef struct __bss_init_info
 __declspec(section ".init")
 #endif
 	extern __bss_init_info _bss_init_info[];
+
+#endif
