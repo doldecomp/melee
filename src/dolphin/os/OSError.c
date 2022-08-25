73a5c44e1e7b07fefc00210d315c22f7ce7745a6
Robin Avery <robin@ribbanya.dev>
2022-08-25 14:10:48 -0400

    Match `OSReport`

--- src/dolphin/os/OSError.c
+++ src/dolphin/os/OSError.c
@@ -1,2 +1,11 @@
 #include <dolphin/os/OSError.h>
+#include <stdio.h>
 
+void OSReport(char* msg, ...)
+{
+    va_list args;
+
+    va_start(args, msg);
+    vprintf(msg, args);
+    va_end(args);
+}
