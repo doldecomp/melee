9f9388173863b07b54507675a32f2124d1c09d09
Robin Avery <robin@ribbanya.dev>
2022-08-27 13:08:25 -0400

    Fix names of `HSD_GObj* fp`

--- src/melee/ft/chara/ftGameWatch/ftGameWatch_SpecialS.c
+++ src/melee/ft/chara/ftGameWatch/ftGameWatch_SpecialS.c
@@ -1,9 +1,9 @@
 #include <ftgamewatch.h>
 
-void ftGameWatch_ItemJudgementExitHitlag(HSD_GObj* fp);
+void ftGameWatch_ItemJudgementExitHitlag(HSD_GObj* fighter_gobj);
 extern void func_8028FAF4(HSD_GObj*, Vec3*);
 extern HSD_GObj* func_802C7774(f32, HSD_GObj*, Vec3*, s32, u32);
-void ftGameWatch_ItemJudgementEnterHitlag(HSD_GObj* fp);
+void ftGameWatch_ItemJudgementEnterHitlag(HSD_GObj* fighter_gobj);
 
 // 0x8014C46C 
 // https://decomp.me/scratch/ohXu0 // Create Judgement item
