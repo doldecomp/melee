07896153cee350ffd487baef9729c8159925fd9c
Robin Avery <robin@ribbanya.dev>
2022-08-26 23:16:56 -0400

    Reformat `__ppc_eabi_init`

--- src/dolphin/os/init/__ppc_eabi_init.c
+++ src/dolphin/os/init/__ppc_eabi_init.c
@@ -1,6 +1,6 @@
 #include <dolphin/os/init/__ppc_eabi_init.h>
 
-typedef void (*voidfunc) (void);
+typedef void (*voidfunc)(void);
 
 extern voidfunc lbl_803B7240[]; // _ctors
 
@@ -10,14 +10,14 @@ extern void PPCHalt(void);
 
 void __init_user(void)
 {
-   __init_cpp();
+    __init_cpp();
 }
 
 #pragma push
 #pragma peephole off
 static void __init_cpp(void)
 {
-    voidfunc *constructor;
+    voidfunc* constructor;
 
     for (constructor = lbl_803B7240; *constructor; constructor++) {
         (*constructor)();
