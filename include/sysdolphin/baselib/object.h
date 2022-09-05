0aee007c5b4f3a733a9aba5314a5137db38de3a8
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2022-09-05 19:52:09 +0100

    Add matching DObjForeachAnim and nonmatching JObjForeachAnim (#552)
    * Add matching DObjForeachAnim and nonmatching JObjForeachAnim
    
    * Simplify assert condition
    
    * Clarify ForeachAnim arg types
    
    * Create HSD_TypeMask enum for aobj ForeachAnim functions
    
    * Fix NON_MATCHING JObjForeachAnim type masks
    
    * Use MASK_OF macro to compute type masks
    
    Co-authored-by: Revo <projectrevotpp@hotmail.com>

--- include/sysdolphin/baselib/object.h
+++ include/sysdolphin/baselib/object.h
@@ -30,6 +30,28 @@ typedef enum _HSD_Type {
     HSD_MAX_TYPE,
 } HSD_Type;
 
+#define MASK_OF(type) (1 << ((type) - 1))
+
+typedef enum _HSD_TypeMask {
+    AOBJ_MASK     = MASK_OF(AOBJ_TYPE),
+    COBJ_MASK     = MASK_OF(COBJ_TYPE),
+    DOBJ_MASK     = MASK_OF(DOBJ_TYPE),
+    FOBJ_MASK     = MASK_OF(FOBJ_TYPE),
+    FOG_MASK      = MASK_OF(FOG_TYPE),
+    JOBJ_MASK     = MASK_OF(JOBJ_TYPE),
+    LOBJ_MASK     = MASK_OF(LOBJ_TYPE),
+    MOBJ_MASK     = MASK_OF(MOBJ_TYPE),
+    POBJ_MASK     = MASK_OF(POBJ_TYPE),
+    ROBJ_MASK     = MASK_OF(ROBJ_TYPE),
+    TOBJ_MASK     = MASK_OF(TOBJ_TYPE),
+    WOBJ_MASK     = MASK_OF(WOBJ_TYPE),
+    RENDER_MASK   = MASK_OF(RENDER_TYPE),
+    CHAN_MASK     = MASK_OF(CHAN_TYPE),
+    TEVREG_MASK   = MASK_OF(TEVREG_TYPE),
+    CBOBJ_MASK    = MASK_OF(CBOBJ_TYPE),
+    ALL_TYPE_MASK = MASK_OF(HSD_MAX_TYPE),
+} HSD_TypeMask;
+
 typedef struct _HSD_Obj {
     struct _HSD_Class parent;
     u16 ref_count;
