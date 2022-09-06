5424f9dde44bbf8c730eac7e960675775d79c9af
Robin Avery <robin@ribbanya.dev>
2022-09-06 09:38:43 -0400

    Migrate `GXStubs` to C

--- asm/dolphin/gx/GXStubs.s
+++
@@ -1,8 +0,0 @@
-.include "macros.inc"
-
-.section .text
-
-# https://decomp.me/scratch/4EbpN // 0 (100%)
-.global __GXSetRange
-__GXSetRange:
-/* 803410D4 0033DCB4  4E 80 00 20 */	blr 
--- obj_files.mk
+++ obj_files.mk
@@ -567,7 +567,7 @@ TEXT_O_FILES +=\
 	$(BUILD_DIR)/src/dolphin/gx/GXBump.c.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXTev.c.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXPixel.c.o\
-	$(BUILD_DIR)/asm/dolphin/gx/GXStubs.s.o\
+	$(BUILD_DIR)/src/dolphin/gx/GXStubs.c.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXDisplayList.c.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXTransform.c.o\
 	$(BUILD_DIR)/src/dolphin/gx/GXPerf.c.o\
