a658e52f0f2045c560fcafaec384ad9ed434f88a
Ryan Burns <rtburns@protonmail.com>
2022-09-09 12:18:18 -0700

    Rename m2c placeholder variables

--- src/sysdolphin/baselib/fog.c
+++ src/sysdolphin/baselib/fog.c
@@ -9,21 +9,21 @@ HSD_FogInfo hsdFog = { FogInfoInit };
 HSD_ClassInfo hsdFogAdj = { FogAdjInfoInit };
 
 typedef struct _GXFogAdjTbl {
-	u16 r[10];
+    u16 r[10];
 } GXFogAdjTbl;
 
 const GXColor lbl_804DE6F0 = { 0 };
 
 void HSD_FogSet(HSD_Fog* fog)
 {
-    GXFogAdjTbl sp88;
-    f32 sp70[6];
+    GXFogAdjTbl tbl;
+    f32 v[6];
     HSD_CObj* cobj;
-    s32 var_r31;
+    s32 range;
     struct {
         f32 x0;
-        f32 arr[6];
-    } sp54;
+        f32 v[6];
+    } proj;
 
     if (fog == NULL) {
         GXSetFog(0, lbl_804DE6F0, 0.0F, 0.0F, 0.0F, 0.0F);
@@ -37,42 +37,42 @@ void HSD_FogSet(HSD_Fog* fog)
         HSD_CObjGetNear(cobj), HSD_CObjGetFar(cobj));
 
     if (fog->fog_adj != NULL) {
-        GXGetViewportv(sp70);
-        var_r31 = sp70[0] + sp70[2] * (fog->fog_adj->center + 320) / 640.0F;
-        if (var_r31 < 0) {
-            var_r31 = 0;
+        GXGetViewportv(v);
+        range = v[0] + v[2] * (fog->fog_adj->center + 320) / 640.0F;
+        if (range < 0) {
+            range = 0;
         }
-        if (var_r31 > 640) {
-            var_r31 = 640;
+        if (range > 640) {
+            range = 640;
         }
         if (fog->fog_adj->width != 0) {
-            GXInitFogAdjTable(&sp88, fog->fog_adj->width, fog->fog_adj->mtx);
+            GXInitFogAdjTable(&tbl, fog->fog_adj->width, fog->fog_adj->mtx);
         } else {
-            Mtx44 sp14 = { 0 };
-            GXGetProjectionv(&sp54);
-            switch ((s32) sp54.x0) {
+            Mtx44 mtx = { 0 };
+            GXGetProjectionv(&proj);
+            switch ((s32) proj.x0) {
             case 0:
-                sp14[0][0] = sp54.arr[0];
-                sp14[0][2] = sp54.arr[1];
-                sp14[1][1] = sp54.arr[2];
-                sp14[1][2] = sp54.arr[3];
-                sp14[2][2] = sp54.arr[4];
-                sp14[2][3] = sp54.arr[5];
-                sp14[3][2] = -1.0F;
+                mtx[0][0] = proj.v[0];
+                mtx[0][2] = proj.v[1];
+                mtx[1][1] = proj.v[2];
+                mtx[1][2] = proj.v[3];
+                mtx[2][2] = proj.v[4];
+                mtx[2][3] = proj.v[5];
+                mtx[3][2] = -1.0F;
                 break;
             case 1:
-                sp14[0][0] = sp54.arr[0];
-                sp14[0][3] = sp54.arr[1];
-                sp14[1][1] = sp54.arr[2];
-                sp14[1][3] = sp54.arr[3];
-                sp14[2][2] = sp54.arr[4];
-                sp14[2][3] = sp54.arr[5];
-                sp14[3][3] = 1.0F;
+                mtx[0][0] = proj.v[0];
+                mtx[0][3] = proj.v[1];
+                mtx[1][1] = proj.v[2];
+                mtx[1][3] = proj.v[3];
+                mtx[2][2] = proj.v[4];
+                mtx[2][3] = proj.v[5];
+                mtx[3][3] = 1.0F;
                 break;
             }
-            GXInitFogAdjTable(&sp88, sp70[2], sp14);
+            GXInitFogAdjTable(&tbl, v[2], mtx);
         }
-        GXSetFogRangeAdj(1, var_r31, &sp88);
+        GXSetFogRangeAdj(1, range, &tbl);
         return;
     }
     GXSetFogRangeAdj(0, 0, NULL);
@@ -105,11 +105,11 @@ void HSD_FogInit(HSD_Fog* fog, HSD_FogDesc* desc)
             fog->end = desc->end;
             fog->color = desc->color;
         } else {
-            f32 sp10[6];
-            GXGetViewportv(sp10);
+            f32 v[6];
+            GXGetViewportv(v);
             fog->type = 2;
-            fog->start = sp10[4];
-            fog->end = sp10[5];
+            fog->start = v[4];
+            fog->end = v[5];
             fog->color.r = 0xFF;
             fog->color.g = 0xFF;
             fog->color.b = 0xFF;
@@ -135,16 +135,15 @@ HSD_FogAdj* HSD_FogAdjLoadDesc(HSD_FogAdjDesc* desc)
 
 void HSD_FogAdjInit(HSD_FogAdj* adj, HSD_FogAdjDesc* desc)
 {
-    f32 sp10[6];
-
     if (adj != NULL) {
         if (desc != NULL) {
             adj->width = desc->width;
             adj->center = desc->center;
             PSMTXCopy(desc->mtx, adj->mtx);
         } else {
-            GXGetViewportv(sp10);
-            adj->width = sp10[2];
+            f32 v[6];
+            GXGetViewportv(v);
+            adj->width = v[2];
             adj->center = adj->width / 2;
             PSMTXIdentity(adj->mtx);
             adj->mtx[3][0] = 0.0F;
