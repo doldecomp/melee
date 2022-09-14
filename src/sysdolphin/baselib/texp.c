e99c499cbcde0bef728679a4fbf93f009b564064
Will C <wcarter312@gmail.com>
2022-09-14 14:15:43 -0400

    Match HSD_TExpFreeList

--- src/sysdolphin/baselib/texp.c
+++ src/sysdolphin/baselib/texp.c
@@ -67,3 +67,87 @@ void HSD_TExpUnref(HSD_TExp* texp, u8 sel)
         return;
     }
 }
+
+inline HSD_TExpFree(HSD_TExp* texp)
+{
+    switch (HSD_TExpGetType(texp)) {
+    case HSD_TE_TEV:
+        hsdFreeMemPiece(texp, sizeof(HSD_TETev));
+        break;
+    case HSD_TE_CNST:
+        hsdFreeMemPiece(texp, sizeof(HSD_TECnst));
+        break;
+    }
+}
+
+HSD_TExp* HSD_TExpFreeList(HSD_TExp* texp_list, HSD_TExpType type, s32 all)
+{
+    HSD_TExp** handle;
+    HSD_TExp* ptr;
+    HSD_TExp* next;
+
+    handle = &texp_list;
+
+    if (all != 0) {
+        while (*handle != NULL) {
+            if (type == HSD_TE_ALL || type == (*handle)->type) {
+                switch ((*handle)->type) {
+                case HSD_TE_TEV:
+                    next = (*handle)->tev.next;
+                    HSD_TExpFree(*handle);
+                    *handle = next;
+                    continue;
+                case HSD_TE_CNST:
+                    next = (*handle)->tev.next;
+                    HSD_TExpFree(*handle);
+                    *handle = next;
+                    continue;
+                default:
+                    __assert(__FILE__, 0xDB, "0");
+                }
+            }
+            handle = &(*handle)->tev.next;
+        }
+    } else {
+        if (type == HSD_TE_ALL || type == HSD_TE_TEV) {
+            for (ptr = texp_list; ptr != NULL; ptr = ptr->tev.next) {
+                switch (ptr->type) {
+                case HSD_TE_TEV:
+                    if (ptr->tev.c_ref == 0 && ptr->tev.c_ref == 0) {
+                        HSD_TExpUnref(ptr, 1);
+                        HSD_TExpUnref(ptr, 5);
+                    }
+                    break;
+                }
+            }
+        }
+
+        while (*handle != NULL) {
+            if (type == HSD_TE_ALL || type == (*handle)->type) {
+                switch ((*handle)->type) {
+                case HSD_TE_TEV:
+                    if ((*handle)->tev.c_ref != 0 || (*handle)->tev.a_ref != 0) {
+                        break;
+                    }
+                    next = (*handle)->tev.next;
+                    HSD_TExpFree(*handle);
+                    *handle = next;
+                    continue;
+                case HSD_TE_CNST:
+                    if ((*handle)->cnst.ref != 0) {
+                        break;
+                    }
+                    next = (*handle)->tev.next;
+                    HSD_TExpFree(*handle);
+                    *handle = next;
+                    continue;
+                default:
+                    __assert(__FILE__, 0x10B, "0");
+                }
+            }
+            handle = &(*handle)->tev.next;
+        }
+    }
+
+    return texp_list;
+}
