949538b1d43f0607179ccdf2d8e92a06e2918bb2
Will C <wcarter312@gmail.com>
2022-09-14 16:01:12 -0400

    Stop mixing game structs with sysdolphin

--- src/melee/it/item.h
+++ src/melee/it/item.h
@@ -688,6 +688,175 @@ inline Item* GetItemDirect(HSD_GObj* item_gobj)
     return item_gobj->user_data;
 }
 
+typedef struct ItemCommonData
+{
+    u32 x0;
+    u32 x4;
+    u32 x8;
+    u32 xC;
+    u32 x10;
+    u32 x14;
+    u32 x18;
+    u32 x1C;
+    u32 x20;
+    u32 x24;
+    u32 x28;
+    u32 x2C;
+    s32 x30;
+    u32 x34;
+    s32 x38_float;
+    s32 x3C_float;
+    s32 x40_float;
+    s32 x44_float;
+    s32 x48_float;
+    s32 x4C_float;
+    s32 x50_float;
+    s32 x54_float;
+    s32 x58_float;
+    s32 x5C_float;
+    s32 x60_float;
+    s32 x64_float;
+    s32 x68_float;
+    f32 x6C_float;
+    f32 x70_float;
+    f32 x74_float;
+    f32 x78_float;
+    f32 x7C_float;
+    f32 x80_float[13];
+    s32 xB4;
+    f32 xB8;
+    f32 xBC;
+    f32 xC0;
+    f32 xC4;
+    f32 xC8;
+    f32 xCC;
+    f32 xD0;
+    f32 xD4;
+    s32 xD8;
+    s32 xDC;
+    u8 filler[0x148 - 0xE0];
+    s32 x148;
+    f32 x14C;
+    f32 x150;
+    f32 x154;
+    f32 x158;
+    f32 x15C;
+} ItemCommonData;
+
+extern s8 lbl_804D6D00;
+extern s32 lbl_804D6D08;
+extern s32 lbl_804D6D0C;
+extern s32 lbl_804D6D10;
+extern s32 lbl_804D6D14;
+extern ItemCommonData* lbl_804D6D28;
+
+typedef struct Item_r13_Data
+{
+    ItemCommonData* item_common;
+    void** common_items;
+    void** adventure_items;
+    void** pokeball_items;
+    s32 x10;
+    s32 x14;
+}Item_r13_Data;
+
+typedef struct HSD_ObjAllocUnk2
+{
+    u8 x0_filler[0x144];
+    u32 x144;
+    u32 x148;
+    u32 x14C;
+    UnkFlagStruct x150;
+    u32 x154;
+    u32 x158;
+    u32 x15C;
+}HSD_ObjAllocUnk2;
+
+typedef struct x1C_struct
+{
+    u32 x1C;
+}x1C_struct;
+
+typedef struct HSD_ObjAllocUnk
+{
+    u32 x0;
+    u32 x4;
+    u32 x8;
+    u32 xC;
+    u32 x10;
+    u32 x14;
+    u32 x18;
+    x1C_struct x1C_struct;
+    u32 x20;
+    u32 x24;
+    u32 x28;
+    u32 x2C;
+    u32 x30;
+    u32 x34;
+    u32 x38;
+    u32 x3C;
+    u32 x40;
+    u32 x44;
+    u32 x48;
+    u32 x4C;
+    u32 x50;
+    u32 x54;
+    u32 x58;
+    u32 x5C;
+    u32 x60;
+    u32 x64;
+    u32 x68;
+} HSD_ObjAllocUnk;
+
+typedef struct HSD_ObjAllocUnkStruct
+{
+    HSD_ObjAllocData x0;
+    HSD_ObjAllocData x2C;
+    HSD_ObjAllocData x58;
+    HSD_ObjAllocUnk x84;
+    HSD_ObjAllocUnk2 xEC;
+}HSD_ObjAllocUnkStruct;
+
+typedef struct HSD_ObjAllocUnk3
+{
+    u32 x0;
+    u32 x4;
+    u32 x8;
+    u32 xC;
+}HSD_ObjAllocUnk3;
+
+typedef struct HSD_ObjAllocUnk4
+{
+    u8 x0;
+    u8 x1;
+    u8 x2;
+    u8 x3;
+    u8 x4;
+    u16 x6;
+    u16 x8;
+    u16 xA;
+    u16 xC;
+    u32 x10;
+}HSD_ObjAllocUnk4;
+
+typedef struct HSD_ObjAllocUnk5
+{
+    u8 x0;
+    u32 x4;
+    u16 x8;
+    u16 xA;
+    u16 xC;
+}HSD_ObjAllocUnk5;
+
+typedef struct HSD_ObjAllocUnk6
+{
+    u8 x0;
+    u32 x4;
+    u16 x8;
+    u16 xA;
+    u16 xC;
+}HSD_ObjAllocUnk6;
+
 // FUNCTIONS //
 s32 func_80266F3C(void);                                       // Check if items are enabled
 void func_80266F70(void);                                      // Check to load ItCo.dat/usd
