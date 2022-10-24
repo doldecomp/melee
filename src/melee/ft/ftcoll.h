9e0d8d6499b274014bb4aa2109c587809e34149b
Robin Avery <robin@ribbanya.dev>
2022-10-23 21:03:05 -0400

    Fix missing semicolon
    Co-authored-by: Ryan Burns <52847440+r-burns@users.noreply.github.com>

--- src/melee/ft/ftcoll.h
+++ src/melee/ft/ftcoll.h
@@ -14,7 +14,7 @@ void func_80076528(HSD_GObj* fighter_gobj); // Combo count something + adjust To
 void func_800765AC(HSD_GObj* victim); // Clear victim pointer from attacker upon freeing memory? //
 void func_800765E0(void); // Reset hitbox and phantom collision count? //
 f32 func_800765F0(Fighter* fp, HSD_GObj* victim, f32 unk_floatvar); // Unk knockback related ? //
-void func_8007B5AC(Fighter *arg0, ftHurt *arg1, s32 *arg2)
+void func_8007B5AC(Fighter *arg0, ftHurt *arg1, s32 *arg2);
 BOOL func_8007B868(HSD_GObj* fighter_gobj);
 
 #endif 
