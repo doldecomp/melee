9ccb62c2013435dd6d5d9e1a8c8def7e5ee2c5d5
Robin Avery <robin@ribbanya.dev>
2022-10-25 13:36:15 -0400

    Migrate `OSFont` to C

--- obj_files.mk
+++ obj_files.mk
@@ -595,7 +595,7 @@ TEXT_O_FILES +=\
 	$(BUILD_DIR)/asm/dolphin/os/OSError_data.s.o\
 	$(BUILD_DIR)/src/dolphin/os/OSExi.c.o\
 	$(BUILD_DIR)/asm/dolphin/os/OSExi_data.s.o\
-	$(BUILD_DIR)/asm/dolphin/os/OSFont.s.o\
+	$(BUILD_DIR)/src/dolphin/os/OSFont.c.o\
 	$(BUILD_DIR)/asm/dolphin/os/OSInterrupt.s.o\
 	$(BUILD_DIR)/src/dolphin/os/OSLink.c.o\
 	$(BUILD_DIR)/src/dolphin/os/OSMemory.c.o\
