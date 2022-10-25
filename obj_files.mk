f0c4f967a3ecdb945fbae1224271677a44bad35d
Robin Avery <robin@ribbanya.dev>
2022-10-25 19:47:54 -0400

    Migrate `SIGetTypeAsync` to C

--- obj_files.mk
+++ obj_files.mk
@@ -610,7 +610,7 @@ TEXT_O_FILES +=\
 	$(BUILD_DIR)/src/dolphin/os/OSRtc.c.o\
 	$(BUILD_DIR)/asm/dolphin/os/OSRtc_data.s.o\
 	$(BUILD_DIR)/src/dolphin/os/OSSerial.c.o\
-	$(BUILD_DIR)/asm/dolphin/os/OSSerial.s.o\
+	$(BUILD_DIR)/asm/dolphin/os/OSSerial_data.s.o\
 	$(BUILD_DIR)/src/dolphin/os/OSSync.c.o\
 	$(BUILD_DIR)/asm/dolphin/os/OSThread.s.o\
 	$(BUILD_DIR)/asm/dolphin/os/OSTime.s.o\
