12f244a64d77b4cfb85599202d00fa7a80a10241
Robin Avery <robin@ribbanya.dev>
2022-10-25 12:48:17 -0400

    Migrate `OSDumpContext` to C

--- include/dolphin/os/OSContext.h
+++ include/dolphin/os/OSContext.h
@@ -5,21 +5,21 @@
 #include <dolphin/types.h>
 
 typedef struct OSContext {
-    u32 gprs[32]; // at 0x0
-    u32 cr;       // at 0x80
-    u32 lr;       // at 0x84
-    u32 ctr;      // at 0x88
-    u32 xer;      // at 0x8C
-    f64 fprs[32]; // at 0x90
-    u32 WORD_0x190; // at 0x190
-    u32 fpscr; // at 0x194
-    u32 srr0;  // at 0x198
-    u32 srr1;  // at 0x19C
-    u16 SHORT_0x1A0; // at 0x1A0
-    u16 SHORT_0x1A2; // at 0x1A2
-    u32 gqrs[8]; // at 0x1A4
+    u32 gprs[32];                  // at 0x0
+    u32 cr;                        // at 0x80
+    u32 lr;                        // at 0x84
+    u32 ctr;                       // at 0x88
+    u32 xer;                       // at 0x8C
+    f64 fprs[32];                  // at 0x90
+    u32 WORD_0x190;                // at 0x190
+    u32 fpscr;                     // at 0x194
+    u32 srr0;                      // at 0x198
+    u32 srr1;                      // at 0x19C
+    u16 SHORT_0x1A0;               // at 0x1A0
+    u16 SHORT_0x1A2;               // at 0x1A2
+    u32 gqrs[8];                   // at 0x1A4
     char UNK_0x1C4[0x1C8 - 0x1C4]; // at 0x1C4
-    f64 psfs[32]; // at 0x1C8
+    f64 psfs[32];                  // at 0x1C8
 } OSContext;
 
 OSContext* OS_CURRENT_CONTEXT
