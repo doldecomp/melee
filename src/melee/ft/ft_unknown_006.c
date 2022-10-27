8ca97074380cc9c501774dc717be5a6e6d5bac83
Robin Avery <robin@ribbanya.dev>
2022-10-26 22:28:33 -0400

    Migrate `func_8008CE0C` to C

--- src/melee/ft/ft_unknown_006.c
+++ src/melee/ft/ft_unknown_006.c
@@ -6944,3 +6944,41 @@ lbl_8008CDF4:
 /* 8008CE08 000899E8  4E 80 00 20 */	blr 
 } // clang-format on
 #pragma pop
+
+extern unk_t func_8008CF70();
+extern unk_t func_800EB42C();
+
+#pragma push
+asm unk_t func_8008CE0C()
+{ // clang-format off
+    nofralloc
+/* 8008CE0C 000899EC  7C 08 02 A6 */	mflr r0
+/* 8008CE10 000899F0  90 01 00 04 */	stw r0, 4(r1)
+/* 8008CE14 000899F4  94 21 FF F8 */	stwu r1, -8(r1)
+/* 8008CE18 000899F8  80 83 00 2C */	lwz r4, 0x2c(r3)
+/* 8008CE1C 000899FC  80 04 00 04 */	lwz r0, 4(r4)
+/* 8008CE20 00089A00  2C 00 00 14 */	cmpwi r0, 0x14
+/* 8008CE24 00089A04  41 82 00 20 */	beq lbl_8008CE44
+/* 8008CE28 00089A08  40 80 00 10 */	bge lbl_8008CE38
+/* 8008CE2C 00089A0C  2C 00 00 06 */	cmpwi r0, 6
+/* 8008CE30 00089A10  41 82 00 14 */	beq lbl_8008CE44
+/* 8008CE34 00089A14  48 00 00 20 */	b lbl_8008CE54
+lbl_8008CE38:
+/* 8008CE38 00089A18  2C 00 00 18 */	cmpwi r0, 0x18
+/* 8008CE3C 00089A1C  41 82 00 10 */	beq lbl_8008CE4C
+/* 8008CE40 00089A20  48 00 00 14 */	b lbl_8008CE54
+lbl_8008CE44:
+/* 8008CE44 00089A24  48 05 E5 E9 */	bl func_800EB42C
+/* 8008CE48 00089A28  48 00 00 10 */	b lbl_8008CE58
+lbl_8008CE4C:
+/* 8008CE4C 00089A2C  48 0B E8 01 */	bl ftGameWatch_AttackAir_DecideAction
+/* 8008CE50 00089A30  48 00 00 08 */	b lbl_8008CE58
+lbl_8008CE54:
+/* 8008CE54 00089A34  48 00 01 1D */	bl func_8008CF70
+lbl_8008CE58:
+/* 8008CE58 00089A38  80 01 00 0C */	lwz r0, 0xc(r1)
+/* 8008CE5C 00089A3C  38 21 00 08 */	addi r1, r1, 8
+/* 8008CE60 00089A40  7C 08 03 A6 */	mtlr r0
+/* 8008CE64 00089A44  4E 80 00 20 */	blr 
+} // clang-format on
+#pragma pop
