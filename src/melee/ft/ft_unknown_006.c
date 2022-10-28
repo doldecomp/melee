a65bb79a016265cb8fe44bce484b00eb5376be11
Robin Avery <robin@ribbanya.dev>
2022-10-28 05:20:25 -0400

    Migrate `func_8009B170` to C

--- src/melee/ft/ft_unknown_006.c
+++ src/melee/ft/ft_unknown_006.c
@@ -25627,3 +25627,32 @@ asm unk_t func_8009B150()
 /* 8009B16C 00097D4C  4E 80 00 20 */	blr 
 } // clang-format on
 #pragma pop
+
+extern unk_t func_8009B1B8();
+
+#pragma push
+asm unk_t func_8009B170()
+{ // clang-format off
+    nofralloc
+/* 8009B170 00097D50  7C 08 02 A6 */	mflr r0
+/* 8009B174 00097D54  90 01 00 04 */	stw r0, 4(r1)
+/* 8009B178 00097D58  94 21 FF E0 */	stwu r1, -0x20(r1)
+/* 8009B17C 00097D5C  93 E1 00 1C */	stw r31, 0x1c(r1)
+/* 8009B180 00097D60  7C 7F 1B 78 */	mr r31, r3
+/* 8009B184 00097D64  48 02 FC FD */	bl func_800CAE80
+/* 8009B188 00097D68  2C 03 00 00 */	cmpwi r3, 0
+/* 8009B18C 00097D6C  41 82 00 14 */	beq lbl_8009B1A0
+/* 8009B190 00097D70  7F E3 FB 78 */	mr r3, r31
+/* 8009B194 00097D74  48 00 00 25 */	bl func_8009B1B8
+/* 8009B198 00097D78  38 60 00 01 */	li r3, 1
+/* 8009B19C 00097D7C  48 00 00 08 */	b lbl_8009B1A4
+lbl_8009B1A0:
+/* 8009B1A0 00097D80  38 60 00 00 */	li r3, 0
+lbl_8009B1A4:
+/* 8009B1A4 00097D84  80 01 00 24 */	lwz r0, 0x24(r1)
+/* 8009B1A8 00097D88  83 E1 00 1C */	lwz r31, 0x1c(r1)
+/* 8009B1AC 00097D8C  38 21 00 20 */	addi r1, r1, 0x20
+/* 8009B1B0 00097D90  7C 08 03 A6 */	mtlr r0
+/* 8009B1B4 00097D94  4E 80 00 20 */	blr 
+} // clang-format on
+#pragma pop
