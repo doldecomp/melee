936df1c1d0f889e51c82586167be5e6eee157823
Robin Avery <robin@ribbanya.dev>
2022-10-28 02:48:52 -0400

    Migrate `lbl_80099644` to C

--- src/melee/ft/chara/ftYoshi/ftyoshi.h
+++ src/melee/ft/chara/ftYoshi/ftyoshi.h
@@ -40,10 +40,13 @@ struct S_UNK_YOSHI1 {
 void func_8012B6E8(Fighter* fp, struct S_UNK_YOSHI1* unk_struct_arg);
 void func_8012B804(Fighter* fp, struct S_UNK_YOSHI1* unk_struct_arg, f32 start_frame);
 void func_8012C850(HSD_GObj* fighter_gobj);
+void func_8012B8A4(HSD_GObj* fighter_gobj);
+void func_8012BE3C(HSD_GObj* fighter_gobj);
 unk_t func_8012BECC();
 unk_t func_8012C1D4();
 unk_t func_8012C49C();
 unk_t func_8012C600();
 unk_t func_8012BDA0();
+unk_t func_8012CC1C();
 
 #endif
