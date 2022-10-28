fb12a84e2a83dd399cee4b1a9d2d491df92d07fb
Robin Avery <robin@ribbanya.dev>
2022-10-28 18:41:50 -0400

    Migrate `func_8009E7B4` to C

--- src/melee/ft/ft_unknown_006.h
+++ src/melee/ft/ft_unknown_006.h
@@ -130,5 +130,6 @@ void func_800CB438(HSD_GObj* fighter_gobj);
 void func_8009CF84(Fighter* fp);
 void func_8009E0A8(HSD_GObj* fighter_gobj);
 void func_8009E0D4(Fighter* fp);
+func_8009E7B4(Fighter* fp, u8*);
 
 #endif
