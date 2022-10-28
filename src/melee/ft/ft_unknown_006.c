1e2b9fa7d9bab6209b3c885efe7c9462edf7584c
Robin Avery <robin@ribbanya.dev>
2022-10-28 16:26:17 -0400

    Migrate `func_8009BD24` to C

--- src/melee/ft/ft_unknown_006.c
+++ src/melee/ft/ft_unknown_006.c
@@ -26761,3 +26761,22 @@ asm unk_t func_8009BD04()
 /* 8009BD20 00098900  4E 80 00 20 */	blr 
 } // clang-format on
 #pragma pop
+
+extern unk_t lbl_8009BD4C();
+
+#pragma push
+asm unk_t func_8009BD24()
+{ // clang-format off
+    nofralloc
+/* 8009BD24 00098904  7C 08 02 A6 */	mflr r0
+/* 8009BD28 00098908  3C 80 80 0A */	lis r4, lbl_8009BD4C@ha
+/* 8009BD2C 0009890C  90 01 00 04 */	stw r0, 4(r1)
+/* 8009BD30 00098910  38 84 BD 4C */	addi r4, r4, lbl_8009BD4C@l
+/* 8009BD34 00098914  94 21 FF F8 */	stwu r1, -8(r1)
+/* 8009BD38 00098918  4B FE 6F 3D */	bl func_80082C74
+/* 8009BD3C 0009891C  80 01 00 0C */	lwz r0, 0xc(r1)
+/* 8009BD40 00098920  38 21 00 08 */	addi r1, r1, 8
+/* 8009BD44 00098924  7C 08 03 A6 */	mtlr r0
+/* 8009BD48 00098928  4E 80 00 20 */	blr 
+} // clang-format on
+#pragma pop
