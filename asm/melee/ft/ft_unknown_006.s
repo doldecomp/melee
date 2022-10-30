02fed83d88efa22670bdbac6eeb37569fa068270
Robin Avery <robin@ribbanya.dev>
2022-10-30 02:00:50 -0400

    Match `func_800C0FC8`

--- asm/melee/ft/ft_unknown_006.s
+++ asm/melee/ft/ft_unknown_006.s
@@ -2,10 +2,6 @@
 
 .section .text  # 0x80087818 - 0x800DF934
 
-.global func_800C0FC8
-func_800C0FC8:
-/* 800C0FC8 000BDBA8  4E 80 00 20 */	blr 
-
 .global func_800C0FCC
 func_800C0FCC:
 /* 800C0FCC 000BDBAC  7C 08 02 A6 */	mflr r0
--- src/melee/ft/ft_unknown_006.c
+++ src/melee/ft/ft_unknown_006.c
@@ -71826,3 +71826,8 @@ lbl_800C0FB0:
 /* 800C0FC4 000BDBA4  4E 80 00 20 */	blr 
 } // clang-format on
 #pragma pop
+
+void func_800C0FC8(void)
+{
+    return;
+}
