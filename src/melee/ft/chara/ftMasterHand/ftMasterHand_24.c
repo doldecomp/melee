d110e2eb0795699091d5b97b2c68c7d2a476e9fa
Robin Avery <robin@ribbanya.dev>
2022-11-01 18:01:31 -0400

    Clean up `ftMasterHand_24`

--- src/melee/ft/chara/ftMasterHand/ftMasterHand_24.c
+++ src/melee/ft/chara/ftMasterHand/ftMasterHand_24.c
@@ -1,34 +1,30 @@
 #include <melee/ft/chara/ftMasterHand/__ftMasterHand.h>
-#include <melee/ft/chara/ftMasterHand/ftMasterHand.h>
 
-// 80154C78 151858
-// https://decomp.me/scratch/j7foW
-void ftMasterHand_80154C78(HSD_GObj* arg0)
+void ftMasterHand_80154C78(HSD_GObj* fighter_gobj)
 {
-    Fighter* temp_r31;
-    MasterHandAttributes* temp_r30;
+    Fighter* fp = fighter_gobj->user_data;
+    MasterHandAttributes* ext_attr = fp->x10C_ftData->ext_attr;
 
-    temp_r31 = arg0->user_data;
-    temp_r30 = temp_r31->x10C_ftData->ext_attr;
-    Fighter_ActionStateChange_800693AC(arg0, 0x178, 0, 0, 0.0f, 1.0f, 0.0f);
-    func_8006EBA4(arg0);
-    temp_r31->x234C_pos.x = temp_r30->x118_pos.x;
-    temp_r31->x234C_pos.y = temp_r30->x118_pos.y;
-    temp_r31->x234C_pos.z = 0.0f;
+    Fighter_ActionStateChange_800693AC(fighter_gobj, 0x178, 0, 0, 0.0F, 1.0F, 0.0F);
+    func_8006EBA4(fighter_gobj);
+
+    fp->x234C_pos.x = ext_attr->x118_pos.x;
+    fp->x234C_pos.y = ext_attr->x118_pos.y;
+    fp->x234C_pos.z = 0.0F;
 }
 
-// 80154CF8 1518D8
-// https://decomp.me/scratch/XwAlv
-void ftMasterHand_80154CF8(HSD_GObj* gobj, Fighter* fp)
+// todo: fp_1 is part of an inline
+void ftMasterHand_80154CF8(HSD_GObj* fighter_gobj, Fighter* fp)
 {
-    Fighter* temp_r31;
-    MasterHandAttributes* temp_r30;
+    Fighter* fp_1 = fighter_gobj->user_data;
+    MasterHandAttributes* ext_attr = fp_1->x10C_ftData->ext_attr;
 
-    temp_r31 = gobj->user_data;
-    temp_r30 = temp_r31->x10C_ftData->ext_attr;
-    Fighter_ActionStateChange_800693AC(gobj, 0x179, 0, 0, 0.0f, 1.0f, 0.0f);
-    func_8006EBA4(gobj);
-    temp_r31->x234C_pos.x = temp_r30->x30_pos2.x;
-    temp_r31->x234C_pos.y = temp_r30->x30_pos2.y;
-    temp_r31->x234C_pos.z = 0.0f;
+    Fighter_ActionStateChange_800693AC(fighter_gobj, 0x179, 0, 0, 0.0F, 1.0F, 0.0F);
+    func_8006EBA4(fighter_gobj);
+
+    /* second inline */ {
+        fp_1->x234C_pos.x = ext_attr->x30_pos2.x;
+        fp_1->x234C_pos.y = ext_attr->x30_pos2.y;
+        fp_1->x234C_pos.z = 0.0F;
+    }
 }
