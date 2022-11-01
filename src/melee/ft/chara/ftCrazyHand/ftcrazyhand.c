d6d7b2cc7a35c884d63d66ad38fbce628896ec24
Robin Avery <robin@ribbanya.dev>
2022-11-01 04:47:52 -0400

    Remove `func_8015BDB4` from `functions.h`

--- src/melee/ft/chara/ftCrazyHand/ftcrazyhand.c
+++ src/melee/ft/chara/ftCrazyHand/ftcrazyhand.c
@@ -1,56 +1,57 @@
 #include <melee/ft/chara/ftCrazyHand/ftcrazyhand.h>
 #include <melee/ft/ftbosslib.h>
 
-void ftCrazyhand_OnDeath(HSD_GObj* fighter_gobj) {
-
+void ftCrazyhand_OnDeath(HSD_GObj* fighter_gobj)
+{
+    return;
 }
 
-void ftCrazyhand_OnLoad(HSD_GObj* fighter_gobj) {
-    ftData* ftdata;
-    ftCrazyHandAttributes* ftData_attr;
+void ftCrazyhand_OnLoad(HSD_GObj* fighter_gobj)
+{
+    ftData* ft_data;
+    ftCrazyHandAttributes* ext_attr;
     void** items;
     Fighter* fp;
 
-    
     fp = fighter_gobj->user_data;
-    ftdata = fp->x10C_ftData;
-    ftData_attr = ftdata->ext_attr;
-    items = ftdata->x48_items;
+    ft_data = fp->x10C_ftData;
+    ext_attr = ft_data->ext_attr;
+    items = ft_data->x48_items;
 
     PUSH_ATTRS(fp, ftCrazyHandAttributes);
-    
+
     ftbosslib_8015BDB4(fighter_gobj);
     func_8026B3F8(items[0], 0x7F);
     func_8026B3F8(items[1], 0x80);
     func_8026B3F8(items[2], 0x81);
-    fp->x2229_b5_no_normal_motion = 1;
-    fp->x2229_b6 = 1;
-    fp->x2229_b7 = 1;
-    fp->x222A_flag.bits.b0 = 1;
-    fp->x222A_flag.bits.b1 = 1;
-    fp->x2229_b3 = 1;
-    fp->xB0_pos.x = ftData_attr->x18;
-    fp->xB0_pos.y = ftData_attr->x1C;
-    fp->xB0_pos.z = 0.0f;
+    fp->x2229_b5_no_normal_motion = TRUE;
+    fp->x2229_b6 = TRUE;
+    fp->x2229_b7 = TRUE;
+    fp->x222A_flag.bits.b0 = TRUE;
+    fp->x222A_flag.bits.b1 = TRUE;
+    fp->x2229_b3 = TRUE;
+    fp->xB0_pos.x = ext_attr->x18;
+    fp->xB0_pos.y = ext_attr->x1C;
+    fp->xB0_pos.z = 0.0F;
     fp->x2368 = 0;
-    fp->x236C = 0; 
+    fp->x236C = 0;
     fp->x2370 = 0;
     fp->x2374 = 0;
     fp->x2378 = -1;
     fp->x237C = -1;
     fp->x2380 = -1;
-    fp->x235C = 0.0f;
+    fp->x235C = 0.0F;
     fp->x2360 = FALSE;
     fp->sa.masterhand.x222C = func_8015C244(fighter_gobj, &fp->xB0_pos);
-    fp->sa.masterhand.x2238 = 1.0f;
+    fp->sa.masterhand.x2238 = 1.0F;
     fp->sa.masterhand.x224C = 0;
-    fp->sa.masterhand.x2250 = 0x159; 
+    fp->sa.masterhand.x2250 = 0x159;
     fp->sa.masterhand.x2254 = 0;
     fp->x1A98 = 1;
-    ftbosslib_8015BD24(fp->x1A98, &fp->sa.masterhand.x223C, fp->sa.crazyhand.x2238, ftData_attr->x0, ftData_attr->x8, ftData_attr->x4);
+    ftbosslib_8015BD24(fp->x1A98, &fp->sa.masterhand.x223C, fp->sa.crazyhand.x2238, ext_attr->x0, ext_attr->x8, ext_attr->x4);
 }
 
-
-void ftCrazyHand_LoadSpecialAttrs(HSD_GObj* gobj) {
+void ftCrazyHand_LoadSpecialAttrs(HSD_GObj* gobj)
+{
     COPY_ATTRS(gobj, ftCrazyHandAttributes);
 }
--- src/melee/ft/chara/ftMasterHand/ftMasterHand_1.c
+++ src/melee/ft/chara/ftMasterHand/ftMasterHand_1.c
@@ -7,31 +7,30 @@ void ftMasterhand_OnDeath(HSD_GObj* fighter_gobj)
 
 void ftMasterhand_OnLoad(HSD_GObj* fighter_gobj)
 {
-
-    ftData* ftdata;
-    MasterHandAttributes* ftData_attr;
+    ftData* ft_data;
+    MasterHandAttributes* ext_attr;
     void** items;
     Fighter* fp;
 
     fp = fighter_gobj->user_data;
-    ftdata = fp->x10C_ftData;
-    ftData_attr = ftdata->ext_attr;
-    items = ftdata->x48_items;
+    ft_data = fp->x10C_ftData;
+    ext_attr = ft_data->ext_attr;
+    items = ft_data->x48_items;
 
     PUSH_ATTRS(fp, MasterHandAttributes);
 
     ftbosslib_8015BDB4(fighter_gobj);
     func_8026B3F8(items[0], 0x7D);
     func_8026B3F8(items[1], 0x7E);
-    fp->x2229_b5_no_normal_motion = 1;
-    fp->x2229_b6 = 1;
-    fp->x2229_b7 = 1;
-    fp->x222A_flag.bits.b0 = 1;
-    fp->x222A_flag.bits.b1 = 1;
-    fp->x2229_b3 = 1;
-    fp->xB0_pos.x = ftData_attr->x30_pos2.x;
-    fp->xB0_pos.y = ftData_attr->x30_pos2.y;
-    fp->xB0_pos.z = 0.0f;
+    fp->x2229_b5_no_normal_motion = TRUE;
+    fp->x2229_b6 = TRUE;
+    fp->x2229_b7 = TRUE;
+    fp->x222A_flag.bits.b0 = TRUE;
+    fp->x222A_flag.bits.b1 = TRUE;
+    fp->x2229_b3 = TRUE;
+    fp->xB0_pos.x = ext_attr->x30_pos2.x;
+    fp->xB0_pos.y = ext_attr->x30_pos2.y;
+    fp->xB0_pos.z = 0.0F;
     fp->x2374 = 0;
     fp->x2378 = 0;
     fp->x237C = 0;
@@ -42,10 +41,10 @@ void ftMasterhand_OnLoad(HSD_GObj* fighter_gobj)
     fp->x235C = 0.0f;
     fp->x2360 = FALSE;
     fp->sa.masterhand.x222C = func_8015C244(fighter_gobj, &fp->xB0_pos);
-    fp->sa.masterhand.x2238 = 1.0f;
+    fp->sa.masterhand.x2238 = 1.0F;
     fp->sa.masterhand.x224C = 0;
     fp->sa.masterhand.x2250 = 0x15B;
     fp->sa.masterhand.x2254 = 0;
     fp->x1A98 = 1;
-    ftbosslib_8015BD24(fp->x1A98, &fp->sa.masterhand.x223C, fp->sa.masterhand.x2238, ftData_attr->x18, ftData_attr->x20, ftData_attr->x1C);
+    ftbosslib_8015BD24(fp->x1A98, &fp->sa.masterhand.x223C, fp->sa.masterhand.x2238, ext_attr->x18, ext_attr->x20, ext_attr->x1C);
 }
