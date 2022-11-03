7cdd9b31c4d9ce0b5fdc378a3e66cad1c40daeda
Robin Avery <robin@ribbanya.dev>
2022-11-03 07:50:16 -0400

    Create `ctypes.h`

--- src/dolphin/types.h
+++ src/dolphin/types.h
@@ -1,44 +1,7 @@
-#ifndef __TYPES_H__
-#define __TYPES_H__
+#ifndef DOLPHIN_TYPES_H
+#define DOLPHIN_TYPES_H
 
-typedef signed char         s8;
-typedef signed short        s16;
-typedef signed long         s32;
-typedef signed long long    s64;
-typedef unsigned char       u8;
-typedef unsigned short      u16;
-typedef unsigned long       u32;
-typedef unsigned long long  u64;
-
-typedef volatile u8         vu8;
-typedef volatile u16        vu16;
-typedef volatile u32        vu32;
-typedef volatile u64        vu64;
-typedef volatile s8         vs8;
-typedef volatile s16        vs16;
-typedef volatile s32        vs32;
-typedef volatile s64        vs64;
-
-typedef float               f32;
-typedef double              f64;
-typedef volatile f32        vf32;
-typedef volatile f64        vf64;
-
-typedef int                 BOOL;
-
-typedef int                 intptr_t;
-typedef unsigned int        uintptr_t;
-
-typedef unsigned long size_t;
-typedef void *unk_t;
-typedef void* any_t;
-typedef void (*jmp_t)(void);
-typedef jmp_t jtbl_t[];
-
-#define TRUE 1
-#define FALSE 0
-
-#define NULL ((void*)0)
+#include <ctypes.h>
 
 #define FLT_MAX 3.4028235E38
 
