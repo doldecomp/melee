4989a8f14f9eea5fde0b3296ac9e9620a4c4577d
Robin Avery <robin@ribbanya.dev>
2022-11-03 12:07:06 -0400

    Migrate `lbl_8013D06C` to C

--- src/melee/ft/chara/ftPurin/ftpurin.c
+++ src/melee/ft/chara/ftPurin/ftpurin.c
@@ -811,3 +811,18 @@ asm unk_t lbl_8013D04C()
 /* 8013D068 00139C48  4E 80 00 20 */	blr 
 } // clang-format on
 #pragma pop
+
+#pragma push
+asm unk_t lbl_8013D06C()
+{ // clang-format off
+    nofralloc
+/* 8013D06C 00139C4C  7C 08 02 A6 */	mflr r0
+/* 8013D070 00139C50  90 01 00 04 */	stw r0, 4(r1)
+/* 8013D074 00139C54  94 21 FF F8 */	stwu r1, -8(r1)
+/* 8013D078 00139C58  4B F4 7E 75 */	bl func_80084EEC
+/* 8013D07C 00139C5C  80 01 00 0C */	lwz r0, 0xc(r1)
+/* 8013D080 00139C60  38 21 00 08 */	addi r1, r1, 8
+/* 8013D084 00139C64  7C 08 03 A6 */	mtlr r0
+/* 8013D088 00139C68  4E 80 00 20 */	blr 
+} // clang-format on
+#pragma pop
