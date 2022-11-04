c7c3d65a643761b6e3c431a9b2693cb57b1f8a72
Robin Avery <robin@ribbanya.dev>
2022-11-04 17:11:50 -0400

    Remove `func_80164840` from `functions.h`

--- src/functions.h
+++ src/functions.h
@@ -12,7 +12,6 @@ typedef struct _HSD_JObj HSD_JObj;
 typedef struct _HSD_ImageDesc HSD_ImageDesc;
 
 #pragma region "melee/gm/code_801601C4"
-BOOL func_80164840(u8);
 s32 func_8016AEDC(void);
 s32 func_8016B1C4();
 s32 func_8016B238(void);
