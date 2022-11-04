02ceb9b500399683b56363c1ef24848fb2121e16
Robin Avery <robin@ribbanya.dev>
2022-11-04 11:00:06 -0400

    Remove `func_8003FAA8` from `functions.h`

--- src/melee/pl/pl_unknown_001.h
+++ src/melee/pl/pl_unknown_001.h
@@ -1,7 +1,7 @@
 #ifndef MELEE_PL_PL_UNKNOWN_001_H
 #define MELEE_PL_PL_UNKNOWN_001_H
 
-#include <ctypes.h>
+#include <dolphin/mtx/types.h>
 
 unk_t func_8003EC30();
 unk_t func_800403C0();
@@ -9,5 +9,6 @@ unk_t func_8003FDF4();
 void func_80037C60();
 void func_8003E058(s32, s32, s32, s32);
 void func_8003EC9C(s32, s32, f32, f32);
+void func_8003FAA8(s32 slot, s32 index, Vec* pos, Vec* prevPos);
 
 #endif
