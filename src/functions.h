ba6515ada5028065a1955504e8f498964835fb4c
Robin Avery <robin@ribbanya.dev>
2022-11-04 14:01:44 -0400

    Remove `func_8007D494` from `functions.h`

--- src/functions.h
+++ src/functions.h
@@ -12,7 +12,6 @@ typedef struct _HSD_JObj HSD_JObj;
 typedef struct _HSD_ImageDesc HSD_ImageDesc;
 
 #pragma region "melee/ft/ftcommon"
-void func_8007D494(Fighter*, f32, f32);
 void func_8007D4B8(Fighter*);
 void func_8007D5BC();
 void func_8007D5D4(Fighter*);
