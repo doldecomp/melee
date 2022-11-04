073e1f1f7f62dbd7b1ef1bcb14ac940ae1b36410
Robin Avery <robin@ribbanya.dev>
2022-11-04 14:14:27 -0400

    Remove `ftGetParasolStatus` from `functions.h`

--- src/functions.h
+++ src/functions.h
@@ -12,7 +12,6 @@ typedef struct _HSD_JObj HSD_JObj;
 typedef struct _HSD_ImageDesc HSD_ImageDesc;
 
 #pragma region "melee/ft/ftcommon"
-s32 ftGetParasolStatus(HSD_GObj*);
 void func_8007EA90(Fighter*, s32);
 void func_8007EBAC(Fighter*, u32, u32);
 void func_8007ECD4();
