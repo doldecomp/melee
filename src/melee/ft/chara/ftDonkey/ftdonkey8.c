667b5355156d709020e14c15ca930fe7da6dc49d
Robin Avery <robin@ribbanya.dev>
2022-11-04 16:22:28 -0400

    Remove `func_800D5AEC` from `functions.h`

--- src/melee/ft/chara/ftDonkey/ftdonkey8.c
+++ src/melee/ft/chara/ftDonkey/ftdonkey8.c
@@ -1,24 +1,27 @@
 #include <melee/ft/chara/ftDonkey/ftdonkey.h>
+#include <melee/ft/ft_unknown_006.h>
 
-
-void ftDonkey_800E0568(HSD_GObj* fighter_gobj) {
+void ftDonkey_800E0568(HSD_GObj* fighter_gobj)
+{
     if (!func_80094EA4(fighter_gobj)) {
         func_800CB59C(fighter_gobj);
     }
 }
 
-void ftDonkey_800E05A4(HSD_GObj* fighter_gobj) {
+void ftDonkey_800E05A4(HSD_GObj* fighter_gobj)
+{
     func_80084F3C(fighter_gobj);
 }
 
-
-void ftDonkey_800E05C4(HSD_GObj* fighter_gobj) {
+void ftDonkey_800E05C4(HSD_GObj* fighter_gobj)
+{
     ftDonkey_800DFA70(fighter_gobj);
 }
 
 #include <melee/ft/ftanim.h>
 
-void ftDonkey_800E05E4(HSD_GObj* fighter_gobj) {
+void ftDonkey_800E05E4(HSD_GObj* fighter_gobj)
+{
     Fighter* fp = fighter_gobj->user_data;
     ftDonkeyAttributes* donkey_attr = getFtSpecialAttrs2CC(fp);
     fp->x2344_f32 = donkey_attr->cargo_hold.x28_LANDING_LAG;
@@ -27,7 +30,8 @@ void ftDonkey_800E05E4(HSD_GObj* fighter_gobj) {
     ftAnim_SetAnimRate(fighter_gobj, 0.0f);
 }
 
-void ftDonkey_800E0648(HSD_GObj* fighter_gobj) {
+void ftDonkey_800E0648(HSD_GObj* fighter_gobj)
+{
     Fighter* fp = fighter_gobj->user_data;
     if (fp->x2344_f32 <= 0.0f) {
         ftDonkey_800DF980(fighter_gobj);
--- src/melee/ft/ft_unknown_006.h
+++ src/melee/ft/ft_unknown_006.h
@@ -204,5 +204,6 @@ void func_800D1E80(HSD_GObj* fighter_gobj);
 void func_800D237C(HSD_GObj* fighter_gobj);
 void func_800D3158(HSD_GObj* fighter_gobj);
 void func_800D4F24(HSD_GObj* fighter_gobj, BOOL);
+void func_800D5AEC(HSD_GObj* fighter_gobj, s32 action_state, BOOL, BOOL, Fighter* fp, f32, f32);
 
 #endif
