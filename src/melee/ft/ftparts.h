c6f76f1a3423eca20e4970b4acc60c7cbe6cbe65
Robin Avery <robin@ribbanya.dev>
2022-11-04 13:23:30 -0400

    Remove `func_80073758` from `functions.h`

--- src/melee/ft/ftparts.h
+++ src/melee/ft/ftparts.h
@@ -4,10 +4,12 @@
 #include <ctypes.h>
 
 typedef struct _HSD_ObjAllocData HSD_ObjAllocData;
+typedef struct _HSD_JObj HSD_JObj;
 
 unk_t func_8007506C();
 extern HSD_ObjAllocData lbl_80459080;
 void func_80074B0C(HSD_GObj* fighter_gobj, s32, s32);
 void func_80074148(void);
+void func_80073758(HSD_JObj* jobj);
 
 #endif
