1ec97e488bef5f47947ca098a5fc27aec1350546
Robin Avery <robin@ribbanya.dev>
2022-11-04 12:51:10 -0400

    Remove `func_80070C48` from `functions.h`

--- src/melee/ft/ftanim.h
+++ src/melee/ft/ftanim.h
@@ -23,5 +23,6 @@ void func_80070654(HSD_GObj* fighter_gobj);
 void func_80070758(HSD_JObj* jobj);
 void func_8007077C(HSD_GObj* fighter_gobj);
 void ftAnim_ApplyPartAnim(HSD_GObj* fighter_gobj, s32, s32, f32);
+void func_80070C48(HSD_GObj* fighter_gobj, BOOL);
 
 #endif
