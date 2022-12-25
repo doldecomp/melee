1e85956dc85e90f181a7929e2f9ba62227263f8b
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2022-12-24 17:43:14 -0800

    Misc dolphin matching from prime (#624)
    * DSP cleanup
    
    * Match DSPAssertTask
    
    * Match dolphin pad
    
    * Finish labeling dolphin PAD
    
    * Match dolphin OSCache
    
    * Label start of HIO
    
    * Match OSMemory
    
    * Migrate OSReboot data to c

--- src/dolphin/dsp/dsp.c
+++ src/dolphin/dsp/dsp.c
@@ -2,22 +2,21 @@
 
 BOOL __DSP_init_flag;
 
-extern void* __DSP_curr_task;
-extern void* __DSP_first_task;
-extern void* __DSP_last_task;
-extern void* __DSP_tmp_task;
-
-extern char lbl_80400C08[]; //"DSPInit(): Build Date: %s %s\n"
+extern DSPTaskInfo* __DSP_curr_task;
+extern DSPTaskInfo* __DSP_first_task;
+extern DSPTaskInfo* __DSP_last_task;
+extern DSPTaskInfo* __DSP_tmp_task;
+extern DSPTaskInfo* __DSP_rude_task;
+extern BOOL __DSP_rude_task_pending;
 
 u32 DSPCheckMailToDSP(void)
 {
-    return (HW_REG(0xCC005000, u16) >> 15) & 1;
+    return (__DSPRegs[0] >> 15) & 1;
 }
 
 u32 DSPCheckMailFromDSP(void)
 {
-    return (HW_REG(0xCC005004, u16) >> 15) & 1;
-    ;
+    return (__DSPRegs[2] >> 15) & 1;
 }
 
 u32 DSPReadMailFromDSP(void)
@@ -27,8 +26,8 @@ u32 DSPReadMailFromDSP(void)
 
 void DSPSendMailToDSP(u32 mail)
 {
-    __DSPRegs[0] = (u16) ((mail >> 16) & 0xffff);
-    __DSPRegs[1] = (u16) (mail & 0xffff);
+    __DSPRegs[0] = mail >> 16;
+    __DSPRegs[1] = mail & 0xffff;
 }
 
 void DSPInit(void)
@@ -38,25 +37,23 @@ void DSPInit(void)
 
     __DSP_debug_printf("DSPInit(): Build Date: %s %s\n", "Sep  8 2001", "01:51:48"); // __DATE__, __TIME__
 
-    if (TRUE == __DSP_init_flag) {
+    if (__DSP_init_flag == TRUE) {
         return;
     }
 
     intr = OSDisableInterrupts();
 
-    __OSSetInterruptHandler(7, __DSPHandler);
-    __OSUnmaskInterrupts(0x80000000u >> 7);
+    __OSSetInterruptHandler(OS_INTR_DSP_DSP, __DSPHandler);
+    __OSUnmaskInterrupts(0x80000000U >> OS_INTR_DSP_DSP);
 
+    __DSPRegs[5] = (__DSPRegs[5] & ~0xA8) | 0x0800;
     tmp = __DSPRegs[5];
-    tmp = (u16) ((tmp & ~(0xA8)) | 0x0800);
-    __DSPRegs[5] = tmp;
+    __DSPRegs[5] = tmp & ~0xAC;
 
-    tmp = __DSPRegs[5];
-    tmp = (u16) (tmp & ~(0xAC));
-    __DSPRegs[5] = tmp;
-
-    __DSP_first_task = __DSP_last_task = __DSP_curr_task = __DSP_tmp_task = NULL;
-
+    __DSP_tmp_task = NULL;
+    __DSP_curr_task = NULL;
+    __DSP_last_task = NULL;
+    __DSP_first_task = NULL;
     __DSP_init_flag = TRUE;
 
     OSRestoreInterrupts(intr);
@@ -64,7 +61,7 @@ void DSPInit(void)
 
 BOOL DSPCheckInit(void)
 {
-    return (__DSP_init_flag);
+    return __DSP_init_flag;
 }
 
 DSPTaskInfo* DSPAddTask(DSPTaskInfo* task)
@@ -76,7 +73,39 @@ DSPTaskInfo* DSPAddTask(DSPTaskInfo* task)
     task->flags = 1;
 
     OSRestoreInterrupts(intrEnabled);
-    if (task == __DSP_first_task)
+    if (task == __DSP_first_task) {
         __DSP_boot_task(task);
+    }
     return task;
+}
+
+// Unknown DSP command
+// Similar to DSPReset, DSPHalt
+void DSPSomething(void)
+{
+    BOOL enabled = OSDisableInterrupts();
+    __DSPRegs[5] = (__DSPRegs[5] & ~0xA8) | 2;
+    OSRestoreInterrupts(enabled);
+}
+
+void* DSPAssertTask(DSPTaskInfo* task)
+{
+    BOOL enabled = OSDisableInterrupts();
+    if (__DSP_curr_task == task) {
+        __DSP_rude_task = task;
+        __DSP_rude_task_pending = 1;
+        OSRestoreInterrupts(enabled);
+        return task;
+    }
+    if (task->priority < __DSP_curr_task->priority) {
+        __DSP_rude_task = task;
+        __DSP_rude_task_pending = 1;
+        if (__DSP_curr_task->state == 1) {
+            DSPSomething();
+        }
+        OSRestoreInterrupts(enabled);
+        return task;
+    }
+    OSRestoreInterrupts(enabled);
+    return NULL;
 }
