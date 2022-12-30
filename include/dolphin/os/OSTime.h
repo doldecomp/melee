b2bfaba6ef992dfcb2267a21e8a5bb7c1f21cbb0
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2022-12-30 00:21:54 -0800

    Match dolphin OSExi (#634)
    Based on prime decomp
    (OSExi has been moved to EXIBios in the dolphin version used by prime)

--- include/dolphin/os/OSTime.h
+++ include/dolphin/os/OSTime.h
@@ -35,6 +35,7 @@ extern u32 __OSBusClock
 #define OS_BUS_CLOCK __OSBusClock
 #define OS_TIMER_CLOCK (OS_BUS_CLOCK / 4)
 #define OSTicksToSeconds(ticks) ((ticks) / (OS_TIMER_CLOCK))
+#define OSTicksToMilliseconds(ticks) ((ticks) / (OS_TIMER_CLOCK / 1000))
 #define OSSecondsToTicks(sec) ((sec) * (OS_TIMER_CLOCK))
 #define OSMillisecondsToTicks(msec) ((msec) * (OS_TIMER_CLOCK / 1000))
 #define OSMicrosecondsToTicks(usec) (((usec) * (OS_TIMER_CLOCK / 125000)) / 8)
