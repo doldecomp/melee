23b84cfd8c06b1858620000dbce30b3163ed1cd4
AltimorTASDK <altimormc@gmail.com>
2023-01-01 08:47:43 -0500

    working on translating ftparts to C
    name Fighter_JObjSetRotation
    
    match prototypes to functions.h
    
    fix typos
    
    add pragma push/pop
    
    HSD_PObjInfo::update doesn't exist in melee
    
    fix another typo
    
    get rid of old duplicate Fighter_IntpJObjInfoInit
    
    fix missing pragma pops
    
    fix bss ordering
    
    fix wrong ordered stores
    
    fix headers

--- src/melee/ft/ftparts.h
+++ src/melee/ft/ftparts.h
@@ -2,20 +2,22 @@
 #define MELEE_FT_FTPARTS_H
 
 #include <dolphin/types.h>
+#include <dolphin/mtx/mtxtypes.h>
 
-void func_80074B0C(HSD_GObj*, s32, s32);
-void func_80073758();
-void func_80074148();
-void func_80074170();
-void func_800743E0();
-void func_800749CC();
+void func_80074B0C(struct _HSD_GObj*, s32, s32);
+void func_80073758(struct _HSD_JObj*);
+void func_80074148(void);
+void func_80074170(void);
+void func_800743E0(void);
+void func_800749CC(struct _HSD_GObj*);
 void func_80074A4C(struct _HSD_GObj*, u32, s32);
-void func_80074A8C();
-void func_80074E58();
+void func_80074A8C(struct _HSD_GObj*);
+void func_80074E58(struct _Fighter*);
 s32 func_8007500C(struct _Fighter*, s32);
-void func_8007584C();
+void Fighter_JObjSetRotation(struct _HSD_JObj*, Quaternion*);
 void func_8007592C(struct _Fighter*, s32, f32);
 void func_80075AF0(struct _Fighter*, s32, f32);
 void func_80075CB4(struct _Fighter*, s32, f32);
+s32 func_8007500C(struct _Fighter*, s32);
 
 #endif
