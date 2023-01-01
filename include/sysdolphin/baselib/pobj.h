34104ec5cf823ed3f572115e3a3f2ee1d4f66ba8
AltimorTASDK <altimormc@gmail.com>
2023-01-01 08:12:53 -0500

    add HSD_PObjClearMtxMark to the header

--- include/sysdolphin/baselib/pobj.h
+++ include/sysdolphin/baselib/pobj.h
@@ -147,6 +147,7 @@ void HSD_PObjReqAnimAllByFlags(HSD_PObj* pobj, f32 startframe, u32 flags);
 void HSD_ClearVtxDesc();
 struct _HSD_PObj* HSD_PObjLoadDesc(struct _HSD_PObjDesc*);
 
+void HSD_PObjClearMtxMark(void* obj, u32 mark);
 void HSD_PObjSetMtxMark(int idx, void* obj, u32 mark);
 void HSD_PObjGetMtxMark(int idx, void** obj, u32* mark);
 
