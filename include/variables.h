ee68cbca35acbf6548d7ea4d3863364c012d42e5
Robin Avery <robin@ribbanya.dev>
2023-01-04 11:31:10 -0500

    Remove `ftFighter` from `variables.h`

--- include/variables.h
+++ include/variables.h
@@ -1,31 +1,4 @@
 #ifndef _variables_h_
 #define _variables_h_
 
-/// @addtogroup ftfighter ftFighter
-/// @{
-
-/// @todo #lbl_804D64FC..#lbl_804D6550 are initialized to 0 in
-///          #Fighter_LoadCommonData, probably all pointers
-extern void* lbl_804D64FC;
-
-extern void* lbl_804D6500;
-extern void* lbl_804D6504;
-extern void* lbl_804D6508;
-extern void* lbl_804D650C;
-extern void* lbl_804D6510;
-extern void* lbl_804D6514;
-extern void* lbl_804D6518;
-extern void* lbl_804D651C;
-extern void* lbl_804D6524;
-extern void* lbl_804D6528;
-extern void* lbl_804D6530;
-extern void* lbl_804D6534;
-extern void* lbl_804D6538;
-extern void* lbl_804D653C;
-extern s32** lbl_804D6540;
-extern void* lbl_804D6548;
-extern void* lbl_804D654C;
-extern void* lbl_804D6550;
-/// @}
-
 #endif
--- src/melee/ft/fighter.h
+++ src/melee/ft/fighter.h
@@ -20,6 +20,30 @@
 #include <sysdolphin/baselib/jobj.h>
 #include <sysdolphin/baselib/random.h>
 
+/// @todo #lbl_804D64FC..#lbl_804D6550 are initialized to 0 in
+///       #Fighter_LoadCommonData, probably all pointers
+extern void* lbl_804D64FC;
+
+extern void* lbl_804D6500;
+extern void* lbl_804D6504;
+extern void* lbl_804D6508;
+extern void* lbl_804D650C;
+extern void* lbl_804D6510;
+extern void* lbl_804D6514;
+extern void* lbl_804D6518;
+extern void* lbl_804D651C;
+extern void* lbl_804D6524;
+extern void* lbl_804D6528;
+extern void* lbl_804D6530;
+extern void* lbl_804D6534;
+extern void* lbl_804D6538;
+extern void* lbl_804D653C;
+extern s32** lbl_804D6540;
+extern void* lbl_804D6548;
+extern void* lbl_804D654C;
+extern void* lbl_804D6550;
+/// @}
+
 BOOL func_80081298(HSD_GObj* gobj);
 
 typedef enum FighterKind {
