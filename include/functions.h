ea9da172449c7dba03e9252133ab7fec9334e816
Robin Avery <robin@ribbanya.dev>
2023-01-04 04:14:13 -0500

    Remove `plUnk1` from `functions.h`

--- include/functions.h
+++ include/functions.h
@@ -9,21 +9,6 @@ typedef struct _Fighter Fighter;
 typedef struct _Item Item;
 typedef struct _Article Article;
 
-/// @addtogroup pl_unknown_001 plUnk1
-/// @{
-void func_80037C60();
-void func_8003E058(s32, s32, s32, s32);
-void func_8003EC9C(s32, s32, f32, f32);
-void func_8003FAA8(s32 slot, s32 index, Vec* pos, Vec* prevPos);
-void func_8003FE1C();
-void func_8003FFDC(u8, s32, u8, s32, s32);
-void func_80040048(u8, s32);
-void func_80040330();
-void func_80040B8C();
-void func_800411C4();
-void func_80041280();
-/// @}
-
 /// @addtogroup mpcoll mpColl
 /// @{
 void func_8004CBF4(struct _CollData*);
