0c158ef5b9be8d1ac5964c0b0940942467ce005b
Ryan Burns <rtburns@protonmail.com>
2023-01-04 21:40:24 -0800

    Match exception runtime functions

---
+++ src/Runtime/global_destructor_chain.c
@@ -0,0 +1,16 @@
+typedef void (*Destructor)(void*, int);
+typedef struct DestructorChain {
+    struct DestructorChain* next;
+    Destructor destructor;
+    void* object;
+} DestructorChain;
+DestructorChain* __global_destructor_chain;
+
+void __destroy_global_chain(void)
+{
+    while (__global_destructor_chain) {
+        DestructorChain* cur = __global_destructor_chain;
+        __global_destructor_chain = cur->next;
+        cur->destructor(cur->object, -1);
+    }
+}
