da48925ca5d3452179a1799bf9d073695705f565
Robin Avery <robin@ribbanya.dev>
2023-01-09 15:57:05 -0500

    Fix `ftsamus5` includes

--- src/melee/ft/chara/ftSamus/ftsamus5.c
+++ src/melee/ft/chara/ftSamus/ftsamus5.c
@@ -3,6 +3,9 @@
 #include <melee/ef/eflib.h>
 #include <melee/ef/efsync.h>
 #include <melee/ft/ftparts.h>
+#include <melee/ft/ft_unknown_006.h>
+#include <melee/ft/code_80081B38.h>
+#include <melee/ft/ftcliffcommon.h>
 
 void ftSamus_SpecialHi_StartAction(HSD_GObj* fighter_gobj)
 {
