9e43c8cda66b321b83bc9f677289189bed4eec3b
Robin Avery <robin@ribbanya.dev>
2023-01-09 21:00:15 -0500

    Fighter cleanup pass 4 by Altimor (#652)
    * working on translating ftparts to C
    
    name Fighter_JObjSetRotation
    
    match prototypes to functions.h
    
    fix typos
    
    add pragma push/pop
    
    HSD_PObjInfo::update doesn't exist in melee
    
    fix another typo
    
    get rid of old duplicate Fighter_IntpJObjInfoInit
    
    fix missing pragma pops
    
    fix bss ordering
    
    fix wrong ordered stores
    
    fix headers
    
    * align
    
    * add per-component get scale funcs
    
    * name HSD_MtxInverse
    
    Co-authored-by: AltimorTASDK <altimormc@gmail.com>

--- src/melee/ft/fighter.c
+++ src/melee/ft/fighter.c
@@ -1,5 +1,4 @@
 #include <melee/ft/fighter.h>
-
 #include <dolphin/os/os.h>
 #include <melee/cm/camera.h>
 #include <melee/ef/efasync.h>
@@ -1305,8 +1304,8 @@ void Fighter_ActionStateChange_800693AC(HSD_GObj* fighter_gobj,
                         fp->x5E8_fighterBones[bone_index].x0_jobj,
                         &translation);
                     HSD_JObjGetRotation(temp_joint, &quat);
-                    func_8007584C(fp->x5E8_fighterBones[bone_index].x0_jobj,
-                                  &quat);
+                    Fighter_JObjSetRotation(
+                        fp->x5E8_fighterBones[bone_index].x0_jobj, &quat);
                 }
 
                 if (fp->x594_animCurrFlags1.bits.b0 != 0U) {
@@ -2206,6 +2205,8 @@ void Fighter_procUpdate(HSD_GObj* fighter_gobj, s32 dummy)
                 func_8007CCA0(fp,
                               /*effective friction - ground multiplier is
                                  usually 1. last factor was 1 when I looked*/
+                              /*effective friction - ground multiplier is
+                                 usually 1. last factor was 1 when I looked*/
                               Stage_GetGroundFrictionMultiplier(fp) *
                                   pAttr->x128_GroundFriction *
                                   p_ftCommonData->x200);
@@ -2263,7 +2264,8 @@ void Fighter_procUpdate(HSD_GObj* fighter_gobj, s32 dummy)
                     Stage_GetGroundFrictionMultiplier(fp) *
                         pAttr->x128_GroundFriction *
                         p_ftCommonData->x3EC_shieldGroundFrictionMultiplier);
-
+                /* effectiveFriction - the last constant variable differs from
+                 * the one for the knockback friction above*/
                 pAtkShieldKB->x =
                     pNormal->y * fp->xF4_ground_attacker_shield_kb_vel;
                 pAtkShieldKB->y =
@@ -2421,7 +2423,7 @@ void Fighter_procUpdate(HSD_GObj* fighter_gobj, s32 dummy)
 inline HSD_JObj* Fighter_UnkApplyTransformation_8006C0F0_Inner1(HSD_JObj* jobj,
                                                                 Mtx* mtx)
 {
-    func_80379310(&jobj->mtx, mtx);
+    HSD_MtxInverse(&jobj->mtx, mtx);
     return jobj;
 }
 
--- src/melee/ft/ftparts.h
+++ src/melee/ft/ftparts.h
@@ -14,10 +14,11 @@ void func_80074A4C(struct _HSD_GObj*, u32, s32);
 void func_80074A8C(struct _HSD_GObj*);
 void func_80074E58(struct _Fighter*);
 s32 func_8007500C(struct _Fighter*, s32);
-void func_8007584C(struct _HSD_JObj*, Quaternion*);
+void Fighter_JObjSetRotation(struct _HSD_JObj*, Quaternion*);
 void func_8007592C(struct _Fighter*, s32, f32);
 void func_80075AF0(struct _Fighter*, s32, f32);
 void func_80075CB4(struct _Fighter*, s32, f32);
+s32 func_8007500C(struct _Fighter*, s32);
 void func_800753D4(struct _Fighter*, s32, unk_t);
 f32 func_80075F48(Fighter*, s32);
 
