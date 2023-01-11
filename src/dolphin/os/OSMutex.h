7fc5256a6b4dbd36d1a9832898982dba082793e0
Robin Avery <robin@ribbanya.dev>
2023-01-11 18:46:14 -0500

    Fix duplicate headers

--- src/dolphin/os/OSMutex.h
+++ src/dolphin/os/OSMutex.h
@@ -2,12 +2,19 @@
 #define DOLPHIN_OS_OSMUTEX_H
 
 #include <dolphin/os/OSThread.h>
+#include <Runtime/platform.h>
 
 typedef struct OSMutex {
     OSThreadQueue queue; // at 0x00
     OSThread* thread;    // at 0x08
-    s32 count;           // at 0x0C
+    s32 lock;            // at 0x0C
     OSMutexLink link;    // at 0x10
 } OSMutex;
+
+void OSInitMutex(OSMutex*);
+void OSLockMutex(OSMutex*);
+void OSUnlockMutex(OSMutex*);
+void __OSUnlockAllMutex(OSThread*);
+BOOL OSTryLockMutex(OSMutex*);
 
 #endif
