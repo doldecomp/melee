892213de74a1565bf9b1fae62feb836e93702f9c
Robin Avery <robin@ribbanya.dev>
2023-01-12 06:47:07 -0500

    Fix `generate_context.sh`

--- tools/generate_context.sh
+++ tools/generate_context.sh
@@ -2,7 +2,7 @@ HERE=$(dirname "$(readlink -f $0)")
 
 pushd $HERE/..
 
-find include/ src/ -type f -name "*.h" | sed -r -e 's/((include|src)\/)?(.*)/#include <\3>/' > ctx_includes.h
+find src -type f -name "*.h" | sed -r -e 's/((src\/MSL|src)\/)?(.*)/#include <\3>/' > ctx_includes.h
 python tools/m2ctx/m2ctx.py ctx_includes.h
 
 popd
