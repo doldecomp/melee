c779e4794804e541710e7126f916cf5b19f0d7fc
Robin Avery <robin@ribbanya.dev>
2023-01-12 07:37:52 -0500

    Fix context syntax issues

--- src/MetroTRK/mem_TRK.h
+++ src/MetroTRK/mem_TRK.h
@@ -3,14 +3,8 @@
 
 #include <Runtime/platform.h>
 
-#ifdef MWERKS_GEKKO
-__declspec(section ".init")
-#endif
-    void* TRK_memcpy(void* dst, const void* src, unsigned long n);
+SECTION_INIT void* TRK_memcpy(void* dst, const void* src, unsigned long n);
 
-#ifdef MWERKS_GEKKO
-__declspec(section ".init")
-#endif
-    void* TRK_memset(void* dst, int val, unsigned long n);
+SECTION_INIT void* TRK_memset(void* dst, int val, unsigned long n);
 
 #endif
--- src/Runtime/__mem.h
+++ src/Runtime/__mem.h
@@ -3,19 +3,8 @@
 
 #include <Runtime/platform.h>
 
-#ifdef MWERKS_GEKKO
-__declspec(section ".init")
-#endif
-    void* memset(void* dst, int val, unsigned long n);
-
-#ifdef MWERKS_GEKKO
-__declspec(section ".init")
-#endif
-    void __fill_mem(void* dst, int val, unsigned long n);
-
-#ifdef MWERKS_GEKKO
-__declspec(section ".init")
-#endif
-    void* memcpy(void* dst, const void* src, unsigned long n);
+SECTION_INIT void* memset(void* dst, int val, unsigned long n);
+SECTION_INIT void __fill_mem(void* dst, int val, unsigned long n);
+SECTION_INIT void* memcpy(void* dst, const void* src, unsigned long n);
 
 #endif
