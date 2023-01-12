dc09050846c8f3c26bd3006396536c194ca2980a
Robin Avery <robin@ribbanya.dev>
2023-01-12 15:56:45 -0500

    Suppress `pushd`/`popd` output and run `clang-format`

--- tools/generate_context.sh
+++ tools/generate_context.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+
+set +x
 HERE=$(dirname "$(readlink -f $0)")
 
-pushd $HERE/..
+pushd $HERE/.. > /dev/null
 
 find src -type f -name "*.h" | sed -r -e 's/((src\/MSL|src)\/)?(.*)/#include <\3>/' > ctx_includes.h
-python tools/m2ctx/m2ctx.py ctx_includes.h
 
-popd
+python tools/m2ctx/m2ctx.py ctx_includes.h | clang-format
+
+popd > /dev/null
