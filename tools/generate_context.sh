a7350a58f5a3c1589e7d7300f04bf8e9bec6fb14
Robin Avery <robin@ribbanya.dev>
2023-01-12 16:41:21 -0500

    Clean up `generate_context.sh`

--- tools/generate_context.sh
+++ tools/generate_context.sh
@@ -5,8 +5,11 @@ HERE=$(dirname "$(readlink -f $0)")
 
 pushd $HERE/.. > /dev/null
 
-find src -type f -name "*.h" | sed -r -e 's/((src\/MSL|src)\/)?(.*)/#include <\3>/' > ctx_includes.h
+find src -type f -name "*.h" | \
+  sed -r -e 's/((src\/MSL|src)\/)?(.*)/#include <\3>/' \
+  > build/ctx_includes.h
 
-python tools/m2ctx/m2ctx.py ctx_includes.h | clang-format
+python tools/m2ctx/m2ctx.py build/ctx_includes.h \
+  | clang-format
 
 popd > /dev/null
