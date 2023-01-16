cd12d4113c13d4ab175d7d371d323f61edb6d05a
Robin Avery <robin@ribbanya.dev>
2023-01-16 17:40:37 -0500

    ok

--- src/melee/ft/chara/ftMasterHand/ftMasterHand_16.h
+++ src/melee/ft/chara/ftMasterHand/ftMasterHand_16.h
@@ -2,7 +2,13 @@
 #define MELEE_FT_CHARA_FTMASTERHAND_FTMASTERHAND_16_H
 
 #include <Runtime/platform.h>
+#include <sysdolphin/baselib/gobj.h>
 
-void func_80153820(struct _HSD_GObj*);
+void func_80153820(HSD_GObj*);
+void lbl_801537B8(HSD_GObj* arg0);
+void lbl_801537FC(HSD_GObj* gobj);
+void lbl_8015381C(HSD_GObj* gobj);
+void func_80153820(HSD_GObj* arg0);
+void lbl_8015377C(HSD_GObj* arg0);
 
 #endif
--- src/melee/ft/chara/ftMasterHand/ftMasterHand_18.h
+++ src/melee/ft/chara/ftMasterHand/ftMasterHand_18.h
@@ -2,7 +2,12 @@
 #define MELEE_FT_CHARA_FTMASTERHAND_FTMASTERHAND_18_H
 
 #include <Runtime/platform.h>
+#include <sysdolphin/baselib/gobj.h>
 
-void func_80153A64(struct _HSD_GObj*);
+void func_80153A64(HSD_GObj*);
+void lbl_80153A30(HSD_GObj* gobj);
+void lbl_801539EC(HSD_GObj* arg0);
+void lbl_80153A60(HSD_GObj* gobj);
+void func_80153A64(HSD_GObj* arg0);
 
 #endif
