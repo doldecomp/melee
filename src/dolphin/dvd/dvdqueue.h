ab3a1ac3d05eb7a64cda0351c6ad0f5b4a726268
Robin Avery <robin@ribbanya.dev>
2023-01-17 07:20:17 -0500

    ok

--- src/dolphin/dvd/dvdqueue.h
+++ src/dolphin/dvd/dvdqueue.h
@@ -1,11 +1,13 @@
 #ifndef DOLPHIN_DVD_DVDQUEUE_H
 #define DOLPHIN_DVD_DVDQUEUE_H
 
-#include <dolphin/dvd/dvd.h>
+#include <dolphin/dvd/forward.h>
+#include <stdbool.h>
 
 void __DVDClearWaitingQueue(void);
 bool __DVDCheckWaitingQueue(void);
 int __DVDPushWaitingQueue(int a, DVDCommandBlock* b);
 bool __DVDDequeueWaitingQueue(DVDCommandBlock* a);
+DVDCommandBlock* __DVDPopWaitingQueue(void);
 
 #endif
--- src/dolphin/dvd/fstload.h
+++ src/dolphin/dvd/fstload.h
@@ -1,6 +1,11 @@
 #ifndef DOLPHIN_DVD_FSTLOAD_H
 #define DOLPHIN_DVD_FSTLOAD_H
 
+#include <dolphin/dvd/forward.h>
+#include <Runtime/platform.h>
+
 void __fstLoad(void);
+
+void cb(s32 param_1, DVDCommandBlock* block);
 
 #endif
