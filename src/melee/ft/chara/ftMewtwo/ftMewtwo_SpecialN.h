549d6827a371c29cb1a6abe31a47d5f902664d9c
Robin Avery <robin@ribbanya.dev>
2023-01-17 07:27:51 -0500

    ok

---
+++ src/melee/ft/chara/ftMewtwo/ftMewtwo_SpecialN.h
@@ -0,0 +1,9 @@
+#ifndef MELEE_FT_CHARA_FTMEWTWO_FTMEWTWO_SPECIALN_H
+#define MELEE_FT_CHARA_FTMEWTWO_FTMEWTWO_SPECIALN_H
+
+#include <Runtime/platform.h>
+
+void ftMewtwo_SpecialN_GetPos(Fighter* fp, Vec3* sp38);
+void ftMewtwo_SpecialN_SetRecoil(HSD_GObj* fighter_gobj);
+
+#endif
