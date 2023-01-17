e8e415880d9feb6b7b2311a0ba06cf0556be0686
Robin Avery <robin@ribbanya.dev>
2023-01-17 07:39:45 -0500

    ok

--- src/dolphin/dvd/forward.h
+++ src/dolphin/dvd/forward.h
@@ -4,7 +4,6 @@
 #include <Runtime/platform.h>
 
 typedef struct DVDCommandBlock DVDCommandBlock;
-typedef struct DVDNode DVDNode;
 
 typedef void (*DVDCBCallback)(s32 result, DVDCommandBlock* block);
 
