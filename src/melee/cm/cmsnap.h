a1d0833339d51e9442fbf792af95264b0c8a7c4a
Robin Avery <robin@ribbanya.dev>
2023-01-17 07:24:09 -0500

    ok

--- src/melee/cm/cmsnap.h
+++ src/melee/cm/cmsnap.h
@@ -1,7 +1,13 @@
 #ifndef _CMSNAP_H_
 #define _CMSNAP_H_
 
+#include <Runtime/platform.h>
+#include <sysdolphin/baselib/gobj.h>
+
 void func_800315C8(void);
 void func_800316B4(void);
+
+s32 func_80031618(void);
+void func_80031640(HSD_GObj* arg0, s32 arg1);
 
 #endif
---
+++ src/melee/ft/forward.h
@@ -0,0 +1,8 @@
+#ifndef MELEE_FT_FORWARD_H
+#define MELEE_FT_FORWARD_H
+
+#include <Runtime/platform.h>
+
+typedef struct UnkFloat6_Camera UnkFloat6_Camera;
+
+#endif
