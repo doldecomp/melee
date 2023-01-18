bf4ffb1eea2e283ca4b93c58fa99b5c92c658a38
Robin Avery <robin@ribbanya.dev>
2023-01-18 11:10:59 -0500

    ok

--- src/melee/lb/lbcollision.h
+++ src/melee/lb/lbcollision.h
@@ -1,17 +1,16 @@
 #ifndef MELEE_LB_LBCOLLISION_H
 #define MELEE_LB_LBCOLLISION_H
 
+#include <melee/ft/forward.h>
 #include <melee/lb/forward.h>
 #include <sysdolphin/baselib/jobj.h>
 
-/// @todo Does not belong here.
 struct Hitbox {
     Tangibility tangiblity;
     s32 x4;
     s32 x8;
     f32 xC;
     u8 x10[0x30 - 0x10];
-
     /// @at{30} @sz{4}
     HitElement element;
 
@@ -34,20 +33,20 @@ struct Hurtbox {
     Vec3 x10_hurt2_offset;  // 0x10
     f32 x1C_scale;          // 0x1c
     HSD_JObj* x20_jobj;     // 0x20
-    unsigned char
-        x24_1_is_updated : 1; // 0x24, 0x80, if enabled, wont update position
-    unsigned char x24_2 : 1;  // 0x24 0x40
-    unsigned char x24_3 : 1;  // 0x24 0x20
-    unsigned char x24_4 : 1;  // 0x24 0x10
-    unsigned char x24_5 : 1;  // 0x24 0x08
-    unsigned char x24_6 : 1;  // 0x24 0x04
-    unsigned char x24_7 : 1;  // 0x24 0x02
-    unsigned char x24_8 : 1;  // 0x24 0x01
-    Vec3 x28_hurt1_pos;       // 0x28
-    Vec3 x34_hurt2_pos;       // 0x34
-    u32 x40_bone_index;       // 0x40
-    u32 x44_hurt_kind;        // 0x44. 0 = low, 1 = mid, 2 = high
-    u32 x48_is_grabbable;     // 0x48
+    unsigned char x24_1_is_updated : 1; // 0x24, 0x80, if enabled, wont
+                                        // update position
+    unsigned char x24_2 : 1;            // 0x24 0x40
+    unsigned char x24_3 : 1;            // 0x24 0x20
+    unsigned char x24_4 : 1;            // 0x24 0x10
+    unsigned char x24_5 : 1;            // 0x24 0x08
+    unsigned char x24_6 : 1;            // 0x24 0x04
+    unsigned char x24_7 : 1;            // 0x24 0x02
+    unsigned char x24_8 : 1;            // 0x24 0x01
+    Vec3 x28_hurt1_pos;                 // 0x28
+    Vec3 x34_hurt2_pos;                 // 0x34
+    u32 x40_bone_index;                 // 0x40
+    u32 x44_hurt_kind;                  // 0x44. 0 = low, 1 = mid, 2 = high
+    u32 x48_is_grabbable;               // 0x48
 };
 
 void* HSD_JObjUnkMtxPtr(HSD_JObj*);
