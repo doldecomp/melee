8ba6654a6b6c3f092204b80fdc35e230da939b76
Robin Avery <robin@ribbanya.dev>
2023-01-18 11:24:04 -0500

    ok

--- src/melee/lb/forward.h
+++ src/melee/lb/forward.h
@@ -7,4 +7,34 @@ typedef enum Tangibility {
     Intangible
 } Tangibility;
 
+typedef enum HitElement {
+    HitElement_Normal,
+    HitElement_Fire,
+    HitElement_Electric,
+    HitElement_Slash,
+    HitElement_Coin,
+    HitElement_Ice,
+
+    /// Sleep for 103 frames
+    HitElement_Nap,
+
+    /// Sleep for 412 frames
+    HitElement_Sleep,
+
+    HitElement_Catch,
+    HitElement_Ground,
+    HitElement_Cape,
+    HitElement_Inert,
+    HitElement_Disable,
+    HitElement_Dark,
+
+    /// Screw Attack
+    HitElement_Scball,
+
+    HitElement_Lipstick,
+
+    /// Formerly presumed empty, this hitbox element is used by
+    ///  ReDead grab attacks
+    HitElement_Leadead,
+} HitElement;
 #endif
