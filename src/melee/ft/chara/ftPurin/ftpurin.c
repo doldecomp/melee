dda3c1f07c0d965830abc2d630a90cd781d6079f
Robin Avery <robin@ribbanya.dev>
2023-01-18 15:05:21 -0500

    ok

--- src/melee/ft/chara/ftPurin/ftpurin.c
+++ src/melee/ft/chara/ftPurin/ftpurin.c
@@ -1,5 +1,7 @@
 #include <melee/ft/chara/ftPurin/ftpurin.h>
 
+#include <melee/ef/eflib.h>
+#include <melee/ef/efsync.h>
 #include <melee/ft/fighter.h>
 #include <melee/ft/ft_unknown_005.h>
 #include <melee/ft/ft_unknown_006.h>
@@ -188,3 +190,45 @@ void ftPurin_OnKnockbackExit(HSD_GObj* fighter_gobj)
 {
     Fighter_OnKnockbackExit(fighter_gobj, 1);
 }
+
+#ifdef MWERKS_GEKKO
+#pragma push
+asm void lbl_8013C94C(void)
+{ // clang-format off
+    nofralloc
+/* 8013C94C 0013952C  7C 08 02 A6 */	mflr r0
+/* 8013C950 00139530  90 01 00 04 */	stw r0, 4(r1)
+/* 8013C954 00139534  94 21 FF E8 */	stwu r1, -0x18(r1)
+/* 8013C958 00139538  93 E1 00 14 */	stw r31, 0x14(r1)
+/* 8013C95C 0013953C  83 E3 00 2C */	lwz r31, 0x2c(r3)
+/* 8013C960 00139540  88 1F 22 19 */	lbz r0, 0x2219(r31)
+/* 8013C964 00139544  54 00 CF FF */	rlwinm. r0, r0, 0x19, 0x1f, 0x1f
+/* 8013C968 00139548  40 82 00 2C */	bne lbl_8013C994
+/* 8013C96C 0013954C  80 BF 05 E8 */	lwz r5, 0x5e8(r31)
+/* 8013C970 00139550  38 83 00 00 */	addi r4, r3, 0
+/* 8013C974 00139554  4C C6 31 82 */	crclr 6
+/* 8013C978 00139558  38 60 04 D6 */	li r3, 0x4d6
+/* 8013C97C 0013955C  80 A5 00 50 */	lwz r5, 0x50(r5)
+/* 8013C980 00139560  4B F2 34 5D */	bl ef_Spawn
+/* 8013C984 00139564  88 1F 22 19 */	lbz r0, 0x2219(r31)
+/* 8013C988 00139568  38 60 00 01 */	li r3, 1
+/* 8013C98C 0013956C  50 60 3E 30 */	rlwimi r0, r3, 7, 0x18, 0x18
+/* 8013C990 00139570  98 1F 22 19 */	stb r0, 0x2219(r31)
+lbl_8013C994:
+/* 8013C994 00139574  3C 60 80 06 */	lis r3, efLib_PauseAll@ha
+/* 8013C998 00139578  38 03 BA 40 */	addi r0, r3, efLib_PauseAll@l
+/* 8013C99C 0013957C  3C 60 80 06 */	lis r3, efLib_ResumeAll@ha
+/* 8013C9A0 00139580  90 1F 21 D4 */	stw r0, 0x21d4(r31)
+/* 8013C9A4 00139584  38 03 BA C4 */	addi r0, r3, efLib_ResumeAll@l
+/* 8013C9A8 00139588  90 1F 21 D8 */	stw r0, 0x21d8(r31)
+/* 8013C9AC 0013958C  38 00 00 00 */	li r0, 0
+/* 8013C9B0 00139590  90 1F 21 BC */	stw r0, 0x21bc(r31)
+/* 8013C9B4 00139594  80 01 00 1C */	lwz r0, 0x1c(r1)
+/* 8013C9B8 00139598  83 E1 00 14 */	lwz r31, 0x14(r1)
+/* 8013C9BC 0013959C  38 21 00 18 */	addi r1, r1, 0x18
+/* 8013C9C0 001395A0  7C 08 03 A6 */	mtlr r0
+/* 8013C9C4 001395A4  4E 80 00 20 */	blr
+} // clang-format on
+#pragma pop
+
+#endif
