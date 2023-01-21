e5d2d059a81cc90122c293e766b6b3e93864015e
Robin Avery <robin@ribbanya.dev>
2023-01-21 09:10:43 -0500

    ok

--- obj_files.mk
+++ obj_files.mk
@@ -241,6 +241,7 @@ TEXT_O_FILES +=\
 
 
 TEXT_O_FILES +=\
+    $(BUILD_DIR)/src/melee/ft/chara/ftMewtwo/ftMewtwo_Data.c.o\
     $(BUILD_DIR)/asm/melee/ft/chara/ftmewtwo.s.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftMewtwo/ftmewtwo.c.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftMewtwo/ftMewtwo_SpecialHi.c.o\
