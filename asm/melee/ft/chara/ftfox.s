296cbd044d7770c563f43e9986a2994b052ac87d
Robin Avery <robin@ribbanya.dev>
2023-01-21 11:40:18 -0500

    ok

--- asm/melee/ft/chara/ftfox.s
+++ asm/melee/ft/chara/ftfox.s
@@ -3,18 +3,3 @@
 
 .section .data
     .balign 4
-.global lbl_803C7D9C
-lbl_803C7D9C:
-    .4byte lbl_803C7C00
-    .4byte lbl_803C7C0C
-    .4byte lbl_803C7C24
-    .4byte lbl_803C7C44
-    .4byte lbl_803C7C50
-    .4byte lbl_803C7C68
-    .4byte lbl_803C7C88
-    .4byte lbl_803C7C94
-    .4byte lbl_803C7CAC
-    .4byte lbl_803C7CCC
-    .4byte lbl_803C7CD8
-    .4byte lbl_803C7CF0
-    .4byte NULL
