7688abd83745e0947b08c3c2564ba152a9c8e897
Robin Avery <robin@ribbanya.dev>
2023-01-21 18:21:22 -0500

    ok

--- src/melee/ft/chara/ftKoopa/ftKoopa_Data.c
+++ src/melee/ft/chara/ftKoopa/ftKoopa_Data.c
@@ -80,10 +80,9 @@ Fighter_DemoStrings lbl_803CF25C = {
     lbl_803CF240,
 };
 
-/// @todo #Fighter_CostumeStrings array will not align at the end.
-///       Likely a missing split.
-char* lbl_803CF26C[] = {
-    lbl_803CF0B8, lbl_803CF0C4, lbl_803CF0DC, lbl_803CF0FC,
-    lbl_803CF108, lbl_803CF124, lbl_803CF148, lbl_803CF154,
-    lbl_803CF170, lbl_803CF194, lbl_803CF1A0, lbl_803CF1BC,
+Fighter_CostumeStrings lbl_803CF26C[] = {
+    { lbl_803CF0B8, lbl_803CF0C4, lbl_803CF0DC },
+    { lbl_803CF0FC, lbl_803CF108, lbl_803CF124 },
+    { lbl_803CF148, lbl_803CF154, lbl_803CF170 },
+    { lbl_803CF194, lbl_803CF1A0, lbl_803CF1BC },
 };
