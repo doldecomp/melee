a22466daa7ca90f7650738a3a79bf4c1dccf84dc
Robin Avery <robin@ribbanya.dev>
2023-01-21 23:58:34 -0500

    ok

--- obj_files.mk
+++ obj_files.mk
@@ -183,7 +183,7 @@ TEXT_O_FILES +=\
 TEXT_O_FILES +=\
     $(BUILD_DIR)/src/melee/ft/chara/ftYoshi/ftyoshi.c.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftYoshi/ftyoshi1.c.o\
-    $(BUILD_DIR)/src/melee/ft/chara/ftYoshi/ftYoshi_Data_3.c.o\
+    $(BUILD_DIR)/src/melee/ft/chara/ftYoshi/ftyoshi3.c.o\
     $(BUILD_DIR)/asm/melee/ft/chara/ftyoshi4.s.o\
 
 TEXT_O_FILES +=\
