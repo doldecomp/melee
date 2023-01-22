6dd04a0e1adb0433377b38b44bc9c0ac89dc4adb
Robin Avery <robin@ribbanya.dev>
2023-01-21 22:18:47 -0500

    ok

--- obj_files.mk
+++ obj_files.mk
@@ -251,7 +251,6 @@ TEXT_O_FILES +=\
 TEXT_O_FILES +=\
     $(BUILD_DIR)/src/melee/ft/chara/ftDrMario/ftdrmario.c.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftDrMario/ftdrmario_2.c.o\
-    $(BUILD_DIR)/src/melee/ft/chara/ftDrMario/ftDrMario_Data.c.o\
 
 TEXT_O_FILES +=\
     $(BUILD_DIR)/src/melee/ft/chara/ftFalco/ftfalco.c.o\
