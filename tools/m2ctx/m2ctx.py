610b72edf56f5aa90fd0cf9b4ce0e9cab26c5e9f
Robin Avery <robin@ribbanya.dev>
2023-01-27 10:15:11 -0500

    Add preliminary `pcpp` support

--- tools/m2ctx/m2ctx.py
+++ tools/m2ctx/m2ctx.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from typing import List
 
 here = Path(__file__).parent
 root = (here / '../../').resolve()
@@ -28,6 +29,15 @@ MWCC_FLAGS = [
     "-DM2CTX",
 ]
 
+PCPP_FLAGS = [
+    "-I", "src",
+    "-I", "src/MSL",
+    "-D", "__MWERKS__",
+    "-D" "M2CTX",
+    "--passthru-defines",
+    "--line-directive",
+    "--compress",
+]
 
 def write_header(path: Path):
     files = sorted({f'#include <{file.relative_to(src)}>'
@@ -35,14 +45,7 @@ def write_header(path: Path):
     with open(path, 'w') as f:
         f.write('\n'.join(files))
 
-
-def import_c_file(in_file: Path) -> str:
-    c_command = [str(mwcc_command), *MWCC_FLAGS, "-E", str(in_file)]
-
-    if sys.platform != "win32":
-        wine = os.environ.get("WINE", "wine")
-        c_command = [wine] + c_command
-
+def try_import(c_command: List[str]):
     try:
         out_text = subprocess.check_output(c_command, cwd=root, encoding="utf8")
     except subprocess.CalledProcessError as err:
@@ -59,6 +62,20 @@ def import_c_file(in_file: Path) -> str:
         sys.exit(1)
 
     return out_text
+
+def pcpp_import(in_file: Path) -> str:
+    c_command = ['pcpp', *PCPP_FLAGS, str(in_file)]
+    return try_import(c_command)
+
+
+def mwcc_import(in_file: Path) -> str:
+    c_command = [str(mwcc_command), *MWCC_FLAGS, "-E", str(in_file)]
+
+    if sys.platform != "win32":
+        wine = os.environ.get("WINE", "wine")
+        c_command = [wine] + c_command
+
+    return try_import(c_command)
 
 
 def main():
@@ -77,11 +94,17 @@ def main():
                         help="colorize the output (requires pygments)")
     parser.add_argument("-f", "--format", action="store_true",
                         help="colorize the output (requires clang-format)")
+    parser.add_argument("-p", "--preprocessor", action="store_true",
+                        help="preserve preprocessor directives (requires pcpp)")
     args = parser.parse_args()
 
 
     write_header(header_path)
-    output = import_c_file(header_path)
+
+    if args.preprocessor:
+        output = pcpp_import(header_path)
+    else:
+        output = mwcc_import(header_path)
 
     if args.format:
         try:
