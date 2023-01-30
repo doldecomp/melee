64776c674175f0d54a6e2adf188f04628a09cf11
Robin Avery <robin@ribbanya.dev>
2023-01-30 08:42:06 -0500

    Fix problematic naming

--- tools/cli/setup.py
+++ tools/cli/setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="melee-cli",
+    name="melee_cli",
     version="0.0.1",
     description="An entry point for melee decomp command line tooling",
     packages=find_packages(),
