b96a10d409a28afafffb80288150354d02b47ca0
Robin Avery <robin@ribbanya.dev>
2023-03-17 07:02:13 -0400

    Rename `ftfalcon_4` to `ftCaptain_SpecialLw`

--- obj_files.mk
+++ obj_files.mk
@@ -123,7 +123,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/src/melee/ft/chara/ftCaptain/ftCaptain_SpecialN.c.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftCaptain/ftCaptain_SpecialS.c.o\
     $(BUILD_DIR)/asm/melee/ft/chara/ftCaptain/ftCaptain_SpecialHi.s.o\
-    $(BUILD_DIR)/asm/melee/ft/chara/ftCaptain/ftfalcon_4.s.o\
+    $(BUILD_DIR)/asm/melee/ft/chara/ftCaptain/ftCaptain_SpecialLw.s.o\
 
 TEXT_O_FILES +=\
     $(BUILD_DIR)/src/melee/ft/chara/ftFox/ftfox.c.o\
