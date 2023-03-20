15ccf97f26a0be1c14a6cb60ad27a3c402ac73fa
Robin Avery <robin@ribbanya.dev>
2023-03-20 16:52:56 -0400

    bad

--- src/melee/ft/chara/ftMario/ftMario_SpecialHi.c
+++ src/melee/ft/chara/ftMario/ftMario_SpecialHi.c
@@ -87,8 +87,8 @@ inline void ftMario_SpecialHi_CalcAngle(HSD_GObj* gobj)
     if (fp->x2200_ftcmd_var0 == 0) {
         if (lstick_x > sa->specialhi.momentum_stick_range) {
             f32 deg = (f64) sa->specialhi.angle_diff *
-                      ((lstick_x - sa->specialhi.momentum_stick_range) /
-                       (1.0 - sa->specialhi.momentum_stick_range));
+                      (lstick_x - sa->specialhi.momentum_stick_range) /
+                      (1.0 - sa->specialhi.momentum_stick_range);
 
             f32 rad = fp->input.x620_lstick_x > 0 ? -(DEG_TO_RAD * deg)
                                                   : +(DEG_TO_RAD * deg);
