d255e6002291b69825f0ae13d9d366d38865b6a9
Robin Avery <robin@ribbanya.dev>
2023-03-21 04:42:22 -0400

    bad

--- src/melee/ft/chara/ftZelda/ftZelda_SpecialLw.h
+++ src/melee/ft/chara/ftZelda/ftZelda_SpecialLw.h
@@ -4,6 +4,36 @@
 #include <platform.h>
 #include <baselib/forward.h>
 
+bool ftZelda_8013B574(HSD_GObj* gobj);
+s32 ftZelda_8013B540(HSD_GObj* gobj);
+void ftZelda_8013ADB4(HSD_GObj* gobj);
+void ftZelda_8013AE30(HSD_GObj* gobj);
+void ftZelda_8013AEAC(HSD_GObj* gobj);
+void ftZelda_8013B068(HSD_GObj* gobj);
+void ftZelda_8013B0A8(HSD_GObj* gobj);
+void ftZelda_8013B0E8(HSD_GObj* gobj);
+void ftZelda_8013B0EC(HSD_GObj* gobj);
+void ftZelda_8013B0F0(HSD_GObj* gobj);
+void ftZelda_8013B110(HSD_GObj* gobj);
+void ftZelda_8013B154(HSD_GObj* gobj);
+void ftZelda_8013B190(HSD_GObj* gobj);
+void ftZelda_8013B1CC(HSD_GObj* gobj);
+void ftZelda_8013B238(HSD_GObj* gobj);
+void ftZelda_8013B2A4(HSD_GObj* gobj);
+void ftZelda_8013B2E0(HSD_GObj* gobj);
+void ftZelda_8013B31C(HSD_GObj* gobj);
+void ftZelda_8013B320(HSD_GObj* gobj);
+void ftZelda_8013B324(HSD_GObj* gobj);
+void ftZelda_8013B344(HSD_GObj* gobj);
+void ftZelda_8013B388(HSD_GObj* gobj);
+void ftZelda_8013B3C4(HSD_GObj* gobj);
+void ftZelda_8013B400(HSD_GObj* gobj);
+void ftZelda_8013B46C(HSD_GObj* gobj);
+void ftZelda_8013B4D8(HSD_GObj* gobj);
+void ftZelda_8013B5C4(HSD_GObj* gobj);
+void ftZelda_8013B5EC(HSD_GObj* gobj);
+void ftZelda_SpecialAirLw_StartMotion(HSD_GObj* gobj);
+void ftZelda_SpecialLw_StartMotion(HSD_GObj* gobj);
 void ftZelda_SpecialLw_StartMotion_Helper(HSD_GObj* gobj);
 
 #endif
