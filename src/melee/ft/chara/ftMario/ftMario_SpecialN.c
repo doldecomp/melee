746ebe6b2cedb748bd4278947272582708d1c810
Robin Avery <robin@ribbanya.dev>
2023-03-20 20:02:30 -0400

    ok

--- src/melee/ft/chara/ftMario/ftMario_SpecialN.c
+++ src/melee/ft/chara/ftMario/ftMario_SpecialN.c
@@ -22,7 +22,7 @@ static int pickMegavitamin(Fighter* fp, int* arr, int outpos)
     return result;
 }
 
-// Get random Megavitamin color combo for Dr. Mario //
+/// Get random Megavitamin color combo for Dr. Mario
 int ftMario_SpecialN_VitaminRandom(HSD_GObj* gobj)
 {
     Fighter* fp;
@@ -56,14 +56,12 @@ void ftMario_SpecialN_StartAction(HSD_GObj* gobj)
     fp->cb.x21BC_callback_Accessory4 = ftMario_SpecialN_ItemFireSpawn;
 }
 
-// 0x800E0E18
 void ftMario_SpecialN_Anim(HSD_GObj* gobj)
 {
     if (!ftAnim_IsFramesRemaining(gobj))
         func_8008A2BC(gobj);
 }
 
-// 0x800E0E54
 void ftMario_SpecialN_IASA(HSD_GObj* gobj)
 {
     Fighter* fp = GET_FIGHTER(gobj);
@@ -72,21 +70,17 @@ void ftMario_SpecialN_IASA(HSD_GObj* gobj)
         func_8008A4D4(gobj);
 }
 
-// 0x800E0E84
 void ftMario_SpecialN_Phys(HSD_GObj* gobj)
 {
     func_80084F3C(gobj);
 }
 
-// 0x800E0EA4
 void ftMario_SpecialN_Coll(HSD_GObj* gobj)
 {
     if (func_80082708(gobj) == false)
         ftMario_SpecialN_GroundToAir(gobj);
 }
 
-// 0x800E0EE0
-// https://decomp.me/scratch/Of8qP
 void ftMario_SpecialN_ItemFireSpawn(HSD_GObj* gobj)
 {
     Vec3 coords;
@@ -137,7 +131,6 @@ void ftMario_SpecialAirN_StartAction(HSD_GObj* gobj)
     fp->cb.x21BC_callback_Accessory4 = ftMario_SpecialN_ItemFireSpawn;
 }
 
-// 0x800E10B0
 void ftMario_SpecialAirN_Anim(HSD_GObj* gobj)
 {
     if (!ftAnim_IsFramesRemaining(gobj)) {
@@ -145,7 +138,6 @@ void ftMario_SpecialAirN_Anim(HSD_GObj* gobj)
     }
 }
 
-// 0x800E10EC
 void ftMario_SpecialAirN_IASA(HSD_GObj* gobj)
 {
     Fighter* fp = GET_FIGHTER(gobj);
@@ -154,21 +146,17 @@ void ftMario_SpecialAirN_IASA(HSD_GObj* gobj)
     }
 }
 
-// 0x800E111C
 void ftMario_SpecialAirN_Phys(HSD_GObj* gobj)
 {
     func_80084DB0(gobj);
 }
 
-// 0x800E113C
 void ftMario_SpecialAirN_Coll(HSD_GObj* gobj)
 {
-    if (func_80081D0C(gobj) != false) {
+    if (func_80081D0C(gobj))
         ftMario_SpecialAirN_AirToGround(gobj);
-    }
 }
 
-// 0x800E1178
 void ftMario_SpecialN_GroundToAir(HSD_GObj* gobj)
 {
     Fighter* fp = GET_FIGHTER(gobj);
