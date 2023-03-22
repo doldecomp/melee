608265cda380db1b1dbbf4f168922bca6f109b18
Robin Avery <robin@ribbanya.dev>
2023-03-22 16:47:57 -0400

    ok

--- src/melee/gr/grshrine.c
+++ src/melee/gr/grshrine.c
@@ -15,31 +15,33 @@
 static void* lbl_804D6A18;
 
 static StageCallbacks lbl_803E50E8[3] = {
-    { func_80201E08, func_80201E34, func_80201E3C, func_80201E40, 0 },
-    { func_80201E44, func_80201E88, func_80201E90, func_80201E94, 0 },
-    { func_80201E9C, func_80201F14, func_80201F1C, func_80201F40,
-      (1 << 30) | (1 << 31) },
+    { grShrine_80201E08, grShrine_80201E34, grShrine_80201E3C,
+      grShrine_80201E40, 0 },
+    { grShrine_80201E44, grShrine_80201E88, grShrine_80201E90,
+      grShrine_80201E94, 0 },
+    { grShrine_80201E9C, grShrine_80201F14, grShrine_80201F1C,
+      grShrine_80201F40, (1 << 30) | (1 << 31) },
 };
 
 StageData lbl_803E5130 = {
     (1 << 0) | (1 << 1) | (1 << 2),
     lbl_803E50E8,
     "/GrSh.dat",
-    func_80201C64,
-    func_80201C60,
-    func_80201CF0,
-    func_80201CF4,
-    func_80201D18,
-    func_80201F44,
-    func_80201F4C,
+    grShrine_80201C64,
+    grShrine_80201C60,
+    grShrine_80201CF0,
+    grShrine_80201CF4,
+    grShrine_80201D18,
+    grShrine_80201F44,
+    grShrine_80201F4C,
     (1 << 0),
     NULL,
     0,
 };
 
-static void func_80201C60(int arg0) {}
+static void grShrine_80201C60(int arg0) {}
 
-static void func_80201C64(void)
+static void grShrine_80201C64(void)
 {
     /// @todo Unused stack.
 #ifdef MUST_MATCH
@@ -47,9 +49,9 @@ static void func_80201C64(void)
 #endif
 
     lbl_804D6A18 = Ground_801C49F8();
-    func_80201D20(0);
-    func_80201D20(1);
-    func_80201D20(2);
+    grShrine_80201D20(0);
+    grShrine_80201D20(1);
+    grShrine_80201D20(2);
     Ground_801C39C0();
     Ground_801C3BB4();
 
@@ -66,19 +68,19 @@ static void func_80201C64(void)
     }
 }
 
-static void func_80201CF0(void) {}
+static void grShrine_80201CF0(void) {}
 
-static void func_80201CF4(void)
+static void grShrine_80201CF4(void)
 {
     grZakoGenerator_801CAE04(false);
 }
 
-static bool func_80201D18(void)
+static bool grShrine_80201D18(void)
 {
     return false;
 }
 
-static HSD_GObj* func_80201D20(s32 arg0)
+static HSD_GObj* grShrine_80201D20(s32 arg0)
 {
     HSD_GObj* gobj;
     StageCallbacks* callbacks = &lbl_803E50E8[arg0];
@@ -106,40 +108,40 @@ static HSD_GObj* func_80201D20(s32 arg0)
     return gobj;
 }
 
-static void func_80201E08(HSD_GObj* gobj)
+static void grShrine_80201E08(HSD_GObj* gobj)
 {
     Ground* gp = gobj->user_data;
     grAnime_801C8138(gobj, gp->map_id, 0);
 }
 
-static bool func_80201E34(HSD_GObj* arg0)
+static bool grShrine_80201E34(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void func_80201E3C(HSD_GObj* arg0) {}
+static void grShrine_80201E3C(HSD_GObj* arg0) {}
 
-static void func_80201E40(HSD_GObj* arg0) {}
+static void grShrine_80201E40(HSD_GObj* arg0) {}
 
-static void func_80201E44(HSD_GObj* gobj)
+static void grShrine_80201E44(HSD_GObj* gobj)
 {
     Ground* gp = gobj->user_data;
     grAnime_801C8138(gobj, gp->map_id, 0);
     gp->x11_flags.b012 = 2;
 }
 
-static bool func_80201E88(HSD_GObj* arg0)
+static bool grShrine_80201E88(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void func_80201E90(HSD_GObj* arg0) {}
+static void grShrine_80201E90(HSD_GObj* arg0) {}
 
-static void func_80201E94(HSD_GObj* arg0) {}
+static void grShrine_80201E94(HSD_GObj* arg0) {}
 
-static void func_80201E98(HSD_GObj* arg0) {}
+static void grShrine_80201E98(HSD_GObj* arg0) {}
 
-static void func_80201E9C(HSD_GObj* gobj)
+static void grShrine_80201E9C(HSD_GObj* gobj)
 {
     /// @todo Unused stack.
 #ifdef MUST_MATCH
@@ -149,30 +151,30 @@ static void func_80201E9C(HSD_GObj* gobj)
     Ground* gp = gobj->user_data;
     Ground_801C2ED0(gobj->hsd_obj, gp->map_id);
     grAnime_801C8138(gobj, gp->map_id, 0);
-    Ground_801C10B8(gobj, func_80201E98);
+    Ground_801C10B8(gobj, grShrine_80201E98);
     Ground_801C2FE0(gobj);
     gp->x10_flags.b5 = true;
 }
 
-static bool func_80201F14(HSD_GObj* arg0)
+static bool grShrine_80201F14(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void func_80201F1C(HSD_GObj* arg0)
+static void grShrine_80201F1C(HSD_GObj* arg0)
 {
     Ground_801C2FE0(arg0);
     lb_800115F4();
 }
 
-static void func_80201F40(HSD_GObj* arg0) {}
+static void grShrine_80201F40(HSD_GObj* arg0) {}
 
-static bool func_80201F44(bool arg0)
+static bool grShrine_80201F44(bool arg0)
 {
     return false;
 }
 
-static bool func_80201F4C(Vec3* a, int _, HSD_JObj* joint)
+static bool grShrine_80201F4C(Vec3* a, int _, HSD_JObj* joint)
 {
     Vec3 b;
     lb_8000B1CC(joint, 0, &b);
