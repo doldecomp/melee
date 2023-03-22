de99ec8664ef0b9e85530427ca7c6bb97febbf61
Robin Avery <robin@ribbanya.dev>
2023-03-22 17:21:08 -0400

    ok

--- asm/melee/gr/grtdrmario.s
+++ asm/melee/gr/grtdrmario.s
@@ -2,11 +2,11 @@
 
 .section .text  # 0x80005940 - 0x803B7240
 
-.global func_8022050C
-func_8022050C:
+.global grTDonkey_8022050C
+grTDonkey_8022050C:
 /* 8022050C 0021D0EC  4E 80 00 20 */	blr
-.global lbl_80220510
-lbl_80220510:
+.global grTDonkey_80220510
+grTDonkey_80220510:
 /* 80220510 0021D0F0  7C 08 02 A6 */	mflr r0
 /* 80220514 0021D0F4  3C 60 80 4A */	lis r3, stage_info@ha
 /* 80220518 0021D0F8  90 01 00 04 */	stw r0, 4(r1)
@@ -22,11 +22,11 @@ lbl_80220510:
 /* 80220540 0021D120  88 05 00 00 */	lbz r0, 0(r5)
 /* 80220544 0021D124  50 80 17 7A */	rlwimi r0, r4, 2, 0x1d, 0x1d
 /* 80220548 0021D128  98 05 00 00 */	stb r0, 0(r5)
-/* 8022054C 0021D12C  48 00 00 65 */	bl func_802205B0
+/* 8022054C 0021D12C  48 00 00 65 */	bl grTDonkey_802205B0
 /* 80220550 0021D130  38 60 00 01 */	li r3, 1
-/* 80220554 0021D134  48 00 00 5D */	bl func_802205B0
+/* 80220554 0021D134  48 00 00 5D */	bl grTDonkey_802205B0
 /* 80220558 0021D138  38 60 00 02 */	li r3, 2
-/* 8022055C 0021D13C  48 00 00 55 */	bl func_802205B0
+/* 8022055C 0021D13C  48 00 00 55 */	bl grTDonkey_802205B0
 /* 80220560 0021D140  4B FA 34 61 */	bl Ground_801C39C0
 /* 80220564 0021D144  4B FA 36 51 */	bl Ground_801C3BB4
 /* 80220568 0021D148  4B FA 3C A9 */	bl Ground_801C4210
@@ -35,11 +35,11 @@ lbl_80220510:
 /* 80220574 0021D154  38 21 00 08 */	addi r1, r1, 8
 /* 80220578 0021D158  7C 08 03 A6 */	mtlr r0
 /* 8022057C 0021D15C  4E 80 00 20 */	blr
-.global lbl_80220580
-lbl_80220580:
+.global grTDonkey_80220580
+grTDonkey_80220580:
 /* 80220580 0021D160  4E 80 00 20 */	blr
-.global lbl_80220584
-lbl_80220584:
+.global grTDonkey_80220584
+grTDonkey_80220584:
 /* 80220584 0021D164  7C 08 02 A6 */	mflr r0
 /* 80220588 0021D168  38 60 00 00 */	li r3, 0
 /* 8022058C 0021D16C  90 01 00 04 */	stw r0, 4(r1)
@@ -49,13 +49,13 @@ lbl_80220584:
 /* 8022059C 0021D17C  38 21 00 08 */	addi r1, r1, 8
 /* 802205A0 0021D180  7C 08 03 A6 */	mtlr r0
 /* 802205A4 0021D184  4E 80 00 20 */	blr
-.global lbl_802205A8
-lbl_802205A8:
+.global grTDonkey_802205A8
+grTDonkey_802205A8:
 /* 802205A8 0021D188  38 60 00 00 */	li r3, 0
 /* 802205AC 0021D18C  4E 80 00 20 */	blr
 
-.global func_802205B0
-func_802205B0:
+.global grTDonkey_802205B0
+grTDonkey_802205B0:
 /* 802205B0 0021D190  7C 08 02 A6 */	mflr r0
 /* 802205B4 0021D194  90 01 00 04 */	stw r0, 4(r1)
 /* 802205B8 0021D198  94 21 FF E0 */	stwu r1, -0x20(r1)
@@ -71,7 +71,7 @@ func_802205B0:
 /* 802205E0 0021D1C0  7F BF 02 14 */	add r29, r31, r0
 /* 802205E4 0021D1C4  4B FA 0E ED */	bl Ground_801C14D0
 /* 802205E8 0021D1C8  7C 7E 1B 79 */	or. r30, r3, r3
-/* 802205EC 0021D1CC  41 82 00 70 */	beq lbl_8022065C
+/* 802205EC 0021D1CC  41 82 00 70 */	beq grTDonkey_8022065C
 /* 802205F0 0021D1D0  83 FE 00 2C */	lwz r31, 0x2c(r30)
 /* 802205F4 0021D1D4  38 00 00 00 */	li r0, 0
 /* 802205F8 0021D1D8  3C 60 80 1C */	lis r3, grDisplay_801C5DB0@ha
@@ -84,31 +84,31 @@ func_802205B0:
 /* 80220614 0021D1F4  48 17 00 89 */	bl GObj_SetupGXLink
 /* 80220618 0021D1F8  80 1D 00 0C */	lwz r0, 0xc(r29)
 /* 8022061C 0021D1FC  28 00 00 00 */	cmplwi r0, 0
-/* 80220620 0021D200  41 82 00 08 */	beq lbl_80220628
+/* 80220620 0021D200  41 82 00 08 */	beq grTDonkey_80220628
 /* 80220624 0021D204  90 1F 00 1C */	stw r0, 0x1c(r31)
-lbl_80220628:
+grTDonkey_80220628:
 /* 80220628 0021D208  81 9D 00 00 */	lwz r12, 0(r29)
 /* 8022062C 0021D20C  28 0C 00 00 */	cmplwi r12, 0
-/* 80220630 0021D210  41 82 00 10 */	beq lbl_80220640
+/* 80220630 0021D210  41 82 00 10 */	beq grTDonkey_80220640
 /* 80220634 0021D214  7D 88 03 A6 */	mtlr r12
 /* 80220638 0021D218  38 7E 00 00 */	addi r3, r30, 0
 /* 8022063C 0021D21C  4E 80 00 21 */	blrl
-lbl_80220640:
+grTDonkey_80220640:
 /* 80220640 0021D220  80 9D 00 08 */	lwz r4, 8(r29)
 /* 80220644 0021D224  28 04 00 00 */	cmplwi r4, 0
-/* 80220648 0021D228  41 82 00 2C */	beq lbl_80220674
+/* 80220648 0021D228  41 82 00 2C */	beq grTDonkey_80220674
 /* 8022064C 0021D22C  38 7E 00 00 */	addi r3, r30, 0
 /* 80220650 0021D230  38 A0 00 04 */	li r5, 4
 /* 80220654 0021D234  48 16 F7 01 */	bl func_8038FD54
-/* 80220658 0021D238  48 00 00 1C */	b lbl_80220674
-lbl_8022065C:
+/* 80220658 0021D238  48 00 00 1C */	b grTDonkey_80220674
+grTDonkey_8022065C:
 /* 8022065C 0021D23C  38 DC 00 00 */	addi r6, r28, 0
 /* 80220660 0021D240  4C C6 31 82 */	crclr 6
 /* 80220664 0021D244  38 7F 00 90 */	addi r3, r31, 0x90
 /* 80220668 0021D248  38 9F 00 B4 */	addi r4, r31, 0xb4
 /* 8022066C 0021D24C  38 A0 00 CC */	li r5, 0xcc
 /* 80220670 0021D250  48 12 50 39 */	bl OSReport
-lbl_80220674:
+grTDonkey_80220674:
 /* 80220674 0021D254  7F C3 F3 78 */	mr r3, r30
 /* 80220678 0021D258  80 01 00 24 */	lwz r0, 0x24(r1)
 /* 8022067C 0021D25C  83 E1 00 1C */	lwz r31, 0x1c(r1)
@@ -118,8 +118,8 @@ lbl_80220674:
 /* 8022068C 0021D26C  38 21 00 20 */	addi r1, r1, 0x20
 /* 80220690 0021D270  7C 08 03 A6 */	mtlr r0
 /* 80220694 0021D274  4E 80 00 20 */	blr
-.global lbl_80220698
-lbl_80220698:
+.global grTDonkey_80220698
+grTDonkey_80220698:
 /* 80220698 0021D278  7C 08 02 A6 */	mflr r0
 /* 8022069C 0021D27C  38 A0 00 00 */	li r5, 0
 /* 802206A0 0021D280  90 01 00 04 */	stw r0, 4(r1)
@@ -131,18 +131,18 @@ lbl_80220698:
 /* 802206B8 0021D298  38 21 00 08 */	addi r1, r1, 8
 /* 802206BC 0021D29C  7C 08 03 A6 */	mtlr r0
 /* 802206C0 0021D2A0  4E 80 00 20 */	blr
-.global lbl_802206C4
-lbl_802206C4:
+.global grTDonkey_802206C4
+grTDonkey_802206C4:
 /* 802206C4 0021D2A4  38 60 00 00 */	li r3, 0
 /* 802206C8 0021D2A8  4E 80 00 20 */	blr
-.global lbl_802206CC
-lbl_802206CC:
+.global grTDonkey_802206CC
+grTDonkey_802206CC:
 /* 802206CC 0021D2AC  4E 80 00 20 */	blr
-.global lbl_802206D0
-lbl_802206D0:
+.global grTDonkey_802206D0
+grTDonkey_802206D0:
 /* 802206D0 0021D2B0  4E 80 00 20 */	blr
-.global lbl_802206D4
-lbl_802206D4:
+.global grTDonkey_802206D4
+grTDonkey_802206D4:
 /* 802206D4 0021D2B4  7C 08 02 A6 */	mflr r0
 /* 802206D8 0021D2B8  90 01 00 04 */	stw r0, 4(r1)
 /* 802206DC 0021D2BC  94 21 FF E0 */	stwu r1, -0x20(r1)
@@ -163,12 +163,12 @@ lbl_802206D4:
 /* 80220718 0021D2F8  38 21 00 20 */	addi r1, r1, 0x20
 /* 8022071C 0021D2FC  7C 08 03 A6 */	mtlr r0
 /* 80220720 0021D300  4E 80 00 20 */	blr
-.global lbl_80220724
-lbl_80220724:
+.global grTDonkey_80220724
+grTDonkey_80220724:
 /* 80220724 0021D304  38 60 00 00 */	li r3, 0
 /* 80220728 0021D308  4E 80 00 20 */	blr
-.global lbl_8022072C
-lbl_8022072C:
+.global grTDonkey_8022072C
+grTDonkey_8022072C:
 /* 8022072C 0021D30C  7C 08 02 A6 */	mflr r0
 /* 80220730 0021D310  90 01 00 04 */	stw r0, 4(r1)
 /* 80220734 0021D314  94 21 FF E8 */	stwu r1, -0x18(r1)
@@ -182,11 +182,11 @@ lbl_8022072C:
 /* 80220754 0021D334  38 21 00 18 */	addi r1, r1, 0x18
 /* 80220758 0021D338  7C 08 03 A6 */	mtlr r0
 /* 8022075C 0021D33C  4E 80 00 20 */	blr
-.global lbl_80220760
-lbl_80220760:
+.global grTDonkey_80220760
+grTDonkey_80220760:
 /* 80220760 0021D340  4E 80 00 20 */	blr
-.global lbl_80220764
-lbl_80220764:
+.global grTDonkey_80220764
+grTDonkey_80220764:
 /* 80220764 0021D344  7C 08 02 A6 */	mflr r0
 /* 80220768 0021D348  90 01 00 04 */	stw r0, 4(r1)
 /* 8022076C 0021D34C  94 21 FF E0 */	stwu r1, -0x20(r1)
@@ -207,12 +207,12 @@ lbl_80220764:
 /* 802207A8 0021D388  38 21 00 20 */	addi r1, r1, 0x20
 /* 802207AC 0021D38C  7C 08 03 A6 */	mtlr r0
 /* 802207B0 0021D390  4E 80 00 20 */	blr
-.global lbl_802207B4
-lbl_802207B4:
+.global grTDonkey_802207B4
+grTDonkey_802207B4:
 /* 802207B4 0021D394  38 60 00 00 */	li r3, 0
 /* 802207B8 0021D398  4E 80 00 20 */	blr
-.global lbl_802207BC
-lbl_802207BC:
+.global grTDonkey_802207BC
+grTDonkey_802207BC:
 /* 802207BC 0021D39C  7C 08 02 A6 */	mflr r0
 /* 802207C0 0021D3A0  90 01 00 04 */	stw r0, 4(r1)
 /* 802207C4 0021D3A4  94 21 FF F8 */	stwu r1, -8(r1)
@@ -221,15 +221,15 @@ lbl_802207BC:
 /* 802207D0 0021D3B0  38 21 00 08 */	addi r1, r1, 8
 /* 802207D4 0021D3B4  7C 08 03 A6 */	mtlr r0
 /* 802207D8 0021D3B8  4E 80 00 20 */	blr
-.global lbl_802207DC
-lbl_802207DC:
+.global grTDonkey_802207DC
+grTDonkey_802207DC:
 /* 802207DC 0021D3BC  4E 80 00 20 */	blr
-.global lbl_802207E0
-lbl_802207E0:
+.global grTDonkey_802207E0
+grTDonkey_802207E0:
 /* 802207E0 0021D3C0  38 60 00 00 */	li r3, 0
 /* 802207E4 0021D3C4  4E 80 00 20 */	blr
-.global lbl_802207E8
-lbl_802207E8:
+.global grTDonkey_802207E8
+grTDonkey_802207E8:
 /* 802207E8 0021D3C8  38 60 00 01 */	li r3, 1
 /* 802207EC 0021D3CC  4E 80 00 20 */	blr
 
@@ -238,20 +238,20 @@ lbl_802207E8:
     .balign 8
 .global lbl_803E8850
 lbl_803E8850:
-    .4byte lbl_80220698
-    .4byte lbl_802206C4
-    .4byte lbl_802206CC
-    .4byte lbl_802206D0
+    .4byte grTDonkey_80220698
+    .4byte grTDonkey_802206C4
+    .4byte grTDonkey_802206CC
+    .4byte grTDonkey_802206D0
     .4byte NULL
-    .4byte lbl_80220764
-    .4byte lbl_802207B4
-    .4byte lbl_802207BC
-    .4byte lbl_802207DC
+    .4byte grTDonkey_80220764
+    .4byte grTDonkey_802207B4
+    .4byte grTDonkey_802207BC
+    .4byte grTDonkey_802207DC
     .4byte NULL
-    .4byte lbl_802206D4
-    .4byte lbl_80220724
-    .4byte lbl_8022072C
-    .4byte lbl_80220760
+    .4byte grTDonkey_802206D4
+    .4byte grTDonkey_80220724
+    .4byte grTDonkey_8022072C
+    .4byte grTDonkey_80220760
     .4byte 0xC0000000
     .4byte NULL
     .4byte NULL
@@ -267,13 +267,13 @@ lbl_803E88AC:
     .4byte 0x0000002C
     .4byte lbl_803E8850
     .4byte lbl_803E88A0
-    .4byte lbl_80220510
-    .4byte func_8022050C
-    .4byte lbl_80220580
-    .4byte lbl_80220584
-    .4byte lbl_802205A8
-    .4byte lbl_802207E0
-    .4byte lbl_802207E8
+    .4byte grTDonkey_80220510
+    .4byte grTDonkey_8022050C
+    .4byte grTDonkey_80220580
+    .4byte grTDonkey_80220584
+    .4byte grTDonkey_802205A8
+    .4byte grTDonkey_802207E0
+    .4byte grTDonkey_802207E8
     .4byte 0x00000001
     .4byte NULL
     .4byte NULL
