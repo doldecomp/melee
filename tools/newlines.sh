e56b676998ea4f95c81d93656c2f9ab97f121fae
Robin Avery <robin@ribbanya.dev>
2023-03-22 03:58:55 -0400

    ok

--- tools/newlines.sh
+++ tools/newlines.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 git ls-files -z |
     sed -z -E -e '/\.(sha1|png)$/d' -e '/\btools\/m2c\b/d' |
     while IFS= read -rd '' f; do
-        tail -c1 <"$f" |
-            read -r _ || echo >>"$f"
+        if [ -e "$f" ]; then
+            tail -c1 <"$f" |
+                read -r _ || echo >>"$f"
+        fi
     done
