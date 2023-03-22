5273165ecfdbbc164e437cdabb453ffce3cf6a4b
Robin Avery <robin@ribbanya.dev>
2023-03-22 02:35:32 -0400

    ok

--- obj_files.mk
+++ obj_files.mk
@@ -43,7 +43,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/lb/lbrefract_shared_data.s.o\
     $(BUILD_DIR)/asm/melee/lb/lbrefract1.s.o\
     $(BUILD_DIR)/asm/melee/lb/lbrefract2.s.o\
-    $(BUILD_DIR)/asm/melee/lb/lbrefract_003_data.s.o\
+    $(BUILD_DIR)/asm/melee/lb/lbrefract3_data.s.o\
     $(BUILD_DIR)/src/melee/lb/lbrefract3.c.o\
 
 TEXT_O_FILES +=\
