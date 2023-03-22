03f9eb101fd037e053873788210d184969248d37
Robin Avery <robin@ribbanya.dev>
2023-03-22 14:48:40 -0400

    Remove unused `doxygen-awesome-css` files and fix formatting and CI

--- .github/packages/gen-pages/setup.sh
+++ .github/packages/gen-pages/setup.sh
@@ -13,9 +13,6 @@ python -m venv --upgrade-deps /opt/venv
 . /opt/venv/bin/activate
 pip install --no-cache-dir -r /tmp/requirements.txt
 
-git clone 'https://github.com/jothepro/doxygen-awesome-css' --depth=1 \
-    /opt/doxygen-awesome-css
-
 apt autoremove -y
 apt clean
 rm -rf /var/lib/apt/lists/*
