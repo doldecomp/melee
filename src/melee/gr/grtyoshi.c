186455733b00198d520ccfe5873b16c38b199a0e
Robin Avery <robin@ribbanya.dev>
2023-03-22 18:16:24 -0400

    ok

--- src/melee/gr/grtyoshi.c
+++ src/melee/gr/grtyoshi.c
@@ -10,10 +10,12 @@
 #include <baselib/gobjgxlink.h>
 
 static StageCallbacks lbl_803E9578[] = {
-    { lbl_80223CD4, lbl_80223D00, lbl_80223D08, lbl_80223D0C, 0 },
-    { lbl_80223DA0, lbl_80223DF0, lbl_80223DF8, lbl_80223E18, 0 },
-    { lbl_80223D10, lbl_80223D60, lbl_80223D68, lbl_80223D9C,
-      (1 << 30) | (1 << 31) },
+    { grTYoshi_80223CD4, grTYoshi_80223D00, grTYoshi_80223D08,
+      grTYoshi_80223D0C, 0 },
+    { grTYoshi_80223DA0, grTYoshi_80223DF0, grTYoshi_80223DF8,
+      grTYoshi_80223E18, 0 },
+    { grTYoshi_80223D10, grTYoshi_80223D60, grTYoshi_80223D68,
+      grTYoshi_80223D9C, (1 << 30) | (1 << 31) },
     { NULL, NULL, NULL, NULL, 0 }
 };
 
@@ -21,13 +23,13 @@ StageData lbl_803E95D4 = {
     (1 << 0) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5),
     lbl_803E9578,
     "/GrTYs.dat",
-    lbl_80223B4C,
-    func_80223B48,
-    lbl_80223BBC,
-    lbl_80223BC0,
-    lbl_80223BE4,
-    lbl_80223E1C,
-    lbl_80223E24,
+    grTYoshi_80223B4C,
+    grTYoshi_80223B48,
+    grTYoshi_80223BBC,
+    grTYoshi_80223BC0,
+    grTYoshi_80223BE4,
+    grTYoshi_80223E1C,
+    grTYoshi_80223E24,
     (1 << 0),
     NULL,
     0,
@@ -35,29 +37,29 @@ StageData lbl_803E95D4 = {
 
 extern StageInfo stage_info;
 
-static void func_80223B48(int arg0) {}
-static void lbl_80223B4C(void)
+static void grTYoshi_80223B48(int arg0) {}
+static void grTYoshi_80223B4C(void)
 {
     stage_info.unk8C.b4 = 0;
     stage_info.unk8C.b5 = 1;
-    func_80223BEC(0);
-    func_80223BEC(1);
-    func_80223BEC(2);
+    grTYoshi_80223BEC(0);
+    grTYoshi_80223BEC(1);
+    grTYoshi_80223BEC(2);
     Ground_801C39C0();
     Ground_801C3BB4();
     Ground_801C4210();
     Ground_801C42AC();
 }
-static void lbl_80223BBC(void) {}
-static void lbl_80223BC0(void)
+static void grTYoshi_80223BBC(void) {}
+static void grTYoshi_80223BC0(void)
 {
     grZakoGenerator_801CAE04(false);
 }
-static bool lbl_80223BE4(void)
+static bool grTYoshi_80223BE4(void)
 {
     return false;
 }
-static HSD_GObj* func_80223BEC(int gobj_id)
+static HSD_GObj* grTYoshi_80223BEC(int gobj_id)
 {
     HSD_GObj* gobj;
     StageCallbacks* callbacks = &lbl_803E9578[gobj_id];
@@ -84,22 +86,22 @@ static HSD_GObj* func_80223BEC(int gobj_id)
 
     return gobj;
 }
-static void lbl_80223CD4(HSD_GObj* gobj)
+static void grTYoshi_80223CD4(HSD_GObj* gobj)
 {
     Ground* gp = gobj->user_data;
     grAnime_801C8138(gobj, gp->map_id, 0);
 }
 
-static bool lbl_80223D00(HSD_GObj* arg0)
+static bool grTYoshi_80223D00(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void lbl_80223D08(HSD_GObj* arg0) {}
+static void grTYoshi_80223D08(HSD_GObj* arg0) {}
 
-static void lbl_80223D0C(HSD_GObj* arg0) {}
+static void grTYoshi_80223D0C(HSD_GObj* arg0) {}
 
-static void lbl_80223D10(HSD_GObj* gobj)
+static void grTYoshi_80223D10(HSD_GObj* gobj)
 {
     /// @todo Unused stack.
 #ifdef MUST_MATCH
@@ -111,20 +113,20 @@ static void lbl_80223D10(HSD_GObj* gobj)
     grAnime_801C8138(gobj, gp->map_id, 0);
 }
 
-static bool lbl_80223D60(HSD_GObj* arg0)
+static bool grTYoshi_80223D60(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void lbl_80223D68(HSD_GObj* gobj)
+static void grTYoshi_80223D68(HSD_GObj* gobj)
 {
     lb_800115F4();
     Ground_801C2FE0(gobj);
 }
 
-static void lbl_80223D9C(HSD_GObj* arg0) {}
+static void grTYoshi_80223D9C(HSD_GObj* arg0) {}
 
-static void lbl_80223DA0(HSD_GObj* gobj)
+static void grTYoshi_80223DA0(HSD_GObj* gobj)
 {
     /// @todo Unused stack.
 #ifdef MUST_MATCH
@@ -136,24 +138,24 @@ static void lbl_80223DA0(HSD_GObj* gobj)
     grAnime_801C8138(gobj, gp->map_id, 0);
 }
 
-static bool lbl_80223DF0(HSD_GObj* arg0)
+static bool grTYoshi_80223DF0(HSD_GObj* arg0)
 {
     return false;
 }
 
-static void lbl_80223DF8(HSD_GObj* arg0)
+static void grTYoshi_80223DF8(HSD_GObj* arg0)
 {
     Ground_801C2FE0(arg0);
 }
 
-static void lbl_80223E18(HSD_GObj* arg0) {}
+static void grTYoshi_80223E18(HSD_GObj* arg0) {}
 
-static bool lbl_80223E1C(int arg0)
+static bool grTYoshi_80223E1C(int arg0)
 {
     return false;
 }
 
-static bool lbl_80223E24(Vec3* arg0, int arg1, HSD_JObj* arg2)
+static bool grTYoshi_80223E24(Vec3* arg0, int arg1, HSD_JObj* arg2)
 {
     return true;
 }
