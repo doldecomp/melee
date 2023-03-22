2fb7f95c002a76322d693d505928c6fed32bc40c
Robin Avery <robin@ribbanya.dev>
2023-03-22 17:24:46 -0400

    ok

--- src/melee/gr/grtkoopa.h
+++ src/melee/gr/grtkoopa.h
@@ -5,25 +5,25 @@
 
 #include <baselib/gobj.h>
 
-static void func_80221648(int);
-static void lbl_8022164C(void);
-static void lbl_802216BC(void);
-static void lbl_802216C0(void);
-static bool lbl_802216E4(void);
-static HSD_GObj* func_802216EC(int gobj_id);
-static void lbl_802217D4(HSD_GObj* gobj);
-static bool lbl_80221800(HSD_GObj*);
-static void lbl_80221808(HSD_GObj*);
-static void lbl_8022180C(HSD_GObj*);
-static void lbl_80221810(HSD_GObj* gobj);
-static bool lbl_80221860(HSD_GObj*);
-static void lbl_80221868(HSD_GObj* gobj);
-static void lbl_8022189C(HSD_GObj*);
-static void lbl_802218A0(HSD_GObj* gobj);
-static bool lbl_802218F0(HSD_GObj*);
-static void lbl_802218F8(HSD_GObj*);
-static void lbl_80221918(HSD_GObj*);
-static bool lbl_8022191C(int);
-static bool lbl_80221924(Vec3*, int, HSD_JObj*);
+static void grTKoopa_80221648(int);
+static void grTKoopa_8022164C(void);
+static void grTKoopa_802216BC(void);
+static void grTKoopa_802216C0(void);
+static bool grTKoopa_802216E4(void);
+static HSD_GObj* grTKoopa_802216EC(int gobj_id);
+static void grTKoopa_802217D4(HSD_GObj* gobj);
+static bool grTKoopa_80221800(HSD_GObj*);
+static void grTKoopa_80221808(HSD_GObj*);
+static void grTKoopa_8022180C(HSD_GObj*);
+static void grTKoopa_80221810(HSD_GObj* gobj);
+static bool grTKoopa_80221860(HSD_GObj*);
+static void grTKoopa_80221868(HSD_GObj* gobj);
+static void grTKoopa_8022189C(HSD_GObj*);
+static void grTKoopa_802218A0(HSD_GObj* gobj);
+static bool grTKoopa_802218F0(HSD_GObj*);
+static void grTKoopa_802218F8(HSD_GObj*);
+static void grTKoopa_80221918(HSD_GObj*);
+static bool grTKoopa_8022191C(int);
+static bool grTKoopa_80221924(Vec3*, int, HSD_JObj*);
 
 #endif
