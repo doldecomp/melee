7ffbe3421e2bc6e6699b899defc1199161c1eb1b
Robin Avery <robin@ribbanya.dev>
2023-03-22 17:21:47 -0400

    ok

--- src/melee/gr/grtfalco.c
+++ src/melee/gr/grtfalco.c
@@ -12,24 +12,24 @@
 
 /* static */ StageCallbacks lbl_803E8918[4] = {
     {
-        lbl_80220984,
-        lbl_802209B0,
-        lbl_802209B8,
-        lbl_802209BC,
+        grTFalco_80220984,
+        grTFalco_802209B0,
+        grTFalco_802209B8,
+        grTFalco_802209BC,
         0,
     },
     {
-        lbl_80220A50,
-        lbl_80220AA0,
-        lbl_80220AA8,
-        lbl_80220AC8,
+        grTFalco_80220A50,
+        grTFalco_80220AA0,
+        grTFalco_80220AA8,
+        grTFalco_80220AC8,
         0,
     },
     {
-        lbl_802209C0,
-        lbl_80220A10,
-        lbl_80220A18,
-        lbl_80220A4C,
+        grTFalco_802209C0,
+        grTFalco_80220A10,
+        grTFalco_80220A18,
+        grTFalco_80220A4C,
         (1 << 30) | (1 << 31),
     },
 };
@@ -38,13 +38,13 @@ StageData lbl_803E8974 = {
     45,
     lbl_803E8918,
     "/GrTFc.dat",
-    lbl_802207F4,
-    func_802207F0,
-    lbl_8022086C,
-    lbl_80220870,
-    lbl_80220894,
-    lbl_80220ACC,
-    lbl_80220B78,
+    grTFalco_802207F4,
+    grTFalco_802207F0,
+    grTFalco_8022086C,
+    grTFalco_80220870,
+    grTFalco_80220894,
+    grTFalco_80220ACC,
+    grTFalco_80220B78,
     1,
 };
 
@@ -57,35 +57,35 @@ typedef struct grTFalco_UnkStruct {
 
 grTFalco_UnkStruct* lbl_804D6AF8;
 
-void func_802207F0(int arg0) {}
+void grTFalco_802207F0(int arg0) {}
 
-void lbl_802207F4(void)
+void grTFalco_802207F4(void)
 {
     lbl_804D6AF8 = Ground_801C49F8();
     stage_info.unk8C.b4 = false;
     stage_info.unk8C.b5 = true;
-    func_8022089C(0);
-    func_8022089C(1);
-    func_8022089C(2);
+    grTFalco_8022089C(0);
+    grTFalco_8022089C(1);
+    grTFalco_8022089C(2);
     Ground_801C39C0();
     Ground_801C3BB4();
     Ground_801C4210();
     Ground_801C42AC();
 }
 
-void lbl_8022086C(void) {}
+void grTFalco_8022086C(void) {}
 
-void lbl_80220870(void)
+void grTFalco_80220870(void)
 {
     grZakoGenerator_801CAE04(0);
 }
 
-bool lbl_80220894(void)
+bool grTFalco_80220894(void)
 {
     return false;
 }
 
-HSD_GObj* func_8022089C(int id)
+HSD_GObj* grTFalco_8022089C(int id)
 {
     HSD_GObj* gobj;
 
@@ -116,57 +116,57 @@ HSD_GObj* func_8022089C(int id)
     return gobj;
 }
 
-void lbl_80220984(HSD_GObj* gobj)
+void grTFalco_80220984(HSD_GObj* gobj)
 {
     Ground* gp = (Ground*) HSD_GObjGetUserData(gobj);
     grAnime_801C8138(gobj, gp->map_id, 0);
 }
 
-bool lbl_802209B0(HSD_GObj* arg0)
+bool grTFalco_802209B0(HSD_GObj* arg0)
 {
     return false;
 }
 
-void lbl_802209B8(HSD_GObj* arg0) {}
+void grTFalco_802209B8(HSD_GObj* arg0) {}
 
-void lbl_802209BC(HSD_GObj* arg0) {}
+void grTFalco_802209BC(HSD_GObj* arg0) {}
 
-void lbl_802209C0(HSD_GObj* gobj)
+void grTFalco_802209C0(HSD_GObj* gobj)
 {
     Ground_JObjInline1(gobj);
 }
 
-bool lbl_80220A10(HSD_GObj* arg0)
+bool grTFalco_80220A10(HSD_GObj* arg0)
 {
     return false;
 }
 
-void lbl_80220A18(HSD_GObj* arg0)
+void grTFalco_80220A18(HSD_GObj* arg0)
 {
     lb_800115F4();
     Ground_801C2FE0(arg0);
 }
 
-void lbl_80220A4C(HSD_GObj* arg0) {}
+void grTFalco_80220A4C(HSD_GObj* arg0) {}
 
-void lbl_80220A50(HSD_GObj* gobj)
+void grTFalco_80220A50(HSD_GObj* gobj)
 {
     Ground_JObjInline1(gobj);
 }
 
-bool lbl_80220AA0(HSD_GObj* arg0)
+bool grTFalco_80220AA0(HSD_GObj* arg0)
 {
     return false;
 }
 
-void lbl_80220AA8(HSD_GObj* gobj)
+void grTFalco_80220AA8(HSD_GObj* gobj)
 {
     Ground_801C2FE0(gobj);
 }
 
-void lbl_80220AC8(HSD_GObj* arg0) {}
+void grTFalco_80220AC8(HSD_GObj* arg0) {}
 
-bool lbl_80220ACC(enum_t arg0)
+bool grTFalco_80220ACC(enum_t arg0)
 {
     if (arg0 != -1) {
         enum_t i = mpLib_80056B6C(arg0);
@@ -197,7 +197,7 @@ bool lbl_80220ACC(enum_t arg0)
     return false;
 }
 
-bool lbl_80220B78(Vec3* arg0, int arg1, HSD_JObj* arg2)
+bool grTFalco_80220B78(Vec3* arg0, int arg1, HSD_JObj* arg2)
 {
     return true;
 }
