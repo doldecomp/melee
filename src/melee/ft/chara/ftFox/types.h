c48cc4e8cecd8292511ee47f2749a27d5d76377b
Robin Avery <robin@ribbanya.dev>
2023-04-17 11:08:19 -0400

    ok

--- src/melee/ft/chara/ftFox/types.h
+++ src/melee/ft/chara/ftFox/types.h
@@ -7,54 +7,50 @@
 #include <dolphin/mtx/types.h>
 
 typedef struct ftFoxSpecialN {
-    /// 0x2340 - Check to allow repeated blaster shots
-    bool isBlasterLoop;
+    bool isBlasterLoop; // 0x2340 - Check to allow repeated blaster shots
+
 } ftFoxSpecialN;
 
 typedef struct ftFoxSpecialS {
-    /// 0x2340 - Number of frames to pass before gravity takes effect
-    s32 gravityDelay;
-    /// 0x2344 through 0x2370 - Related to Illusion/Phantasm model's position?
-    Vec3 ghostEffectPos[4];
-    /// 0x2374 through 0x2380 - ColAnim blend frames?
-    f32 blendFrames[4];
-    /// 0x2384 - Illusion/Phantasm GObj pointer
-    HSD_GObj* ghostGObj;
+    s32 gravityDelay; // 0x2340 - Number of frames to pass before gravity takes
+                      // effect
+    Vec3 ghostEffectPos[4]; // 0x2344 through 0x2370 - Related to
+                            // Illusion/Phantasm model's position?
+    f32 blendFrames[4];     // 0x2374 through 0x2380 - ColAnim blend frames?
+    HSD_GObj* ghostGObj;    // 0x2384 - Illusion/Phantasm GObj pointer
+
 } ftFoxSpecialS;
 
 typedef struct ftFoxSpecialHi {
-    /// 0x2340 - Number of frames to pass before gravity takes effect
-    s32 gravityDelay;
-    /// 0x2344 - Used to rotate Fox/Falco's model?
-    f32 rotateModel;
-    /// 0x2348 - Number of frames Firefox/Firebird's launch animation lasts for
-    s32 travelFrames;
-    /// 0x234C - ???
-    s32 unk;
-    /// 0x2350 - ???
-    s32 unk2;
+    s32 gravityDelay; // 0x2340 - Number of frames to pass before gravity takes
+                      // effect
+    f32 rotateModel;  // 0x2344 - Used to rotate Fox/Falco's model?
+    s32 travelFrames; // 0x2348 - Number of frames Firefox/Firebird's launch
+                      // animation lasts for
+    s32 unk;          // 0x234C - ???
+    s32 unk2;         // 0x2350 - ???
+
 } ftFoxSpecialHi;
 
 typedef struct ftFoxSpecialLw {
-    /// 0x2340 - Auto lag frames after initializing Reflector if B is not being
-    /// held. Reflector is immediately released with no lag once these frames
-    /// have passed
-    s32 releaseLag;
-    /// 0x2344 - Number of frames it takes Fox/Falco to turn around on command
-    s32 turnFrames;
-    /// 0x2348 - Check if Fox/Falco is no longer holding B
-    bool isRelease;
-    /// 0x234C - Number of frames to pass before gravity takes effect
-    s32 gravityDelay;
+    s32 releaseLag; // 0x2340 - Auto lag frames after initializing Reflector if
+                    // B is not being held. Reflector is immediately released
+                    // with no lag once these frames have passed
+    s32 turnFrames; // 0x2344 - Number of frames it takes Fox/Falco to turn
+                    // around on command
+    bool isRelease; // 0x2348 - Check if Fox/Falco is no longer holding B
+    s32 gravityDelay; // 0x234C - Number of frames to pass before gravity takes
+                      // effect
+
 } ftFoxSpecialLw;
 
 typedef struct ftFoxAppealS {
-    /// 0x2340 - Fox/Falco's facing direction as a boolean variable, used to
-    /// load indexed ASIDs from the AppealS struct
-    bool facingDir;
-    /// 0x2344 - Incremented every time one of the three stages of the AppealS
-    /// animation is completed
-    s32 animCount;
+    bool facingDir; // 0x2340 - Fox/Falco's facing direction as a boolean
+                    // variable, used to load indexed ASIDs from the AppealS
+                    // struct
+    s32 animCount;  // 0x2344 - Incremented every time one of the three stages
+                    // of the AppealS animation is completed
+
 } ftFoxAppealS;
 
 typedef union ftFox_MotionVars {
@@ -63,6 +59,7 @@ typedef union ftFox_MotionVars {
     ftFoxSpecialHi SpecialHi;
     ftFoxSpecialLw SpecialLw;
     ftFoxAppealS AppealS;
+
 } ftFox_MotionVars;
 
 #endif
