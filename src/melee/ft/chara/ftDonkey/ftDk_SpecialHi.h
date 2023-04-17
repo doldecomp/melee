8e6a867cb37d0416d16eae6c52de203838becf4a
Robin Avery <robin@ribbanya.dev>
2023-04-17 04:48:55 -0400

    ok

--- src/melee/ft/chara/ftDonkey/ftDk_SpecialHi.h
+++ src/melee/ft/chara/ftDonkey/ftDk_SpecialHi.h
@@ -1,21 +1,19 @@
 #ifndef MELEE_FT_CHARA_FTDONKEY_FTDONKEY_SPECIALHI_H
 #define MELEE_FT_CHARA_FTDONKEY_FTDONKEY_SPECIALHI_H
 
-#include <baselib/gobj.h>
+#include <baselib/forward.h>
 
-void ftDk_SpecialHi_DestroyAllEffects_SpecialHi(HSD_GObj* gobj);
-
-void ftDonkey_SetCallbacks_SpecialHi(HSD_GObj* gobj);
-void ftDk_SpecialHi_Coll(HSD_GObj* gobj);
+void ftDk_SpecialAirHi_Anim(HSD_GObj* gobj);
 void ftDk_SpecialAirHi_Coll(HSD_GObj* gobj);
+void ftDk_SpecialAirHi_Enter(HSD_GObj* gobj);
+void ftDk_SpecialAirHi_IASA(HSD_GObj* gobj);
+void ftDk_SpecialAirHi_Phys(HSD_GObj* gobj);
+void ftDk_SpecialHi_Anim(HSD_GObj* gobj);
+void ftDk_SpecialHi_Coll(HSD_GObj* gobj);
 void ftDk_SpecialHi_DestroyAllEffects_SpecialHi(HSD_GObj* gobj);
 void ftDk_SpecialHi_Enter(HSD_GObj* gobj);
-void ftDk_SpecialAirHi_Enter(HSD_GObj* gobj);
-void ftDk_SpecialHi_Anim(HSD_GObj* gobj);
-void ftDk_SpecialAirHi_Anim(HSD_GObj* gobj);
 void ftDk_SpecialHi_IASA(HSD_GObj* gobj);
-void ftDk_SpecialAirHi_IASA(HSD_GObj* gobj);
 void ftDk_SpecialHi_Phys(HSD_GObj* gobj);
-void ftDk_SpecialAirHi_Phys(HSD_GObj* gobj);
+void ftDonkey_SetCallbacks_SpecialHi(HSD_GObj* gobj);
 
 #endif
