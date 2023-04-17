d0af0475cbfe9010212f249b186f4650f95a3d5f
Robin Avery <robin@ribbanya.dev>
2023-04-17 11:16:33 -0400

    ok

---
+++ src/melee/ft/chara/ftLuigi/ftLg_SpecialHi.h
@@ -0,0 +1,19 @@
+/// @file Up Special - Super Jump Punch (SpecialHi)
+#ifndef MELEE_FT_CHARA_FTLUIGI_FTLG_SPECIALHI_H
+#define MELEE_FT_CHARA_FTLUIGI_FTLG_SPECIALHI_H
+
+#include <baselib/forward.h>
+
+void ftLg_SpecialAirHi_Anim(HSD_GObj* gobj);
+void ftLg_SpecialAirHi_Coll(HSD_GObj* gobj);
+void ftLg_SpecialAirHi_IASA(HSD_GObj* gobj);
+void ftLg_SpecialAirHi_Phys(HSD_GObj* gobj);
+void ftLg_SpecialAirHi_Enter(HSD_GObj* gobj);
+void ftLg_SpecialHi_Anim(HSD_GObj* gobj);
+void ftLg_SpecialHi_CheckLanding(HSD_GObj* gobj);
+void ftLg_SpecialHi_Coll(HSD_GObj* gobj);
+void ftLg_SpecialHi_IASA(HSD_GObj* gobj);
+void ftLg_SpecialHi_Phys(HSD_GObj* gobj);
+void ftLg_SpecialHi_Enter(HSD_GObj* gobj);
+
+#endif
