6fbe721a62e4c4646dc89adcee9098ea742bcf20
Robin Avery <robin@ribbanya.dev>
2023-04-17 04:23:37 -0400

    ok

--- src/melee/ft/chara/ftMario/ftMr_SpecialS.h
+++ src/melee/ft/chara/ftMario/ftMr_SpecialS.h
@@ -6,25 +6,25 @@
 #include <baselib/forward.h>
 
 bool ftMr_SpecialS_CheckItemCapeRemove(HSD_GObj* gobj);
+void ftMario_SpecialS_ReflectThink(HSD_GObj* gobj);
+void ftMario_SpecialS_SetCall(Fighter* fp);
+void ftMario_SpecialS_UpdateVarsColl(HSD_GObj* gobj);
 void ftMr_SpecialAirS_AirToGround(HSD_GObj* gobj);
 void ftMr_SpecialAirS_Anim(HSD_GObj* gobj);
 void ftMr_SpecialAirS_Coll(HSD_GObj* gobj);
+void ftMr_SpecialAirS_Enter(HSD_GObj* gobj);
 void ftMr_SpecialAirS_IASA(HSD_GObj* gobj);
 void ftMr_SpecialAirS_Phys(HSD_GObj* gobj);
-void ftMr_SpecialAirS_Enter(HSD_GObj* gobj);
 void ftMr_SpecialS_Anim(HSD_GObj* gobj);
 void ftMr_SpecialS_Coll(HSD_GObj* gobj);
 void ftMr_SpecialS_CreateCape(HSD_GObj* gobj);
+void ftMr_SpecialS_Enter(HSD_GObj* gobj);
 void ftMr_SpecialS_EnterHitlag(HSD_GObj* gobj);
 void ftMr_SpecialS_ExitHitlag(HSD_GObj* gobj);
 void ftMr_SpecialS_GroundToAir(HSD_GObj* gobj);
 void ftMr_SpecialS_IASA(HSD_GObj* gobj);
 void ftMr_SpecialS_Phys(HSD_GObj* gobj);
-void ftMario_SpecialS_ReflectThink(HSD_GObj* gobj);
 void ftMr_SpecialS_RemoveCape(HSD_GObj* gobj);
-void ftMario_SpecialS_SetCall(Fighter* fp);
 void ftMr_SpecialS_SetNULL(HSD_GObj* gobj);
-void ftMr_SpecialS_Enter(HSD_GObj* gobj);
-void ftMario_SpecialS_UpdateVarsColl(HSD_GObj* gobj);
 
 #endif
