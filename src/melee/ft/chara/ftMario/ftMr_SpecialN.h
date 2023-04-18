f9ed4440bf3759e62f23575ad48fb9dd3ada6c26
Robin Avery <robin@ribbanya.dev>
2023-04-18 00:53:48 -0400

    ok

--- src/melee/ft/chara/ftMario/ftMr_SpecialN.h
+++ src/melee/ft/chara/ftMario/ftMr_SpecialN.h
@@ -5,18 +5,18 @@
 #include "ft/forward.h"
 
 int ftMr_SpecialN_VitaminRandom(HSD_GObj*);
-void ftMr_SpecialN_Enter(HSD_GObj*);
-void ftMr_SpecialN_Anim(HSD_GObj*);
-void ftMr_SpecialN_IASA(HSD_GObj*);
-void ftMr_SpecialN_Phys(HSD_GObj*);
-void ftMr_SpecialN_Coll(HSD_GObj*);
-void ftMr_SpecialN_ItemFireSpawn(HSD_GObj*);
-void ftMr_SpecialAirN_Enter(HSD_GObj*);
+void ftMr_SpecialAirN_AirToGround(HSD_GObj*);
 void ftMr_SpecialAirN_Anim(HSD_GObj*);
+void ftMr_SpecialAirN_Coll(HSD_GObj*);
+void ftMr_SpecialAirN_Enter(HSD_GObj*);
 void ftMr_SpecialAirN_IASA(HSD_GObj*);
 void ftMr_SpecialAirN_Phys(HSD_GObj*);
-void ftMr_SpecialAirN_Coll(HSD_GObj*);
+void ftMr_SpecialN_Anim(HSD_GObj*);
+void ftMr_SpecialN_Coll(HSD_GObj*);
+void ftMr_SpecialN_Enter(HSD_GObj*);
 void ftMr_SpecialN_GroundToAir(HSD_GObj*);
-void ftMr_SpecialAirN_AirToGround(HSD_GObj*);
+void ftMr_SpecialN_IASA(HSD_GObj*);
+void ftMr_SpecialN_ItemFireSpawn(HSD_GObj*);
+void ftMr_SpecialN_Phys(HSD_GObj*);
 
 #endif
