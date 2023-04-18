035b14f0c7b4f644c21d464a48fb4a796426884b
Robin Avery <robin@ribbanya.dev>
2023-04-17 23:28:00 -0400

    ok

--- src/melee/ft/chara/ftMario/ftMr_SpecialHi.h
+++ src/melee/ft/chara/ftMario/ftMr_SpecialHi.h
@@ -3,16 +3,16 @@
 
 #include <baselib/forward.h>
 
-void ftMr_SpecialHi_Enter(HSD_GObj* gobj);
-void ftMr_SpecialAirHi_Enter(HSD_GObj* gobj);
-void ftMr_SpecialHi_Anim(HSD_GObj* gobj);
 void ftMr_SpecialAirHi_Anim(HSD_GObj* gobj);
-void ftMr_SpecialHi_IASA(HSD_GObj* gobj);
+void ftMr_SpecialAirHi_Coll(HSD_GObj* gobj);
+void ftMr_SpecialAirHi_Enter(HSD_GObj* gobj);
 void ftMr_SpecialAirHi_IASA(HSD_GObj* gobj);
-void ftMr_SpecialHi_Phys(HSD_GObj* gobj);
 void ftMr_SpecialAirHi_Phys(HSD_GObj* gobj);
+void ftMr_SpecialHi_Anim(HSD_GObj* gobj);
 void ftMr_SpecialHi_CheckLanding(HSD_GObj* gobj);
 void ftMr_SpecialHi_Coll(HSD_GObj* gobj);
-void ftMr_SpecialAirHi_Coll(HSD_GObj* gobj);
+void ftMr_SpecialHi_Enter(HSD_GObj* gobj);
+void ftMr_SpecialHi_IASA(HSD_GObj* gobj);
+void ftMr_SpecialHi_Phys(HSD_GObj* gobj);
 
 #endif
