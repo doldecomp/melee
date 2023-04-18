f071be12b6c2da3b3c1f2ebea745e8fac6869db5
Robin Avery <robin@ribbanya.dev>
2023-04-17 23:11:16 -0400

    ok

--- src/melee/ft/chara/ftPopo/ftPp_Unk1.h
+++ src/melee/ft/chara/ftPopo/ftPp_Unk1.h
@@ -4,25 +4,6 @@
 #include "ft/ftcommon.h"
 #include "lb/lbvector.h"
 
-typedef struct _ftIceClimberAttributes {
-    f32 x0;
-    f32 x4;
-    f32 x8;
-    f32 xC;
-    f32 x10;
-    f32 x14;
-    f32 x18;
-    s32 x1C;
-    s32 x20;
-    s32 x24;
-    s32 x28;
-    s32 x2C;
-    u8 data_filler_1[0xC4 - 0x30];
-    f32 xC4;
-    f32 xC8;
-    u8 data_filler_2[0x90];
-} ftIceClimberAttributes;
-
 void ftPp_Init_OnLoadForNana(Fighter* fp);
 void ftPp_SpecialN_Anim(HSD_GObj*);
 void ftPp_SpecialN_IASA(HSD_GObj*);
