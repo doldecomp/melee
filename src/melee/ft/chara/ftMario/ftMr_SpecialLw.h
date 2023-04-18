dbf61ea3c6dcd5d51ca21bdd105f1c7ab1b12c9c
Robin Avery <robin@ribbanya.dev>
2023-04-17 23:29:10 -0400

    ok

--- src/melee/ft/chara/ftMario/ftMr_SpecialLw.h
+++ src/melee/ft/chara/ftMario/ftMr_SpecialLw.h
@@ -4,16 +4,16 @@
 #include <platform.h>
 #include <baselib/forward.h>
 
-void ftMr_SpecialLw_UpdateRot(HSD_GObj* gobj);
-void ftMr_SpecialLw_Enter(HSD_GObj* gobj);
-void ftMr_SpecialAirLw_Enter(HSD_GObj* gobj);
-void ftMr_SpecialLw_Anim(HSD_GObj* gobj);
 void ftMr_SpecialAirLw_Anim(HSD_GObj* gobj);
-void ftMr_SpecialLw_IASA(HSD_GObj* gobj);
-void ftMr_SpecialAirLw_IASA(HSD_GObj* gobj);
-void ftMr_SpecialLw_Phys(HSD_GObj* gobj);
-void ftMr_SpecialAirLw_Phys(HSD_GObj* gobj);
-void ftMr_SpecialLw_Coll(HSD_GObj* gobj);
 void ftMr_SpecialAirLw_Coll(HSD_GObj* gobj);
+void ftMr_SpecialAirLw_Enter(HSD_GObj* gobj);
+void ftMr_SpecialAirLw_IASA(HSD_GObj* gobj);
+void ftMr_SpecialAirLw_Phys(HSD_GObj* gobj);
+void ftMr_SpecialLw_Anim(HSD_GObj* gobj);
+void ftMr_SpecialLw_Coll(HSD_GObj* gobj);
+void ftMr_SpecialLw_Enter(HSD_GObj* gobj);
+void ftMr_SpecialLw_IASA(HSD_GObj* gobj);
+void ftMr_SpecialLw_Phys(HSD_GObj* gobj);
+void ftMr_SpecialLw_UpdateRot(HSD_GObj* gobj);
 
 #endif
