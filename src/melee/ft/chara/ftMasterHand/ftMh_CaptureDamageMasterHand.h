391b961a9757b79acb1c02546509f97900f38b01
Robin Avery <robin@ribbanya.dev>
2023-04-20 06:17:43 -0400

    bad

--- src/melee/ft/chara/ftMasterHand/ftMh_CaptureDamageMasterHand.h
+++ src/melee/ft/chara/ftMasterHand/ftMh_CaptureDamageMasterHand.h
@@ -1,12 +1,12 @@
-#ifndef MELEE_FT_CHARA_FTMASTERHAND_FTMASTERHAND_33_H
-#define MELEE_FT_CHARA_FTMASTERHAND_FTMASTERHAND_33_H
+#ifndef GALE01_155C94
+#define GALE01_155C94
 
-#include <baselib/gobj.h>
+#include <baselib/forward.h>
 
-void ftMh_CaptureDamageMasterHand_80155C94(HSD_GObj*);
-
-void ftMh_CaptureDamageMasterHand_Phys(HSD_GObj* gobj);
-void ftMh_CaptureDamageMasterHand_Coll(HSD_GObj* gobj);
-void ftMh_CaptureDamageMasterHand_IASA(HSD_GObj* gobj);
+/* 155C94 */ void ftMh_CaptureDamageMasterHand_80155C94(HSD_GObj* gobj);
+/* 155C20 */ void ftMh_CaptureDamageMasterHand_Anim(HSD_GObj* gobj);
+/* 155C90 */ void ftMh_CaptureDamageMasterHand_Coll(HSD_GObj* gobj);
+/* 155C88 */ void ftMh_CaptureDamageMasterHand_IASA(HSD_GObj* gobj);
+/* 155C8C */ void ftMh_CaptureDamageMasterHand_Phys(HSD_GObj* gobj);
 
 #endif
