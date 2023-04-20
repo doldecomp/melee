716c2060d44f50921ae614ac49ab752a14e33f4e
Robin Avery <robin@ribbanya.dev>
2023-04-20 06:02:03 -0400

    bad

--- src/melee/ft/chara/ftMewtwo/ftMt_Init.h
+++ src/melee/ft/chara/ftMewtwo/ftMt_Init.h
@@ -8,30 +8,6 @@
 #include "it/item.h"
 #include "lb/lbvector.h"
 
-typedef enum ftMewtwoAction {
-    ftMt_MS_SpecialNStart = ftCo_MS_Count,
-    ftMt_MS_SpecialNLoop,
-    ftMt_MS_SpecialNFull,
-    ftMt_MS_SpecialNCancel,
-    ftMt_MS_SpecialNEnd,
-    ftMt_MS_SpecialAirNStart,
-    ftMt_MS_SpecialAirNLoop,
-    ftMt_MS_SpecialAirNFull,
-    ftMt_MS_SpecialAirNCancel,
-    ftMt_MS_SpecialAirNEnd,
-    ftMt_MS_SpecialS,
-    ftMt_MS_SpecialAirS,
-    ftMt_MS_SpecialHiStart,
-    ftMt_MS_SpecialHi,
-    ftMt_MS_SpecialHiLost,
-    ftMt_MS_SpecialAirHiStart,
-    ftMt_MS_SpecialAirHi,
-    ftMt_MS_SpecialAirHiLost,
-    ftMt_MS_SpecialLw,
-    ftMt_MS_SpecialAirLw,
-
-} ftMewtwoAction;
-
 // Flag values read during Confusion's reflect think function
 
 #define CONFUSION_REFLECT_NONE                                                \
