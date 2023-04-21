305d1396bacbb217ac08343a90bc6879b4c4e4a4
Robin Avery <robin@ribbanya.dev>
2023-04-21 03:05:12 -0400

    ok

--- src/melee/ft/chara/ftDonkey/types.h
+++ src/melee/ft/chara/ftDonkey/types.h
@@ -3,11 +3,6 @@
 
 #include <platform.h>
 
-struct ftDK_FighterVars {
-    /* 0x222C */ s32 x222C;
-    /* 0x2230 */ s32 x2230;
-};
-
 struct ftDonkey_FighterVars {
     /* 0x222C */ s32 x222C;
     /* 0x2230 */ s32 x2230;
