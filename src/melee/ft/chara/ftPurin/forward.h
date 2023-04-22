8ce338af85d2753716289bb1d8511e158b1ddc35
Robin Avery <robin@ribbanya.dev>
2023-04-22 18:11:56 -0400

    Clean up `ftPurin` motion definitions

--- src/melee/ft/chara/ftPurin/forward.h
+++ src/melee/ft/chara/ftPurin/forward.h
@@ -2,7 +2,29 @@
 #define MELEE_FT_CHARA_FTPURIN_FORWARD_H
 
 #include <platform.h>
+#include "ft/forward.h"
 #include "ftCommon/forward.h"
+
+typedef enum ftPr_MotionFlags {
+    ftPr_MF_Special = FtStateChange_SkipUpdateModel |
+                      FtStateChange_SkipUpdateItemVis |
+                      FtStateChange_Unk_UpdatePhys | FtStateChange_FreezeState,
+    ftPr_MF_SpecialHi = ftPr_MF_Special | FtStateChange_PreserveFastFall |
+                        FtStateChange_PreserveGfx,
+    ftPr_MF_SpecialLw =
+        ftPr_MF_Special | FtStateChange_PreserveColAnimHitStatus,
+    ftPr_MF_SpecialN = ftPr_MF_Special | FtStateChange_PreserveFastFall |
+                       FtStateChange_PreserveSfx,
+    ftPr_MF_SpecialS = ftPr_MF_Special | FtStateChange_PreserveGfx |
+                       FtStateChange_PreserveSfx,
+    ftPr_MF_SpecialAirHi = ftPr_MF_SpecialHi | FtStateChange_SkipUpdateParasol,
+    ftPr_MF_SpecialAirLw = ftPr_MF_SpecialLw | FtStateChange_SkipUpdateParasol,
+    ftPr_MF_SpecialAirN = ftPr_MF_SpecialN | FtStateChange_SkipUpdateParasol,
+    ftPr_MF_SpecialAirS = ftPr_MF_SpecialS | FtStateChange_SkipUpdateParasol,
+    ftPr_MF_SpecialNCharged = ftPr_MF_SpecialN | FtStateChange_Unk_19,
+    ftPr_SpecialAirNCharged =
+        ftPr_MF_SpecialNCharged | FtStateChange_SkipUpdateParasol,
+} ftPr_MotionFlags;
 
 typedef enum ftPurin_MotionState {
     ftPr_MS_Jump2 = ftCo_MS_Count,
