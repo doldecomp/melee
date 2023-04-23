122f054096430a83279ddb295355d8a881b7606e
Robin Avery <robin@ribbanya.dev>
2023-04-22 20:34:04 -0400

    Clean up `ftZelda` motion definitions

--- src/melee/ft/chara/ftZelda/forward.h
+++ src/melee/ft/chara/ftZelda/forward.h
@@ -1,7 +1,26 @@
 #ifndef MELEE_FT_CHARA_FTZELDA_FORWARD_H
 #define MELEE_FT_CHARA_FTZELDA_FORWARD_H
 
+#include "ft/forward.h"
 #include "ftCommon/forward.h"
+
+typedef enum ftZd_MotionFlags {
+    ftZd_MF_Special = FtStateChange_SkipUpdateModel |
+                      FtStateChange_SkipUpdateItemVis |
+                      FtStateChange_Unk_UpdatePhys | FtStateChange_FreezeState,
+    ftZd_MF_SpecialN = ftZd_MF_Special | FtStateChange_PreserveFastFall |
+                       FtStateChange_SkipUpdateColAnim,
+    ftZd_MF_SpecialHi = ftZd_MF_Special | FtStateChange_PreserveFastFall |
+                        FtStateChange_PreserveGfx,
+    ftZd_MF_SpecialLw =
+        ftZd_MF_Special | FtStateChange_PreserveColAnimHitStatus,
+    ftZd_MF_SpecialS = ftZd_MF_Special | FtStateChange_PreserveGfx |
+                       FtStateChange_SkipUpdateThrowException,
+    ftZd_MF_SpecialAirN = ftZd_MF_SpecialN | FtStateChange_SkipUpdateParasol,
+    ftZd_MF_SpecialAirHi = ftZd_MF_SpecialHi | FtStateChange_SkipUpdateParasol,
+    ftZd_MF_SpecialAirLw = ftZd_MF_SpecialLw | FtStateChange_SkipUpdateParasol,
+    ftZd_MF_SpecialAirS = ftZd_MF_SpecialS | FtStateChange_SkipUpdateParasol,
+} ftZd_MotionFlags;
 
 enum ftZelda_AS {
     ftZd_MS_SpecialN = ftCo_MS_Count,
