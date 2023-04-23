a76805bda1d2035253251f82c3a2e847be411c5c
Robin Avery <robin@ribbanya.dev>
2023-04-23 12:53:58 -0400

    Clean up `ftNess` motion definitions (#811)

--- src/melee/ft/chara/ftNess/forward.h
+++ src/melee/ft/chara/ftNess/forward.h
@@ -1,7 +1,66 @@
 #ifndef MELEE_FT_CHARA_FTNESS_FORWARD_H
 #define MELEE_FT_CHARA_FTNESS_FORWARD_H
 
+#include <platform.h>
+#include "ft/forward.h"
 #include "ftCommon/forward.h"
+
+static u32 const ftNs_MF_Attack4 ATTRIBUTE_USED =
+    FtStateChange_SkipUpdateHit | FtStateChange_SkipUpdateRumble |
+    FtStateChange_SkipUpdateItemVis | FtStateChange_FreezeState;
+
+static u32 const ftNs_MF_AttackHi4 ATTRIBUTE_USED =
+    ftNs_MF_Attack4 | FtStateChange_PreserveGfx;
+
+static u32 const ftNs_MF_AttackLw4 ATTRIBUTE_USED =
+    ftNs_MF_AttackHi4 | FtStateChange_PreserveFastFall;
+
+static u32 const ftNs_MF_AttackHi4Start ATTRIBUTE_USED =
+    ftNs_MF_AttackHi4 | FtStateChange_PreserveSfx;
+
+static u32 const ftNs_MF_AttackLw4Start ATTRIBUTE_USED =
+    ftNs_MF_AttackLw4 | FtStateChange_PreserveSfx;
+
+static u32 const ftNs_MF_AttackS4 ATTRIBUTE_USED =
+    ftNs_MF_Attack4 | FtStateChange_PreserveFastFall |
+    FtStateChange_PreserveSfx | FtStateChange_SkipUpdateColAnim;
+
+static u32 const ftNs_MF_Special ATTRIBUTE_USED =
+    FtStateChange_SkipUpdateModel | FtStateChange_SkipUpdateItemVis |
+    FtStateChange_Unk_UpdatePhys | FtStateChange_FreezeState;
+
+static u32 const ftNs_MF_SpecialLw ATTRIBUTE_USED =
+    ftNs_MF_Special | FtStateChange_PreserveColAnimHitStatus;
+
+static u32 const ftNs_MF_Special_SkipUpdateThrowException ATTRIBUTE_USED =
+    ftNs_MF_Special | FtStateChange_SkipUpdateThrowException;
+
+static u32 const ftNs_MF_SpecialN ATTRIBUTE_USED =
+    ftNs_MF_Special_SkipUpdateThrowException | FtStateChange_PreserveFastFall;
+
+static u32 const ftNs_MF_SpecialS ATTRIBUTE_USED =
+    ftNs_MF_Special_SkipUpdateThrowException | FtStateChange_PreserveGfx;
+
+static u32 const ftNs_MF_SpecialHi ATTRIBUTE_USED =
+    ftNs_MF_SpecialN | FtStateChange_PreserveGfx;
+
+static u32 const ftNs_MF_SpecialAirLw ATTRIBUTE_USED =
+    ftNs_MF_SpecialLw | FtStateChange_SkipUpdateParasol;
+
+static u32 const ftNs_MF_SpecialAirN ATTRIBUTE_USED =
+    ftNs_MF_SpecialN | FtStateChange_SkipUpdateParasol;
+
+static u32 const ftNs_MF_SpecialAirS ATTRIBUTE_USED =
+    ftNs_MF_SpecialS | FtStateChange_SkipUpdateParasol;
+
+static u32 const ftNs_MF_SpecialAirHi ATTRIBUTE_USED =
+    ftNs_MF_SpecialHi | FtStateChange_SkipUpdateParasol;
+
+static u32 const ftNs_MF_SpecialLwLoop ATTRIBUTE_USED =
+    ftNs_MF_SpecialLw | FtStateChange_Unk_19;
+
+static u32 const ftNs_MF_SpecialAirLwLoop ATTRIBUTE_USED =
+    ftNs_MF_SpecialLwLoop | FtStateChange_SkipUpdateParasol;
 
 typedef enum ftNess_MotionState {
     ftNs_MS_AttackS4 = ftCo_MS_Count,
