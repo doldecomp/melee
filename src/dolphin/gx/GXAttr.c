2b8a8600be74fe2fc9e71d3a874ce7c0b10adf27
Robin Avery <robin@ribbanya.dev>
2023-05-19 07:23:06 -0400

    Clean up Doxygen formatting

--- src/dolphin/gx/GXAttr.c
+++ src/dolphin/gx/GXAttr.c
@@ -4,6 +4,7 @@
 #include <dolphin/gx/__GX_unknown_001.h>
 #include <dolphin/gx/__GXInit.h>
 #include <dolphin/gx/GXAttr.h>
+#include <dolphin/gx/GXEnum.h>
 #include <dolphin/gx/GXTransform.h>
 
 #ifdef MWERKS_GEKKO
@@ -158,20 +159,21 @@ lbl_8033BEC4:
 #endif
 
 /**
- * This function sets the type of a single attribute (attr) in the current
- * vertex descriptor. The current vertex descriptor defines which attributes
- * are present in a vertex and how each attribute is referenced. The current
- * vertex descriptor is used by the Graphics Processor (GP) to interpret the
- * graphics command stream produced by the GX API. In particular, the current
- * vertex descriptor is used to parse the vertex data that is present in the
- * command stream.
+ * Sets the type of a single attribute (@attr) in the current vertex
+ * descriptor.
  *
- * @param attr Specifies the name of the attribute. One of GXAttr.
- *             Enabled attribute data must be sent in ascending order.
- * @param type Specifies the reference type of the attribute. One of
- * GXAttrType. Accepted values are GX_NONE, GX_DIRECT, GX_INDEX8, GX_INDEX16.
+ * The current vertex descriptor defines which attributes are present in a
+ * vertex and how each attribute is referenced. It is used by the Graphics
+ * Processor (GP) to interpret the graphics command stream produced by the GX
+ * API. In particular, the current vertex descriptor is used to parse the
+ * vertex data that is present in the command stream.
+ *
+ * @param attr The name of the attribute. Enabled attribute data must be sent
+ *             in ascending order.
+ * @param type The reference type of the attribute. Accepted values are
+ *             #GX_NONE, #GX_DIRECT, #GX_INDEX8, and #GX_INDEX16.
  */
-void GXSetVtxDesc(s32 attr, s32 type)
+void GXSetVtxDesc(GXAttr attr, s32 type)
 {
     switch (attr) {
     case GX_VA_PNMTXIDX:
@@ -360,20 +362,12 @@ lbl_8033C3B4:
 /* 8033C3C4 00338FA4  4E 80 00 20 */	blr
 } // clang-format on
 #pragma pop
-
-#else
-
-void __GXSetVCD(void)
-{
-    NOT_IMPLEMENTED;
-}
-
 #endif
+
 void GXClearVtxDesc(void)
 {
     GXContext* temp_r3;
     GXContext* temp_r3_2;
-
     __GXContexts.main->x14 = 0;
     temp_r3 = __GXContexts.main;
     temp_r3->x14 = (temp_r3->x14 & 0xFFFFF9FF) | 0x200;
@@ -385,47 +379,53 @@ void GXClearVtxDesc(void)
 }
 
 /**
- * This function sets the attribute format (attr) for a single attribute
- * in the Vertex Attribute Format Table (VAT). Each attribute format
- * describes the data type, number of elements (count), and fixed point format
- * (frac), if required. The are eight vertex formats available in the VAT.
- * The vertex format describes the format of all attributes in a vertex.
- * The application can pre-program all eight vertex formats, and then select
- * one of them during the actual drawing of geometry (See GXBegin). Note
- * that all vertices used to draw a particular graphics primitive will have
- * the same format. The vertex format set using GXSetVtxAttrFmt, along with
- * the current vertex descriptor set using GXSetVtxDesc, completely define
- * the vertex data format.
+ * Sets the attribute format (@p attr) for a single attribute in the Vertex
+ * Attribute Format Table (VAT).
+ *
+ * Each attribute format describes the data type, number of elements
+ * (@p count), and fixed point format (@p frac), if required. The are eight
+ * vertex formats available in the VAT. The vertex format describes the format
+ * of all attributes in a vertex. The application can pre-program all eight
+ * vertex formats, and then select one of them during the actual drawing of
+ * geometry (see #GXBegin). Note that all vertices used to draw a particular
+ * graphics primitive will have the same format. The vertex format set using
+ * #GXSetVtxAttrFmt, along with the current vertex descriptor set using
+ * #GXSetVtxDesc, completely define the vertex data format.
  *
  * The vertex format allows data to be sent to the graphics processor in its
  * most quantized format. The graphics hardware will inverse-quantize the
  * data (into floating point format) before it is used. The vertex attribute
  * format is used to communicate the data quantization format to the hardware.
  *
- * Note that GX_VA_NRM and GX_VA_NBT attributes share the same type. Also,
- * the frac for these attributes is fixed according to the type. The component
- * count (count) for GX_VA_NRM must be set to GX_NRM_XYZ. The component count
- * for GX_VA_NBT must be set to GX_NRM_NBT (see exception for HW2 below).
+ * Note that #GX_VA_NRM and #GX_VA_NBT attributes share the same type. Also,
+ * the @p frac for these attributes is fixed according to the type. The
+ * component count (@p count) for #GX_VA_NRM must be set to #GX_NRM_XYZ. The
+ * component count for #GX_VA_NBT must be set to #GX_NRM_NBT (see exception for
+ * HW2 below).
  *
- * Note also that a HW1 bug means that for a component type of U8 or S8 the
+ * Note also that a HW1 bug means that for a component type of #u8 or #s8, the
  * fixed-point fraction is ignored and assumed to be zero.
  *
- * @param vtxfmt Specifies the vertex format number.
- * @param attr   Specifies the attribute name. One of GXAttr.
+ * @param vtxfmt The vertex format number.
+ * @param attr   The attribute name.
  * @param count  May be one of several different enumerations, depending
- *               on the attribute name; one of GXCompCnt, GXPosCompCnt,
- *               GXNrmCompCnt, GXClrCompCnt, or GXTexCompCnt. Ultimately
+ *               on the attribute name; one of #GXCompCnt, #GXPosCompCnt,
+ *               #GXNrmCompCnt, #GXClrCompCnt, or #GXTexCompCnt. Ultimately
  *               aids in determining the number of dimensions for this
  *               attribute. For example, 4 for an RGBA color, or 3 for
  *               vertex position data.
  * @param type   May be one of several different enumerations, depending
  *               on the attribute name. Specifies the data format for
- *               a single value in a dimension, such as u8, s16, or f32.
+ *               a single value in a dimension, such as #u8, #s16, or #f32.
  * @param frac   Specifies the number of fractional bits in a fixed point
- *               number, where 0 <= frac <= 31.  frac is ignored for
- *               floating point types and color types.
+ *               number, where <tt>0 <= frac <= 31</tt>. @p frac is ignored for
+ *               floating-point types and color types.
+ * @see          GXBegin
+ * @see          GXSetVtxAttrFmt
+ * @see          GXSetVtxDesc
  */
-void GXSetVtxAttrFmt(u32 vtxfmt, u32 attr, s32 count, u32 type, u8 frac)
+void GXSetVtxAttrFmt(size_t vtxfmt, GXAttr attr, enum_t count, GXAttrType type,
+                     u8 frac)
 {
     s32* temp_r4 = &__GXContexts.main->x1C_data[vtxfmt];
     s32* temp_r8 = &__GXContexts.main->x3C_data[vtxfmt];
@@ -537,47 +537,51 @@ void __GXSetVAT(void)
 }
 
 /**
- * This function sets the array base pointer and stride for a single
- * attribute. The array base and stride are used to compute the address
- * of indexed attribute data using the equation:
+ * Sets the array base pointer and stride for a single attribute.
  *
+ * The array base and stride are used to compute the address of indexed
+ * attribute data using the equation:
+ *
+ * @code{c}
  *      attr_addr = base_ptr + attr_idx * stride
+ * @endcode
  *
  * When drawing a graphics primitive that has been enabled to use indexed
- * attributes (see GXSetVtxDesc), attr_idx is supplied in the vertex data.
+ * attributes (see #GXSetVtxDesc), @p attr_idx is supplied in the vertex data.
  * The format and size of the data in the array must also be described
- * using GXSetVtxAttrFmt.
+ * using #GXSetVtxAttrFmt.
  *
- * You can also index other data, such as matrices (see GXLoadPosMtxIndx,
- * GXLoadNrmMtxIndx3x3, and GXLoadTexMtxIndx), and light objects (see
- * GXLoadLightObjIndx). In the case of matrices and light objects, the
+ * You can also index other data, such as matrices (see #GXLoadPosMtxIndx,
+ * #GXLoadNrmMtxIndx3x3, and #GXLoadTexMtxIndx), and light objects (see
+ * #GXLoadLightObjIndx). In the case of matrices and light objects, the
  * size and format of the data to be loaded is implied by the function call.
  *
- * There is a base pointer, base_ptr, for each type of attribute as well as
+ * There is a base pointer, @p base_ptr, for each type of attribute as well as
  * for light data and matrices. Each attribute can be stored in its own
  * array for maximum data compression (i.e., removal of redundant attribute
  * data). The stride is in byte units and is the distance between
  * attributes in the array.
  *
  * Indexed data is loaded into a vertex cache in the graphics processor.
- * The vertex cache fetches 32B of data for each cache miss; therefore,
+ * The vertex cache fetches 32 bytes of data for each cache miss; therefore,
  * there is a small performance benefit to aligning attribute arrays to
- * 32B, and possibly for arranging vertex data so that it doesn't span
- * 32B boundaries. Conveniently enough, OSAlloc returns 32-byte aligned
- * pointers. For static data arrays, you can use the ATTRIBUTE_ALIGN(32)
- * macro (specific to the MetroWerks compiler) to align the base_ptr to
- * 32B, as in:
+ * 32 bytes, and possibly for arranging vertex data so that it doesn't span
+ * 32-byte boundaries. Conveniently enough, #OSAlloc returns 32-byte aligned
+ * pointers. For static data arrays, you can use the #ATTRIBUTE_ALIGN macro
+ * (specific to the MetroWerks compiler) to align the @p base_ptr to 32 bytes,
+ * as in:
  *
+ * @code{c}
  *      u32 mydata[] ATTRIBUTE_ALIGN(32) = {
  *          // initialize data
  *      };
+ * @endcode
  *
- * @param attr      Specifies the attribute array name. Accepted
- *                  values are members of the GXAttr enumeration.
+ * @param attr      The attribute array name.
  * @param base_ptr  Pointer to first element in attribute data array.
- * @param stride    Stride in bytes (B) between attribute data elements.
+ * @param stride    Stride in bytes between attribute data elements.
  */
-void GXSetArray(s32 attr, s32 base_ptr, u8 stride)
+void GXSetArray(GXAttr attr, s32 base_ptr, u8 stride)
 {
     s32 idx;
     s32 temp_r6;
@@ -606,16 +610,20 @@ void GXSetArray(s32 attr, s32 base_ptr, u8 stride)
 }
 
 /**
- * This function is called by GXInit and invalidates the vertex cache tags.
+ * Called by #GXInit and invalidates the vertex cache tags.
+ *
  * This function should be used whenever you relocate or modify data that
  * is read by, or may be cached by, the vertex cache. The invalidate is
  * very fast, taking only two Graphics Processor (GP) clock cycles to complete.
  *
  * The vertex cache is used to cache indexed attribute data. Any attribute
- * that is set to GX_INDEX8 or GX_INDEX16 in the current vertex descriptor
- * (see GXSetVtxDesc) is indexed. Direct data bypasses the vertex cache.
- * Direct data is any attribute that is set to GX_DIRECT in the current
+ * that is set to #GX_INDEX8 or #GX_INDEX16 in the current vertex descriptor
+ * (see #GXSetVtxDesc) is indexed. Direct data bypasses the vertex cache.
+ * Direct data is any attribute that is set to #GX_DIRECT in the current
  * vertex descriptor.
+ *
+ * @see GXInit
+ * @see GXSetVtxDesc
  */
 void GXInvalidateVtxCache(void)
 {
@@ -623,7 +631,6 @@ void GXInvalidateVtxCache(void)
 }
 
 #ifdef MWERKS_GEKKO
-
 // https://decomp.me/scratch/9TphI // 640 (96.44%)
 #pragma push
 asm void GXSetTexCoordGen2(u32, s32, u32, s32, s32, s32)
@@ -843,29 +850,30 @@ lbl_8033CB60:
 /* 8033CB74 00339754  4E 80 00 20 */	blr
 } // clang-format on
 #pragma pop
-
 #endif
 
 /**
- * This function sets the number of texture coordinates that are generated
- * and available for use in the Texture Environment (TEV) stages. Texture
- * coordinates are generated from input data as described by GXSetTexCoordGen.
- * The generated texture coordinates are linked to specific textures and
- * specific Texture Environment (TEV) stages using GXSetTevOrder.
+ * Sets the number of texture coordinates that are generated and available for
+ * use in the Texture Environment (TEV) stages.
+ *
+ * Texture coordinates are generated from input data as described by
+ * #GXSetTexCoordGen. The generated texture coordinates are linked to specific
+ * textures and specific Texture Environment (TEV) stages using #GXSetTevOrder.
  *
  * A consecutive number of texture coordinates may be generated, starting
- * at GX_TEXCOORD0. A maximum of 8 texture coordinates may be generated.
- * If nTexGens is set to 0, no texture coordinates will be generated. In
- * this case, at least one color channel must be output, see GXSetNumChans.
+ * at #GX_TEXCOORD0. A maximum of 8 texture coordinates may be generated.
+ * If @p num is set to 0, no texture coordinates will be generated. In
+ * this case, at least one color channel must be output, see #GXSetNumChans.
  *
- * @param nTexGens  Number of texture coordinates to generate.
- *                  Minimum value is 0, maximum value is 8.
+ * @param num Number of texture coordinates to generate. Minimum value is 0,
+ *            maximum value is 8.
+ * @see       GXSetNumChans
  */
-void GXSetNumTexGens(u8 nTexGens)
+void GXSetNumTexGens(u8 num)
 {
-    INSERT_FIELD(__GXContexts.main->x204, nTexGens, 4, 0);
+    INSERT_FIELD(__GXContexts.main->x204, num, 4, 0);
     WGPIPE.u8 = GX_LOAD_XF_REG;
     WGPIPE.u32 = 0x103F;
-    WGPIPE.u32 = nTexGens;
+    WGPIPE.u32 = num;
     __GXContexts.main->x4F0_flags |= 4;
 }
