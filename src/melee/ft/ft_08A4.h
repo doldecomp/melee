59edcf825124867d9a9a47fa00a04c25cf04869e
Robin Avery <robin@ribbanya.dev>
2023-05-27 07:57:39 -0400

    Match `ftCo_ShieldBreakStand`

--- src/melee/ft/ft_08A4.h
+++ src/melee/ft/ft_08A4.h
@@ -230,6 +230,6 @@
 /* 0C8B60 */ void ft_800C8B60(ftCo_Fighter* fp, int, s8);
 /* 09E714 */ void ft_8009E714(u8, s16, char*, u8, float, float, float, float,
                               float, float);
-/* 099010 */ /* static */ UNK_RET ftCo_80099010(UNK_PARAMS);
+/* 099010 */ void ftCo_80099010(ftCo_GObj* gobj);
 
 #endif
