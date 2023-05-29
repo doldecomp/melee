15ca224a7ce0bb345cf116395ff9dadf43efc5d5
Robin Avery <robin@ribbanya.dev>
2023-05-29 07:53:21 -0400

    Match `ftCo_CargoFall` (#898)

---
+++ src/melee/ft/chara/ftCommon/ftCo_CargoFall.c
@@ -0,0 +1,44 @@
+#include <platform.h>
+
+#include "ftCo_CargoFall.h"
+
+#include "ftCo_CargoLanding.h"
+
+#include "ft/fighter.h"
+#include "ft/ft_081B.h"
+#include "ft/ft_08A4.h"
+#include "ft/ftanim.h"
+#include "ft/ftcommon.h"
+
+void ftCo_8009BC58(ftCo_GObj* gobj)
+{
+    /// @todo Unused stack.
+#ifdef MUST_MATCH
+    u8 _[16] = { 0 };
+#endif
+    ftCo_Fighter* fp = gobj->user_data;
+    Fighter_ChangeMotionState(gobj, fp->x2CC->x4_motion_state + 6,
+                              Ft_MF_KeepFastFall, NULL, 0, 1, 0);
+    ftAnim_SetAnimRate(gobj, 0);
+    if (fp->ground_or_air == GA_Ground) {
+        ftCommon_8007D5D4(fp);
+    }
+    ftCo_8009C5A4(fp->x1A58_interactedFighter, ftCo_MS_ShoulderedWait);
+}
+
+void ftCo_CargoFall_Anim(ftCo_GObj* gobj) {}
+
+void ftCo_CargoFall_IASA(ftCo_GObj* gobj)
+{
+    RETURN_IF(ftCo_8009BF3C(gobj))
+}
+
+void ftCo_CargoFall_Phys(ftCo_GObj* gobj)
+{
+    ft_80084DB0(gobj);
+}
+
+void ftCo_CargoFall_Coll(ftCo_GObj* gobj)
+{
+    ft_80082C74(gobj, ftCo_8009BD4C);
+}
--- src/melee/ft/chara/ftCommon/ftCo_CargoJump.h
+++ src/melee/ft/chara/ftCommon/ftCo_CargoJump.h
@@ -5,7 +5,6 @@
 #include "ftCommon/forward.h"
 
 /* 09BB1C */ bool ftCo_8009BB1C(ftCo_GObj* gobj);
-/* 09BC58 */ void ftCo_8009BC58(ftCo_GObj* gobj);
 /* 09BBF0 */ void ftCo_CargoJump_Anim(ftCo_GObj* gobj);
 /* 09BBF4 */ void ftCo_CargoJump_IASA(ftCo_GObj* gobj);
 /* 09BC18 */ void ftCo_CargoJump_Phys(ftCo_GObj* gobj);
