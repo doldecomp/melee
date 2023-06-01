777c33eb2b58cb2a5fe686e62b487c40066e7001
Robin Avery <robin@ribbanya.dev>
2023-06-01 13:46:01 -0400

    Fix argument name

--- src/melee/ft/chara/ftCommon/ftCo_CaptureYoshi.h
+++ src/melee/ft/chara/ftCommon/ftCo_CaptureYoshi.h
@@ -4,7 +4,7 @@
 #include <platform.h>
 #include "ftCommon/forward.h"
 
-/* 09D6E8 */ void ftCo_800BBB8C(ftCo_GObj* gobj, ftCo_GObj* vic_gobj);
+/* 09D6E8 */ void ftCo_800BBB8C(ftCo_GObj* gobj, ftCo_GObj* attacker_gobj);
 /* 09D7D4 */ void ftCo_CaptureYoshi_Anim(ftCo_GObj* gobj);
 /* 09D7D8 */ void ftCo_CaptureYoshi_IASA(ftCo_GObj* gobj);
 /* 09D7DC */ void ftCo_CaptureYoshi_Phys(ftCo_GObj* gobj);
