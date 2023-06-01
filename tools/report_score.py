3c67568836972a389c0a3460036e6ceabed65e1a
Robin Avery <robin@ribbanya.dev>
2023-06-01 13:06:37 -0400

    Split `ftCo_0B3E`

--- tools/report_score.py
+++ tools/report_score.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 from typing import List, Tuple
 
+import humanfriendly
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
@@ -20,6 +21,7 @@ def get_functions_info(file_path):
                     functions.append(
                         {
                             "name": symbol.name,
+                            "size": symbol["st_size"],
                         }
                     )
 
@@ -37,9 +39,10 @@ def main(obj_path: str) -> None:
     wip_path = wip_root / obj_path
     obj_name = str(expected_path.relative_to(expected_root)).replace(".o", "")
 
-    entries: List[Tuple[str, int, int, float]] = []
+    entries: List[Tuple[str, int, int, int, float]] = []
 
     functions_info = get_functions_info(expected_path)
+    file_size = 0
     file_current_score = 0
     file_max_score = 0
     for function in functions_info:
@@ -65,7 +68,10 @@ def main(obj_path: str) -> None:
             max_score = int(result["max_score"])
 
             percent = calc_percent(current_score, max_score)
-            entries.append((function["name"], current_score, max_score, percent))
+            entries.append(
+                (function["name"], function["size"], current_score, max_score, percent)
+            )
+            file_size += function["size"]
             file_current_score += current_score
             file_max_score += max_score
         except Exception as e:
@@ -74,16 +80,29 @@ def main(obj_path: str) -> None:
             print(e, file=sys.stderr)
 
     # sort by percentage (ascending)
+    # then by size (descending)
     # then by max score (descending)
     # then by name (ascending)
-    entries.sort(key=lambda x: (x[3], -x[2], x[0]))
+    entries.sort(key=lambda x: (x[4], -x[1], -x[3], x[0]))
 
     print(f"## Report of `{obj_name}`")
-    print("Function|Score|Max|%\n-|-|-|-")
+    print("Function|Size|Score|Max|%\n-|-|-|-|-")
     file_percent = calc_percent(file_current_score, file_max_score)
-    print(f"**File**|`{file_current_score}`|`{file_max_score}`|`{file_percent:.2f}%`")
+    print(
+        f"**File**|"
+        f"`{humanfriendly.format_size(file_size)}`|"
+        f"`{humanfriendly.format_number(file_current_score)}`|"
+        f"`{humanfriendly.format_number(file_max_score)}`|"
+        f"`{file_percent:.2f}%`"
+    )
     for entry in entries:
-        print(f"`{entry[0]}`|`{entry[1]}`|`{entry[2]}`|`{entry[3]:.2f}%`")
+        print(
+            f"`{entry[0]}`|"
+            f"`{humanfriendly.format_size(entry[1])}`|"
+            f"`{humanfriendly.format_number(entry[2])}`|"
+            f"`{humanfriendly.format_number(entry[3])}`|"
+            f"`{entry[4]:.2f}%`"
+        )
 
 
 if __name__ == "__main__":
