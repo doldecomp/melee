9dbb0e0ef8f7b9986e11a0fa986f7b60da0a0d87
Robin Avery <robin@ribbanya.dev>
2023-06-04 19:35:42 -0400

    Match `ftCo_CaptureMewtwo` and `ftCo_ThrownMewtwo`
    Co-authored-by: vabold <86704247+vabold@users.noreply.github.com>

---
+++ src/melee/ft/chara/ftCommon/ftCo_CaptureMewtwo.c
@@ -0,0 +1,46 @@
+#include <platform.h>
+#include "ft/forward.h"
+#include "ftCommon/forward.h"
+
+#include "ftCo_CaptureMewtwo.h"
+
+#include "ftCo_ThrownMewtwo.h"
+
+#include "ft/fighter.h"
+#include "ft/ft_08A4.h"
+#include "ft/ftanim.h"
+#include "ft/ftcommon.h"
+
+static inline void doEnter(ftCo_GObj* gobj, ftCo_GObj* victim_gobj,
+                           FtMotionId msid, FtMotionId victim_msid)
+{
+    ftCo_Fighter* victim_fp = GET_FIGHTER(victim_gobj);
+    ftCo_Fighter* fp = GET_FIGHTER(gobj);
+    ftCommon_8007DB58(gobj);
+    ftCo_8009750C(gobj);
+    ftCo_800DD168(gobj);
+    fp->x1A5C = victim_gobj;
+    fp->victim_gobj = victim_gobj;
+    fp->x221B_b5 = false;
+    fp->x221B_b7 = false;
+    fp->facing_dir = victim_fp->facing_dir;
+    ftCo_800DB368(victim_fp, fp);
+    Fighter_ChangeMotionState(gobj, msid, Ft_MF_None, 0, 1, 0,
+                              fp->victim_gobj);
+    ftCommon_8007D5D4(fp);
+    ftCommon_8007E2F4(fp, 0x1FF);
+    ftAnim_8006EBA4(gobj);
+    ftCommon_8007E2FC(gobj);
+    ftCo_800BD0E8(gobj, victim_msid);
+}
+
+void ftCo_800BCF18(ftCo_GObj* gobj, ftCo_GObj* victim_gobj)
+{
+    doEnter(gobj, victim_gobj, ftCo_MS_CaptureMewtwo, ftCo_MS_ThrownMewtwo);
+}
+
+void ftCo_800BD000(ftCo_GObj* gobj, ftCo_GObj* victim_gobj)
+{
+    doEnter(gobj, victim_gobj, ftCo_MS_CaptureMewtwoAir,
+            ftCo_MS_ThrownMewtwoAir);
+}
---
+++ src/melee/ft/chara/ftCommon/ftCo_CaptureMewtwo.h
@@ -0,0 +1,10 @@
+#ifndef GALE01_0BCF18
+#define GALE01_0BCF18
+
+#include <platform.h>
+#include "ftCommon/forward.h"
+
+/* 0BCF18 */ void ftCo_800BCF18(ftCo_GObj* gobj, ftCo_GObj* victim_gobj);
+/* 0BD000 */ void ftCo_800BD000(ftCo_GObj* gobj, ftCo_GObj* victim_gobj);
+
+#endif
