45a6ffe8ff1a48dca83ee2ea29be5513a7b75a82
Will C <wcarter312@gmail.com>
2023-09-13 15:20:57 -0400

    Fixup functions, formatting, and remove .s file

--- src/melee/it/items/ithammer.c
+++ src/melee/it/items/ithammer.c
@@ -4,29 +4,76 @@
 #include <melee/it/it_266F.h>
 #include <melee/it/it_26B1.h>
 #include <melee/it/item.h>
+#include <melee/lb/lb_00B0.h>
 #include <melee/lb/lbrefract.h>
 #include <melee/lb/lbvector.h>
 #include <MSL/math.h>
 
+extern void it_8026D62C(Item_GObj*, HSD_GObjEvent);
+extern s32 it_8026DF34();
+extern void it_8026E15C(Item_GObj*, HSD_GObjEvent);
+extern void it_8026E8C4(Item_GObj*, HSD_GObjEvent, HSD_GObjEvent);
+extern HSD_JObj* it_80272CC0(Item_GObj*, int);
+extern void it_80299C48(HSD_GObj*, Vec3*, Vec3*, f32);
 
-static inline void thing(void* arg0) {
-    Item* it = GET_ITEM(arg0);
+ItemStateTable it_803F6150[] = { {
+                                     -1,
+                                     it_80293FF4,
+                                     it_80293FFC,
+                                     it_80294000,
+                                 },
+                                 {
+                                     -1,
+                                     it_80294080,
+                                     it_80294088,
+                                     it_802940B8,
+                                 },
+                                 {
+                                     0,
+                                     it_80294170,
+                                     NULL,
+                                     NULL,
+                                 },
+                                 {
+                                     1,
+                                     it_80294170,
+                                     NULL,
+                                     NULL,
+                                 },
+                                 {
+                                     -1,
+                                     it_80294080,
+                                     it_8029424C,
+                                     it_8029427C,
+                                 },
+                                 {
+                                     -1,
+                                     it_80294304,
+                                     it_8029430C,
+                                     it_80294310,
+                                 } };
+
+static inline void thing(void* gobj)
+{
+    Item* it = GET_ITEM(gobj);
     if (it->xD4C <= 0) {
-        it_80272A3C(it_80272CC0((Item_GObj* ) arg0, 3));
+        it_80272A3C(it_80272CC0((Item_GObj*) gobj, 3));
     }
 }
 
-void it_80293D94(Item_GObj* arg0) {
-    Item* item = GET_ITEM((HSD_GObj*)arg0);
+void it_80293D94(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM((HSD_GObj*) gobj);
     if (!item->xDC8_word.flags.xB && item->xDC8_word.flags.xA) {
-        it_8026BC14(arg0);
+        it_8026BC14(gobj);
     }
 }
 
-s32 it_80293DCC(Item_GObj* arg0) {
-    Item* item = GET_ITEM((HSD_GObj*)arg0);
+int it_80293DCC(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM((HSD_GObj*) gobj);
     Article* article = item->xC4_article_data;
-    ItemCommonData *temp_r31 = article->x4_specialAttributes;
+    ItemCommonData* temp_r31 = article->x4_specialAttributes;
     if (HSD_Randf() < temp_r31->x8 / 100.0f) {
         int rng = HSD_Randi(temp_r31->x4 - temp_r31->x0);
         return rng + temp_r31->x0;
@@ -34,7 +81,8 @@ s32 it_80293DCC(Item_GObj* arg0) {
     return 0;
 }
 
-void it_80293E34(Item_GObj* arg0) {
+void it_80293E34(Item_GObj* gobj)
+{
     Vec3 sp30;
     Vec3 sp24;
     Vec3 result;
@@ -42,12 +90,12 @@ void it_80293E34(Item_GObj* arg0) {
     s32 temp_r3;
     float asd;
 
-    temp_r31 = GET_ITEM((HSD_GObj*) arg0);
+    temp_r31 = GET_ITEM((HSD_GObj*) gobj);
     temp_r3 = temp_r31->xD4C;
     if (temp_r3 > 0) {
         temp_r31->xD4C = temp_r3 - 1;
-        lb_8000B1CC(it_80272CC0(arg0, 3), NULL, &sp30);
-        lb_8000B1CC(it_80272CC0(arg0, 1), NULL, &sp24);
+        lb_8000B1CC(it_80272CC0(gobj, 3), NULL, &sp30);
+        lb_8000B1CC(it_80272CC0(gobj, 1), NULL, &sp24);
         result.x = sp30.x - sp24.x;
         result.y = sp30.y - sp24.y;
         result.z = 0.0f;
@@ -65,118 +113,132 @@ void it_80293E34(Item_GObj* arg0) {
         result.x = result.x < 0.0f ? -result.x : result.x;
         it_80299C48(temp_r31->owner, &sp30, &result, asd);
     }
-    thing(arg0);
+    thing(gobj);
 }
 
-void it_80293F58(void* arg0) {
-    GET_ITEM(arg0)->xD4C = 1;
-    it_8029402C(arg0);
+void it_80293F58(HSD_GObj* gobj)
+{
+    GET_ITEM(gobj)->xD4C = 1;
+    it_8029402C(gobj);
 }
 
-void it_80293F84(Item_GObj* arg0) {
-    Item* temp_r4 = GET_ITEM((HSD_GObj*)arg0);
+void it_80293F84(Item_GObj* gobj)
+{
+    Item* temp_r4 = GET_ITEM((HSD_GObj*) gobj);
     temp_r4->x40_vel.z = 0.0f;
     temp_r4->x40_vel.y = 0.0f;
     temp_r4->x40_vel.x = 0.0f;
-    it_8026B390(arg0);
-    Item_80268E5C((HSD_GObj* ) arg0, 0, ITEM_ANIM_UPDATE);
-    thing(arg0);
+    it_8026B390(gobj);
+    Item_80268E5C((HSD_GObj*) gobj, 0, ITEM_ANIM_UPDATE);
+    thing(gobj);
 }
 
-int it_80293FF4(void) {
+bool it_80293FF4(HSD_GObj* gobj)
+{
     return 0;
 }
 
-void it_80293FFC(void) {
+void it_80293FFC(HSD_GObj* gobj) {}
 
-}
-
-HSD_GObj* it_80294000(void* arg0) {
-    it_8026D62C(arg0, it_8029402C);
+bool it_80294000(HSD_GObj* gobj)
+{
+    it_8026D62C(gobj, it_8029402C);
     return 0;
 }
 
-void it_8029402C(HSD_GObj* arg0) {
-    Item_80268E5C(arg0, 1, ITEM_ANIM_UPDATE);
-    thing(arg0);
+void it_8029402C(HSD_GObj* gobj)
+{
+    Item_80268E5C(gobj, 1, ITEM_ANIM_UPDATE);
+    thing(gobj);
 }
 
-int it_80294080(void) {
+int it_80294080(HSD_GObj* gobj)
+{
     return 0;
 }
 
-void it_80294088(void* arg0) {
-    Item* it = GET_ITEM(arg0);
+void it_80294088(HSD_GObj* gobj)
+{
+    Item* it = GET_ITEM(gobj);
     ItemAttr* temp_r4 = it->xCC_item_attr;
-    it_80272860(arg0, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
+    it_80272860(gobj, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
 }
 
-s32 it_802940B8(void* arg0) {
-    Item* it = GET_ITEM(arg0);
+int it_802940B8(HSD_GObj* gobj)
+{
+    Item* it = GET_ITEM(gobj);
     if (it->xD4C != 0) {
-        it_8026E15C(arg0, it_80293F84);
+        it_8026E15C(gobj, it_80293F84);
         return 0;
     }
     return it_8026DF34();
 }
 
-void it_802940FC(HSD_GObj* arg0) {
-    Item *item = GET_ITEM(arg0);
-    Item_80268E5C(arg0, 2, ITEM_ANIM_UPDATE);
-    item->entered_hitlag = (void*)(Item_GObj*) it_80293D94;
-    thing(arg0);
-    it_802725D4(arg0);
+void it_802940FC(HSD_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    Item_80268E5C(gobj, 2, ITEM_ANIM_UPDATE);
+    item->entered_hitlag = (void*) (Item_GObj*) it_80293D94;
+    thing(gobj);
+    it_802725D4(gobj);
 }
 
-int it_80294170(HSD_GObj* arg0) {
-    Item* it = GET_ITEM(arg0);
+int it_80294170(HSD_GObj* gobj)
+{
+    Item* it = GET_ITEM(gobj);
     if (it->xD4C == 0) {
-        it_802941A4(arg0);
+        it_802941A4(gobj);
     }
     return 0;
 }
 
-void it_802941A4(HSD_GObj* arg0) {
-    Item_80268E5C(arg0, 3, ITEM_ANIM_UPDATE);
-    thing(arg0);
+void it_802941A4(HSD_GObj* gobj)
+{
+    Item_80268E5C(gobj, 3, ITEM_ANIM_UPDATE);
+    thing(gobj);
 }
 
-void it_802941F8(HSD_GObj* arg0) {
-    Item_80268E5C(arg0, 1, 6);
-    thing(arg0);
+void it_802941F8(HSD_GObj* gobj)
+{
+    Item_80268E5C(gobj, 1, 6);
+    thing(gobj);
 }
 
-void it_8029424C(void* arg0) {
-    Item* it = GET_ITEM(arg0);
+void it_8029424C(HSD_GObj* gobj)
+{
+    Item* it = GET_ITEM(gobj);
     ItemAttr* temp_r4 = it->xCC_item_attr;
-    it_80272860(arg0, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
+    it_80272860(gobj, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
 }
 
-int it_8029427C(void) {
-    if (it_8026DA08() != 0) {
+bool it_8029427C(Item_GObj* gobj)
+{
+    if (it_8026DA08(gobj) != 0) {
         return 1;
     }
     return 0;
 }
 
-void it_802942B0(HSD_GObj* arg0) {
-    Item_80268E5C(arg0, 5, ITEM_ANIM_UPDATE);
-    thing(arg0);
+void it_802942B0(HSD_GObj* gobj)
+{
+    Item_80268E5C(gobj, 5, ITEM_ANIM_UPDATE);
+    thing(gobj);
 }
 
-int it_80294304(void) {
+int it_80294304(HSD_GObj* gobj)
+{
     return 0;
 }
 
-void it_8029430C(void) {
+void it_8029430C(HSD_GObj* gobj) {}
 
-}
-
-int it_80294310(HSD_GObj* arg0) {
-    it_8026E8C4(arg0, it_80293F84, it_8029402C);
+int it_80294310(HSD_GObj* gobj)
+{
+    it_8026E8C4(gobj, it_80293F84, it_8029402C);
     return 0;
 }
 
-void it_80294344(Item_GObj* arg0, HSD_GObj* arg1) {
-    it_8026B894(arg0, arg1);
+void it_80294344(Item_GObj* igobj, HSD_GObj* gobj)
+{
+    it_8026B894(igobj, gobj);
 }
--- src/melee/it/items/ithammer.h
+++ src/melee/it/items/ithammer.h
@@ -3,28 +3,28 @@
 
 #include <melee/it/types.h>
 
-
-extern void it_8026D62C(void*, void (*)(HSD_GObj*));                 /* extern */
-s32 it_8026DA08();                                  /* extern */
-s32 it_8026DF34();                                  /* extern */
-extern void it_8026E15C(void*, void (*)(Item_GObj*));                /* extern */
-extern void it_8026E8C4(void*, void (*)(Item_GObj*), void (*)(HSD_GObj*)); /* extern */
-extern void it_80272860(void*, f32, f32);                     /* extern */
-HSD_JObj* it_80272CC0(Item_GObj*, int);               /* extern */
-extern void it_80299C48(HSD_GObj*, Vec3*, Vec3*, f32);         /* extern */
-int it_80293FF4();                                    /* static */
-void it_80293FFC();                                 /* static */
-HSD_GObj* it_80294000();                                    /* static */
-void it_8029402C(HSD_GObj* arg0);                   /* static */
-int it_80294080();                                    /* static */
-void it_80294088(void* arg0);                       /* static */
-s32 it_802940B8(void* arg0);                        /* static */
-int it_80294170(HSD_GObj* arg0);                          /* static */
-void it_802941A4(HSD_GObj* arg0);                   /* static */
-void it_8029424C(void* arg0);                       /* static */
-int it_8029427C();                                    /* static */
-int it_80294304();                                    /* static */
-void it_8029430C();                                 /* static */
-int it_80294310();                                    /* static */
+void it_80293D94(Item_GObj* gobj);
+int it_80293DCC(Item_GObj* gobj);
+void it_80293E34(Item_GObj* gobj);
+void it_80293F58(HSD_GObj* gobj);
+void it_80293F84(Item_GObj* gobj);
+static bool it_80293FF4(HSD_GObj* gobj);
+static void it_80293FFC(HSD_GObj* gobj);
+static bool it_80294000(HSD_GObj* gobj);
+static void it_8029402C(HSD_GObj* gobj);
+static int it_80294080(HSD_GObj* gobj);
+static void it_80294088(HSD_GObj* gobj);
+static int it_802940B8(HSD_GObj* gobj);
+void it_802940FC(HSD_GObj* gobj);
+static int it_80294170(HSD_GObj* gobj);
+void it_802941F8(HSD_GObj* gobj);
+static void it_802941A4(HSD_GObj* gobj);
+static void it_8029424C(HSD_GObj* gobj);
+static bool it_8029427C(Item_GObj* gobj);
+void it_802942B0(HSD_GObj* gobj);
+static int it_80294304(HSD_GObj* gobj);
+static void it_8029430C(HSD_GObj* gobj);
+static int it_80294310(HSD_GObj* gobj);
+void it_80294344(Item_GObj* gobj, HSD_GObj* gobj2);
 
 #endif
