72057304b2559fbe48d14fc7961b066d8a2f0216
Will C <wcarter312@gmail.com>
2023-09-13 17:31:12 -0400

    Fix indent to spaces on obj_files.mk

--- obj_files.mk
+++ obj_files.mk
@@ -598,7 +598,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/it/items/itfflower.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itkinoko.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itdkinoko.s.o\
-	$(BUILD_DIR)/src/melee/it/items/ithammer.c.o\
+    $(BUILD_DIR)/src/melee/it/items/ithammer.c.o\
     $(BUILD_DIR)/asm/melee/it/items/itwstar.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itscball.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itrabbitc.s.o\
