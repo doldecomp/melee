d7698922283936bc9877fc6be0cf3a3d683f6508
Will C <wcarter312@gmail.com>
2023-09-26 11:17:59 -0400

    Match PObjReqAnimAllByFlags

--- src/sysdolphin/baselib/pobj.c
+++ src/sysdolphin/baselib/pobj.c
@@ -66,3 +66,27 @@ void HSD_PObjAddAnimAll(HSD_PObj* pobj, HSD_ShapeAnim* shapeanim)
         HSD_PObjAddAnim(po, sa);
     }
 }
+
+void HSD_PObjReqAnimByFlags(HSD_PObj* pobj, float startframe, u32 flags)
+{
+    if (pobj == NULL) {
+        return;
+    }
+
+    if (flags & POBJ_ANIM && pobj_type(pobj) == POBJ_SHAPEANIM) {
+        HSD_AObjReqAnim(pobj->u.x14_unk->aobj, startframe);
+    }
+}
+
+void HSD_PObjReqAnimAllByFlags(HSD_PObj* pobj, float startframe, u32 flags)
+{
+    HSD_PObj* pp;
+
+    if (pobj == NULL) {
+        return;
+    }
+
+    for (pp = pobj; pp; pp = pp->next) {
+        HSD_PObjReqAnimByFlags(pp, startframe, flags);
+    }
+}
