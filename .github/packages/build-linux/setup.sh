ea0067ae574563caecee927d7bed524f9decbe3d
Robin Avery <robin@ribbanya.dev>
2023-10-29 17:02:52 -0400

    Upload asm dump to GitHub Pages
    Also clean up the CI scripts a bit.

--- .github/packages/build-linux/setup.sh
+++ .github/packages/build-linux/setup.sh
@@ -5,14 +5,24 @@ apt update
 
 apt install -y --no-install-recommends \
     git \
+    doxygen \
+    python3-full \
+    python-is-python3 \
+    git \
     make \
     gcc \
     libc6-dev \
     python3-full \
     python-is-python3 \
     megatools \
-    libarchive-tools
+    libarchive-tools \
 
+# Create and update Python venv
+python -m venv --upgrade-deps /opt/venv
+. /opt/venv/bin/activate
+pip install --no-cache-dir -r /tmp/requirements.txt
+
+# Install melee compiler
 megadl --no-progress "$MELEE_COMPILERS_URL" --path - |
     bsdtar -xvf- -C /tmp
 mv /tmp/GC /tmp/mwcc_compiler
--- .github/packages/gen-pages/entrypoint.sh
+++ .github/packages/gen-pages/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 git clone /input /tmp/input
 cd /tmp/input
+ln -s /opt/mwcc_compiler tools/
 
 . /opt/venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
@@ -11,5 +12,16 @@ mkdir -p build/doxygen
 doxygen Doxyfile
 touch build/doxygen/html/.nojekyll
 python tools/m2ctx/m2ctx.py -pqr
+melee-mwcc build/ctx.c -v -o build/ctx.c.o
+make -j"$(nproc)" WINE="$WINE" GENERATE_MAP=1 MAX_ERRORS=1
+mkdir build/doxygen/html/dump
+cp build/ssbm.us.1.2/GALE01.map build/doxygen/html/dump/
+python tools/parse_map.py
+dadosod dol build/ssbm.us.1.2/main.dol \
+    -m build/map.csv \
+    -o build/doxygen/html/dump
+python tools/dump_tree.py \
+    build/doxygen/html/dump \
+    build/doxygen/html/dump/index.html
 cp build/ctx.html build/doxygen/html/
 cp -r build/doxygen/html/* /output
