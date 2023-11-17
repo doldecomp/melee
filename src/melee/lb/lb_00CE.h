824ae63a862456f115e3e8162bc7de300921efd8
Robin Avery <robin@ribbanya.dev>
2023-11-17 15:35:29 -0500

    Remove unnecessary include

--- src/melee/lb/lb_00CE.h
+++ src/melee/lb/lb_00CE.h
@@ -1,8 +1,6 @@
 #ifndef GALE01_00CE50
 #define GALE01_00CE50
 
-#include <placeholder.h>
-
 /* 00CEE0 */ float powf(float, float);
 /* 00D008 */ float lb_8000D008(float, float);
 /* 00D148 */ int lb_8000D148(float, float, float, float, float, float, float);
