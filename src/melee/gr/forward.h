addfd4e3feaf85a963317aad3dea1360ac20331a
Robin Avery <robin@ribbanya.dev>
2023-11-19 04:51:42 -0500

    Match `grTPeach`
    Co-authored-by: LakeinLove <110374467+LakeinLove@users.noreply.github.com>

--- src/melee/gr/forward.h
+++ src/melee/gr/forward.h
@@ -1,7 +1,33 @@
 #ifndef MELEE_GR_FORWARD_H
 #define MELEE_GR_FORWARD_H
 
-#include <platform.h>
+#include <baselib/forward.h>
+
+#ifdef M2CTX
+typedef struct Ground_GObj Ground_GObj;
+struct Ground_GObj {
+    /*  +0 */ u16 classifier;
+    /*  +2 */ u8 p_link;
+    /*  +3 */ u8 gx_link;
+    /*  +4 */ u8 p_priority;
+    /*  +5 */ u8 render_priority;
+    /*  +6 */ u8 obj_kind;
+    /*  +7 */ u8 user_data_kind;
+    /*  +8 */ Ground_GObj* next;
+    /*  +C */ Ground_GObj* prev;
+    /* +10 */ Ground_GObj* next_gx;
+    /* +14 */ Ground_GObj* prev_gx;
+    /* +18 */ HSD_GObjProc* proc;
+    /* +1C */ void (*rendered)(Ground_GObj* gobj, s32 code);
+    /* +20 */ u64 gxlink_prios;
+    /* +28 */ HSD_JObj* hsd_obj;
+    /* +2C */ Ground* user_data;
+    /* +30 */ void (*user_data_remove_func)(Ground* data);
+    /* +34 */ void* x34_unk;
+};
+#else
+typedef HSD_GObj Ground_GObj;
+#endif
 
 typedef struct grDynamicAttr_UnkStruct grDynamicAttr_UnkStruct;
 
--- tools/convert_data.py
+++ tools/convert_data.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Any, Callable, List, Match, TextIO, Tuple, Union
 
 root = Path(__file__).parent.parent
-options = {"input_glob": r"**/it/items/itsword.s"}
+options = {"input_glob": r"**/grtpeach.s"}
 
 default_options = {
     "zero": "float",  # How to treat zero-value dwords
