836857f74489540d0936be0ac37dcc902be9c52f
Robin Avery <robin@ribbanya.dev>
2023-11-19 22:38:47 -0500

    Match 97.90% of `ftCo_DamageSong` (1.04 KB)

--- obj_files.mk
+++ obj_files.mk
@@ -200,6 +200,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/ft/chara/ftCommon/ftCo_PassiveWall.s.o\
     $(BUILD_DIR)/src/melee/ft/chara/ftCommon/ftCo_PassiveCeil.c.o\
     $(BUILD_DIR)/src/melee/ft/ftafterimage.c.o\
+    $(BUILD_DIR)/src/melee/ft/chara/ftCommon/ftCo_DamageSong.c.o\
     $(BUILD_DIR)/asm/melee/ft/ft_0C31.s.o\
     $(BUILD_DIR)/asm/melee/ft/ftmetal.s.o\
     $(BUILD_DIR)/asm/melee/ft/ft_0C88.s.o\
