ce78d69a84cf1ea27d10a6db063dd5fa73a885ec
Robin Avery <robin@ribbanya.dev>
2023-11-21 13:45:26 -0500

    Match `ftCo_HammerFall` (692 bytes)

--- src/melee/ft/ft_0C31.h
+++ src/melee/ft/ft_0C31.h
@@ -6,14 +6,7 @@
 
 #include <placeholder.h>
 
-/* 0C5CD4 */ bool ftCo_800C5CD4(ftCo_GObj* gobj);
-/* 0C5D34 */ void ftCo_800C5D34(ftCo_GObj* gobj);
-/* 0C5DBC */ void ftCo_HammerFall_Anim(ftCo_GObj* gobj);
-/* 0C5DDC */ bool ftCo_800C5DDC(ftCo_GObj* gobj);
-/* 0C5E94 */ void ftCo_HammerFall_IASA(ftCo_GObj* gobj);
-/* 0C5F40 */ void ftCo_HammerFall_Phys(ftCo_GObj* gobj);
-/* 0C5F60 */ void ftCo_HammerFall_Coll(ftCo_GObj* gobj);
-/* 0C5F88 */ UNK_RET ftCo_800C5F88(UNK_PARAMS);
+/* 0C5F88 */ void ftCo_800C5F88(ftCo_GObj* gobj);
 /* 0C6020 */ void ftCo_HammerLanding_Anim(ftCo_GObj* gobj);
 /* 0C6084 */ void ftCo_HammerLanding_IASA(ftCo_GObj* gobj);
 /* 0C6088 */ void ftCo_HammerLanding_Phys(ftCo_GObj* gobj);
--- src/melee/pl/plbonuslib.h
+++ src/melee/pl/plbonuslib.h
@@ -5,5 +5,6 @@
 
 /* 03EC30 */ void pl_8003EC30(u8, int, int, float);
 /* 0403C0 */ void pl_800403C0(u8, int);
+/* 03FDF4 */ void pl_8003FDF4(u8, int);
 
 #endif
