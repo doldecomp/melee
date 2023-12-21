13b5fe0b430cb6512532f69ebe4ac213818b2556
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2023-12-20 22:14:00 -0800

    Match itmato, itmetamon (#1037)
    * Match itmato
    
    * Match itmetamon
    
    * Fix issues

--- obj_files.mk
+++ obj_files.mk
@@ -731,7 +731,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/it/items/itmarumine.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itlugia.s.o\
     $(BUILD_DIR)/asm/melee/it/items/ithouou.s.o\
-    $(BUILD_DIR)/asm/melee/it/items/itmetamon.s.o\
+    $(BUILD_DIR)/src/melee/it/items/itmetamon.c.o\
     $(BUILD_DIR)/asm/melee/it/items/itpippi.s.o\
     $(BUILD_DIR)/asm/melee/it/items/ittogepy.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itmew.s.o\
@@ -745,7 +745,7 @@ TEXT_O_FILES +=\
 
 TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/it/items/itoldkuri.s.o\
-    $(BUILD_DIR)/asm/melee/it/items/itmato.s.o\
+    $(BUILD_DIR)/src/melee/it/items/itmato.c.o\
     $(BUILD_DIR)/asm/melee/it/items/itheiho.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itlikelike.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itnokonoko.s.o\
