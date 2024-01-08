4969046280d2170fcc9b0e4f768113025f2e9cf3
Will C <wcarter312@gmail.com>
2024-01-08 13:16:08 -0500

    Match initial vi0601 functions

--- src/melee/vi/vi0601.c
+++ src/melee/vi/vi0601.c
@@ -1,10 +1,15 @@
-#include "vi/vi0402.h"
+#include "vi/vi0601.h"
 
 #include "vi/vi.h"
 
 #include <sysdolphin/baselib/jobj.h>
 
-void un_8031D6E4(HSD_GObj* gobj)
+void un_8031E6CC(void)
+{
+    un_8031CAAC();
+}
+
+void un_8031E6EC(HSD_GObj* gobj)
 {
     HSD_JObjAnimAll((HSD_JObj*) gobj->hsd_obj);
 }
---
+++ src/melee/vi/vi0601.h
@@ -0,0 +1,11 @@
+#ifndef _vi0601_h_
+#define _vi0601_h_
+
+#include <platform.h>
+
+#include <sysdolphin/baselib/gobj.h>
+
+void un_8031E6CC(void);
+void un_8031E6EC(HSD_GObj*);
+
+#endif
