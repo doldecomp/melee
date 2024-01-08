23b85fc835c9b3af52edc78220ca9dc58b0d8884
Will C <wcarter312@gmail.com>
2024-01-08 13:28:24 -0500

    Match initial vi1202

--- obj_files.mk
+++ obj_files.mk
@@ -813,6 +813,7 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/vi/vi1201v1.s.o\
     $(BUILD_DIR)/src/melee/vi/vi1201v2.c.o\
     $(BUILD_DIR)/asm/melee/vi/vi1201v2.s.o\
+    $(BUILD_DIR)/src/melee/vi/vi1202.c.o\
     $(BUILD_DIR)/asm/melee/vi/vi1202.s.o\
 
 TEXT_O_FILES +=\
