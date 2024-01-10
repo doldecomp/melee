01e8f07d6d9e75ca6d6ed0df7e3d710f306b9594
Will <wcarter312@gmail.com>
2024-01-10 13:10:25 -0500

    Ifmagnify initial match (#1053)
    * Match 802FB6E8
    
    * Bring ifMagnify_804A1DE0 into C
    
    * Fix function boundaries and labels in ifmagnify.s
    
    Remove comment

--- src/melee/if/ifmagnify.h
+++ src/melee/if/ifmagnify.h
@@ -3,7 +3,36 @@
 
 #include <platform.h>
 
-s32 ifMagnify_802FB6E8(u8);
-bool ifMagnify_802FC998(u8 ply_slot);
+#include <sysdolphin/baselib/gobj.h>
+#include <sysdolphin/baselib/jobj.h>
+#include <sysdolphin/baselib/tobj.h>
+
+typedef struct ifMagnify {
+    HSD_Joint* joint; // // ifMagnify_802fc3c0 accesses 0x804A1DE0 for a Joint
+    int x4;
+    int x8;
+    int xC;
+    int x10;
+
+    struct {
+        HSD_GObj* gobj; // ifMagnify_802fc750 accesses 0x804A1DF4 + slot * 0x10
+                        // for a GObj
+        HSD_TObj* tobj; // ifMagnify_802fc3c0 accesses 0x804A1DF8 + slot * 0x10
+                        // for a TObj
+        HSD_ImageDesc*
+            idesc; // ifMagnify_802FBBDC access 0x804A1DFC for an ImageDesc
+        struct {
+            u8 is_offscreen : 1;
+            u8 ignore_offscreen : 1;
+            u8 unk : 6;
+        } state;
+    } player[6];
+    u8 unk[0xF0 - 0x74];
+} ifMagnify;
+
+extern ifMagnify ifMagnify_804A1DE0;
+
+s32 ifMagnify_802FB6E8(s32);
+bool ifMagnify_802FC998(s32 ply_slot);
 
 #endif
