61a6bc31404e36bee963ecaa9ad896e9171d611e
Robin Avery <robin@ribbanya.dev>
2024-01-26 05:45:56 -0500

    Remove deprecated shell scripts (#1126)

--- tools/asmdiff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pushd "$(dirname "$(readlink -f $0)")/.." >/dev/null
-OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko"
-OPTIONS="--start-address=$(($1)) --stop-address=$(($1 + $2))"
-$OBJDUMP $OPTIONS baserom.dol > baserom.dump
-$OBJDUMP $OPTIONS build/ssbm.us.1.2/main.dol > main.dump
-diff -u --color=always baserom.dump main.dump
-popd >/dev/null
--- tools/objdiff-make.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-make GENERATE_MAP=1 MAX_ERRORS=1 WIP=1 "-j$(nproc)" WINE=wibo "$@"
