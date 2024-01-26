a49e809010c280143ff8191c61a86e608b3baf35
wyatt-avilla <wyattmurphy1@gmail.com>
2024-01-25 16:46:55 -0800

    cleaned up parameter names. followed function call examples from `itstar.c`. changed comments

--- src/melee/it/items/itspycloak.c
+++ src/melee/it/items/itspycloak.c
@@ -3,19 +3,19 @@
 #include <melee/it/it_26B1.h>
 #include <melee/it/item.h>
 
-int it_80276CB8(Item_GObj*);                        /* extern */
-void it_80295D04(HSD_GObj* gobj);                   /* extern */
+int it_80276CB8(Item_GObj*);
+void it_80295D04(HSD_GObj* gobj);
 const float it_804DCB70;
 
-void it_80295C48(Item_GObj* arg0) {
-    it_80295D04(arg0);
+void it_80295C48(Item_GObj* item_gobj) {
+    it_80295D04(item_gobj);
 }
 
-void it_80295C68(Item_GObj* arg0) {
-    Item* temp_r4 = arg0->user_data;
-    temp_r4->x40_vel.x = temp_r4->x40_vel.y = temp_r4->x40_vel.z = it_804DCB70;
-    it_8026B390(arg0);
-    Item_80268E5C(arg0, 0, ITEM_ANIM_UPDATE);
+void it_80295C68(Item_GObj* item_gobj) {
+    Item* item = GET_ITEM(item_gobj);
+    item->x40_vel.x = item->x40_vel.y = item->x40_vel.z = it_804DCB70;
+    it_8026B390(item_gobj);
+    Item_80268E5C(item_gobj, 0, ITEM_ANIM_UPDATE);
 }
 
 bool it_80295CB8(void) {
@@ -26,61 +26,61 @@ void it_80295CC0(void) {
 
 }
 
-bool it_80295CC4(Item_GObj* arg0) {
-    it_8026D62C(arg0, it_80295D04);
-    it_80276CB8(arg0);
+bool it_80295CC4(Item_GObj* item_gobj) {
+    it_8026D62C(item_gobj, it_80295D04);
+    it_80276CB8(item_gobj);
     return false;
 }
 
-void it_80295D04(HSD_GObj* arg0) {
-    Item_80268E5C(arg0, 1, ITEM_ANIM_UPDATE);
+void it_80295D04(HSD_GObj* gobj) {
+    Item_80268E5C(gobj, 1, ITEM_ANIM_UPDATE);
 }
 
 bool it_80295D2C(void) {
     return false;
 }
 
-void it_80295D34(Item_GObj* arg0) {
-    Item* item = GET_ITEM(arg0);
+void it_80295D34(Item_GObj* item_gobj) {
+    Item* item = GET_ITEM(item_gobj);
     ItemAttr* attr = item->xCC_item_attr;
 
-    it_80272860(arg0, attr->x10_fall_speed, attr->x14_fall_speed_max);
+    it_80272860(item_gobj, attr->x10_fall_speed, attr->x14_fall_speed_max);
 }
 
-bool it_80295D64(Item_GObj* arg0) {
-    it_8026E15C(arg0, it_80295C68);
+bool it_80295D64(Item_GObj* item_gobj) {
+    it_8026E15C(item_gobj, it_80295C68);
     return false;
 }
 
-void it_80295D90(Item_GObj* arg0) {
-    Item_80268E5C((HSD_GObj* ) arg0, 2, ITEM_ANIM_UPDATE);
+void it_80295D90(Item_GObj* item_gobj) {
+    Item_80268E5C((HSD_GObj* ) item_gobj, 2, ITEM_ANIM_UPDATE);
 }
 
 bool it_80295DB8(void) {
     return false;
 }
 
-void it_80295DC0(Item_GObj* arg0) {
-    Item_80268E5C((HSD_GObj* ) arg0, 1, 6);
+void it_80295DC0(Item_GObj* item_gobj) {
+    Item_80268E5C((HSD_GObj* ) item_gobj, 1, 6);
 }
 
 
-void it_80295DE8(Item_GObj* arg0) {
-    Item* item = GET_ITEM(arg0);
+void it_80295DE8(Item_GObj* item_gobj) {
+    Item* item = GET_ITEM(item_gobj);
     ItemAttr* attr = item->xCC_item_attr;
 
-    it_80272860(arg0, attr->x10_fall_speed, attr->x14_fall_speed_max);
+    it_80272860(item_gobj, attr->x10_fall_speed, attr->x14_fall_speed_max);
 }
 
-bool it_80295E18(Item_GObj* arg0) {
-    if (it_8026DA08(arg0)) {
+bool it_80295E18(Item_GObj* item_gobj) {
+    if (it_8026DA08(item_gobj)) {
         return true;
     }
     return false;
 }
 
-void it_80295E4C(Item_GObj* arg0) {
-    Item_80268E5C((HSD_GObj* ) arg0, 4, ITEM_ANIM_UPDATE);
+void it_80295E4C(Item_GObj* item_gobj) {
+    Item_80268E5C((HSD_GObj* ) item_gobj, 4, ITEM_ANIM_UPDATE);
 }
 
 bool it_80295E74(void) {
@@ -91,20 +91,20 @@ void it_80295E7C(void) {
 
 }
 
-bool it_80295E80(Item_GObj* arg0) {
-    it_8026E8C4(arg0, it_80295C68, it_80295D04);
+bool it_80295E80(Item_GObj* item_gobj) {
+    it_8026E8C4(item_gobj, it_80295C68, it_80295D04);
     return false;
 }
 
-void it_80295EB4(Item_GObj* arg0, Item_GObj* arg1) {
-    it_8026B894(arg0, (HSD_GObj* ) arg1);
+void it_80295EB4(Item_GObj* item_gobj, HSD_GObj* ref) {
+    it_8026B894(item_gobj, ref);
 }
 
 
-ItemStateTable it_803F6370[] = {
-    {   // maybe
-        0,
-        it_80295CC4,
+ItemStateTable it_803F6370[] = {      // Function pointers are an educated guess
+    {                                 // based on the table in itstar.c.
+        0,                            // Zeros were added to make the size of
+        it_80295CC4,                  // .data match in objdiff.
         it_80295D04,
         it_80276CB8,
     },
@@ -126,4 +126,4 @@ ItemStateTable it_803F6370[] = {
     0,
 };
 
-const float it_804DCB70 = 0x00000000;
+const float it_804DCB70 = 0x00000000; // initialized here to get it into .sdata2
