b30fbabf7e339aaeb9bdfdd9e2dadbc8ed054422
Will C <wcarter312@gmail.com>
2024-01-31 15:08:08 -0500

    Add proper type information to DevComRequest

--- src/sysdolphin/baselib/devcom.h
+++ src/sysdolphin/baselib/devcom.h
@@ -7,7 +7,7 @@
 #include "baselib/archive.h"
 
 s32 HSD_DevComIsBusy(s32 idx);
-void HSD_DevComRequest(int, int, unsigned long, int, int, int,
-                       HSD_DevComCallback, int);
+void HSD_DevComRequest(int file, unsigned long src, unsigned long dest, unsigned long size, int type, int pri,
+                       HSD_DevComCallback callback, void* args);
 
 #endif
