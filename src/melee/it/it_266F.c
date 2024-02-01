d3dc7df96eca1f22e2c81d820d3f4823e7c4b65a
Robin Avery <robin@ribbanya.dev>
2024-01-31 19:37:23 -0500

    Match `it_8026D9A0` (#1167)
    Co-authored-by: Walnut356 <39544927+Walnut356@users.noreply.github.com>
    Co-authored-by: ncough <nick@coughlin.au>

---
+++ src/melee/it/it_266F.c
@@ -0,0 +1,25 @@
+#include <platform.h>
+
+#include "inlines.h"
+#include "it_2725.h"
+#include "types.h"
+
+#include "mp/mpcoll.h"
+
+#include <baselib/gobj.h>
+
+bool it_8026D9A0(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    CollData* coll_data = &item->x378_itemColl;
+    it_80276214(gobj);
+    {
+        u8 _[4];
+        bool temp = mpColl_800477E0(coll_data);
+        item->pos = coll_data->x4_vec;
+        if (temp) {
+            item->xC30 = coll_data->floor.index;
+        }
+        return temp;
+    }
+}
