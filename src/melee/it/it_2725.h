d41d2b4d1789c8f3db44f45325832f04ef7ff6ed
Robin Avery <robin@ribbanya.dev>
2024-01-31 19:33:31 -0500

    Match `it_8026D9A0`
    Co-authored-by: Walnut356 <39544927+Walnut356@users.noreply.github.com>
    Co-authored-by: ncough <nick@coughlin.au>

--- src/melee/it/it_2725.h
+++ src/melee/it/it_2725.h
@@ -11,11 +11,12 @@
 /* 272CC0 */ HSD_JObj* it_80272CC0(Item_GObj*, enum_t);
 /* 273408 */ void it_80273408(Item_GObj* gobj);
 /* 274C88 */ void it_80274C88(Item_GObj* gobj);
-/* 274ECC */ void it_80274ECC(Item_GObj* gobj, bool arg1);
-/* 274F48 */ void it_80274F48(Item_GObj* gobj, int bone_attach_id,
-                              HSD_GObj* arg2, Fighter_Part arg3);
+/* 274ECC */ void it_80274ECC(Item_GObj* gobj, bool);
+/* 274F48 */ void it_80274F48(Item_GObj* gobj, int bone_attach_id, HSD_GObj*,
+                              Fighter_Part);
 /* 275070 */ void it_80275070(Item_GObj* gobj, int bone_attach_id);
 /* 2750F8 */ void it_802750F8(Item_GObj*);
+/* 276214 */ bool it_80276214(Item_GObj*);
 /* 276CB8 */ void it_80276CB8(Item_GObj* gobj);
 /* 27781C */ bool it_8027781C(Item_GObj*);
 /* 279FF8 */ void it_80279FF8(Item_GObj*);
