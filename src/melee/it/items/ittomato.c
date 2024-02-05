31b03184cd1fbd31e460acac927fb1dd1b2d3c5e
wyatt-avilla <wyattmurphy1@gmail.com>
2024-02-04 18:31:04 -0800

    argument naming cleanup

--- src/melee/it/items/ittomato.c
+++ src/melee/it/items/ittomato.c
@@ -67,11 +67,11 @@ HSD_GObj* it_802841B4(s32 arg0, Vec3* arg1, s32 arg2)
     return var_r3;
 }
 
-void it_8028428C(Item_GObj* arg0)
+void it_8028428C(Item_GObj* gobj)
 {
     Item* temp_r4;
 
-    temp_r4 = arg0->user_data;
+    temp_r4 = gobj->user_data;
     temp_r4->xDD4_itemVar.capsule.x0 =
         M2C_FIELD(temp_r4->xC4_article_data->x4_specialAttributes, s32*, 4);
 }
@@ -107,130 +107,130 @@ void it_80284324(Item_GObj* arg0)
     }
 }
 
-void it_80284358(Item_GObj* arg0)
+void it_80284358(Item_GObj* gobj)
 {
-    Item_80268E5C(arg0, 0, ITEM_ANIM_UPDATE);
+    Item_80268E5C(gobj, 0, ITEM_ANIM_UPDATE);
 }
 
-bool it_80284380(HSD_GObj* arg0)
-{
-    return false;
-}
-
-void it_80284388(Item_GObj* arg0)
-{
-    ItemAttr* temp_r4;
-
-    Item* item = GET_ITEM(arg0);
-    temp_r4 = item->xCC_item_attr;
-    it_80272860(arg0, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
-}
-
-bool it_802843B8(Item_GObj* arg0)
-{
-    it_8026E15C(arg0, it_802843E4);
-    return false;
-}
-
-void it_802843E4(HSD_GObj* arg0)
-{
-    void* temp_r6;
-
-    temp_r6 = arg0->user_data;
-    M2C_FIELD(temp_r6, f32*, 0x40) = it_804DC8B0;
-    M2C_FIELD(temp_r6, f32*, 0x44) = it_804DC8B0;
-    M2C_FIELD(temp_r6, f32*, 0x48) = it_804DC8B0;
-    Item_80268E5C(arg0, 1, ITEM_ANIM_UPDATE);
-}
-
-bool it_80284420(HSD_GObj* arg0)
+bool it_80284380(HSD_GObj* gobj)
 {
     return false;
 }
 
-void it_80284428(Item_GObj* arg0) {}
-
-bool it_8028442C(Item_GObj* arg0)
+void it_80284388(Item_GObj* gobj)
 {
-    it_8026D62C(arg0, it_80284458);
+    ItemAttr* attr;
+
+    Item* item = GET_ITEM(gobj);
+    attr = item->xCC_item_attr;
+    it_80272860(gobj, attr->x10_fall_speed, attr->x14_fall_speed_max);
+}
+
+bool it_802843B8(Item_GObj* gobj)
+{
+    it_8026E15C(gobj, it_802843E4);
     return false;
 }
 
-void it_80284458(HSD_GObj* arg0)
+void it_802843E4(HSD_GObj* gobj)
 {
-    Item_80268E5C(arg0, 2, ITEM_ANIM_UPDATE);
+    void* ip;
+
+    ip = gobj->user_data;
+    M2C_FIELD(ip, f32*, 0x40) = it_804DC8B0;
+    M2C_FIELD(ip, f32*, 0x44) = it_804DC8B0;
+    M2C_FIELD(ip, f32*, 0x48) = it_804DC8B0;
+    Item_80268E5C(gobj, 1, ITEM_ANIM_UPDATE);
 }
 
-bool it_80284480(Item_GObj* arg0)
-{
-    return false;
-}
-
-void it_80284488(Item_GObj* arg0)
-{
-    ItemAttr* temp_r4;
-
-    Item* item = GET_ITEM(arg0);
-    temp_r4 = item->xCC_item_attr;
-    it_80272860(arg0, temp_r4->x10_fall_speed, temp_r4->x14_fall_speed_max);
-}
-
-bool it_802844B8(Item_GObj* arg0)
-{
-    it_8026E15C(arg0, it_802843E4);
-    return false;
-}
-
-void it_802844E4(Item_GObj* arg0) // contains branch arrows
-{
-    HSD_JObj* temp_r3 = GET_JOBJ(arg0);
-    HSD_JObj* var_r0 = (!temp_r3) ? NULL : temp_r3->child;
-
-    HSD_JObjClearFlagsAll(var_r0, 0x10);
-    Item_80268E5C(arg0, 3, ITEM_ANIM_UPDATE);
-}
-
-bool it_80284540(Item_GObj* arg0)
+bool it_80284420(HSD_GObj* gobj)
 {
     return false;
 }
 
-void it_80284548(Item_GObj* arg0) {}
+void it_80284428(Item_GObj* gobj) {}
 
-void it_8028454C(Item_GObj* arg0) // contains branch arrows
+bool it_8028442C(Item_GObj* gobj)
 {
-    HSD_JObj* temp_r3 = GET_JOBJ(arg0);
-    HSD_JObj* var_r0 = (!temp_r3) ? NULL : temp_r3->child;
-
-    it_8026B390(arg0);
-    HSD_JObjClearFlagsAll(var_r0, 0x10);
-    Item_80268E5C(arg0, 4, 6);
+    it_8026D62C(gobj, it_80284458);
+    return false;
 }
 
-void it_802845BC(Item_GObj* arg0)
+void it_80284458(HSD_GObj* gobj)
 {
-    Item_80268E5C(arg0, 5, ITEM_ANIM_UPDATE);
+    Item_80268E5C(gobj, 2, ITEM_ANIM_UPDATE);
 }
 
-bool it_802845E4(Item_GObj* arg0)
+bool it_80284480(Item_GObj* gobj)
 {
     return false;
 }
 
-void it_802845EC(Item_GObj* arg0)
+void it_80284488(Item_GObj* gobj)
+{
+    ItemAttr* attr;
+
+    Item* item = GET_ITEM(gobj);
+    attr = item->xCC_item_attr;
+    it_80272860(gobj, attr->x10_fall_speed, attr->x14_fall_speed_max);
+}
+
+bool it_802844B8(Item_GObj* gobj)
+{
+    it_8026E15C(gobj, it_802843E4);
+    return false;
+}
+
+void it_802844E4(Item_GObj* gobj) // contains branch arrows
+{
+    HSD_JObj* jobj = GET_JOBJ(gobj);
+    HSD_JObj* jobj2 = (!jobj) ? NULL : jobj->child;
+
+    HSD_JObjClearFlagsAll(jobj2, 0x10);
+    Item_80268E5C(gobj, 3, ITEM_ANIM_UPDATE);
+}
+
+bool it_80284540(Item_GObj* gobj)
+{
+    return false;
+}
+
+void it_80284548(Item_GObj* gobj) {}
+
+void it_8028454C(Item_GObj* gobj) // contains branch arrows
+{
+    HSD_JObj* jobj = GET_JOBJ(gobj);
+    HSD_JObj* jobj2 = (!jobj) ? NULL : jobj->child;
+
+    it_8026B390(gobj);
+    HSD_JObjClearFlagsAll(jobj2, 0x10);
+    Item_80268E5C(gobj, 4, 6);
+}
+
+void it_802845BC(Item_GObj* gobj)
+{
+    Item_80268E5C(gobj, 5, ITEM_ANIM_UPDATE);
+}
+
+bool it_802845E4(Item_GObj* gobj)
+{
+    return false;
+}
+
+void it_802845EC(Item_GObj* gobj)
 {
     return;
 }
 
-bool it_802845F0(Item_GObj* arg0)
+bool it_802845F0(Item_GObj* gobj)
 {
-    it_8026E8C4(arg0, it_802843E4, it_80284458);
+    it_8026E8C4(gobj, it_802843E4, it_80284458);
     return false;
 }
 
-void it_80284624(Item_GObj* arg0, Item_GObj* arg1)
+void it_80284624(Item_GObj* gobj, Item_GObj* gobj_ref)
 {
-    it_8026B894(arg0, arg1);
+    it_8026B894(gobj, gobj_ref);
 }
 
 ItemStateTable it_803F5740[] = {
