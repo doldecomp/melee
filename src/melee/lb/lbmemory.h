b197a41e3516ee78072407fe1f4b9118222b8b14
Robin Avery <robin@ribbanya.dev>
2024-02-12 08:38:46 -0500

    Match 9.26% of `lbheap` (2.19 KB) (#1203)
    Co-authored-by: Nagaa Dhaba <30734056+seizefire@users.noreply.github.com>

--- src/melee/lb/lbmemory.h
+++ src/melee/lb/lbmemory.h
@@ -2,7 +2,9 @@
 #define GALE01_014E24
 
 #include <platform.h>
+#include <baselib/forward.h>
 
+/* 014FC8 */ HSD_Archive* lbMemory_80014FC8(s32, s32);
 /* 0154BC */ void lbMemory_800154BC(uintptr_t*, uintptr_t*);
 /* 01564C */ void lbMemory_8001564C(void);
 
