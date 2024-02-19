41d1c65bc54c6a8a209fa0cc55a9e4f2785fa4b6
Will C <wcarter312@gmail.com>
2024-02-19 07:20:39 -0500

    Match it_802ADC54

--- src/melee/it/items/itkirbyhammer.c
+++ src/melee/it/items/itkirbyhammer.c
@@ -29,6 +29,49 @@ void it_802ADC34(Item_GObj* gobj)
     Item_8026A8EC(gobj);
 }
 
+static void setupHammerParticles(HSD_GObj* parent, Item_GObj* item_gobj,
+                                 u8 sfx, u32 vars)
+{
+    HSD_JObj* jobj;
+    u32 particle_id;
+    Item* it = GET_ITEM((HSD_GObj*) item_gobj);
+
+    it->xDD4_itemVar.kirbyhammer.x0 = vars;
+    db_80225DD8((HSD_GObj*) item_gobj, parent);
+    Item_8026AB54((HSD_GObj*) item_gobj, parent, sfx);
+    if (((Item*) item_gobj->user_data)->xDD4_itemVar.kirbyhammer.x0 == 1) {
+        particle_id = 1177;
+    } else {
+        particle_id = 1176;
+    }
+    jobj = it_80272C90(item_gobj);
+    efAsync_Spawn((HSD_GObj*) item_gobj, &((Item*) item_gobj->user_data)->xBC0,
+                  0, particle_id, jobj);
+}
+
+Item_GObj* it_802ADC54(HSD_GObj* parent, Vec3* pos, u8 sfx, u32 vars, f32 dir)
+{
+    Item_GObj* item_gobj;
+    SpawnItem spawn;
+    PAD_STACK(12);
+
+    spawn.kind = It_Kind_Kirby_Hammer;
+    spawn.prev_pos = *pos;
+    it_8026BB68((Item_GObj*) parent, &spawn.pos);
+    spawn.facing_dir = dir;
+    spawn.x3C_damage = 0;
+    spawn.vel.x = spawn.vel.y = spawn.vel.z = 0.0F;
+    spawn.x0_parent_gobj = parent;
+    spawn.x4_parent_gobj2 = spawn.x0_parent_gobj;
+    spawn.x44_flag.bits.b0 = true;
+    spawn.x40 = 0;
+
+    item_gobj = Item_80268B18(&spawn);
+    if (item_gobj != NULL) {
+        setupHammerParticles(parent, item_gobj, sfx, vars);
+    }
+    return item_gobj;
+}
 
 void it_802ADD88(Item_GObj* gobj)
 {
