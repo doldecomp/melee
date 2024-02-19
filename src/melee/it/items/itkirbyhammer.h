d828aef03559a6030d7aa584b47c67684538de40
Will C <wcarter312@gmail.com>
2024-02-19 07:33:34 -0500

    Add func proto, remove usage of forward, detete .s file, use .c for make compilation

--- src/melee/it/items/itkirbyhammer.h
+++ src/melee/it/items/itkirbyhammer.h
@@ -1,12 +1,13 @@
 #ifndef GALE01_2ADC04
 #define GALE01_2ADC04
 
-#include "it/forward.h"
+#include <platform.h>
 
 #include "it/types.h"
 
 void it_802ADC04(Item_GObj* gobj);
 void it_802ADC34(Item_GObj* gobj);
+Item_GObj* it_802ADC54(HSD_GObj* parent, Vec3* pos, u8 sfx, u32 vars, f32 dir);
 void it_802ADD88(Item_GObj* gobj);
 void it_802ADDB0(Item_GObj* gobj, HSD_GObj* ref_gobj);
 extern ItemStateTable it_803F6C98[];
