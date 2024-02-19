a238b219505a0cf9b698959a2c91fd36dc141edc
Will C <wcarter312@gmail.com>
2024-02-19 05:20:10 -0500

    Match some very easy parts of itzrshell

--- src/melee/it/items/itzrshell.c
+++ src/melee/it/items/itzrshell.c
@@ -1,1 +1,71 @@
+#include "itzrshell.h"
 
+#include "itzgshell.h"
+
+#include "gr/grzakogenerator.h"
+#include "it/it_26B1.h"
+
+void it_802E02E8(Item_GObj* gobj)
+{
+    it_802DE040(gobj);
+}
+
+void it_802E0308(Item_GObj* gobj)
+{
+    it_802DE434(gobj);
+}
+
+void it_802E0328(Item_GObj* gobj)
+{
+    it_802DE4FC(gobj);
+}
+
+void it_802E0348(Item_GObj* gobj)
+{
+    it_802DE648(gobj);
+}
+
+void it_802E0368(Item_GObj* gobj)
+{
+    it_802DEEF4(gobj);
+}
+
+void it_802E0388(Item_GObj* gobj)
+{
+    grZakoGenerator_801CACB8((Ground_GObj*) gobj);
+}
+
+void it_802E03A8(Item_GObj* gobj)
+{
+    it_802DFC34(gobj);
+}
+
+void it_802E03C8(Item_GObj* gobj)
+{
+    it_802DFC58(gobj);
+}
+
+void it_802E03E8(Item_GObj* gobj)
+{
+    it_802DFC90(gobj);
+}
+
+void it_802E0408(Item_GObj* gobj)
+{
+    it_802E0408(gobj);
+}
+
+void it_802E0428(Item_GObj* gobj)
+{
+    it_802DFD60(gobj);
+}
+
+void it_802E0448(Item_GObj* gobj)
+{
+    it_802DFE40(gobj);
+}
+
+void it_802E0468(Item_GObj* gobj, HSD_GObj* ref_gobj)
+{
+    it_8026B894(gobj, ref_gobj);
+}
