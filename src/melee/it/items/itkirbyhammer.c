5638da38b773195c9025c5cb9486de6aedc7f819
Will C <wcarter312@gmail.com>
2024-02-19 05:35:19 -0500

    Match it_802ADC04

--- src/melee/it/items/itkirbyhammer.c
+++ src/melee/it/items/itkirbyhammer.c
@@ -1,1 +1,14 @@
+#include "itkirbyhammer.h"
 
+#include "ft/chara/ftKirby/ftKb_Init.h"
+#include "it/inlines.h"
+
+void it_802ADC04(Item_GObj* gobj)
+{
+    Item* it = GET_ITEM((HSD_GObj*) gobj);
+
+    if (it->owner == NULL) {
+        return;
+    }
+    ftKb_SpecialAirLw_800F539C((ftKb_GObj*) it->owner);
+}
