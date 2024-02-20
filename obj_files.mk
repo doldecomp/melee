76e9736fcb2f304eb44e151f290dd20af445852b
Will <wcarter312@gmail.com>
2024-02-20 04:05:28 -0500

    Match itraikou and itsuikun (#1264)
    * Match itraikou
    
    * Match itsuikun

--- obj_files.mk
+++ obj_files.mk
@@ -681,8 +681,8 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/melee/it/items/ithassam.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itunknown.s.o\
     $(BUILD_DIR)/src/melee/it/items/itentei.c.o\
-    $(BUILD_DIR)/asm/melee/it/items/itraikou.s.o\
-    $(BUILD_DIR)/asm/melee/it/items/itsuikun.s.o\
+    $(BUILD_DIR)/src/melee/it/items/itraikou.c.o\
+    $(BUILD_DIR)/src/melee/it/items/itsuikun.c.o\
     $(BUILD_DIR)/asm/melee/it/items/itkireihana.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itmarumine.s.o\
     $(BUILD_DIR)/asm/melee/it/items/itlugia.s.o\
--- src/melee/it/items/itraikou.h
+++ src/melee/it/items/itraikou.h
@@ -1,12 +1,24 @@
 #ifndef GALE01_2CF814
 #define GALE01_2CF814
 
-#include "it/forward.h"
+#include <platform.h>
 
+#include "it/item.h"
 #include "it/items/types.h"
 
-/* 2CF814 */ void it_802CF814(Item_GObj*);
-/* 2CF884 */ void it_802CF884(Item_GObj*, Item_GObj*);
-/* 3F7DE8 */ extern ItemStateTable it_803F7DE8[];
+void it_802CF814(Item_GObj*);
+void it_802CF880(Item_GObj*);
+void it_802CF884(Item_GObj*, Item_GObj*);
+void it_802CF8A4(Item_GObj*);
+void it_802CF908(Item_GObj*);
+bool it_802CF978(Item_GObj*);
+void it_802CFA74(Item_GObj*);
+bool it_802CFAB0(Item_GObj*);
+void it_802CFAFC(Item_GObj*);
+void it_802CFB78(Item_GObj*);
+bool it_802CFBDC(Item_GObj*);
+void it_802CFC00(Item_GObj*);
+bool it_802CFC20(Item_GObj*);
+extern ItemStateTable it_803F7DE8[];
 
 #endif
