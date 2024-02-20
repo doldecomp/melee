4ec2ebf2b94d49612e752838e4227e537b3f2ce7
Will C <wcarter312@gmail.com>
2024-02-20 13:39:43 -0500

    Match most of itmariofireball

--- src/melee/it/items/itmariofireball.c
+++ src/melee/it/items/itmariofireball.c
@@ -11,10 +11,11 @@
 
 #include <common_structs.h>
 #include <math.h>
+#include <math_ppc.h>
 #include <dolphin/mtx/vec.h>
 #include <baselib/gobj.h>
 
-void it_8029B6F8(Item_GObj* gobj, Vec3* pos, ItemKind kind, float facing_dir)
+void it_8029B6F8(Item_GObj* gobj, Vec3* pos, ItemKind kind, f32 facing_dir)
 {
     Item_GObj* spawned_gobj;
 
@@ -35,4 +36,72 @@ void it_8029B6F8(Item_GObj* gobj, Vec3* pos, ItemKind kind, float facing_dir)
     it_802C027C(spawned_gobj);
     db_80225DD8(spawned_gobj, gobj);
     it_802750F8(spawned_gobj);
+}
+
+bool it_8029B868(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    --item->xD44_lifeTimer;
+    return item->xD44_lifeTimer <= 0 ? true : false;
+}
+
+static double calc_dist_2d_accurate(Vec3* v)
+{
+    return sqrtf_accurate(VEC2_SQ_LEN(*v));
+}
+
+bool it_8029B8EC(Item_GObj* gobj)
+{
+    it_8026D9A0(gobj);
+    if (it_8027781C(gobj)) {
+        Item* it = GET_ITEM(gobj);
+        HSD_JObj* jobj = GET_JOBJ(gobj);
+        itUnkAttributes* attrs = it->xC4_article_data->x4_specialAttributes;
+        if (calc_dist_2d_accurate(&it->x40_vel) < attrs->x10) {
+            return true;
+        }
+        if (it->kind == It_Kind_Mario_Fire) {
+            Item_8026AE84(it, ~0xFFFD40C6, 0x7f,
+                          0x40); // What the fuck is this ID?
+            efAsync_Spawn(gobj, &(GET_ITEM(gobj)->xBC0), 1, 1147, jobj);
+        } else {
+            efAsync_Spawn(gobj, &(GET_ITEM(gobj)->xBC0), 1, 1184, jobj);
+        }
+    }
+    return false;
+}
+
+bool it_8029BA38(Item_GObj* gobj)
+{
+    return true;
+}
+
+bool it_8029BA40(Item_GObj* gobj)
+{
+    return it_80273030(gobj);
+}
+
+bool it_8029BA60(Item_GObj* gobj)
+{
+    return true;
+}
+
+bool it_8029BA68(Item_GObj* gobj)
+{
+    return true;
+}
+
+bool it_8029BA70(Item_GObj* gobj)
+{
+    return true;
+}
+
+bool it_8029BA78(Item_GObj* gobj)
+{
+    return itColl_BounceOffShield(gobj);
+}
+
+void it_8029BA98(Item_GObj* gobj, HSD_GObj* referenced_gobj)
+{
+    it_8026B894(gobj, referenced_gobj);
 }
