5ab8b21d6071d84a5f58e442d79745e67383c5bb
Robin Avery <robin@ribbanya.dev>
2024-02-19 23:03:15 -0500

    Bring progress site back from the dead
    Co-authored-by: FluentCoding <33455186+FluentCoding@users.noreply.github.com>

---
+++ tools/progress-site/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
