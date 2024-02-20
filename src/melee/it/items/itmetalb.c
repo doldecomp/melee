94dcc376b7be8dfafa01bf6400c646a9dd35a6a7
Will C <wcarter312@gmail.com>
2024-02-20 00:46:26 -0500

    Match it_802953C8

--- src/melee/it/items/itmetalb.c
+++ src/melee/it/items/itmetalb.c
@@ -1,1 +1,11 @@
+#include "itmetalb.h"
 
+#include "it/inlines.h"
+#include "it/item.h"
+
+void it_802953C8(Item_GObj* gobj)
+{
+    Item* it = GET_ITEM((HSD_GObj*) gobj);
+    it->xDCE_flag.bits.b7 = 0;
+    it_80295498(gobj, it);
+}
--- src/melee/it/items/itmetalb.h
+++ src/melee/it/items/itmetalb.h
@@ -3,15 +3,19 @@
 
 #include <platform.h>
 #include "it/forward.h"
+#include <dolphin/mtx/forward.h>
+#include <baselib/forward.h>
 
 #include "it/items/types.h"
 
-/* 2953C8 */ void it_802953C8(Item_GObj*);
-/* 295524 */ void it_80295524(Item_GObj*);
-/* 295554 */ void it_80295554(Item_GObj*);
-/* 2955E0 */ bool it_802955E0(Item_GObj*);
-/* 295684 */ void it_80295684(Item_GObj*);
-/* 2956EC */ void it_802956EC(Item_GObj*, Item_GObj*);
-/* 3F62C0 */ extern ItemStateTable it_803F62C0[];
+void it_802953C8(Item_GObj*);
+void it_802953FC(Item_GObj*, Vec3*);
+void it_80295498(Item_GObj*, Item*);
+void it_80295524(Item_GObj*);
+void it_80295554(Item_GObj*);
+bool it_802955E0(Item_GObj*);
+void it_80295684(Item_GObj*);
+void it_802956EC(Item_GObj*, Item_GObj*);
+extern ItemStateTable it_803F62C0[];
 
 #endif
