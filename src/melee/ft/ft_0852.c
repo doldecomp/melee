941cd9a67e8500f709e9240a81975f1fd076e920
Robin Avery <robin@ribbanya.dev>
2024-02-22 10:01:24 -0500

    Match `ft_8008521C` (148 bytes) (#1305)
    Co-authored-by: BR- <716157+BR-@users.noreply.github.com>

--- src/melee/ft/ft_0852.c
+++ src/melee/ft/ft_0852.c
@@ -1,1 +1,18 @@
+#include "ft/inlines.h"
+#include "ft/types.h"
 
+#include <placeholder.h>
+#include <baselib/gobj.h>
+#include <baselib/jobj.h>
+
+void ft_8008521C(HSD_GObj* gobj)
+{
+    Fighter* fp = GET_FIGHTER(gobj);
+    HSD_JObj* jobj = GET_JOBJ(gobj);
+    Vec3 pos;
+
+    HSD_JObjGetTranslation(jobj, &pos);
+    fp->self_vel.x = pos.x - fp->cur_pos.x;
+    fp->self_vel.y = pos.y - fp->cur_pos.y;
+    fp->self_vel.z = pos.z - fp->cur_pos.z;
+}
