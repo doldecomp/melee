31065642012b563445a4391f49629f0e9b4718cb
Robin Avery <robin@ribbanya.dev>
2024-02-22 22:48:29 -0500

    Match `ft_8008A2BC` (104 bytes)
    Co-authored-by: EricK928 <16601238+EricK928@users.noreply.github.com>

--- src/melee/ft/ft_0892.c
+++ src/melee/ft/ft_0892.c
@@ -1,1 +1,18 @@
+#include "ft/ftlib.h"
+#include "ftCrazyHand/ftCh_Init.h"
+#include "ftMasterHand/ftMh_Wait1_2.h"
 
+#include <baselib/gobj.h>
+
+/* 08A348 */ static void ft_8008A348(HSD_GObj*, float);
+
+void ft_8008A2BC(HSD_GObj* gobj)
+{
+    if (ftLib_800872A4(gobj) == FTKIND_MASTERH) {
+        ftMh_MS_389_80151018(gobj);
+    } else if (ftLib_800872A4(gobj) == FTKIND_CREZYH) {
+        ftCh_GrabUnk1_8015BC88(gobj);
+    } else {
+        ft_8008A348(gobj, 0);
+    }
+}
