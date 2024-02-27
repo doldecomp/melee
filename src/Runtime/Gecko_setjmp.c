2dafc27264e6a0ca794cfceb08719c5c779ce2c1
Will C <wcarter312@gmail.com>
2024-02-27 11:19:33 -0500

    Match Gecko_setjmp

--- src/Runtime/Gecko_setjmp.c
+++ src/Runtime/Gecko_setjmp.c
@@ -2,6 +2,74 @@
 
 #include <Gecko_setjmp.h>
 
-/// #__setjmp
+asm int __setjmp(register __jmp_buf* env)
+{ // clang-format off
+    nofralloc
+    mflr	r5
+    mfcr	r6
+    stw		r5,env->pc		//	save PC (LR)
+    stw		r6,env->cr		//	save CR
+    stw		SP,env->sp		//	save SP
+    stw		RTOC,env->rtoc	//	save #RTOC
+    stmw	r13,env->gprs	//	save R13-R31
+    mffs	fp0
+    stfd	fp14,env->fp14	//	save FP14-FP31
+    stfd	fp15,env->fp15
+    stfd	fp16,env->fp16
+    stfd	fp17,env->fp17
+    stfd	fp18,env->fp18
+    stfd	fp19,env->fp19
+    stfd	fp20,env->fp20
+    stfd	fp21,env->fp21
+    stfd	fp22,env->fp22
+    stfd	fp23,env->fp23
+    stfd	fp24,env->fp24
+    stfd	fp25,env->fp25
+    stfd	fp26,env->fp26
+    stfd	fp27,env->fp27
+    stfd	fp28,env->fp28
+    stfd	fp29,env->fp29
+    stfd	fp30,env->fp30
+    stfd	fp31,env->fp31
+    stfd	fp0,env->fpscr	//	save FPSCR
+    li		r3,0
+    blr
+} // clang-format on
 
 /// #longjmp
+asm void longjmp(register __jmp_buf* env, register int val)
+{ // clang-format off
+    nofralloc
+    lwz		r5,env->pc
+    lwz		r6,env->cr
+    mtlr	r5				//	restore PC (LR)
+    mtcrf	255,r6			//	restore CR
+    lwz		SP,env->sp		//	restore SP
+    lwz		RTOC,env->rtoc	//	restore RTOC
+    lmw		r13,env->gprs	//	restore R13-R31
+    lfd		fp14,env->fp14	//	restore FP14-FP31
+    lfd		fp15,env->fp15
+    lfd		fp16,env->fp16
+    lfd		fp17,env->fp17
+    lfd		fp18,env->fp18
+    lfd		fp19,env->fp19
+    lfd		fp20,env->fp20
+    lfd		fp21,env->fp21
+    lfd		fp22,env->fp22
+    lfd		fp23,env->fp23
+    lfd		fp24,env->fp24
+    lfd		fp25,env->fp25
+    lfd		fp26,env->fp26
+    lfd		fp27,env->fp27
+    lfd		fp28,env->fp28
+    lfd		fp29,env->fp29
+    lfd		fp30,env->fp30
+    lfd		fp0,env->fpscr
+    lfd		fp31,env->fp31
+    cmpwi	val,0
+    mr		r3,val
+    mtfsf	255,fp0			//	restore FPSCR
+    bnelr					//	return 'val'
+    li		r3,1			//	return 1
+    blr
+} // clang-format on
--- src/Runtime/Gecko_setjmp.h
+++ src/Runtime/Gecko_setjmp.h
@@ -5,7 +5,35 @@
 
 #include <placeholder.h>
 
-int __setjmp(void* env, UNK_T, UNK_T);
-void longjmp(void* env, int val);
+typedef struct __jmp_buf {
+    unsigned long pc;       /*	0: saved PC			*/
+    unsigned long cr;       /*	4: saved CR			*/
+    unsigned long sp;       /*  8: saved SP			*/
+    unsigned long rtoc;     /* 12: saved RTOC		*/
+    unsigned long reserved; /* 16: not used			*/
+    unsigned long gprs[19]; /* 20: saved R13-R31	*/
+    double fp14;            /* 96: saved FP14-FP31	 */
+    double fp15;
+    double fp16;
+    double fp17;
+    double fp18;
+    double fp19;
+    double fp20;
+    double fp21;
+    double fp22;
+    double fp23;
+    double fp24;
+    double fp25;
+    double fp26;
+    double fp27;
+    double fp28;
+    double fp29;
+    double fp30;
+    double fp31;
+    double fpscr; /* 240: saved FPSCR		*/
+} __jmp_buf;
+
+int __setjmp(register __jmp_buf*);
+void longjmp(register __jmp_buf* env, register int val);
 
 #endif
