991a19b5b067807dc39f0b2d9169b2bcdb9dc62d
Robin Avery <robin@ribbanya.dev>
2024-02-27 14:34:21 -0500

    Match 46.47% of `grmaterial` (3.86 KB) (#1344)
    Co-authored-by: Ryan Burns <52847440+r-burns@users.noreply.github.com>

--- src/melee/it/items/it_2E6A.h
+++ src/melee/it/items/it_2E6A.h
@@ -6,6 +6,7 @@
 
 #include "it/items/types.h"
 
+/* 2E6AEC */ void it_802E6AEC(int, int, int, int, int, int, int, int, int);
 /* 2E6F7C */ bool it_802E6F7C(Item_GObj*);
 /* 2E6FC0 */ bool it_802E6FC0(Item_GObj*);
 /* 2E709C */ void it_802E709C(Item_GObj*, Item_GObj*);
