8646dda0b4819c9e481af102fa3eff990a21ec20
Robin Avery <robin@ribbanya.dev>
2024-02-27 12:42:47 -0500

    Match 37.77% of `granime` (8.74 KB)
    Co-authored-by: BR- <716157+BR-@users.noreply.github.com>
    Co-authored-by: Ryan Burns <52847440+r-burns@users.noreply.github.com>

--- src/melee/lb/lbheap.h
+++ src/melee/lb/lbheap.h
@@ -8,7 +8,7 @@
 /* 0158D0 */ void lbHeap_800158D0(int, int);
 /* 015BB8 */ bool lbHeap_80015BB8(int);
 /* 015BD0 */ void* lbHeap_80015BD0(int, int);
-/* 015CA8 */ void lbHeap_80015CA8(int, u32*);
+/* 015CA8 */ void lbHeap_80015CA8(int, void*);
 /* 015D6C */ int lbHeap_80015D6C(int heap0, UNK_T cb, int heap1);
 /* 015F3C */ void lbHeap_80015F3C(void);
 
