0baa3823a44e38db05b95bb06b91500caf73ad66
Ryan Burns <rtburns@protonmail.com>
2024-02-27 17:03:21 -0800

    Fix compiler zip url

--- nix/mwcc.nix
+++ nix/mwcc.nix
@@ -5,7 +5,7 @@
 stdenv.mkDerivation {
   name = "GC_WII_COMPILERS";
   src = fetchzip {
-    url = "https://cdn.discordapp.com/attachments/727918646525165659/1129759991696457728/GC_WII_COMPILERS.zip";
+    url = "https://files.decomp.dev/compilers_20230715.zip";
     stripRoot = false;
     sha256 = "sha256-IX3byvEUVJB6Rmc+NqO9ZNt1jl95nQpEIqxbHI+uUio=";
   };
