05ba39409b10456dfbfac7f4c3d5b1ca2a9d4c56
Will C <wcarter312@gmail.com>
2024-02-29 12:50:14 -0500

    Add DevCom struct

--- src/sysdolphin/baselib/devcom.c
+++ src/sysdolphin/baselib/devcom.c
@@ -1,5 +1,7 @@
 #include "devcom.h"
 
+#include <dolphin/ar/arq.h>
+
 bool devComStatus[4];
 
 s32 HSD_DevComIsBusy(s32 idx)
--- src/sysdolphin/baselib/devcom.h
+++ src/sysdolphin/baselib/devcom.h
@@ -6,7 +6,23 @@
 
 #include "baselib/archive.h"
 
+typedef struct HSD_DevCom HSD_DevCom;
+
+struct HSD_DevCom {
+    HSD_DevCom* next;
+    int dcReq;
+    int file;
+    u32 src;
+    u32 dest;
+    u32 size;
+    u16 type;
+    u16 cancelflag;
+    HSD_DevComCallback callback;
+    void* args;
+};
+
 s32 HSD_DevComIsBusy(s32 idx);
+void HSD_DevComUnlink(HSD_DevCom* dc, int link, HSD_DevCom* adc);
 void HSD_DevComRequest(int file, unsigned long src, unsigned long dest,
                        unsigned long size, int type, int pri,
                        HSD_DevComCallback callback, void* args);
