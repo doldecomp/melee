86cb76fa89b662fc2692c354ab42df3726203f67
Will C <wcarter312@gmail.com>
2024-02-29 03:45:08 -0500

    Match 80284854 and 80284910

--- src/melee/it/items/itbat.c
+++ src/melee/it/items/itbat.c
@@ -1,1 +1,47 @@
+#include "itbat.h"
 
+#include "it/inlines.h"
+#include "it/it_266F.h"
+#include "it/it_26B1.h"
+#include "it/itCommonItems.h"
+#include "it/item.h"
+
+#include <baselib/gobj.h>
+#include <baselib/jobj.h>
+
+Item_GObj* it_80284854(Vec3* pos)
+{
+    SpawnItem spawn;
+    Item_GObj* item_gobj = NULL;
+
+    pos->z = 0.0F;
+    spawn.kind = It_Kind_Bat;
+    spawn.prev_pos = *pos;
+    spawn.pos = spawn.prev_pos;
+    spawn.facing_dir = -1.0F;
+    spawn.x3C_damage = 0;
+    spawn.vel.x = spawn.vel.y = spawn.vel.z = 0.0F;
+    spawn.x0_parent_gobj = NULL;
+    spawn.x4_parent_gobj2 = spawn.x0_parent_gobj;
+    spawn.x44_flag.bits.b0 = true;
+    spawn.x40 = 0;
+    item_gobj = Item_80268B18(&spawn);
+
+    if (item_gobj != NULL) {
+        it_80284974(item_gobj);
+    }
+
+    return item_gobj;
+}
+
+void it_80284910(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    itBatAttributes* ap = item->xC4_article_data->x4_specialAttributes;
+
+    it_8026B390(gobj);
+    item->x40_vel.x = 0.0f;
+    item->x40_vel.y = ap->xC_vel;
+    item->x40_vel.z = 0.0f;
+    it_802849F0(gobj);
+}
