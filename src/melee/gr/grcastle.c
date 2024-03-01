3c4c5676c41bdfe2117f793a9d7e92777fc2fab4
Robin Avery <robin@ribbanya.dev>
2024-03-01 00:33:36 -0500

    Match 67.59% of `grCastle_801D0B0C` (176 bytes) (#1365)

--- src/melee/gr/grcastle.c
+++ src/melee/gr/grcastle.c
@@ -1,1 +1,27 @@
+#include <platform.h>
 
+#include "gr/ground.h"
+#include "lb/lb_00B0.h"
+
+#include <dolphin/mtx/types.h>
+#include <baselib/jobj.h>
+
+bool grCastle_801D0B0C(Vec3* v, u32 arg1, HSD_JObj* jobj)
+{
+    Vec3 pos;
+    float temp_f30;
+    float temp_f31;
+    float temp_f31_2;
+
+    temp_f31 = 192.0f;
+    temp_f30 = 137.0f;
+    temp_f31_2 = temp_f31 * Ground_801C0498();
+    if ((v->x > temp_f31_2) && (v->y < (temp_f30 * Ground_801C0498()))) {
+        return 0;
+    }
+    lb_8000B1CC(jobj, NULL, &pos);
+    if ((1 + v->y) > pos.y) {
+        return 1;
+    }
+    return 0;
+}
--- src/melee/gr/grcastle.h
+++ src/melee/gr/grcastle.h
@@ -1,8 +1,12 @@
 #ifndef GALE01_1CD338
 #define GALE01_1CD338
 
+#include <dolphin/mtx/forward.h>
+#include <baselib/forward.h>
+
 #include <placeholder.h>
 
 /* 1CDF54 */ UNK_RET grCastle_801CDF54(UNK_PARAMS);
+/* 1D0B0C */ bool grCastle_801D0B0C(Vec3* v, u32 arg1, HSD_JObj* jobj);
 
 #endif
