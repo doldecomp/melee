e1b3f5bd06087b013990b9808bd98c1098bb7a60
Robin Avery <robin@ribbanya.dev>
2024-03-02 01:15:25 -0500

    Match `grKongo_801D828C` (136 bytes) (#1370)
    Co-authored-by: Ryan Burns <52847440+r-burns@users.noreply.github.com>

--- src/melee/gr/grkongo.c
+++ src/melee/gr/grkongo.c
@@ -1,1 +1,18 @@
+#include "grmaterial.h"
+#include "types.h"
 
+#include <baselib/gobj.h>
+
+void grKongo_801D828C(HSD_GObj* gobj)
+{
+    Ground* gp = gobj->user_data;
+    if (gp->gv.kongo.xC6 != 1) {
+        return;
+    }
+    HSD_ASSERT(1719, gp->gv.kongo.u.taru.keep);
+    if (((u8*) gp->gv.kongo.u.taru.keep)[2] == 8) {
+        gp->gv.kongo.xC6 = 0;
+        gp->gv.kongo.u.taru.keep = NULL;
+        grMaterial_801C95C4(gobj);
+    }
+}
