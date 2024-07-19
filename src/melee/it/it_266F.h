c090e460320ab89c4f5cf79df64ec094d8b8d3a6
foxcam <118768446+thefoxcam@users.noreply.github.com>
2024-07-19 16:17:30 +0000

    Match 79% of `itlinkarrow` (#1398)
    * initial dump
    
    * marginal matches
    
    * wrangle another inline
    
    * clean up

--- src/melee/it/it_266F.h
+++ src/melee/it/it_266F.h
@@ -20,6 +20,7 @@
 /* 26DA70 */ s32 it_8026DA70(HSD_GObj*);
 /* 26DF34 */ bool it_8026DF34(void);
 /* 26DFB0 */ bool it_8026DFB0(void);
+/* 26EA20 */ bool it_8026EA20(Item_GObj*, Vec3*, Vec3*, Vec3*, s32*, UNK_T);
 /* 26E0F4 */ void it_8026E0F4(Item_GObj*);
 /* 26E15C */ void it_8026E15C(Item_GObj*, HSD_GObjEvent);
 /* 26E248 */ void it_8026E248(Item_GObj*, HSD_GObjEvent);
