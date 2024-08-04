bb2a9d192a863569bfd737fdff50e8c7eb3c02f6
Robin Avery <robin@ribbanya.dev>
2024-08-04 11:29:17 -0400

    Match 65.13% of `ftCo_09F7` (2.32 KB)

--- src/melee/ft/chara/ftCommon/ftCo_09F7.h
+++ src/melee/ft/chara/ftCommon/ftCo_09F7.h
@@ -1,13 +1,17 @@
 #ifndef GALE01_09F7F8
 #define GALE01_09F7F8
 
+#include "ft/forward.h"
 #include "ftCommon/forward.h"
 #include <dolphin/mtx/forward.h>
 
 #include <placeholder.h>
 
-/* 09F834 */ void ftCo_8009F834(ftCo_GObj*, int, int, int, int, Vec3*, Vec3*,
-                                float);
-/* 0A0098 */ UNK_RET ftCo_800A0098(UNK_PARAMS);
+/* 09F834 */ void ftCo_8009F834(ftCo_GObj*, int, Fighter_Part, int, int, Vec3*,
+                                Vec3*, float);
+/* 0A0098 */ void ftCo_800A0098(int);
+
+extern void ftCo_800B46B8(int, int, int);
+extern void ftCo_800B463C(int, int);
 
 #endif
