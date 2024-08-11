a260298ba0c751be7452730810f42e4d8da17a51
Will C <wcarter312@gmail.com>
2024-08-11 14:32:45 -0400

    Match plStale_8003722C

--- src/melee/pl/plstale.c
+++ src/melee/pl/plstale.c
@@ -1,5 +1,7 @@
 #include "plstale.h"
 
+#include "ft/inlines.h"
+#include "ft/types.h"
 #include "pl/player.h"
 #include "pl/types.h"
 
@@ -29,4 +31,34 @@ u16 plStale_80037208(void)
         unk_804D6478 = 1;
     }
     return before;
+}
+
+void plStale_8003722C(HSD_GObj* gobj, HSD_GObj* gobj_2)
+{
+    int i;
+    s32 attack_id;
+    u16 num_states;
+    StaleMoveTable* smtp;
+
+    if (gobj != gobj_2) {
+        Fighter* ft = GET_FIGHTER(gobj);
+
+        num_states = ft->x206C_num_action_states;
+        attack_id = ft->x2068_attackID;
+        smtp = Player_GetStaleMoveTableIndexPtr(ft->player_id);
+        if (attack_id != 1) {
+            for (i = 0; i < 10; i++) {
+                if (attack_id == smtp->StaleMoves[i].move_id &&
+                    num_states == smtp->StaleMoves[i].num_action_states)
+                {
+                    return;
+                }
+            }
+            smtp->StaleMoves[smtp->current_index].move_id = attack_id;
+            smtp->StaleMoves[smtp->current_index].num_action_states =
+                num_states;
+            smtp->current_index =
+                smtp->current_index == 9 ? 0 : smtp->current_index + 1;
+        }
+    }
 }
