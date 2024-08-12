c5d340dbccaa842ae3d181f258537f16f3eb8a2c
Robin Avery <robin@ribbanya.dev>
2024-08-11 20:20:31 -0400

    Fix nix

--- .nix/decomp-toolkit.nix
+++ .nix/decomp-toolkit.nix
@@ -7,13 +7,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "decomp-toolkit";
-  version = "0.7.5";
+  version = "0.9.3";
 
   src = fetchFromGitHub {
     owner = "encounter";
     repo = "decomp-toolkit";
     rev = "v${version}";
-    hash = "sha256-DvZCJRGn6C2FXVlMQTUdmT5+J8raM+RE2Jhzx08VhT4=";
+    hash = "sha256-5EWZwAQNso58WIWKtXNiDDBjMUYYArdRREtyD0bXurY=";
   };
 
   nativeBuildInputs = [
@@ -22,8 +22,9 @@ rustPlatform.buildRustPackage rec {
 
   cargoLock.lockFile = "${src}/Cargo.lock";
   cargoLock.outputHashes."ar-0.8.0" = "sha256-OLyo+cRRWMsI1i8NsgsBKRJH1XsKW1CculQnJ/wcya0=";
-  cargoLock.outputHashes."dol-0.1.0" = "sha256-YfMXWNtmZJhK39R3w98DEGm4y9x59osFGliG36BcuLU=";
-  cargoLock.outputHashes."objdiff-core-1.0.0" = "sha256-KXQ8oTRhe7zsmQH69dz4/qgiwcLfEBjTbSi5IJHlL08=";
+  cargoLock.outputHashes."nod-1.2.0" = "sha256-M7jSBo1Dqrhy/F0osoUtTMNm2BkFnRy2MOmkTs1pvdM=";
+  cargoLock.outputHashes."objdiff-core-2.0.0-alpha.3" = "sha256-E597zRlSpxrTGino7jqoQmyxWkLYXT1P6U2PRolm0Ek=";
+  cargoLock.outputHashes."ppc750cl-0.3.0" = "sha256-nMJk+rgu7Ydi2VZfodJk0kBz9xLLVBVz0vEZPee8Q6M=";
 
   meta = with lib; {
     description = "A GameCube & Wii decompilation toolkit";
