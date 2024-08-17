4c84093be9e4eef676fee5a3971d675f79172543
Will C <wcarter312@gmail.com>
2024-08-17 01:48:29 -0400

    Move functions into header

--- src/melee/mn/mnhyaku.c
+++ src/melee/mn/mnhyaku.c
@@ -1,6 +1,6 @@
-#include <platform.h>
 #include "sc/forward.h"
-#include <baselib/forward.h>
+
+#include "mnhyaku.h"
 
 #include "lb/lb_00F9.h"
 #include "lb/lbarchive.h"
@@ -9,25 +9,6 @@
 #include "mn/mn_2295.h"
 #include "mn/types.h"
 #include "sc/types.h"
-
-#include <dolphin/os.h>
-#include <baselib/gobj.h>
-#include <baselib/gobjgxlink.h>
-#include <baselib/gobjobject.h>
-#include <baselib/gobjplink.h>
-#include <baselib/gobjproc.h>
-#include <baselib/gobjuserdata.h>
-#include <baselib/jobj.h>
-#include <baselib/memory.h>
-#include <baselib/sislib.h>
-
-void mnHyaku_8024C68C(HSD_GObj*);
-void mnHyaku_8024CB94(u8);
-void mnHyaku_8024CD64(u8);
-
-void mnHyaku_8024C9F0(HSD_GObj*);
-void mnHyaku_8024CA50(HSD_GObj*);
-void mnHyaku_8024CAC8(HSD_GObj*);
 
 extern HSD_GObj* mnHyaku_804D6C58;
 
