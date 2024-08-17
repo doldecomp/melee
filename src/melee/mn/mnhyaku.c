5843c2c0b34a21e7785de7b35b382add1659d5b2
Will <wcarter312@gmail.com>
2024-08-17 01:54:13 -0400

    Finish mnhyaku (#1433)
    * Match 8024C9F0
    
    * Match 8024CAC8
    
    * Match 8024CB94
    
    * Match 8024C68C
    
    * Match 8024CD64
    
    * Mark mnhyaku as matched
    
    * Move functions into header
    
    * Remove extern

--- src/melee/mn/mnhyaku.c
+++ src/melee/mn/mnhyaku.c
@@ -1,6 +1,6 @@
-#include <platform.h>
 #include "sc/forward.h"
-#include <baselib/forward.h>
+
+#include "mnhyaku.h"
 
 #include "lb/lb_00F9.h"
 #include "lb/lbarchive.h"
@@ -10,30 +10,11 @@
 #include "mn/types.h"
 #include "sc/types.h"
 
-#include <dolphin/os.h>
-#include <baselib/gobj.h>
-#include <baselib/gobjgxlink.h>
-#include <baselib/gobjobject.h>
-#include <baselib/gobjplink.h>
-#include <baselib/gobjproc.h>
-#include <baselib/gobjuserdata.h>
-#include <baselib/jobj.h>
-#include <baselib/memory.h>
-#include <baselib/sislib.h>
-
-void mnHyaku_8024C68C(HSD_GObj*);
-void mnHyaku_8024CB94(u8);
-void mnHyaku_8024CD64(u8);
-
-void fn_8024C9F0(HSD_GObj*);
-void fn_8024CA50(HSD_GObj*);
-void fn_8024CAC8(HSD_GObj*);
-
-extern HSD_GObj* mnHyaku_804D6C58;
+static HSD_GObj* mnHyaku_804D6C58;
 
 void gm_801677E8(void);
 
-void lbAudioAx_8002702C(s32, s32, s32);
+void lbAudioAx_8002702C(s32, u64);
 void lbAudioAx_80027168(void);
 f32 lbAudioAx_80027648(void);
 
@@ -72,6 +53,8 @@ void mnHyaku_8024C68C(HSD_GObj* arg0)
 {
     u64 ret;
     Menu* menu = GET_MENU(mnHyaku_804D6C58);
+    PAD_STACK(16);
+
     if (mn_804D6BC8.x0 != 0) {
         mn_804D6BC8.x0--;
         mn_804D6BC8.x2 = 0;
@@ -85,7 +68,7 @@ void mnHyaku_8024C68C(HSD_GObj* arg0)
         mn_80229894(9, 2, 3);
         return;
     }
-    if (ret) {
+    if (ret & 0x10) {
         lbAudioAx_80024030(1);
         mn_802295AC();
         gm_801677E8();
@@ -112,61 +95,87 @@ void mnHyaku_8024C68C(HSD_GObj* arg0)
     } else {
         if (ret & 4) {
             lbAudioAx_80024030(2);
-            menu->cursor = (menu->cursor == 0) ? 5 : (menu->cursor - 1);
+            if (menu->cursor == 0) {
+                menu->cursor = 5;
+            } else {
+                menu->cursor--;
+            }
             mnHyaku_8024C68C_inline(mnHyaku_804D6C58->hsd_obj, menu->cursor);
             mnHyaku_8024C68C_inline_2(mnHyaku_804D6C58);
             return;
         }
         if (ret & 8) {
             lbAudioAx_80024030(2);
-            menu->cursor = (menu->cursor == 5) ? 0 : (menu->cursor + 1);
+            if (menu->cursor == 5) {
+                menu->cursor = 0;
+            } else {
+                menu->cursor++;
+            }
             mnHyaku_8024C68C_inline(mnHyaku_804D6C58->hsd_obj, menu->cursor);
             mnHyaku_8024C68C_inline_2(mnHyaku_804D6C58);
         }
     }
 }
 
-void fn_8024C9F0(HSD_GObj* gobj)
+void mnHyaku_8024C9F0(HSD_GObj* gobj)
 {
-    f32 ret = mn_8022EC18(gobj->hsd_obj, &mnHyaku_803EF674[0], 0x80);
+    HSD_JObj* jobj = (HSD_JObj*) gobj->hsd_obj;
+    f32 ret = mn_8022EC18(jobj, &mnHyaku_803EF674[0], 0x80);
     if (ret >= mnHyaku_803EF674[1]) {
         HSD_GObjPLink_80390228(gobj);
     }
 }
 
-void fn_8024CA50(HSD_GObj* gobj)
+void mnHyaku_8024CA50(HSD_GObj* gobj)
 {
     HSD_GObjProc* gobj_proc;
     Menu* menu = GET_MENU(gobj);
     if (mn_804A04F0.x0 != 0x21) {
         HSD_GObjProc_8038FE24(HSD_GObj_804D7838);
-        gobj_proc = HSD_GObjProc_8038FD54(gobj, fn_8024C9F0, 0U);
+        gobj_proc = HSD_GObjProc_8038FD54(gobj, mnHyaku_8024C9F0, 0U);
         gobj_proc->flags_3 = HSD_GObj_804D783C;
         HSD_SisLib_803A5CC4(menu->text);
     }
 }
 
-void fn_8024CAC8(HSD_GObj* gobj)
+void mnHyaku_8024CAC8(HSD_GObj* gobj)
 {
-    {
-        HSD_GObjProc* gobj_proc;
-        Menu* menu = GET_MENU(gobj);
-        if (mn_804A04F0.x0 != 0x21) {
-            HSD_GObjProc_8038FE24(HSD_GObj_804D7838);
-            gobj_proc = HSD_GObjProc_8038FD54(gobj, fn_8024C9F0, 0U);
-            gobj_proc->flags_3 = HSD_GObj_804D783C;
-            HSD_SisLib_803A5CC4(menu->text);
-            return;
-        }
-    }
-    if (mn_8022EC18(gobj->hsd_obj, &mnHyaku_803EF668[0], 0x80) ==
-        mnHyaku_803EF668[1])
-    {
-        HSD_GObjProc* gobj_proc;
+    HSD_JObj* jobj;
+    HSD_GObjProc* gobj_proc;
+    Menu* menu;
+    f32 res;
+    PAD_STACK(12);
+
+    jobj = (HSD_JObj*) gobj->hsd_obj;
+    menu = GET_MENU(gobj);
+    if (mn_804A04F0.x0 != 0x21) {
         HSD_GObjProc_8038FE24(HSD_GObj_804D7838);
-        gobj_proc = HSD_GObjProc_8038FD54(gobj, fn_8024CA50, 0U);
+        gobj_proc = HSD_GObjProc_8038FD54(gobj, mnHyaku_8024C9F0, 0U);
+        gobj_proc->flags_3 = HSD_GObj_804D783C;
+        HSD_SisLib_803A5CC4(menu->text);
+        return;
+    }
+    res = mn_8022EC18(jobj, &mnHyaku_803EF668[0], 0x80);
+    if (res == mnHyaku_803EF668[1]) {
+        HSD_GObjProc_8038FE24(HSD_GObj_804D7838);
+        gobj_proc = HSD_GObjProc_8038FD54(gobj, mnHyaku_8024CA50, 0U);
         gobj_proc->flags_3 = HSD_GObj_804D783C;
     }
+}
+
+inline static Menu* allocMenu(u8 arg0, HSD_GObj* gobj)
+{
+    Menu* user_data;
+
+    user_data = HSD_MemAlloc(8);
+    if (user_data == NULL) {
+        OSReport("Can't get user_data.\n");
+        __assert(__FILE__, 360, "user_data");
+    }
+    user_data->cursor = arg0;
+    user_data->text = 0;
+    GObj_InitUserData(gobj, 0, HSD_Free, user_data);
+    return user_data;
 }
 
 void mnHyaku_8024CB94(u8 arg0)
@@ -174,53 +183,46 @@ void mnHyaku_8024CB94(u8 arg0)
     HSD_GObj* gobj;
     HSD_GObjProc* proc;
     HSD_JObj* jobj;
-    u8 temp_r29;
     Menu* menu;
-    HSD_GObjProc* temp_r3_4;
 
     gobj = GObj_Create(HSD_GOBJ_CLASS_ITEM, 7U, 0x80);
     mnHyaku_804D6C58 = gobj;
     jobj = HSD_JObjLoadJoint(mnHyaku_804A08E8.joint);
-    temp_r29 = HSD_GObj_804D7849;
-    HSD_GObjObject_80390A70(gobj, temp_r29, jobj);
+    HSD_GObjObject_80390A70(gobj, HSD_GObj_804D7849, jobj);
     GObj_SetupGXLink(gobj, HSD_GObj_80391070, 4, 0x80);
     HSD_JObjAddAnimAll(jobj, mnHyaku_804A08E8.animjoint,
                        mnHyaku_804A08E8.matanim_joint,
                        mnHyaku_804A08E8.shapeanim_joint);
     HSD_JObjReqAnimAll(jobj, 0.0F);
     HSD_JObjAnimAll(jobj);
-    menu = HSD_MemAlloc(8);
-    if (menu == NULL) {
-        OSReport("Can't get menu.\n");
-        __assert("mnhyaku.c", 344, "menu");
-    }
-    menu->cursor = arg0;
-    menu->text = 0;
-    GObj_InitUserData(gobj, 0, HSD_Free, menu);
 
+    menu = allocMenu(arg0, gobj);
     mnHyaku_8024C68C_inline(jobj, menu->cursor);
-    temp_r3_4 = HSD_GObjProc_8038FD54(gobj, fn_8024CAC8, 0U);
-    temp_r3_4->flags_3 = HSD_GObj_804D783C;
-    mnHyaku_8024C68C_inline_2(gobj);
+
+    proc = HSD_GObjProc_8038FD54(gobj, mnHyaku_8024CAC8, 0U);
+    proc->flags_3 = HSD_GObj_804D783C;
+
+    menu = gobj->user_data;
+    if (menu->text) {
+        HSD_SisLib_803A5CC4(menu->text);
+    }
+    Menu_InitCenterText(menu, vals[menu->cursor]);
 }
 
 void mnHyaku_8024CD64(u8 arg0)
 {
-    HSD_GObjProc* temp_r3;
+    HSD_GObjProc* proc;
     HSD_Archive* archive;
-    Vec3 pos;
 
     mn_804D6BC8.x0 = 5;
     mn_804A04F0.x1 = mn_804A04F0.x0;
-    mn_804A04F0.x0 = 0x15;
+    mn_804A04F0.x0 = 33;
     mn_804A04F0.x2 = 0;
 
     lbAudioAx_80026F2C(0x12);
-    lbAudioAx_8002702C(2, 0, 8);
+    lbAudioAx_8002702C(2, 8);
     lbAudioAx_80027168();
-    pos.x = lbAudioAx_80027648();
-    pos.y = mnHyaku_803EF668[0];
-    pos.z = 0;
+    lbAudioAx_80027648();
     archive = mn_804D6BB8;
     lbArchive_LoadSections(
         archive, (void**) &mnHyaku_804A08E8, "MenMainConKm_Top_joint",
@@ -229,8 +231,8 @@ void mnHyaku_8024CD64(u8 arg0)
         &mnHyaku_804A08E8.shapeanim_joint, "MenMainConKm_Top_shapeanim_joint",
         0);
     mnHyaku_8024CB94(arg0);
-    temp_r3 = HSD_GObjProc_8038FD54(GObj_Create(0U, 1U, 0x80U),
-                                    mnHyaku_8024C68C, 0U);
-    temp_r3->flags_3 = HSD_GObj_804D783C;
+    proc = HSD_GObjProc_8038FD54(GObj_Create(0U, 1U, 0x80U), mnHyaku_8024C68C,
+                                 0U);
+    proc->flags_3 = HSD_GObj_804D783C;
     lbAudioAx_800237A8(0x7530, 0x7FU, 0x40U);
 }
--- src/melee/mn/mnhyaku.h
+++ src/melee/mn/mnhyaku.h
@@ -1,1 +1,26 @@
+#ifndef _MN_HYAKU_
+#define _MN_HYAKU_
 
+#include <platform.h>
+#include <baselib/forward.h>
+
+#include <dolphin/os.h>
+#include <baselib/gobj.h>
+#include <baselib/gobjgxlink.h>
+#include <baselib/gobjobject.h>
+#include <baselib/gobjplink.h>
+#include <baselib/gobjproc.h>
+#include <baselib/gobjuserdata.h>
+#include <baselib/jobj.h>
+#include <baselib/memory.h>
+#include <baselib/sislib.h>
+
+void mnHyaku_8024C68C(HSD_GObj*);
+void mnHyaku_8024CB94(u8);
+void mnHyaku_8024CD64(u8);
+
+void mnHyaku_8024C9F0(HSD_GObj*);
+void mnHyaku_8024CA50(HSD_GObj*);
+void mnHyaku_8024CAC8(HSD_GObj*);
+
+#endif
