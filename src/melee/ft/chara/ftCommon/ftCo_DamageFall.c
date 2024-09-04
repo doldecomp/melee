14d4faf1b94c203c9e23033c821c23507fdcaaa5
Will C <wcarter312@gmail.com>
2024-09-04 17:42:32 -0400

    Tweak cast

--- src/melee/ft/chara/ftCommon/ftCo_DamageFall.c
+++ src/melee/ft/chara/ftCommon/ftCo_DamageFall.c
@@ -38,31 +38,32 @@ static u16 calcShift(ftCommonData* fcp, f32 hitlag)
 void ftCo_80090594(ftCo_Fighter* fp, enum_t element, int dmg, FtMotionId msid,
                    GroundOrAir ground_or_air, float hitlag_mul)
 {
-    f32 hitlag;
-    ftCommonData* fcp;
-
+    ftCo_Fighter* fp2 = fp;
+    ftCommonData* fCom;
     if (element == 10 || element == 12 || element == 6 || element == 7) {
         return;
     }
-    if (fp->motion_id == ftCo_MS_DamageIce) {
+    if (fp2->motion_id == ftCo_MS_DamageIce) {
         return;
     }
 
-    fcp = p_ftCommonData;
-    hitlag = ftCommon_CalcHitlag(dmg, msid, hitlag_mul);
-    fp->dmg.x18fa_model_shift_frames = calcShift(fcp, hitlag);
-    fp->dmg.x18FC = 0;
+    fCom = p_ftCommonData;
+    fp2->dmg.x18fa_model_shift_frames =
+        (u16) (fCom->x168 * ftCommon_CalcHitlag(dmg, msid, hitlag_mul) +
+               p_ftCommonData->x16C);
+    fp2->dmg.x18FC = 0;
 
     if (element == 2) {
-        fp->dmg.x18F8 = 2;
+        fp2->dmg.x18F8 = 2;
     } else if (ground_or_air == GA_Air) {
-        fp->dmg.x18F8 = 0;
+        fp2->dmg.x18F8 = 0;
     } else {
-        fp->dmg.x18F8 = 1;
-        fp->dmg.x1900 = fp->coll_data.floor.normal.x;
-        fp->dmg.x1904 = fp->coll_data.floor.normal.y;
+        fp2->dmg.x18F8 = 1;
+        fp2->dmg.x1900 = fp2->coll_data.floor.normal.x;
+        fp2->dmg.x1904 = fp2->coll_data.floor.normal.y;
     }
-    fp->dmg.x18FD = (u8) (Fighter_804D6530 + (fp->dmg.x18F8 * 2))[1];
+    fp2->dmg.x18FD =
+        (u8) ((Vec2*) (Fighter_804D6530 + (fp2->dmg.x18F8 * 2))[1]);
 }
 
 Vec2* ftCo_80090690(Fighter* fp, Vec2* shift)
