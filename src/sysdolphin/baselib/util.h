714d1abc863bb6411d95769650d1d01c6660d35f
Will <wcarter312@gmail.com>
2024-09-06 13:50:09 -0400

    Work on CObj (#1443)
    * Misc work on getting all CObj functions closer to matching
    
    * Match HSD_CObjGetLeftVector, various other work
    
    * Fix the match of HSD_CObjGetEyeVector to properly inline
    
    * Match setupBottomHalfCamera
    
    * Get upvec2roll closer
    
    * Tweaks
    
    * Fix float
    
    * Get HSD_CObjSetUpVector closer
    
    * Cleanup

--- src/sysdolphin/baselib/util.h
+++ src/sysdolphin/baselib/util.h
@@ -4,6 +4,10 @@
 #include <platform.h>
 #include <dolphin/gx/forward.h>
 
+#include "mtx.h"
+
+#include <dolphin/mtx/vec.h>
+
 // functions
 void HSD_MulColor(GXColor* arg0, GXColor* arg1, GXColor* dest);
 u32 HSD_GetNbBits(u32 arg0);
@@ -11,5 +15,21 @@ s32 HSD_Index2PosNrmMtx(u32 arg0);
 
 // variables
 extern float HSD_identityMtx[];
+
+#define FLT_MIN 1.17549435e-38f
+
+static inline int vec_normalize_check(Vec3* src, Vec3* dst)
+{
+    if (!src || !dst) {
+        return -1;
+    }
+    if (fabsf_bitwise(src->x) <= FLT_MIN && fabsf_bitwise(src->y) <= FLT_MIN &&
+        fabsf_bitwise(src->z) <= FLT_MIN)
+    {
+        return -1;
+    }
+    PSVECNormalize(src, dst);
+    return 0;
+}
 
 #endif // _UTIL_H_
