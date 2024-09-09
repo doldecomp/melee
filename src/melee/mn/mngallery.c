5fa3d4f9cfa5eb8ab52332eba2d68f6e962ed728
Will C <wcarter312@gmail.com>
2024-09-09 14:17:49 -0400

    Match mnGallery_80258940

--- src/melee/mn/mngallery.c
+++ src/melee/mn/mngallery.c
@@ -1,1 +1,11 @@
+#include "mngallery.h"
 
+#include <baselib/cobj.h>
+#include <baselib/memory.h>
+
+extern void* mnGallery_804D6C8C;
+
+void mnGallery_80258940(void)
+{
+    mnGallery_804D6C8C = HSD_MemAlloc(0x271000);
+}
