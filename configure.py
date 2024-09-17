9ad8b976658fe031b3222f3b3b8c6c825297ee53
Will C <wcarter312@gmail.com>
2024-09-17 15:46:09 -0400

    Mark AXAlloc as matching

--- configure.py
+++ configure.py
@@ -1490,7 +1490,7 @@ config.libs = [
         "ax",
         [
             Object(Matching, "dolphin/ax/AX.c"),
-            Object(NonMatching, "dolphin/ax/AXAlloc.c"),
+            Object(Matching, "dolphin/ax/AXAlloc.c"),
             Object(Matching, "dolphin/ax/AXAux.c"),
             Object(NonMatching, "dolphin/ax/AXCL.c"),
             Object(NonMatching, "dolphin/ax/AXOut.c"),
