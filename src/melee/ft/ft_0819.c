a58468bd6ebb8938159d9e5dbbf10b9ac75daad1
Robin Avery <robin@ribbanya.dev>
2024-10-02 05:38:49 -0400

    Match `ft_800819A8`
    Co-authored-by: freyja <github.com.tidy739@passinbox.com>

--- src/melee/ft/ft_0819.c
+++ src/melee/ft/ft_0819.c
@@ -23,6 +23,21 @@ void ft_80081938(HSD_GObj* gobj)
     fp->dmg.x1930 = fp->dmg.x193c;
 }
 
-/// #ft_800819A8
+void ft_800819A8(HSD_GObj* gobj)
+{
+    f32 lx, rx;
+    ftECB* p_var;
+    u8 unused1[36];
+    Vec3 sp44;
+    u8 unused2[56];
+    Fighter* fp = gobj->user_data;
+    sp44 = fp->coll_data.cur_topn;
+    p_var = &fp->coll_data.xA4_ecbCurrCorrect;
+    lx = p_var->left.x;
+    rx = p_var->right.x;
+    fp->dmg.x1930.x = lx + sp44.x;
+    fp->dmg.x1930.y = rx + sp44.x;
+    fp->dmg.x1930.z = fp->cur_pos.y + fp->co_attrs.x15C;
+}
 
 /// #ft_80081A00
