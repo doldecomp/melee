e88fcc1b140dbd04ca4341376139300d0cb698fa
eperdew <eperdew17@gmail.com>
2024-10-21 22:00:53 -0400

    Decompile some camera functions (#1484)
    * Match Camera_80029124
    
    * Match Camera_800293E0
    
    * Remove Camera_800293E0 comment
    
    * Partial match on Camera_80029C88
    
    * Partially match Camera_8002A28C
    
    * Remove duplicate decompilation
    
    * Decent progress on Camera_8002A28C
    
    * Couple of small functions
    
    * More progress in camera.c
    
    Couple of other small functions
    
    Decent progress on Camera_8002A28C
    
    Remove duplicate decompilation

--- src/melee/cm/camera.h
+++ src/melee/cm/camera.h
@@ -18,14 +18,14 @@
 /* 029020 */ CameraBox* Camera_80029020(void);
 /* 029044 */ CameraBox* Camera_80029044(int);
 /* 0290D4 */ void Camera_800290D4(CameraBox*);
-/* 029124 */ UNK_T Camera_80029124(Vec3*, int);
+/* 029124 */ u32 Camera_80029124(Vec3*, s32);
 /* 02928C */ s32 Camera_8002928C(CameraBox*);
 /* 0293E0 */ UNK_RET Camera_800293E0(UNK_PARAMS);
 /* 02958C */ UNK_RET Camera_8002958C(UNK_PARAMS);
 /* 029AAC */ void Camera_80028F5C(CameraBox*, s32);
 /* 029BC4 */ void Camera_80029BC4(CameraBounds* bounds,
                                   CameraMovement* movement);
-/* 029C88 */ UNK_RET Camera_80029C88(UNK_PARAMS);
+/* 029C88 */ void Camera_80029C88(CameraMovement* movement, f32);
 /* 029CF8 */ UNK_RET Camera_80029CF8(UNK_PARAMS);
 /* 02A0C0 */ UNK_RET Camera_8002A0C0(UNK_PARAMS);
 /* 02A278 */ void Camera_8002A278(float x, float y);
