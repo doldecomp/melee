54bf50ee31ef7242c74f398529279c62394c68c6
Luke Street <luke@street.dev>
2024-11-24 00:16:24 -0700

    Update tool versions

--- configure.py
+++ configure.py
@@ -144,8 +144,8 @@ if not config.non_matching:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20240706"
-config.dtk_tag = "v1.1.4"
-config.objdiff_tag = "v2.3.3"
+config.dtk_tag = "v1.3.0"
+config.objdiff_tag = "v2.4.0"
 config.sjiswrap_tag = "v1.2.0"
 config.wibo_tag = "0.6.11"
 
