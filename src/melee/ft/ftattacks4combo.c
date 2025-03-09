ee2d4b264713acf6132a1b32e19fae827378d127
Braden Wilson <braden.wilson44@gmail.com>
2025-03-08 23:19:03 -0800

    Started working on ft/ftattacks4combo

--- src/melee/ft/ftattacks4combo.c
+++ src/melee/ft/ftattacks4combo.c
@@ -1,1 +1,32 @@
 
+#include "ft/ftattacks4combo.h"
+
+#include "ft/chara/ftCommon/ftCo_AttackS4.h"
+#include "ft/chara/ftCommon/ftCo_Wait.h"
+#include "ft/fighter.h"
+
+// bool ftCo_800CECE8(ftCo_GObj* gobj) {}
+
+// UNK_RET ftCo_800CED30(UNK_PARAMS) {}
+
+void ftCo_AttackS42_Anim(ftCo_GObj* gobj)
+{
+    ftCo_AttackS4_Anim(gobj);
+}
+
+void ftCo_AttackS42_IASA(ftCo_GObj* gobj)
+{
+    if ((((Fighter*) gobj->user_data)->x2218 >> 7U) & 1) {
+        ftCo_Wait_IASA(gobj);
+    }
+}
+
+void ftCo_AttackS42_Phys(ftCo_GObj* gobj)
+{
+    ftCo_AttackS4_Phys(gobj);
+}
+
+void ftCo_AttackS42_Coll(ftCo_GObj* gobj)
+{
+    ftCo_AttackS4_Coll(gobj);
+}
