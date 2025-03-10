534a3720980e16439ea88ca526c6d208d367c6d8
Braden Wilson <braden.wilson44@gmail.com>
2025-03-09 18:51:18 -0700

    Almost finished ftattacks4combo

--- src/melee/ft/ftattacks4combo.c
+++ src/melee/ft/ftattacks4combo.c
@@ -5,9 +5,32 @@
 #include "ft/chara/ftCommon/ftCo_Wait.h"
 #include "ft/fighter.h"
 
-// bool ftCo_800CECE8(ftCo_GObj* gobj) {}
+bool ftCo_800CECE8(ftCo_GObj* gobj)
+{
+    Fighter* fp = gobj->user_data;
+    if (((u32) fp->cmd_vars[0] != 0) && (fp->input.x668 & 0x100)) {
+        ftCo_800CED30(gobj);
+        return true;
+    }
+    return false;
+}
 
-// UNK_RET ftCo_800CED30(UNK_PARAMS) {}
+// slightly wrong
+void ftCo_800CED30(Fighter_GObj* gobj)
+{
+    Fighter* fp = gobj->user_data;
+    switch (fp->kind) {
+    case FTKIND_LINK:
+    case FTKIND_CLINK:
+        break;
+    default:
+        OSReport("don't have smash42 motion!!!\n");
+        __assert("ftattacks4combo.c", 0x36, "0");
+    }
+    fp->allow_interrupt = 0;
+    Fighter_ChangeMotionState(gobj, 0x155, 0U, 0.0f, 1.0f, 0.0f, NULL);
+    ftAnim_8006EBA4(gobj);
+}
 
 void ftCo_AttackS42_Anim(ftCo_GObj* gobj)
 {
@@ -16,7 +39,7 @@ void ftCo_AttackS42_Anim(ftCo_GObj* gobj)
 
 void ftCo_AttackS42_IASA(ftCo_GObj* gobj)
 {
-    if ((((Fighter*) gobj->user_data)->x2218 >> 7U) & 1) {
+    if ((((Fighter*) gobj->user_data)->x2218 >> 7) & 1) {
         ftCo_Wait_IASA(gobj);
     }
 }
--- src/melee/ft/ftattacks4combo.h
+++ src/melee/ft/ftattacks4combo.h
@@ -6,7 +6,7 @@
 #include "ftCommon/forward.h"
 
 /* 0CECE8 */ bool ftCo_800CECE8(ftCo_GObj* gobj);
-/* 0CED30 */ UNK_RET ftCo_800CED30(UNK_PARAMS);
+/* 0CED30 */ void ftCo_800CED30(ftCo_GObj* gobj);
 /* 0CEDE0 */ void ftCo_AttackS42_Anim(ftCo_GObj* gobj);
 /* 0CEE00 */ void ftCo_AttackS42_IASA(ftCo_GObj* gobj);
 /* 0CEE30 */ void ftCo_AttackS42_Phys(ftCo_GObj* gobj);
