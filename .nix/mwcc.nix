8bc4f8a4dcae180b538204d90b3785e8f1467766
William Carter <wcarter312@gmail.com>
2025-05-21 02:38:07 -0400

    Change compiler path in dockerfile and nix build

--- .nix/mwcc.nix
+++ .nix/mwcc.nix
@@ -5,7 +5,7 @@
 stdenv.mkDerivation {
   name = "GC_WII_COMPILERS";
   src = fetchzip {
-    url = "https://files.decomp.dev/compilers_20240706.zip";
+    url = "https://files.decomp.dev/compilers_20250520.zip";
     stripRoot = false;
     sha256 = "sha256-+j3bLERQ6CFkqRUaQN6Q8QIE+NMJcNZcQM8b2wvE7RQ=";
   };
