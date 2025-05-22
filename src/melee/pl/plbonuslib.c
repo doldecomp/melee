1ec36d6d0eb32d558c81ce6a2342d2e346cfa7c9
Avery Brinkman <averybrinkman@gmail.com>
2025-05-21 22:10:57 -0400

    pl_8003FE64

--- src/melee/pl/plbonuslib.c
+++ src/melee/pl/plbonuslib.c
@@ -147,6 +147,20 @@ void pl_8003FC88(s32 arg0, s32 arg1, s32 arg2)
     }
 }
 
+void pl_8003FE64(s32 arg0)
+{
+    s32 var_r31;
+
+    for (var_r31 = 0; var_r31 < 6; var_r31++) {
+        if (pl_80039418(var_r31, 0x1A) != 0U) {
+            break;
+        }
+    }
+    if (var_r31 == 6) {
+        pl_80038788(arg0, 0x1A, 1);
+    }
+}
+
 void pl_800402D0(int slot, bool arg1, bool arg2)
 {
     StaleMoveTable* stale_moves = Player_GetStaleMoveTableIndexPtr2(slot);
--- src/melee/pl/plbonuslib.h
+++ src/melee/pl/plbonuslib.h
@@ -12,6 +12,7 @@
 /* 03E70C */ void pl_8003E70C(Item_GObj*);
 /* 03EC30 */ void pl_8003EC30(int slot, int, int, float);
 /* 03FC88 */ void pl_8003FC88(s32, s32, s32);
+/* 03FE64 */ void pl_8003FE64(s32 arg0);
 /* 03FDF4 */ void pl_8003FDF4(u8, int);
 /* 0402D0 */ void pl_800402D0(int slot, bool, bool);
 /* 0403C0 */ void pl_800403C0(int, int);
