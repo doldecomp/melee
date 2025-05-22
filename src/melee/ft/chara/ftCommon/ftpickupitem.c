71e3a965ef4657786e891077d87441cc332c1d22
William Carter <wcarter312@gmail.com>
2025-05-22 10:27:11 -0400

    Fixup ftpickup item's sdata2 by switching to PAD_STACK

--- src/melee/ft/chara/ftCommon/ftpickupitem.c
+++ src/melee/ft/chara/ftCommon/ftpickupitem.c
@@ -36,10 +36,11 @@
 
 bool ftpickupitem_80094150(ftCo_GObj* gobj, Item_GObj* item_gobj)
 {
-    u8 _[8] = { 0 };
     itPickup* pickup;
     Vec4* offset0;
     ftCo_Fighter* fp = gobj->user_data;
+    PAD_STACK(8);
+
     if (Item_IsGrabbable(item_gobj) &&
         (!fp->x2222_b4 || !it_8026B47C(item_gobj)) &&
         (fp->item_gobj == NULL || it_8026B4F0(item_gobj)))
@@ -226,7 +227,7 @@ void ftpickupitem_80094694(ftCo_GObj* gobj, FtMotionId msid, bool loop)
 
 bool ftpickupitem_80094790(ftCo_GObj* gobj)
 {
-    u8 _[8] = { 0 };
+    PAD_STACK(8);
     if (GET_FIGHTER(gobj)->x1978 == NULL) {
         Item_GObj* unk_gobj = ftpickupitem_800942A0(gobj, 3);
         if (unk_gobj != NULL) {
@@ -243,8 +244,8 @@ bool ftpickupitem_80094790(ftCo_GObj* gobj)
 
 void ftpickupitem_80094818(ftCo_GObj* gobj, int arg1)
 {
-    u8 _[8] = { 0 };
     ftCo_Fighter* fp = gobj->user_data;
+    PAD_STACK(8);
     if (fp->x1978 != NULL) {
         pl_8003E17C(fp->player_id, fp->x221F_b4, fp->x1978);
     } else if (fp->item_gobj != NULL) {
@@ -257,9 +258,8 @@ void ftpickupitem_80094818(ftCo_GObj* gobj, int arg1)
 
 static inline void inlineB0(ftCo_GObj* gobj)
 {
-    u64 _ = { 0 };
-
     ftCo_Fighter* fp = gobj->user_data;
+    PAD_STACK(8);
     if (fp->x1978 != NULL) {
         pl_8003E17C(fp->player_id, fp->x221F_b4, fp->x1978);
     } else if (fp->item_gobj != NULL) {
@@ -353,8 +353,8 @@ void ftpickupitem_Coll(ftCo_GObj* gobj)
 void ftpickupitem_80094B6C(ftCo_GObj* gobj, Item_GObj* item_gobj)
 {
     Vec3 vec;
-    u8 _[4] = { 0 };
     ftCo_Fighter* fp = gobj->user_data;
+    PAD_STACK(4);
     if (item_gobj == NULL) {
         OSReport("ftGetImmItem item_gobj is NULL!!\n");
         __assert(__FILE__, 399, "item_gobj");
