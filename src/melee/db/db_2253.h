2f03daf94610a3b9f49920a0de41336e7b8f7cec
BR <wishingwellwellwishing@hotmail.com>
2025-05-24 06:36:27 -0700

    fn_802287D8

--- src/melee/db/db_2253.h
+++ src/melee/db/db_2253.h
@@ -76,7 +76,7 @@
 /* 228620 */ void fn_80228620(int);
 /* 22873C */ void fn_8022873C(int);
 /* 2287C4 */ UNK_RET fn_802287C4(UNK_PARAMS);
-/* 2287D8 */ void fn_802287D8(int);
+/* 2287D8 */ void fn_802287D8(int arg0);
 /* 228820 */ UNK_RET fn_80228820(UNK_PARAMS);
 /* 22886C */ void db_8022886C(void);
 /* 22887C */ void db_8022887C(void);
