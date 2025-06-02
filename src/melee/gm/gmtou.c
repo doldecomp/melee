9a9d715bcb3206b6fe7c7a2fdd4d1ba98903bbd8
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2025-06-01 18:44:09 -0700

    Split gmtou (#1555)

---
+++ src/melee/gm/gmtou.c
@@ -0,0 +1,54 @@
+#include <melee/gm/gmtou.h>
+
+// Filename is just a guess, based on some strings in the file
+// Seems to be Tournament game code
+
+/// #gm_8019B8C4
+
+/// #gm_8019B9C8
+
+void fn_8019BA04(void) {}
+
+/// #fn_8019BA08
+
+/// #fn_8019BF18
+
+/// #fn_8019BF8C
+
+/// #fn_8019C048
+
+/// #fn_8019C3EC
+
+/// #fn_8019C570
+
+/// #fn_8019C6AC
+
+/// #fn_8019C744
+
+/// #fn_8019CA38
+
+/// #fn_8019CBFC
+
+/// #fn_8019CC74
+
+/// #fn_8019CDBC
+
+/// #fn_8019CFA4
+
+/// #fn_8019D074
+
+/// #fn_8019D1BC
+
+/// #fn_8019DD60
+
+/// #gm_8019DF8C
+
+/// #gm_8019E634
+
+/// #gm_8019ECAC
+
+/// #gm_8019EE54
+
+/// #fn_8019EE80
+
+/// #fn_8019EF08
