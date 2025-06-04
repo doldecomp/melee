386007aa97d454be8850e2865ea3b84608dbd27c
Ryan Burns <rtburns@protonmail.com>
2025-06-03 18:57:33 -0700

    Split out ansi_files, match ansi_files and printf

--- src/MSL/console_io.h
+++ src/MSL/console_io.h
@@ -4,7 +4,8 @@
 #include <platform.h>
 
 s32 MSL_ConsoleIo_80325F18(void);
-s32 __write_console(s32, s32 arg1, s32* arg2);
-u8 __read_console(u32, u8* buf, u32* n);
+int __write_console(u32, u8* buf, u32* n, void(*)());
+int __read_console(u32, u8* buf, u32* n, void(*)());
+int __close_console(u32);
 
 #endif
