9063650e3bf414c88e103c6d125a48d316fccf25
Ryan Burns <rtburns@protonmail.com>
2025-06-06 18:49:55 -0700

    Match init_cpp_exceptions

--- src/Runtime/__init_cpp_exceptions.c
+++ src/Runtime/__init_cpp_exceptions.c
@@ -6,11 +6,7 @@
 
 static int fragmentID = -2;
 
-/**
- * @todo HACK: Should be @c _eti_init_info. We can't use the appropriate
- * name yet due to the linker not being able to generate it.
- */
-SECTION_INIT extern __eti_init_info _eti_init_info_[];
+SECTION_INIT extern __eti_init_info _eti_init_info[];
 
 #ifdef MWERKS_GEKKO
 static asm char* GetR2(void)
@@ -31,9 +27,13 @@ void __init_cpp_exceptions(void)
 {
 #ifdef MWERKS_GEKKO
     if (fragmentID == -2) {
-        fragmentID = __register_fragment(_eti_init_info_, GetR2());
+        fragmentID = __register_fragment(_eti_init_info, GetR2());
     }
 #else
     NOT_IMPLEMENTED;
 #endif
 }
+
+SECTION_CTORS void* const __init_cpp_exceptions_reference  = __init_cpp_exceptions;
+SECTION_DTORS void* const __destroy_global_chain_reference = __destroy_global_chain;
+SECTION_DTORS void* const __fini_cpp_exceptions_reference  = __fini_cpp_exceptions;
