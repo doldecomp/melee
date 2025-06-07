f6406df17dee2b11a226a2c217e06cd0f161d6a7
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2025-06-06 19:37:36 -0700

    Match it_2E6A (ityaku) (#1577)

--- src/melee/it/items/it_2E6A.h
+++ src/melee/it/items/it_2E6A.h
@@ -11,7 +11,9 @@
 #include <dolphin/mtx.h>
 
 /* 2E6AEC */ Item_GObj* it_802E6AEC(Ground*, int, int, HSD_JObj*, Vec3*, int,
-                                    int, HSD_GObjEvent, int);
+                                    void (*)(Item_GObj*, Ground*),
+                                    void (*)(Item_GObj*, Ground*, Vec3*, HSD_GObj*, f32),
+                                    void (*)(Item_GObj*, Ground*, HSD_GObj*));
 /* 2E6F7C */ bool it_802E6F7C(Item_GObj*);
 /* 2E6FC0 */ bool it_802E6FC0(Item_GObj*);
 /* 2E709C */ void it_802E709C(Item_GObj*, HSD_GObj*);
