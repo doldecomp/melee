eb34ffca16540122c01f87a8d393e97dd691e81d
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2025-06-07 17:15:30 -0700

    Match itkirbygamewatchchefpan (#1582)

--- src/melee/it/items/itkirbygamewatchchefpan.h
+++ src/melee/it/items/itkirbygamewatchchefpan.h
@@ -1,13 +1,17 @@
-#ifndef GALE01_2C74D8
-#define GALE01_2C74D8
+#ifndef ITKIRBY_GAMEWATCH_CHEFPAN_H
+#define ITKIRBY_GAMEWATCH_CHEFPAN_H
 
-#include "it/forward.h"
+#include <melee/it/forward.h>
+#include <melee/it/items/types.h>
 
-#include "it/items/types.h"
-
+/* 2C74D8 */ Item_GObj* it_802C74D8(HSD_GObj* parent, Vec3* pos, Fighter_Part, float dir);
 /* 2C75DC */ void it_802C75DC(Item_GObj*);
+/* 2C760C */ void it_802C760C(Item_GObj*);
+/* 2C7658 */ void it_802C7658(Item_GObj*);
+/* 2C7678 */ void it_802C7678(Item_GObj*);
 /* 2C7698 */ void it_802C7698(Item_GObj*);
-/* 2C7754 */ void it_802C7754(Item_GObj*, Item_GObj*);
+/* 2C76E8 */ bool it_802C76E8(Item_GObj*);
+/* 2C7754 */ void it_802C7754(Item_GObj*, HSD_GObj* ref_gobj);
 /* 3F7958 */ extern ItemStateTable it_803F7958[];
 
 #endif
