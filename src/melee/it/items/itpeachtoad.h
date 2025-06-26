e07049984e19ff44b9dce46dd27ac48a21519655
darsilrod <96585662+darsilrod@users.noreply.github.com>
2025-06-26 22:32:58 +0200

    Match 100% itpeachtoad.c (#1642)

--- src/melee/it/items/itpeachtoad.h
+++ src/melee/it/items/itpeachtoad.h
@@ -8,16 +8,16 @@
 
 #include <dolphin/mtx.h>
 
-/* 2BDE18 */ HSD_GObj* it_802BDE18(Item_GObj*, Vec3*, Fighter_Part, enum_t,
-                                   float);
+/* 2BDE18 */ Item_GObj* it_802BDE18(Item_GObj*, Vec3*, Fighter_Part, ItemKind,
+                                    f32);
 /* 2BDEFC */ void it_802BDEFC(Item_GObj*);
 /* 2BDF40 */ void it_802BDF40(Item_GObj* gobj);
 /* 2BDFA0 */ void it_802BDFA0(Item_GObj* gobj);
 /* 2BDFC0 */ void it_802BDFC0(Item_GObj* gobj);
 /* 2BDFE0 */ void it_802BDFE0(Item_GObj*);
-/* 2BE02C */ UNK_RET it_802BE02C(UNK_PARAMS);
+/* 2BE02C */ bool it_802BE02C(Item_GObj*);
 /* 2BE100 */ void it_802BE100(Item_GObj* gobj);
-/* 2BE168 */ UNK_RET it_802BE168(UNK_PARAMS);
+/* 2BE168 */ bool it_802BE168(Item_GObj*);
 /* 2BE1F4 */ void it_802BE1F4(Item_GObj*, Item_GObj*);
 /* 3F7528 */ extern ItemStateTable it_803F7528[];
 
