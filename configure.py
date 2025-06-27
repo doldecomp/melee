eac4536f8f6ad805b3d3dbae797299d52a660e94
Ryan Burns <rtburns@protonmail.com>
2025-06-26 23:14:18 -0700

    Restore OSMemory match
    Was matched before switch to extern/dolphin

--- configure.py
+++ configure.py
@@ -1407,7 +1407,7 @@ config.libs = [
             Object(Matching, "dolphin/os/OSFont.c"),
             Object(Matching, "dolphin/os/OSInterrupt.c"),
             Object(Matching, "dolphin/os/OSLink.c"),
-            Object(NonMatching, "dolphin/os/OSMemory.c"),
+            Object(Matching, "dolphin/os/OSMemory.c"),
             Object(Matching, "dolphin/os/OSMutex.c"),
             Object(NonMatching, "dolphin/os/OSReboot.c"),
             Object(Matching, "dolphin/os/OSReset.c"),
