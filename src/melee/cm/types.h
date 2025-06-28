d81a73f08284363cb6ca21d3f88d917fd8930ed5
KELLZ <forestruns@gmail.com>
2025-06-27 22:01:40 -0230

    Camera work

--- src/melee/cm/types.h
+++ src/melee/cm/types.h
@@ -17,10 +17,10 @@ struct CameraBox {
     /* +C:2 */ u8 xC_b2 : 1;
     s16 xE;
     Vec3 x10; // might be Vec2?
-    Vec3 x1C;
-    float x28;
+    Vec3 pos;
+    float dir;
     Vec2 x2C;
-    Vec3 x34;
+    Vec3 size;
     Vec2 x40;
     Vec3 x48;
     Vec3 x54;
@@ -45,11 +45,9 @@ struct CameraBounds {
     float z_pos;
 };
 
-struct UnkInternalCameraStruct {
-    /* 0x0 */ s32 x0;
-    /* 0x4 */ s32 x4;
-    /* 0x8 */ s32 x8;
-    /* 0xC */ s32 xC;
+struct CameraQuake {
+    /* 0x0 */ Vec3 x0;
+    /* 0xC */ int type;
 };
 
 // global vars for camera mode 7 and 8
@@ -85,8 +83,8 @@ typedef struct Camera {
     /* 0x0A4 */ f32 unk_A4;
     /* 0x0A8 */ f32 unk_A8;
     /* 0x0AC */ f32 unk_AC; /* inferred */
-    /* 0x0B0 */ struct UnkInternalCameraStruct unk_B0[2][8];
-    /* 0x1B0 */ struct UnkInternalCameraStruct unk_1B0[2][8];
+    /* 0x0B0 */ struct CameraQuake unk_B0[2][8];
+    /* 0x1B0 */ struct CameraQuake unk_1B0[2][8];
     /* 0x2B0 */ float unk_2B0;
     /* 0x2B4 */ float unk_2B4;
     /* 0x2B8 */ s16 unk_2B8;
@@ -96,18 +94,32 @@ typedef struct Camera {
     /* 0x2C4 */ s8 unk_2C4; /* unk player slot */
     /* 0x2C5 */ s8 unk_2C5;
     /* 0x2C6 */ char pad_2C6[0x2C8 - 0x2C6];
-    /* 0x2C8 */ float unk_2C8;
-    /* 0x2CC */ float unk_2CC;
-    /* 0x2D0 */ char pad_2D0[0x341 - 0x2D0];
+    /* 0x2C8 */ float pitch_offset;
+    /* 0x2CC */ float yaw_offset;
+    /* 0x2D0 */ char pad_2D0[0x2F8 - 0x2D0];
+    /* 0x2F8 */ f32 unk_2F8;
+    /* 0x2FC */ f32 unk_2FC;
+    /* 0x300 */ char pad_300[0x320 - 0x300];
+    /* 0x320 */ Vec3 unk_320;
+    /* 0x32C */ f32 unk_32C;
+    /* 0x330 */ f32 unk_330;
+    /* 0x334 */ Vec3 unk_334;
+    /* 0x340 */ u8 unk_341;
     /* 0x341:0 */ u8 unk_341_b0 : 1;
-    /* 0x341:1 */ u8 unk_341_b1 : 1;
-    /* 0x341:2 */ u8 unk_341_b2 : 1;
-    /* 0x341:3 */ u8 unk_341_b3 : 1;
-    /* 0x341:4 */ u8 unk_341_b4 : 1;
-    /* 0x341:5 */ u8 unk_341_b5 : 1;
-    /* 0x341:6 */ u8 unk_341_b6 : 1;
+    /* 0x341:1 */ u8 unk_341_b1_b2 : 2;
+    /* 0x341:3 */ u8 unk_341_b3_b4 : 2;
+    /* 0x341:5 */ u8 unk_341_b5_b6 : 2;
     /* 0x341:7 */ u8 unk_341_b7 : 1;
-    /* 0x342 */ char pad_342[0x56]; /* maybe part of unk_341[0x57]? */
+    /* 0x342 */ char pad_342[0x350 - 0x342]; /* maybe part of unk_341[0x57]? */
+    /* 0x350 */ Vec3 unk_350;
+    /* 0x35C */ union {
+        Vec3          vec;
+        s32 (*cb)(f32*);
+    } unk_35C;
+    /* 0x368 */ Vec3 unk_368;
+    /* 0x374 */ f32 unk_374;
+    /* 0x378 */ f32 unk_378;
+    /* 0x378 */ char pad_37C[0x398 - 0x37C];
     /* 0x398:0 */ u8 unk_398_b0 : 1;
     /* 0x398:1 */ u8 unk_398_b1 : 1;
     /* 0x398:2 */ u8 unk_398_b2 : 1;
@@ -148,7 +160,7 @@ struct CameraUnkGlobals {
     /* +44 */ float _44[43];
 };
 struct CameraModeCallbacks {
-    void* callback[9];
+    void (*(callback[9]))(void*);
 };
 
 struct CameraFixednessMult {
