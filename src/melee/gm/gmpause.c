0a7d23fcccda3e0afa9375642a3ed372c99e1688
Ryan Burns <rtburns@protonmail.com>
2025-07-17 18:59:36 -0700

    Clean up fn_801A1134

--- src/melee/gm/gmpause.c
+++ src/melee/gm/gmpause.c
@@ -4,6 +4,7 @@
 #include <melee/gm/types.h>
 #include <melee/lb/lb_00F9.h>
 #include <melee/lb/lbarchive.h>
+#include <melee/sc/types.h>
 #include <sysdolphin/baselib/controller.h>
 #include <sysdolphin/baselib/gobj.h>
 #include <sysdolphin/baselib/gobjgxlink.h>
@@ -11,6 +12,15 @@
 #include <sysdolphin/baselib/gobjproc.h>
 #include <sysdolphin/baselib/jobj.h>
 #include <math.h>
+
+struct PauseData {
+/* +0 */ HSD_JObj* background;
+/* +4 */ HSD_JObj* analog_stick;
+/* +8 */ HSD_JObj* lras;
+/* +C */ HSD_JObj* z;
+/* +10 */ HSD_JObj* analog_stick_outline;
+/* +14 */ s32 slot;
+};
 
 static struct PauseData lbl_80479B10;
 static HSD_Archive* lbl_804D6700;
@@ -66,23 +76,23 @@ void gm_801A10FC(int unused)
 
 void fn_801A1134(void)
 {
-    HSD_Joint**** joint;
+    SceneDesc* scene;
     HSD_GObj* gobj;
     HSD_JObj* jobj;
 
-    lbl_804D6700 = lbArchive_80016DBC("GmPause", &joint, "ScGamPause_scene_data", 0);
+    lbl_804D6700 = lbArchive_80016DBC("GmPause", &scene, "ScGamPause_scene_data", 0);
     gobj = GObj_Create(0xEU, 2U, 0U);
     lbl_804D6704 = gobj;
-    jobj = HSD_JObjLoadJoint(***joint);
+    jobj = HSD_JObjLoadJoint(scene->models[0]->joint);
     lbl_80479B10.background = jobj;
     lb_80011E24(jobj, &lbl_80479B10.analog_stick, 1, -1);
     lb_80011E24(jobj, &lbl_80479B10.lras, 9, -1);
     lb_80011E24(jobj, &lbl_80479B10.z, 10, -1);
     lb_80011E24(jobj, &lbl_80479B10.analog_stick_outline, 11, -1);
     lbl_80479B10.analog_stick = HSD_JObjGetChild(lbl_80479B10.analog_stick);
-    HSD_GObjObject_80390A70(gobj, (u8) HSD_GObj_804D7849, jobj);
+    HSD_GObjObject_80390A70(gobj, HSD_GObj_804D7849, jobj);
     GObj_SetupGXLink(gobj, HSD_GObj_JObjCallback, 0xBU, 0U);
-    gm_8016895C(jobj, **joint, 0);
+    gm_8016895C(jobj, scene->models[0], 0);
     HSD_JObjReqAnimAll(jobj, 1.0f);
     HSD_JObjAnimAll(jobj);
     HSD_JObjSetFlagsAll(jobj, 0x10U);
