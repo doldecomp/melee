16f5075fb246f61ac99345a6c89b015d4d01d278
Ryan Burns <rtburns@protonmail.com>
2025-08-06 17:15:06 -0700

    Split out game mode files from gm_1B03

---
+++ src/melee/gm/gmallstar.h
@@ -0,0 +1,22 @@
+#ifndef MELEE_GM_ALLSTAR_H
+#define MELEE_GM_ALLSTAR_H
+
+#include <placeholder.h>
+#include <melee/gm/forward.h>
+#include <melee/mn/forward.h>
+
+/* 1B5324 */ UNK_RET gm_801B5324(UNK_PARAMS);
+/* 1B5624 */ void gm_801B5624(MinorScene*);
+/* 1B59AC */ void gm_801B59AC(MinorScene*);
+/* 1B5AA8 */ void fn_801B5AA8(int);
+/* 1B5ACC */ void gm_801B5ACC(MinorScene*);
+/* 1B5E7C */ void gm_801B5E7C(MinorScene*);
+/* 1B5EB4 */ void gm_801B5EB4(MinorScene*);
+/* 1B5EE4 */ void gm_801B5EE4(MinorScene*);
+/* 1B5F50 */ void gm_801B5F50(MinorScene*);
+/* 1B5FB4 */ void gm_801B5FB4(MinorScene*);
+/* 1B607C */ void gm_801B607C(MinorScene*);
+/* 1B60A4 */ void gm_801B60A4_OnLoad(void);
+/* 1B62D8 */ void gm_801B62D8_OnInit(void);
+
+#endif
---
+++ src/melee/gm/gmmultiman.h
@@ -0,0 +1,45 @@
+#ifndef MELEE_GM_MULTIMAN_H
+#define MELEE_GM_MULTIMAN_H
+
+#include <placeholder.h>
+
+#include <melee/gm/forward.h>
+#include <melee/mn/forward.h>
+
+/* 1B6320 */ UNK_T gm_801B6320(void);
+/* 1B632C */ void gm_801B632C(MinorScene*);
+/* 1B63C4 */ void gm_801B63C4(MinorScene*);
+/* 1B6428 */ void gm_801B6428(MinorScene*);
+/* 1B65D4 */ void gm_801B65D4(MinorScene*);
+/* 1B67E8 */ void gm_801B67E8_OnInit(void);
+/* 1B6808 */ void gm_801B6808_OnLoad(void);
+/* 1B6834 */ UNK_RET gm_801B6834(UNK_PARAMS);
+/* 1B685C */ UNK_RET gm_801B685C(UNK_PARAMS);
+/* 1B688C */ UNK_RET gm_801B688C(UNK_PARAMS);
+/* 1B69C0 */ void gm_801B69C0(StartMeleeData*);
+/* 1B6AD8 */ void gm_801B6AD8(MinorScene*);
+/* 1B6B70 */ void gm_801B6B70(MinorScene*);
+/* 1B6BE8 */ void gm_801B6BE8(MinorScene*);
+/* 1B6F44 */ void gm_801B6F44(MinorScene*);
+/* 1B7044 */ void gm_801B7044(MinorScene*);
+/* 1B70DC */ void gm_801B70DC(MinorScene*);
+/* 1B7154 */ void gm_801B7154(MinorScene*);
+/* 1B74F0 */ void gm_801B74F0(MinorScene*);
+/* 1B75F0 */ void gm_801B75F0(MinorScene*);
+/* 1B7688 */ void gm_801B7688(MinorScene*);
+/* 1B7700 */ void gm_801B7700(MinorScene*);
+/* 1B7AA0 */ void gm_801B7AA0(MinorScene*);
+/* 1B7B74 */ void gm_801B7B74(MinorScene*);
+/* 1B7C0C */ void gm_801B7C0C(MinorScene*);
+/* 1B7C84 */ void gm_801B7C84(MinorScene*);
+/* 1B8024 */ void gm_801B8024(MinorScene*);
+/* 1B8110 */ void gm_801B8110(MinorScene*);
+/* 1B81A8 */ void gm_801B81A8(MinorScene*);
+/* 1B8220 */ void gm_801B8220(MinorScene*);
+/* 1B8580 */ void gm_801B8580(MinorScene*);
+/* 1B863C */ void gm_801B863C(MinorScene*);
+/* 1B86D4 */ void gm_801B86D4(MinorScene*);
+/* 1B874C */ void gm_801B874C(MinorScene*);
+/* 1B8AF8 */ void gm_801B8AF8(MinorScene*);
+
+#endif
