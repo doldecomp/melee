46fb2bd9cc1754f5230278fae4713723f4e37adc
Syro <network.9961@gmail.com>
2025-08-11 13:54:04 -0600

    Match mnNameNew_8023EA08

--- src/melee/mn/mnnamenew.c
+++ src/melee/mn/mnnamenew.c
@@ -1,1 +1,5 @@
+#include "mnnamenew.h"
 
+void mnNameNew_8023EA08(void) {
+    mnNameNew_EnterFromMnCharSel(4);
+}
--- src/melee/mn/mnnamenew.h
+++ src/melee/mn/mnnamenew.h
@@ -1,1 +1,7 @@
+#ifndef MELEE_MN_NAME_NEW_H
+#define MELEE_MN_NAME_NEW_H
 
+/* 23E6D0 */ void mnNameNew_EnterFromMnCharSel(int);
+/* 23EA08 */ void mnNameNew_8023EA08(void);
+
+#endif
