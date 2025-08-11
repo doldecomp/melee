cf36173289c9c9cebf9150487d0d18919e9bdd12
Iván Ayala <network.9961@gmail.com>
2025-08-11 14:13:48 -0600

    Match mnNameNew_8023EA08 (#1827)
    Co-authored-by: Robin Avery <robin@ribbanya.dev>

--- src/melee/mn/mnnamenew.c
+++ src/melee/mn/mnnamenew.c
@@ -1,1 +1,54 @@
+#include "mnnamenew.h"
 
+/// #mnNameNew_8023B0F8
+
+/// #mnNameNew_8023B224
+
+/// #mnNameNew_8023B314
+
+/// #mnNameNew_KeySetup
+
+/// #mnNameNew_8023BAA8
+
+/// #PickAutoName
+
+/// #NameContainsOnlySpaces
+
+/// #WriteCharactersForNameAtIndex
+
+/// #AddCharacterToName
+
+/// #mnNameNew_GlyphVariantInput
+
+/// #mnNameNew_MainInput
+
+/// #mnNameNew_8023CE4C
+
+/// #fn_8023CFC8
+
+/// #fn_8023D0F8
+
+/// #mnNameNew_8023D130
+
+/// #mnNameNew_GlyphVariantSetup
+
+/// #mnNameNew_8023DA08
+
+/// #fn_8023DAEC
+
+/// #fn_8023DBE8
+
+/// #mnNameNew_8023E0D8
+
+/// #InitNameEntryUIState
+
+/// #mnNameNew_8023E32C
+
+/// #mnNameNew_EnterFromMnName
+
+/// #mnNameNew_EnterFromMnCharSel
+
+void mnNameNew_8023EA08(UNK_T arg0)
+{
+    mnNameNew_EnterFromMnCharSel(arg0, 4);
+}
