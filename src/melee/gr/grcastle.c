fe6fa0ec4e032e9b2b82b6c72eef0bcfecdc5bf6
lukechampine <luke.champine@gmail.com>
2025-08-12 16:38:02 -0400

    grcastle work

--- src/melee/gr/grcastle.c
+++ src/melee/gr/grcastle.c
@@ -1,41 +1,53 @@
-#include <platform.h>
+#include "gr/grcastle.h"
 
+#include <platform.h>
+#include "gr/forward.h"
+#include "baselib/psstructs.h"
+#include "placeholder.h"
+
+#include "ft/ftlib.h"
+#include "gr/grlib.h"
+#include "gr/grmaterial.h"
 #include "gr/ground.h"
+#include "gr/grzakogenerator.h"
+#include "it/it_26B1.h"
 #include "lb/lb_00B0.h"
 
 #include <dolphin/mtx.h>
 #include <baselib/jobj.h>
 
-/* 1CD4A0 */ static void grCastle_801CD4A0(void);
-/* 1CD4C8 */ static bool grCastle_801CD4C8(void);
-/* 1CD600 */ static bool grCastle_801CD600(Ground_GObj* arg);
-/* 1CD608 */ static void grCastle_801CD608(Ground_GObj* arg);
-/* 1CD60C */ static void grCastle_801CD60C(Ground_GObj* arg);
-/* 1CD8A0 */ static bool grCastle_801CD8A0(Ground_GObj* arg);
-/* 1CDC3C */ static bool grCastle_801CDC3C(Ground_GObj* arg);
-/* 1CDF50 */ static void grCastle_801CDF50(Ground_GObj* arg);
-/* 1CE3A0 */ static void fn_801CE3A0(void);
-/* 1CE3A4 */ static bool grCastle_801CE3A4(Ground_GObj* arg);
-/* 1CE7E4 */ static void grCastle_801CE7E4(Ground_GObj* arg);
-/* 1CE858 */ static bool grCastle_801CE858(Ground_GObj* arg);
-/* 1CE8E4 */ void grCastle_801CE8E4(Ground_GObj* arg);
-/* 1CE9DC */ static void fn_801CE9DC(void);
-/* 1CE9E0 */ static bool grCastle_801CE9E0(Ground_GObj* arg);
-/* 1CEAC8 */ static void grCastle_801CEAC8(Ground_GObj* arg);
-/* 1CEEFC */ static bool grCastle_801CEEFC(Ground_GObj* arg);
-/* 1CF0F0 */ static void grCastle_801CF0F0(Ground_GObj* arg);
-/* 1CF300 */ static bool grCastle_801CF300(Ground_GObj* arg);
-/* 1CF74C */ static void grCastle_801CF74C(Ground_GObj* arg);
-/* 1D0B04 */ static DynamicsDesc* grCastle_801D0B04(enum_t arg);
-/* 1D0B0C */ static bool grCastle_801D0B0C(Vec3* v, int arg1, HSD_JObj* jobj);
+unkCastleCallback grCs_803B7F28[] = {
+    grCastle_801D0550, grCastle_801D059C, grCastle_801D05E8,
+    grCastle_801D0634, grCastle_801D0680,
+};
 
-/// #grCastle_801CD338
+unkCastleCallback2 grCs_803B7F3C[] = {
+    grCastle_801D06CC, grCastle_801D0744, grCastle_801D07BC,
+    grCastle_801D0834, grCastle_801D08AC,
+};
+
+void grCastle_801CD338(bool arg0)
+{
+    HSD_GObj* gobj;
+    HSD_JObj* jobj;
+
+    gobj = Ground_801C2BA4(6);
+    if (gobj != NULL) {
+        jobj = Ground_801C3FA4(gobj, 4);
+        if (jobj != NULL) {
+            HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+        }
+    }
+}
 
 /// #grCastle_801CD37C
 
 void grCastle_801CD4A0(void) {}
 
-/// #grCastle_801CD4A4
+void grCastle_801CD4A4(void)
+{
+    grZakoGenerator_801CAE04(0);
+}
 
 bool grCastle_801CD4C8(void)
 {
@@ -46,20 +58,20 @@ bool grCastle_801CD4C8(void)
 
 /// #grCastle_801CD5BC
 
-bool grCastle_801CD600(Ground_GObj* arg)
+bool grCastle_801CD600(Ground_GObj* gobj)
 {
     return false;
 }
 
-void grCastle_801CD608(Ground_GObj* arg) {}
+void grCastle_801CD608(Ground_GObj* gobj) {}
 
-void grCastle_801CD60C(Ground_GObj* arg) {}
+void grCastle_801CD60C(Ground_GObj* gobj) {}
 
 /// #fn_801CD610
 
 /// #grCastle_801CD658
 
-bool grCastle_801CD8A0(Ground_GObj* arg)
+bool grCastle_801CD8A0(Ground_GObj* gobj)
 {
     return false;
 }
@@ -72,14 +84,14 @@ bool grCastle_801CD8A0(Ground_GObj* arg)
 
 /// #grCastle_801CDA0C
 
-bool grCastle_801CDC3C(Ground_GObj* arg)
+bool grCastle_801CDC3C(Ground_GObj* gobj)
 {
     return false;
 }
 
 /// #grCastle_801CDC44
 
-void grCastle_801CDF50(Ground_GObj* arg) {}
+void grCastle_801CDF50(Ground_GObj* gobj) {}
 
 /// #grCastle_801CDF54
 
@@ -93,7 +105,7 @@ void grCastle_801CDF50(Ground_GObj* arg) {}
 
 void fn_801CE3A0(void) {}
 
-bool grCastle_801CE3A4(Ground_GObj* arg)
+bool grCastle_801CE3A4(Ground_GObj* gobj)
 {
     return false;
 }
@@ -102,53 +114,53 @@ bool grCastle_801CE3A4(Ground_GObj* arg)
 
 /// #grCastle_801CE578
 
-void grCastle_801CE7E4(Ground_GObj* arg) {}
+void grCastle_801CE7E4(Ground_GObj* gobj) {}
 
 /// #grCastle_801CE7E8
 
-bool grCastle_801CE858(Ground_GObj* arg)
+bool grCastle_801CE858(Ground_GObj* gobj)
 {
     return false;
 }
 
 /// #grCastle_801CE860
 
-void grCastle_801CE8E4(Ground_GObj* arg) {}
+void grCastle_801CE8E4(Ground_GObj* gobj) {}
 
 /// #grCastle_801CE8E8
 
 void fn_801CE9DC(void) {}
 
-bool grCastle_801CE9E0(Ground_GObj* arg)
+bool grCastle_801CE9E0(Ground_GObj* gobj)
 {
     return false;
 }
 
 /// #grCastle_801CE9E8
 
-void grCastle_801CEAC8(Ground_GObj* arg) {}
+void grCastle_801CEAC8(Ground_GObj* gobj) {}
 
 /// #grCastle_801CEACC
 
-bool grCastle_801CEEFC(Ground_GObj* arg)
+bool grCastle_801CEEFC(Ground_GObj* gobj)
 {
     return false;
 }
 
 /// #grCastle_801CEF04
 
-void grCastle_801CF0F0(Ground_GObj* arg) {}
+void grCastle_801CF0F0(Ground_GObj* gobj) {}
 
 /// #grCastle_801CF0F4
 
-bool grCastle_801CF300(Ground_GObj* arg)
+bool grCastle_801CF300(Ground_GObj* gobj)
 {
     return false;
 }
 
 /// #grCastle_801CF308
 
-void grCastle_801CF74C(Ground_GObj* arg) {}
+void grCastle_801CF74C(Ground_GObj* gobj) {}
 
 /// #fn_801CF750
 
@@ -164,29 +176,87 @@ void grCastle_801CF74C(Ground_GObj* arg) {}
 
 /// #grCastle_801D0298
 
+void grCastle_801D0298(void)
+{
+    grCastle_801CFBD4();
+}
+
 /// #grCastle_801D02B8
 
 /// #fn_801D0520
 
-/// #grCastle_801D0550
+static inline void grCastle_801D0550_sub(unkCastle* arg0, s32 i)
+{
+    arg0->x134[i] = 1;
+    grMaterial_801C8CDC(arg0->x10C[i]);
+    arg0->x10C[i] = NULL;
+    Ground_801C53EC(0x53020U);
+}
 
-/// #grCastle_801D059C
+void grCastle_801D0550(void* arg0, unkCastle* arg1)
+{
+    PAD_STACK(16);
+    grCastle_801D0550_sub(arg1, 0);
+}
 
-/// #grCastle_801D05E8
+void grCastle_801D059C(void* arg0, unkCastle* arg1)
+{
+    PAD_STACK(16);
+    grCastle_801D0550_sub(arg1, 1);
+}
 
-/// #grCastle_801D0634
+void grCastle_801D05E8(void* arg0, unkCastle* arg1)
+{
+    PAD_STACK(16);
+    grCastle_801D0550_sub(arg1, 2);
+}
 
-/// #grCastle_801D0680
+void grCastle_801D0634(void* arg0, unkCastle* arg1)
+{
+    PAD_STACK(16);
+    grCastle_801D0550_sub(arg1, 3);
+}
 
-/// #grCastle_801D06CC
+void grCastle_801D0680(void* arg0, unkCastle* arg1)
+{
+    PAD_STACK(16);
+    grCastle_801D0550_sub(arg1, 4);
+}
 
-/// #grCastle_801D0744
+static void grCastle_801D06CC_sub(unkCastle* arg0, Ground_GObj* gobj, s32 i)
+{
+    if ((ftLib_80086960(gobj)) || (itGetKind(gobj) != 0xA0)) {
+        arg0->x134[i] = 1;
+        grMaterial_801C8CDC(arg0->x10C[i]);
+        arg0->x10C[i] = NULL;
+        Ground_801C53EC(0x53020U);
+    }
+}
 
-/// #grCastle_801D07BC
+void grCastle_801D06CC(void* arg0, unkCastle* arg1, Ground_GObj* gobj)
+{
+    grCastle_801D06CC_sub(arg1, gobj, 0);
+}
 
-/// #grCastle_801D0834
+void grCastle_801D0744(void* arg0, unkCastle* arg1, Ground_GObj* gobj)
+{
+    grCastle_801D06CC_sub(arg1, gobj, 1);
+}
 
-/// #grCastle_801D08AC
+void grCastle_801D07BC(void* arg0, unkCastle* arg1, Ground_GObj* gobj)
+{
+    grCastle_801D06CC_sub(arg1, gobj, 2);
+}
+
+void grCastle_801D0834(void* arg0, unkCastle* arg1, Ground_GObj* gobj)
+{
+    grCastle_801D06CC_sub(arg1, gobj, 3);
+}
+
+void grCastle_801D08AC(void* arg0, unkCastle* arg1, Ground_GObj* gobj)
+{
+    grCastle_801D06CC_sub(arg1, gobj, 4);
+}
 
 /// #fn_801D0924
 
@@ -219,10 +289,24 @@ bool grCastle_801D0B0C(Vec3* v, int arg1, HSD_JObj* jobj)
     return 0;
 }
 
+void grCastle_801D0A9C(Vec3* arg0, f32 arg8)
+{
+    HSD_Generator* gen = grLib_801C96F8(0x7530, 0x1E, arg0);
+    if (gen != NULL) {
+        gen = (HSD_Generator* ) gen->appsrt;
+        gen->pos.x *= arg8;
+        gen->pos.y *= arg8;
+        gen->pos.z *= arg8;
+    }
+}
+
 /// #grCastle_801D0BBC
 
 /// #grCastle_801D0D24
 
 /// #grCastle_801D0D84
 
-/// #grCastle_801D0FF0
+f32 grCastle_801D0FF0(void)
+{
+    return 50.0f * Ground_801C0498();
+}
