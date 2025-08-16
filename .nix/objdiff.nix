41da4197b5a0dae0b36499b47621dc5ec207b94c
Ryan Burns <rtburns@protonmail.com>
2025-08-16 14:19:40 -0700

    Fix objdiff build error due to duplicated similar dep in lockfile

--- .nix/objdiff.nix
+++ .nix/objdiff.nix
@@ -4,23 +4,30 @@
 , pkg-config
 , rustPlatform
 , stdenv
+, srcOnly
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "objdiff";
   version = "3.0.0";
 
-  src = fetchFromGitHub {
-    owner = "encounter";
-    repo = "objdiff";
-    rev = "v${version}";
-    hash = "sha256-ycO1koQDRA1WlRmLJrI0xxrIdd+v6IfW+JVAg0cuBa0=";
+  src = srcOnly {
+    name = "objdiff-patched";
+    src = fetchFromGitHub {
+      owner = "encounter";
+      repo = "objdiff";
+      rev = "v${version}";
+      hash = "sha256-ycO1koQDRA1WlRmLJrI0xxrIdd+v6IfW+JVAg0cuBa0=";
+    };
+    patches = [
+      ./duplicate-similar-dep.patch
+    ];
   };
 
   cargoLock.lockFile = "${src}/Cargo.lock";
   cargoLock.outputHashes = {
     "gimli-0.32.0" = "sha256-a00uNPu3YbP/z8Xx+MilnAvHMVvDGnDbMqNLmovosQQ=";
-    "similar-2.7.0" = "";
+    "similar-2.7.0" = "sha256-D25BooCa48IGY7FZQoVW2u8U6BVIcGt7eiwmvT8wsKE=";
   };
 
   nativeBuildInputs = [
