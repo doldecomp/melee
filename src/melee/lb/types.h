df800b68db5ee6f74be7de09427a7fc3b4da8792
Robin Avery <robin@ribbanya.dev>
2025-08-16 18:23:32 -0400

    Fix m2c crash (#1848)

--- src/melee/lb/types.h
+++ src/melee/lb/types.h
@@ -351,40 +351,43 @@ struct ColorOverlay_UnkInner {
     /* +7B */ u8 x7B;
 };
 
+union ColorOverlay_x8_t {
+    GXColor light_color;
+    struct {
+        s32 unk : 6;
+        s32 x : 13;
+        s32 yz : 13;
+    } light_rot1;
+    struct {
+        u32 x0_0 : 1;
+        u32 x0_1 : 1;
+        u32 x0_2 : 1;
+        u32 x0_3 : 1;
+        u32 x0_4 : 1;
+        u32 x0_5 : 1;
+        u32 light_enable : 1;
+        u32 x0_7 : 1;
+        s32 x : 12;
+        s32 yz : 12;
+    } light_rot2;
+    struct {
+        u32 unk : 6;
+        u32 timer : 26;
+    } unk;
+};
+STATIC_ASSERT(sizeof(union ColorOverlay_x8_t) == 0x4);
+
 struct ColorOverlay {
     s32 x0_timer; // 0x0
     s32 x4_pri;   // 0x4  this colanims priority, lower = will persist
-    union ColorOverlay_x8_t {
-        GXColor light_color;
-        struct {
-            s32 unk : 6;
-            s32 x : 13;
-            s32 yz : 13;
-        } light_rot1;
-        struct {
-            u32 x0_0 : 1;
-            u32 x0_1 : 1;
-            u32 x0_2 : 1;
-            u32 x0_3 : 1;
-            u32 x0_4 : 1;
-            u32 x0_5 : 1;
-            u32 light_enable : 1;
-            u32 x0_7 : 1;
-            s32 x : 12;
-            s32 yz : 12;
-        } light_rot2;
-        struct {
-            u32 unk : 6;
-            u32 timer : 26;
-        } unk;
-    }* x8_ptr1;     // 0x8
-    s32 xC_loop;    // 0xc
-    s32* x10_ptr2;  // 0x10
-    s32 x14;        // 0x14
-    s32* x18_alloc; // 0x18
-    s32 x1c;        // 0x1c
-    s32 x20;        // 0x20
-    s32 x24;        // 0x24
+    union ColorOverlay_x8_t* x8_ptr1; // 0x8
+    s32 xC_loop;                      // 0xc
+    s32* x10_ptr2;                    // 0x10
+    s32 x14;                          // 0x14
+    s32* x18_alloc;                   // 0x18
+    s32 x1c;                          // 0x1c
+    s32 x20;                          // 0x20
+    s32 x24;                          // 0x24
     union {
         enum_t i;
         struct ColorOverlay_UnkInner* ptr;
@@ -419,7 +422,6 @@ struct ColorOverlay {
     u8 x7C_flag8 : 1;         // 0x7c, 0x01
 };
 STATIC_ASSERT(sizeof(struct ColorOverlay) == 0x80);
-STATIC_ASSERT(sizeof(union ColorOverlay_x8_t) == 0x4);
 
 struct lb_80011A50_t {
     /*  +0 */ u8 x0;
