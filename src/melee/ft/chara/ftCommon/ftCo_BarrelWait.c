4fc73f81214f883e06634a1e715b144644d9fe82
Robin Avery <robin@ribbanya.dev>
2025-08-19 21:54:04 -0400

    Link `ftCo_BarrelWait` (#1856)

--- src/melee/ft/chara/ftCommon/ftCo_BarrelWait.c
+++ src/melee/ft/chara/ftCommon/ftCo_BarrelWait.c
@@ -24,10 +24,6 @@
 #include <common_structs.h>
 
 /* 09EC44 */ static void ftCo_8009EC44(Fighter_GObj* gobj);
-/* 09EC70 */ static void ftCo_8009EC70(Fighter_GObj* gobj, Vec3* pos,
-                                       UNK_T arg2, float kb_angle);
-
-#pragma force_active on
 
 static inline void setCamData(ftCommonData* cd, CameraBox* cam)
 {
@@ -37,13 +33,14 @@ static inline void setCamData(ftCommonData* cd, CameraBox* cam)
 
 void ftCo_8009EB18(Fighter_GObj* gobj)
 {
-    u8 _[8] = { 0 };
-    Fighter* fp = gobj->user_data;
+    Fighter* fp = GET_FIGHTER(gobj);
+    PAD_STACK(4);
+
     ftCommon_8007DB58(gobj);
     ftCo_8009750C(gobj);
     ftCo_800DD168(gobj);
-    Fighter_ChangeMotionState(gobj, ftCo_MS_BarrelWait, Ft_MF_None, 0, 1, 0,
-                              NULL);
+    Fighter_ChangeMotionState(gobj, ftCo_MS_BarrelWait, Ft_MF_None, 0.0f, 1.0f,
+                              0.0f, NULL);
     ftAnim_8006EBA4(gobj);
     fp->death2_cb = ftCo_8009EAF8;
     fp->accessory1_cb = ftCo_8009EC44;
@@ -71,30 +68,52 @@ void ftCo_BarrelWait_Coll(Fighter_GObj* gobj) {}
 void ftCo_8009EC44(Fighter_GObj* gobj)
 {
     float param;
-    u8 _[4] = { 0 };
+    PAD_STACK(4);
     Ground_801C4DA0(&GET_FIGHTER(gobj)->cur_pos, &param);
 }
 
+/// @todo Fake, need to find real size of #HurtCapsule
+struct SmallerHitCapsule {
+    /*  +0 */ HitCapsuleState state;
+    /*  +4 */ u32 x4;
+    /*  +8 */ u32 unk_count;
+    /*  +C */ float damage;
+    /* +10 */ Vec3 b_offset;
+    /* +1C */ float scale;
+    /* +20 */ int kb_angle;
+    /* +24 */ u32 x24;
+    /* +28 */ u32 x28;
+    /* +2C */ u32 x2C;
+    /* +30 */ u32 element;
+    /* +34 */ char pad_34[0xFC];
+};
+
 void ftCo_8009EC70(Fighter_GObj* gobj, Vec3* pos, UNK_T arg2, float kb_angle)
 {
-    HitCapsule hit;
-    Fighter* fp = gobj->user_data;
-    fp->bury_timer_2 = p_ftCommonData->bury_timer_unk2;
-    fp->cur_pos = *pos;
-    mpColl_80043680(&fp->coll_data, pos);
-    ftCamera_80076064(fp);
-    lbColl_80008D30(&hit, arg2);
-    ftColl_800788D4(gobj);
-    if (kb_angle < 0) {
-        kb_angle += 360;
+    {
+        Fighter* fp = gobj->user_data;
+        fp->bury_timer_2 = p_ftCommonData->bury_timer_unk2;
+        fp->cur_pos = *pos;
+        mpColl_80043680(&fp->coll_data, pos);
+        ftCamera_80076064(fp);
+        {
+            struct SmallerHitCapsule hit;
+            PAD_STACK(4);
+            lbColl_80008D30((HitCapsule*) &hit, arg2);
+            ftColl_800788D4(gobj);
+            if (kb_angle < 0) {
+                kb_angle += 360;
+            }
+            kb_angle = fp->facing_dir < 0.0f ? kb_angle : 180.0f - kb_angle;
+            fp->dmg.kb_applied =
+                ftColl_80079EA8(fp, (HitCapsule*) &hit, hit.unk_count);
+            fp->dmg.x1848_kb_angle = kb_angle;
+            fp->dmg.facing_dir_1 = fp->facing_dir;
+            fp->dmg.x184c_damaged_hurtbox = 0;
+            fp->dmg.x1854_collpos = fp->cur_pos;
+            fp->dmg.x1860_element = hit.element;
+            Fighter_UnkTakeDamage_8006CC30(fp, hit.damage);
+        }
+        ftCo_8008DCE0(gobj, 91, 0);
     }
-    kb_angle = fp->facing_dir < 0 ? kb_angle : 180 - kb_angle;
-    fp->dmg.kb_applied = ftColl_80079EA8(fp, &hit, hit.unk_count);
-    fp->dmg.x1848_kb_angle = kb_angle;
-    fp->dmg.facing_dir_1 = fp->facing_dir;
-    fp->dmg.x184c_damaged_hurtbox = 0;
-    fp->dmg.x1854_collpos = fp->cur_pos;
-    fp->dmg.x1860_element = hit.element;
-    Fighter_UnkTakeDamage_8006CC30(fp, hit.damage);
-    ftCo_8008DCE0(gobj, 91, 0);
 }
