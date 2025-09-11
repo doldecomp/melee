b9e5c9aa20553574d8bb31739edaf91aacaeff24
Jonathan Hart <43473765+Quplet@users.noreply.github.com>
2025-09-10 20:32:19 -0400

    Matched it_802C8158, it_802C81C8, it_802C81E8, it_802C8208, (#1909)
    itGamewatchrescue_UnkMotion1_Anim, and it_2725_Logic81_EvtUnk

--- src/melee/it/items/itgamewatchrescue.h
+++ src/melee/it/items/itgamewatchrescue.h
@@ -8,14 +8,14 @@
 #include <dolphin/mtx.h>
 
 /* 2C8038 */ HSD_GObj* it_802C8038(Item_GObj*, Vec3*, s32, s32, float, float);
-/* 2C8158 */ void it_802C8158(Item_GObj*);
-/* 2C81C8 */ void it_802C81C8(Item_GObj*);
-/* 2C81E8 */ void it_802C81E8(Item_GObj*);
-/* 2C8208 */ UNK_RET it_802C8208(UNK_PARAMS);
+/* 2C8158 */ void it_802C8158(Item_GObj* item_gobj);
+/* 2C81C8 */ void it_802C81C8(Item_GObj* item_gobj);
+/* 2C81E8 */ void it_802C81E8(Item_GObj* item_gobj);
+/* 2C8208 */ void it_802C8208(HSD_GObj* hsd_gobj, enum_t msid);
 /* 2C8240 */ bool itGamewatchrescue_UnkMotion1_Anim(Item_GObj* gobj);
 /* 2C8350 */ void itGamewatchrescue_UnkMotion1_Phys(Item_GObj* gobj);
 /* 2C8354 */ bool itGamewatchrescue_UnkMotion1_Coll(Item_GObj* gobj);
-/* 2C835C */ void it_2725_Logic81_EvtUnk(Item_GObj*, Item_GObj*);
+/* 2C835C */ void it_2725_Logic81_EvtUnk(Item_GObj* item_gobj1, Item_GObj* item_gobj2);
 /* 3F79C0 */ extern ItemStateTable it_803F79C0[];
 
 #endif
