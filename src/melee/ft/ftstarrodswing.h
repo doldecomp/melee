95a2dd9d0fa5425d6adb5b2bef359231185f66eb
BR <wishingwellwellwishing@hotmail.com>
2025-09-16 01:47:11 -0700

    match ftstarrodswing

--- src/melee/ft/ftstarrodswing.h
+++ src/melee/ft/ftstarrodswing.h
@@ -5,11 +5,11 @@
 
 #include "ftCommon/forward.h"
 
-/* 0CD82C */ UNK_RET ftCo_800CD82C(UNK_PARAMS);
+/* 0CD82C */ void ftCo_800CD82C(Fighter_GObj*, int, int, float);
 /* 0CD88C */ void ftCo_StarRodSwing_Anim(Fighter_GObj* gobj);
 /* 0CD8AC */ void ftCo_StarRodSwing_IASA(Fighter_GObj* gobj);
 /* 0CD8CC */ void ftCo_StarRodSwing_Phys(Fighter_GObj* gobj);
 /* 0CD8EC */ void ftCo_StarRodSwing_Coll(Fighter_GObj* gobj);
-/* 0CD914 */ UNK_RET ft_800CD914(UNK_PARAMS);
+/* 0CD914 */ void ft_800CD914(Fighter_GObj*);
 
 #endif
