49d317e468c35bbbb4194302754d1391d9b1bbf0
BR- <BR-@users.noreply.github.com>
2025-09-15 21:51:38 -0700

    some ifstock matches (#1912)

--- src/melee/if/ifstock.h
+++ src/melee/if/ifstock.h
@@ -4,33 +4,37 @@
 #include <placeholder.h>
 #include <platform.h>
 
+#include <baselib/forward.h>
+
+#include <dolphin/gx/GXStruct.h>
+
 /* 2F7EFC */ int ifStock_802F7EFC(int, int);
-/* 2F8298 */ UNK_RET ifStock_802F8298(UNK_PARAMS);
-/* 2F89F8 */ UNK_RET ifStock_802F89F8(UNK_PARAMS);
-/* 2F8DB0 */ UNK_RET fn_802F8DB0(UNK_PARAMS);
-/* 2F8E08 */ UNK_RET fn_802F8E08(UNK_PARAMS);
-/* 2F916C */ UNK_RET fn_802F916C(UNK_PARAMS);
-/* 2F9410 */ UNK_RET fn_802F9410(UNK_PARAMS);
-/* 2F94E0 */ UNK_RET fn_802F94E0(UNK_PARAMS);
-/* 2F9548 */ UNK_RET fn_802F9548(UNK_PARAMS);
-/* 2F9598 */ UNK_RET fn_802F9598(UNK_PARAMS);
-/* 2F95E8 */ UNK_RET fn_802F95E8(UNK_PARAMS);
-/* 2F9680 */ UNK_RET fn_802F9680(UNK_PARAMS);
-/* 2F96D0 */ UNK_RET ifStock_802F96D0(UNK_PARAMS);
-/* 2F98E8 */ void ifStock_802F98E8(void);
-/* 2F9F48 */ UNK_RET ifStock_802F9F48(UNK_PARAMS);
-/* 2FA118 */ UNK_RET ifStock_802FA118(UNK_PARAMS);
-/* 2FA2D0 */ UNK_RET ifStock_802FA2D0(int);
-/* 2FA5BC */ UNK_RET ifStock_802FA5BC(UNK_PARAMS);
-/* 2FA6C4 */ UNK_RET fn_802FA6C4(UNK_PARAMS);
-/* 2FA8C0 */ UNK_RET fn_802FA8C0(UNK_PARAMS);
-/* 2FAC34 */ UNK_RET fn_802FAC34(UNK_PARAMS);
-/* 2FAEC4 */ UNK_RET ifStock_802FAEC4(UNK_PARAMS);
+/* 2F8298 */ void ifStock_802F8298(HSD_GObj*);
+/* 2F89F8 */ void ifStock_802F89F8(HSD_GObj*);
+/* 2F8DB0 */ void fn_802F8DB0(HSD_GObj*);
+/* 2F8E08 */ void fn_802F8E08(HSD_GObj*);
+/* 2F916C */ void fn_802F916C(HSD_GObj*);
+/* 2F9410 */ void fn_802F9410(HSD_GObj*);
+/* 2F94E0 */ void fn_802F94E0(HSD_GObj*, int);
+/* 2F9548 */ void fn_802F9548(HSD_GObj*, int);
+/* 2F9598 */ void fn_802F9598(HSD_GObj*, int);
+/* 2F95E8 */ void fn_802F95E8(HSD_GObj*, int);
+/* 2F9680 */ void fn_802F9680(HSD_GObj*, int);
+/* 2F96D0 */ HSD_GObj* ifStock_802F96D0(int, int, float, float);
+/* 2F98E8 */ void ifStock_802F98E8(unsigned char, int);
+/* 2F9F48 */ HSD_GObj* ifStock_802F9F48(int);
+/* 2FA118 */ HSD_GObj* ifStock_802FA118(int);
+/* 2FA2D0 */ void ifStock_802FA2D0(int);
+/* 2FA5BC */ void ifStock_802FA5BC(int);
+/* 2FA6C4 */ void fn_802FA6C4(HSD_GObj*);
+/* 2FA8C0 */ void fn_802FA8C0(HSD_GObj*);
+/* 2FAC34 */ void fn_802FAC34(HSD_GObj*);
+/* 2FAEC4 */ void ifStock_802FAEC4(void);
 /* 2FB390 */ void ifStock_802FB390(void);
 /* 2FB41C */ void ifStock_802FB41C(void);
 /* 2FB484 */ void ifStock_802FB484(void);
-/* 2FB4EC */ UNK_RET ifStock_802FB4EC(UNK_PARAMS);
-/* 2FB650 */ void ifStock_802FB650(s32);
-/* 2FB6AC */ void ifStock_802FB6AC(s32);
+/* 2FB4EC */ void ifStock_802FB4EC(int, GXColor*);
+/* 2FB650 */ void ifStock_802FB650(int);
+/* 2FB6AC */ void ifStock_802FB6AC(int);
 
 #endif
