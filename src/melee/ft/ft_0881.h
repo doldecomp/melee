7dbdd8071ce9ec4acb10c8fe5c198a12ed4e008c
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2025-09-20 03:16:42 -0700

    ft_0881 matches (#1929)

--- src/melee/ft/ft_0881.h
+++ src/melee/ft/ft_0881.h
@@ -8,26 +8,27 @@
 
 #include "ft/types.h"
 
-/* 0881D8 */ void ft_800881D8(Fighter* fp, int, int, int);
-/* 088328 */ void ft_80088328(Fighter* fp, int, int, int);
-/* 088478 */ void ft_80088478(Fighter*, int, int, int);
-/* 088510 */ void ft_80088510(Fighter*, int, int, int);
-/* 0885A8 */ void ft_800885A8(Fighter* fp, int, int, int);
-/* 088640 */ void ft_80088640(Fighter*, int, int, int);
-/* 0886D8 */ void ftCo_800886D8(Fighter* fp, int, int, int);
+/* 0881D8 */ void ft_800881D8(Fighter* fp, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 088328 */ void ft_80088328(Fighter* fp, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 088478 */ void ft_80088478(Fighter*, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 088510 */ void ft_80088510(Fighter*, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 0885A8 */ void ft_800885A8(Fighter* fp, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 088640 */ void ft_80088640(Fighter*, int sfx_id, u8 sfx_vol, u8 sfx_pan);
+/* 0886D8 */ void ftCo_800886D8(Fighter* fp, int sfx_id, u8 sfx_vol,
+                                u8 sfx_pan);
 /* 088770 */ void ft_80088770(Fighter* fp);
 /* 0887CC */ void ft_800887CC(Fighter* fp);
 /* 088828 */ void ft_80088828(Fighter* fp);
 /* 088884 */ void ft_80088884(Fighter* fp);
 /* 0888E0 */ void ft_800888E0(Fighter* fp);
 /* 08893C */ void ft_8008893C(Fighter* fp);
-/* 088998 */ UNK_RET ft_80088998(UNK_PARAMS);
+/* 088998 */ void ft_80088998(Fighter*);
 /* 0889F4 */ void ft_800889F4(Fighter* fp, FtSFXArr* sfx);
 /* 088A50 */ void ft_80088A50(Fighter* fp);
 /* 088C5C */ void ft_80088C5C(Fighter_GObj* gobj);
 /* 0890BC */ void ft_800890BC(Fighter* fp);
 /* 0890D0 */ void ft_800890D0(Fighter* fp, u32 move_id);
-/* 089228 */ float ft_80089228(Fighter*, s32, s32, f32);
+/* 089228 */ float ft_80089228(Fighter*, int, int, f32);
 /* 0892A0 */ void ft_800892A0(Fighter_GObj* gobj);
 
 #endif
