9882be4d3b2bbf44e9549f1de019819dcb9d1265
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2025-09-23 23:51:15 -0700

    Fix splits for ftYoshi files (#1934)

--- configure.py
+++ configure.py
@@ -781,7 +781,7 @@ config.libs = [
             Object(Matching, "melee/ft/chara/ftSamus/ftSs_SpecialHi.c"),
             Object(Matching, "melee/ft/chara/ftSamus/ftSs_SpecialLw_1.c"),
             # Yoshi
-            Object(NonMatching, "melee/ft/chara/ftYoshi/ftYs_Init.c"),
+            Object(Matching, "melee/ft/chara/ftYoshi/ftYs_Init.c"),
             Object(NonMatching, "melee/ft/chara/ftYoshi/ftYs_Guard.c"),
             Object(Matching, "melee/ft/chara/ftYoshi/ftYs_SpecialN.c"),
             Object(NonMatching, "melee/ft/chara/ftYoshi/ftYs_SpecialHi.c"),
