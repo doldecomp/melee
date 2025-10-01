1b3127f7c6cba8843b70de1a95453c387b58559e
Alex Harrison <alexander.harrison.r@gmail.com>
2025-10-01 13:59:17 -0600

    match itFire_UnkMotion0_Phys

--- src/melee/it/items/itfire.c
+++ src/melee/it/items/itfire.c
@@ -3,9 +3,10 @@
 #include <placeholder.h>
 #include <platform.h>
 
+#include "ef/eflib.h"
 #include "it/inlines.h"
-#include "it/it_26B1.h"
 #include "it/it_266F.h"
+#include "it/it_26B1.h"
 #include "it/it_2725.h"
 
 /// #it_2725_Logic6_Spawned
@@ -53,7 +54,26 @@ bool itFire_UnkMotion0_Anim(Item_GObj* gobj)
     return false;
 }
 
-void itFire_UnkMotion0_Phys(Item_GObj* gobj) {}
+static void itFire_UnkMotion0_Phys_inline(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    Item_80268E5C(gobj, 1, 2);
+    ip->entered_hitlag = efLib_PauseAll;
+    ip->exited_hitlag = efLib_ResumeAll;
+}
+
+void itFire_UnkMotion0_Phys(Item_GObj* gobj)
+{
+    if (it_8027A09C(gobj)) {
+        Item* ip = GET_ITEM(gobj);
+        itFireAttributes* attrs = ip->xC4_article_data->x4_specialAttributes;
+        it_8027ADEC(0x464, gobj, gobj->hsd_obj, 1.10000002384185791016);
+        it_80273454(gobj);
+        ip->x40_vel.y = attrs->x4;
+        itFire_UnkMotion0_Phys_inline(gobj);
+        ip->xDD1_flag.b1 = 1;
+    }
+}
 
 bool itFire_UnkMotion0_Coll(Item_GObj* gobj)
 {
