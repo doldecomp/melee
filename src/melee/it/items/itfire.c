31a4668b4fc9862798a8d87fba8e9756013578dd
Alex Harrison <alexander.harrison.r@gmail.com>
2025-10-01 15:48:29 -0600

    match itfire

--- src/melee/it/items/itfire.c
+++ src/melee/it/items/itfire.c
@@ -4,14 +4,23 @@
 #include <platform.h>
 
 #include "ef/eflib.h"
+#include "gr/stage.h"
 #include "it/inlines.h"
 #include "it/it_266F.h"
 #include "it/it_26B1.h"
 #include "it/it_2725.h"
 
-/// #it_2725_Logic6_Spawned
+void it_2725_Logic6_Spawned(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    itFireAttributes* attrs = ip->xC4_article_data->x4_specialAttributes;
+    ip->facing_dir = 0.0f;
+    ip->xDAC_itcmd_var0 = 0;
+    it_802CC944(gobj);
+    it_80279CDC(gobj, attrs->x0);
+}
 
-void it_802CC740(void) {}
+void it_802CC740(Item_GObj*) {}
 
 void it_2725_Logic6_EvtUnk(Item_GObj* gobj, Item_GObj* ref_gobj)
 {
@@ -36,9 +45,43 @@ bool itFire_UnkMotion1_Coll(Item_GObj* gobj)
 
 void it_802CC7D4(Item_GObj* gobj) {}
 
-void it_802CC7D8(Item_GObj* gobj) {}
+void it_802CC7D8(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    itFireAttributes* attrs = ip->xC4_article_data->x4_specialAttributes;
+    PAD_STACK(8);
 
-/// #itFire_UnkMotion2_Anim
+    it_8027ADEC(0x463, gobj, gobj->hsd_obj, 1.1f);
+    ip->x40_vel.y = attrs->x8;
+    Item_80268E5C(gobj, 2, 2);
+    ip->entered_hitlag = efLib_PauseAll;
+    ip->exited_hitlag = efLib_ResumeAll;
+}
+
+void itFire_UnkMotion2_Anim_inline(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    itFireAttributes* attrs = ip->xC4_article_data->x4_specialAttributes;
+    it_8027ADEC(0x463, gobj, gobj->hsd_obj, 1.1f);
+    ip->x40_vel.y = attrs->x8;
+    Item_80268E5C(gobj, 2, 2);
+    ip->entered_hitlag = efLib_PauseAll;
+    ip->exited_hitlag = efLib_ResumeAll;
+}
+
+bool itFire_UnkMotion2_Anim(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    if (!it_80272C6C(gobj)) {
+        itFire_UnkMotion2_Anim_inline(gobj);
+    }
+
+    if (ip->pos.y > Stage_GetBlastZoneTopOffset()) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 void itFire_UnkMotion2_Phys(Item_GObj* gobj)
 {
@@ -52,7 +95,17 @@ bool itFire_UnkMotion2_Coll(Item_GObj* gobj)
     return false;
 }
 
-/// #it_802CC944
+void it_802CC944(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+
+    it_802762BC(ip);
+    Item_80268E5C(gobj, 0, 2);
+
+    ip->entered_hitlag = efLib_PauseAll;
+    ip->exited_hitlag = efLib_ResumeAll;
+    it_80273670(gobj, 0, 0.0f);
+}
 
 bool itFire_UnkMotion0_Anim(Item_GObj* gobj)
 {
@@ -73,7 +126,7 @@ void itFire_UnkMotion0_Phys(Item_GObj* gobj)
     if (it_8027A09C(gobj)) {
         Item* ip = GET_ITEM(gobj);
         itFireAttributes* attrs = ip->xC4_article_data->x4_specialAttributes;
-        it_8027ADEC(0x464, gobj, gobj->hsd_obj, 1.10000002384185791016);
+        it_8027ADEC(0x464, gobj, gobj->hsd_obj, 1.1f);
         it_80273454(gobj);
         ip->x40_vel.y = attrs->x4;
         itFire_UnkMotion0_Phys_inline(gobj);
@@ -83,5 +136,5 @@ void itFire_UnkMotion0_Phys(Item_GObj* gobj)
 
 bool itFire_UnkMotion0_Coll(Item_GObj* gobj)
 {
-    return it_8027A118(gobj, (void (*)(HSD_GObj*)) it_802CC740);
+    return it_8027A118(gobj, it_802CC740);
 }
--- src/melee/it/items/itfire.h
+++ src/melee/it/items/itfire.h
@@ -6,17 +6,17 @@
 #include "it/items/types.h"
 
 /* 2CC6E4 */ void it_2725_Logic6_Spawned(Item_GObj*);
-/* 2CC740 */ UNK_RET it_802CC740(UNK_PARAMS);
+/* 2CC740 */ void it_802CC740(Item_GObj*);
 /* 2CC744 */ void it_2725_Logic6_EvtUnk(Item_GObj*, Item_GObj*);
 /* 2CC764 */ bool itFire_UnkMotion1_Anim(Item_GObj* gobj);
 /* 2CC7A4 */ void itFire_UnkMotion1_Phys(Item_GObj* gobj);
 /* 2CC7A8 */ bool itFire_UnkMotion1_Coll(Item_GObj* gobj);
 /* 2CC7D4 */ void it_802CC7D4(Item_GObj*);
-/* 2CC7D8 */ UNK_RET it_802CC7D8(UNK_PARAMS);
+/* 2CC7D8 */ void it_802CC7D8(Item_GObj*);
 /* 2CC860 */ bool itFire_UnkMotion2_Anim(Item_GObj* gobj);
 /* 2CC91C */ void itFire_UnkMotion2_Phys(Item_GObj* gobj);
 /* 2CC93C */ bool itFire_UnkMotion2_Coll(Item_GObj* gobj);
-/* 2CC944 */ UNK_RET it_802CC944(UNK_PARAMS);
+/* 2CC944 */ void it_802CC944(Item_GObj* gobj);
 /* 2CC9B8 */ bool itFire_UnkMotion0_Anim(Item_GObj* gobj);
 /* 2CC9DC */ void itFire_UnkMotion0_Phys(Item_GObj* gobj);
 /* 2CCA8C */ bool itFire_UnkMotion0_Coll(Item_GObj* gobj);
