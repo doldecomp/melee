50883aa30f55a7e3cd2e60f7acf048e8be512c27
x-dcs-x <dcs_git@proton.me>
2025-10-05 16:10:53 -0700

    match some grBigBlue functions

--- src/melee/gr/grbigblue.c
+++ src/melee/gr/grbigblue.c
@@ -1,8 +1,50 @@
 #include "grbigblue.h"
 
+#include "grdisplay.h"
+
 #include <platform.h>
 
 #include "gr/inlines.h"
+
+static StageCallbacks grBb_803E29E0[] = {
+    { grBigBlue_801E5AE4, grBigBlue_801E5B10, grBigBlue_801E5B18, grBigBlue_801E5B1C, 0 },
+    { grBigBlue_801E6298, grBigBlue_801E6354, grBigBlue_801E635C, grBigBlue_801E6360, 40000000 },
+    { grBigBlue_801E6200, grBigBlue_801E6288, grBigBlue_801E6290, grBigBlue_801E6294, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { NULL, NULL, NULL, NULL, 0 },
+    { grBigBlue_801E5B20, grBigBlue_801E6114, grBigBlue_801E611C, grBigBlue_801E6120, 0 },
+    { grBigBlue_801E6904, grBigBlue_801E6C58, grBigBlue_801E6C60, grBigBlue_801E855C, 0 },
+    { grBigBlue_801E6364, grBigBlue_801E687C, grBigBlue_801E6884, grBigBlue_801E68B8, 0 },
+    { grBigBlue_801E613C, grBigBlue_801E61BC, grBigBlue_801E61C4, grBigBlue_801E61FC, 0 },
+    { grBigBlue_801E8D64, grBigBlue_801E93D0, grBigBlue_801E93D8, grBigBlue_801E9F38, 80000000 },
+    { grBigBlue_801E9F3C, grBigBlue_801EA054, grBigBlue_801EA05C, grBigBlue_801EAB4C, 0 }
+};
 
 void grBigBlue_801E57BC(bool arg) {}
 
@@ -17,9 +59,38 @@ bool grBigBlue_801E59F0(void)
     return false;
 }
 
-/// #grBigBlue_801E59F8
+HSD_GObj* grBigBlue_801E59F8(s32 id)
+{
+    HSD_GObj* gobj;
+    StageCallbacks* cb = &grBb_803E29E0[id];
 
-/// #grBigBlue_801E5AE4
+    gobj = Ground_801C14D0(id);
+    if (gobj != NULL) {
+        Ground* gp = GET_GROUND(gobj);
+        gp->x8_callback = NULL;
+        gp->xC_callback = NULL;
+
+        GObj_SetupGXLink(gobj, grDisplay_801C5DB0, 3U, 0U);
+        if (cb->callback3 != NULL) {
+            gp->x1C_callback = cb->callback3;
+        }
+        if (cb->callback1 != NULL) {
+            cb->callback1(gobj);
+        }
+        if (cb->callback2 != NULL) {
+            HSD_GObjProc_8038FD54(gobj, cb->callback2, 4U);
+        }
+    } else {
+        OSReport("%s:%d: couldn t get gobj(id=%d)\n", "grbigblue.c", 0x17E,
+                 id);
+    }
+    return gobj;
+}
+
+void grBigBlue_801E5AE4(Ground_GObj* arg0)
+{
+    grAnime_801C8138(arg0, GET_GROUND(arg0)->map_id, 0);
+}
 
 bool grBigBlue_801E5B10(Ground_GObj* arg)
 {
@@ -57,7 +128,22 @@ bool grBigBlue_801E61BC(Ground_GObj* arg)
 
 void grBigBlue_801E61FC(Ground_GObj* arg) {}
 
-/// #grBigBlue_801E6200
+void grBigBlue_801E6200(Ground_GObj* arg0)
+{
+    Ground* gp;
+    Ground* temp_r5;
+    HSD_JObj* jobj;
+    void* filler;
+
+    gp = GET_GROUND(arg0);
+    jobj = arg0->hsd_obj;
+    grAnime_801C8138(arg0, gp->map_id, 0);
+    temp_r5 = GET_GROUND(arg0);
+    temp_r5->x10_flags.b2 = 0;
+    grMaterial_801C8A04(jobj, 0x08000000U);
+    grMaterial_801C8858(jobj, 0x20000000U);
+    gp->x11_flags.b012 = 1;
+}
 
 bool grBigBlue_801E6288(Ground_GObj* arg)
 {
@@ -68,7 +154,25 @@ void grBigBlue_801E6290(Ground_GObj* arg) {}
 
 void grBigBlue_801E6294(Ground_GObj* arg) {}
 
-/// #grBigBlue_801E6298
+void grBigBlue_801E6298(Ground_GObj* arg0)
+{
+    HSD_JObj* temp_r31;
+    HSD_JObj* temp_r30;
+    HSD_JObj* temp_r29;
+    HSD_JObj* temp_r28;
+    HSD_JObj* temp_r27;
+
+    Ground* gp = GET_GROUND(arg0);
+    grAnime_801C8138(arg0, gp->map_id, 0);
+    temp_r27 = Ground_801C3FA4(arg0, 6);
+    temp_r28 = Ground_801C3FA4(arg0, 7);
+    temp_r29 = Ground_801C3FA4(arg0, 3);
+    temp_r30 = Ground_801C3FA4(arg0, 4);
+    temp_r31 = Ground_801C3FA4(arg0, 2);
+    Ground_801C4E70(Ground_801C3FA4(arg0, 5), temp_r31, temp_r30,
+                    temp_r29, temp_r28, temp_r27);
+    gp->x11_flags.b012 = 2;
+}
 
 bool grBigBlue_801E6354(Ground_GObj* arg)
 {
