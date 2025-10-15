50e84642ce23f95f0dc071110710fc2e684be262
unknown <b33gy0sh@gmail.com>
2025-10-15 10:53:27 -0400

    Matches for spline - Matched splArcLengthPoint 100% - Incorporated splGetSplinePoint 100% match by cohnlee (https://decomp.me/scratch/Uukpd) - Incorporated splArcLengthGetParameter 97% match by Giga-Bowser (https://decomp.me/scratch/pEkA9) - TODO: match splArcLengthGetParameter - TODO: fix ground and other files that use changed parameters

--- src/sysdolphin/baselib/spline.h
+++ src/sysdolphin/baselib/spline.h
@@ -1,19 +1,24 @@
 #ifndef SYSDOLPHIN_BASELIB_SPLINE_H
 #define SYSDOLPHIN_BASELIB_SPLINE_H
 
-#include <placeholder.h>
-#include <platform.h>
-
-#include "baselib/forward.h"
+#include "forward.h"
 
 #include <dolphin/mtx.h>
 
 struct HSD_Spline {
-    u8 x0 UNK_SIZE_ARRAY;
+    /*  +0 */ u8 x0;
+    /*  +1 */ u8 x1;
+    /*  +2 */ s16 x2;
+    /*  +4 */ f32 x4;
+    /*  +8 */ Vec3* x8;
+    /*  +C */ f32 xC;
+    /* +10 */ f32* segments;
+    /* +14 */ f32 (*coeffs)[5];
 };
 
 f32 splGetHelmite(f32, f32, f32, f32, f32, f32);
+void splGetSplinePoint(Vec3*, HSD_Spline*, f32);
+f32 splArcLengthGetParameter(HSD_Spline*, f32);
 void splArcLengthPoint(Vec3*, HSD_Spline*, f32);
-void splGetSplinePoint(s32, s32);
 
 #endif
