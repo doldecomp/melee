d793383faec72b2737f0e1d87f97da3096c73eaa
rbasamoyai <86567158+rbasamoyai@users.noreply.github.com>
2025-10-22 14:44:01 -0400

    Spline text near match (#1969)
    * Decompiled mnInfoBonus_802528F8 and almost completed data sections
    - NOTE: need to use const data sections to actually propagate bytes in build
    
    * Added fn_80252E4C (mnInfoBonus), some in-progress context for mninfobonus.h
    - TODO fix inlining of mnInfoBonus_802528F8 infn_80252E4C
    
    * Moved mninfobonus statics to mninfobonus.static.h to include in context using m2ctx
    
    * Decompiled mnInfoBonus_80252ADC
    - Need more stuff in mnInfoBonus.c to get from 99% to 100% I think
    
    * Deleted floating point spaces used in mnInfoBonus_80252ADC
    
    * Decompiled fn_80252C50 (mninfobonus)
    - Implementation contains inadvertent hack that should be addressed
    - Stopped mnInfoBonus_802528F8 from being inlined by using inline wrapper, seems to work
    - Suggested to implement inline getter for HSD_PadCopyStatus->button but not implemented here; find better place for reuse
    
    * Used Menu_GetAllEvents inline in fn_80252C50 decomp (mninfobonus)
    
    * Decompiled mnInfoBonus_802529B4
    
    * Almost decompiled mnInfoBonus_80252F8C
    - Need to fix register alignment
    
    * Reduced regswap to r27 and r29
    
    * Matched mnInfoBonus_80252F8C, fully matched .text of mninfobonus.c
    
    * Formatted mninfobonus.c and mninfobonus.static.h
    
    * Matches for spline
    - Matched splArcLengthPoint 100%
    - Incorporated splGetSplinePoint 100% match by cohnlee (https://decomp.me/scratch/Uukpd)
    - Incorporated splArcLengthGetParameter 97% match by Giga-Bowser (https://decomp.me/scratch/pEkA9)
    - TODO: match splArcLengthGetParameter
    - TODO: fix ground and other files that use changed parameters
    
    * Fixed spline usages in other files
    - ground.c/.h
    - grkongo.c
    - lbshadow.h
    
    * Some improvements on spline.c, not 100% text matched
    - TODO fix all the register/float swaps on splArcLengthGetParameter
    
    * Formatted spline.c
    
    * Fixed r regswaps for splArcLengthGetParameter
    - TODO fix float regswaps

--- src/melee/lb/lbshadow.h
+++ src/melee/lb/lbshadow.h
@@ -10,7 +10,7 @@
 #include <common_structs.h>
 #include <dolphin/mtx.h>
 
-/* 00E9F0 */ void lbShadow_8000E9F0(Vec3*, s32, f32);
+/* 00E9F0 */ void lbShadow_8000E9F0(Vec3*, HSD_Spline*, f32);
 /* 00ED54 */ void lbShadow_8000ED54(LbShadow*, HSD_JObj*);
 /* 00EE8C */ void lbShadow_8000EE8C(LbShadow*);
 /* 00EEE0 */ void lbShadow_8000EEE0(HSD_GObj*);
