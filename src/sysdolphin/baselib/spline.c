28b440bc353c0228163ddcac450d1f766b3b7560
Will <wcarter312@gmail.com>
2025-11-21 12:37:33 -0500

    100% match splGetSplinePoint (#1988)
    * 100% match splGetSplinePoint
    
    * Cleanup spline header

--- src/sysdolphin/baselib/spline.c
+++ src/sysdolphin/baselib/spline.c
@@ -28,113 +28,108 @@ f32 splGetHelmite(f32 fterm, f32 time, f32 p0, f32 p1, f32 d0, f32 d1)
                                      (p1 * (-_2t3_T3 + _3t2_T2))));
 }
 
-static inline void splGetSplinePoint_inline(Vec3* p, Vec3* m, f32 a, f32 b,
-                                            f32 c, f32 d)
+void splGetCardinalPoint(Vec3* p, Vec3* cp, f32 tension, f32 u)
 {
-    p->x = (m[0].x * a) + (m[1].x * b) + (m[2].x * c) + (m[3].x * d);
-    p->y = (m[0].y * a) + (m[1].y * b) + (m[2].y * c) + (m[3].y * d);
-    p->z = (m[0].z * a) + (m[1].z * b) + (m[2].z * c) + (m[3].z * d);
+    f32 u2 = u * u;
+    f32 u3 = u2 * u;
+    f32 car0 = tension * (-u3 + 2.0F * u2 - u);
+    f32 car1 = ((2.0F - tension) * u3) + ((tension - 3.0F) * u2) + 1.0F;
+    f32 car2 = ((tension - 2.0F) * u3) + ((3.0F - (2.0F * tension)) * u2) +
+               (tension * u);
+    f32 car3 = tension * (u3 - u2);
+
+    p->x = (cp[0].x * car0) + (cp[1].x * car1) + (cp[2].x * car2) +
+           (cp[3].x * car3);
+    p->y = (cp[0].y * car0) + (cp[1].y * car1) + (cp[2].y * car2) +
+           (cp[3].y * car3);
+    p->z = (cp[0].z * car0) + (cp[1].z * car1) + (cp[2].z * car2) +
+           (cp[3].z * car3);
 }
 
-static inline void splGetSplinePoint_inline2(Vec3* p, HSD_Spline* arg1,
-                                             s16 idx, f32 t)
+void splGetBSplinePoint(Vec3* p, Vec3* cp, f32 u)
 {
-    switch (arg1->x0) {
-    case 0: {
-        // lerp
-        Vec3* m = &arg1->x8[idx];
-        p->x = (t * (m[1].x - m[0].x)) + m[0].x;
-        p->y = (t * (m[1].y - m[0].y)) + m[0].y;
-        p->z = (t * (m[1].z - m[0].z)) + m[0].z;
-        return;
-    }
-    case 1: {
-        Vec3* m = &arg1->x8[idx * 3];
-        f32 t2 = t * t;
-        f32 it = 1.0F - t;
-        f32 it2 = it * it;
-        f32 a = it2 * it;
-        f32 b = 3.0F * t * it2;
-        f32 c = 3.0F * t2 * it;
-        f32 d = t2 * t;
+    f32 u2 = u * u;
+    f32 u3 = u2 * u;
+    f32 u_1 = 1.0F - u;
+    f32 k1_6 = (1.0F / 6.0F);
+    f32 b0 = k1_6 * u_1 * u_1 * u_1;
+    f32 b1 = k1_6 * (4.0F + (3.0F * u3 - 6.0F * u2));
+    f32 b2 = k1_6 * (3.0F * (-u3 + u2 + u) + 1.0F);
+    f32 b3 = k1_6 * u3;
 
-        p->x = (m[0].x * a) + (m[1].x * b) + (m[2].x * c) + (m[3].x * d);
-        p->y = (m[0].y * a) + (m[1].y * b) + (m[2].y * c) + (m[3].y * d);
-        p->z = (m[0].z * a) + (m[1].z * b) + (m[2].z * c) + (m[3].z * d);
-        return;
-    }
-    case 2: {
-        Vec3* m = &arg1->x8[idx];
-        f32 t2 = t * t;
-        f32 it = 1.0F - t;
-        f32 t3 = t2 * t;
-
-        /* const */ f32 fac = (1.0F / 6.0F);
-        f32 a = fac * it * it * it;
-        f32 b = fac * (3.0F * t3 - 6.0F * t2 + 4.0F);
-        f32 c = fac * (3.0F * (-t3 + t2 + t) + 1.0F);
-        f32 d = fac * t3;
-
-        p->x = (m[0].x * a) + (m[1].x * b) + (m[2].x * c) + (m[3].x * d);
-        p->y = (m[0].y * a) + (m[1].y * b) + (m[2].y * c) + (m[3].y * d);
-        p->z = (m[0].z * a) + (m[1].z * b) + (m[2].z * c) + (m[3].z * d);
-        return;
-    }
-    case 3: {
-        Vec3* m = &arg1->x8[idx];
-        f32 x4 = arg1->x4;
-        f32 t2 = t * t;
-        f32 t3 = t2 * t;
-        f32 a = x4 * (-t3 + 2.0F * t2 - t);
-        f32 b = ((2.0F - x4) * t3) + ((x4 - 3.0F) * t2) + 1.0F;
-        f32 c = ((x4 - 2.0F) * t3) + ((3.0F - (2.0F * x4)) * t2) + (x4 * t);
-        f32 d = x4 * (t3 - t2);
-
-        p->x = (m[0].x * a) + (m[1].x * b) + (m[2].x * c) + (m[3].x * d);
-        p->y = (m[0].y * a) + (m[1].y * b) + (m[2].y * c) + (m[3].y * d);
-        p->z = (m[0].z * a) + (m[1].z * b) + (m[2].z * c) + (m[3].z * d);
-        return;
-    }
-    }
+    p->x = (cp[0].x * b0) + (cp[1].x * b1) + (cp[2].x * b2) + (cp[3].x * b3);
+    p->y = (cp[0].y * b0) + (cp[1].y * b1) + (cp[2].y * b2) + (cp[3].y * b3);
+    p->z = (cp[0].z * b0) + (cp[1].z * b1) + (cp[2].z * b2) + (cp[3].z * b3);
 }
 
-void splGetSplinePoint(Vec3* p, HSD_Spline* arg1, f32 arg2)
+void splGetBezierPoint(Vec3* p, Vec3* cp, f32 u)
 {
-    PAD_STACK(8);
-    if (arg2 < 0.0F || arg2 > 1.0F) {
+    f32 u_1 = 1.0F - u;
+    f32 u2 = u * u;
+    f32 u_12 = u_1 * u_1;
+    f32 bez0 = u_12 * u_1;
+    f32 bez1 = 3.0F * u * u_12;
+    f32 bez2 = 3.0F * u2 * u_1;
+    f32 bez3 = u2 * u;
+
+    p->x = (cp[0].x * bez0) + (cp[1].x * bez1) + (cp[2].x * bez2) +
+           (cp[3].x * bez3);
+    p->y = (cp[0].y * bez0) + (cp[1].y * bez1) + (cp[2].y * bez2) +
+           (cp[3].y * bez3);
+    p->z = (cp[0].z * bez0) + (cp[1].z * bez1) + (cp[2].z * bez2) +
+           (cp[3].z * bez3);
+}
+
+void splGetSplinePoint(Vec3* p, HSD_Spline* spline, f32 u)
+{
+    Vec3* cp;
+    s16 idx;
+    if (u < 0.0F || u > 1.0F) {
         return;
     }
 
-    if (arg2 < 1.0F) {
-        f32 t = (arg2 * (arg1->x2 - 1));
-        s16 idx = t;
-        t = t - idx;
-        splGetSplinePoint_inline2(p, arg1, idx, t);
-    } else {
-        s16 idx = arg1->x2 - 1;
-        switch (arg1->x0) { /* irregular */
+    if (u < 1.0F) {
+        f32 t = (u * (spline->numcv - 1));
+        idx = t;
+        t = t - (f32) idx;
+        switch (spline->type) {
         case 0:
-            *p = arg1->x8[idx];
+            // lerp
+            cp = &spline->cv[idx];
+            p->x = (t * (cp[1].x - cp[0].x)) + cp[0].x;
+            p->y = (t * (cp[1].y - cp[0].y)) + cp[0].y;
+            p->z = (t * (cp[1].z - cp[0].z)) + cp[0].z;
             return;
         case 1:
-            *p = arg1->x8[idx * 3];
+            cp = &spline->cv[idx * 3];
+            splGetBezierPoint(p, cp, t);
             return;
-        case 2: {
-            Vec3* m = &arg1->x8[idx] - 1;
-            f32 t = 1.0F;
-            f32 it = 1.0F - t;
-            f32 a = (1.0F / 6.0F) * it * it * it;
-            f32 c = (1.0F / 6.0F) * (3.0F * (-(t * t * t) + t * t + t) + 1.0F);
-            f32 b = (1.0F / 6.0F) * (3.0F * (t * t * t) - 6.0F * t * t + 4.0F);
-            f32 d = (1.0F / 6.0F) * t * t * t;
-
-            splGetSplinePoint_inline(p, m, a, b, c, d);
-        }
+        case 2:
+            cp = &spline->cv[idx];
+            splGetBSplinePoint(p, cp, t);
             return;
         case 3: {
-            Vec3* m = &arg1->x8[idx];
-            *p = m[1];
+            cp = &spline->cv[idx];
+            splGetCardinalPoint(p, cp, spline->tension, t);
+            return;
         }
+        }
+    } else {
+        idx = spline->numcv - 1;
+        switch (spline->type) {
+        case 0:
+            *p = spline->cv[idx];
+            return;
+        case 1:
+            *p = spline->cv[idx * 3];
+            return;
+        case 2:
+            cp = &spline->cv[idx] - 1;
+            splGetBSplinePoint(p, cp, 1.0f);
+            return;
+        case 3:
+            cp = &spline->cv[idx];
+            *p = cp[1];
             return;
         }
     }
@@ -157,7 +152,7 @@ inline f32 splArcLengthPolynomial(const f32 coeffs[5], f32 t)
 
 inline f32* spl_GetCoeffs(HSD_Spline* spl, s32 idx)
 {
-    return spl->coeffs[idx];
+    return spl->segPoly[idx];
 }
 
 inline f32 spl_IterateSimpsonsMiddle(const f32 coeffs[5], const f32 dx, f32 t)
@@ -190,21 +185,21 @@ f32 splArcLengthGetParameter(HSD_Spline* spl, f32 arg1)
         return end;
     }
 
-    while (spl->segments[idx + 1] < arg1) {
+    while (spl->segLength[idx + 1] < arg1) {
         idx++;
     }
 
-    switch (spl->x0) {
+    switch (spl->type) {
     case 0: {
-        result = (arg1 - spl->segments[idx]) /
-                 (spl->segments[idx + 1] - spl->segments[idx]);
+        result = (arg1 - spl->segLength[idx]) /
+                 (spl->segLength[idx + 1] - spl->segLength[idx]);
     } break;
     case 1:
     case 2:
     case 3: {
         // i belive the bulk of this math is applying simpson's rule
         // https://en.wikipedia.org/wiki/Simpson%27s_rule
-        f32 var_f22 = spl->xC * (arg1 - spl->segments[idx]);
+        f32 var_f22 = spl->totalLength * (arg1 - spl->segLength[idx]);
 
         while (ABS(start - end) >= 0.00001F) {
             const f32* coeffs = spl_GetCoeffs(spl, idx);
@@ -228,7 +223,7 @@ f32 splArcLengthGetParameter(HSD_Spline* spl, f32 arg1)
     } break;
     }
 
-    return (result + idx) / (spl->x2 - 1.0F);
+    return (result + idx) / (spl->numcv - 1.0F);
 }
 
 void splArcLengthPoint(Vec3* vec3, HSD_Spline* spline, f32 farg0)
