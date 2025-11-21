22f37273b32650da6e6a6687da6a402282d2b666
William Carter <wcarter312@gmail.com>
2025-11-21 12:20:18 -0500

    100% match splGetSplinePoint

--- src/sysdolphin/baselib/spline.h
+++ src/sysdolphin/baselib/spline.h
@@ -6,17 +6,19 @@
 #include <dolphin/mtx.h>
 
 struct HSD_Spline {
-    /*  +0 */ u8 x0;
-    /*  +1 */ u8 x1;
-    /*  +2 */ s16 x2;
-    /*  +4 */ f32 x4;
-    /*  +8 */ Vec3* x8;
-    /*  +C */ f32 xC;
-    /* +10 */ f32* segments;
-    /* +14 */ f32 (*coeffs)[5];
+    /*  +0 */ u8 type;
+    /*  +2 */ s16 numcv;
+    /*  +4 */ f32 tension;
+    /*  +8 */ Vec3* cv;
+    /*  +C */ f32 totalLength;
+    /* +10 */ f32* segLength;
+    /* +14 */ f32 (*segPoly)[5];
 };
 
 f32 splGetHelmite(f32, f32, f32, f32, f32, f32);
+void splGetCardinalPoint(Vec3* p, Vec3* cp, f32 tension, f32 u);
+void splGetBSplinePoint(Vec3* p, Vec3* cp, f32 u);
+void splGetBezierPoint(Vec3* p, Vec3* cp, f32 u);
 void splGetSplinePoint(Vec3*, HSD_Spline*, f32);
 f32 splArcLengthGetParameter(HSD_Spline*, f32);
 void splArcLengthPoint(Vec3*, HSD_Spline*, f32);
