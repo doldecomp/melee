6c721f507def72fb608ac16e1a03edce7da0ae8d
asylum14 <michaelanthonysilva18@gmail.com>
2025-12-21 17:49:38 -0800

    matched hsd_803B5C2C function in the hsd_3B34.c file

--- src/sysdolphin/baselib/hsd_3B34.c
+++ src/sysdolphin/baselib/hsd_3B34.c
@@ -1,1 +1,11 @@
+#include "hsd_3B34.h"
 
+//hsd_803B5C2C
+static int lbl_804D6398 = 3;
+
+void hsd_803B5C2C(int mode){
+    lbl_804D6398 = mode;
+    if ((mode < 1) || (mode > 10)) {
+        lbl_804D6398 = 3;
+    }
+}
