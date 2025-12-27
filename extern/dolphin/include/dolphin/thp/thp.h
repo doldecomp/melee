82b54fbead91850344c24986e9ed97e354a2f649
Michael SIlva <michaelanthonysilva18@gmail.com>
2025-12-26 20:45:58 -0800

    Matched the hsd_803B2B20 in hsd_3b2b.c file (#2010)
    * matched hsd_803B5C2C function in the hsd_3B34.c file
    
    * run clang-format
    
    * Matched THPDec_803302EC function in the THPDec.c file
    
    * Matched THPDec_803302EC function in the THPDec.c file
    
    * Matched THPDec_803302EC function in the THPDec.c file
    
    * Matched THPDec_803302EC function in the THPDec.c file
    
    * Matched THPDec_803300E0 function in the THPDec.c file
    
    * Matched THPDec_803300E0 function in the THPDec.c file
    
    * run clang-format
    
    * Matched the hsd_803B2B20 in hsd_3b2b.c file
    
    * Matched the hsd_803B2B20 in hsd_3b2b.c file
    
    * Matched the hsd_803B2B20 in hsd_3b2b.c file
    
    * Matched the hsd_803B2B20 in hsd_3b2b.c file
    
    * Matched the THPDec_8032FD40 in THPDec.c file
    
    ---------
    
    Co-authored-by: BR <wishingwellwellwishing@hotmail.com>

--- extern/dolphin/include/dolphin/thp/thp.h
+++ extern/dolphin/include/dolphin/thp/thp.h
@@ -55,6 +55,14 @@ typedef struct _THPFileInfo {
     THPComponent components[3];
 } THPFileInfo;
 
+typedef struct {    
+    s32 val0;
+    u16 val1;
+    u16 _pad;
+    u8 val2;    
+} THPDec_8032FD40_Data;
+
+
 static u8 __THPSetupBuffers(THPFileInfo*);
 static u8 __THPReadFrameHeader(void);
 static u8 __THPReadScaneHeader(THPFileInfo*);
@@ -78,6 +86,7 @@ static void __THPHuffDecodeDCTCompV(THPFileInfo*, THPCoeff*);
 
 s32 THPVideoDecode(void* file, void* tileY, void* tileU, void* tileV, void* work);
 s32 THPDec_803302EC(u8** data);
+s32 THPDec_8032FD40(THPDec_8032FD40_Data* arg0, u16 arg1);
 void THPDec_803300E0(u32* data);
 BOOL THPInit(void);
 
