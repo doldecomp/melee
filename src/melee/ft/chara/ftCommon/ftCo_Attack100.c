63fbb7409a23a583f6fc9e084136d3021d066a10
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-27 22:41:15 -0800

    Match ftCo_800D7268 (100%)
    Scratch: https://decomp.me/scratch/Me0eo

--- src/melee/ft/chara/ftCommon/ftCo_Attack100.c
+++ src/melee/ft/chara/ftCommon/ftCo_Attack100.c
@@ -18,6 +18,7 @@
 
 #include "ftCommon/ftCo_ItemThrow.h"
 #include "ftCommon/ftCo_Throw.h"
+#include "ftKirby/ftKb_Init.h"
 
 #include <math.h>
 #include <melee/cm/camera.h>
@@ -374,8 +375,14 @@ void ftCo_800D71D8(Fighter_GObj* gobj)
     }
 }
 
-/// #ftCo_800D7268
-
+s32 ftCo_800D7268(void* arg0)
+{
+    struct { Fighter_GObj* x0; int x4; }* s = arg0;
+    if (s->x4 == 4) {
+        return ftKb_SpecialN_800F1CD8(s->x0);
+    }
+    return 0;
+}
 /// #ftCo_800D72A0
 
 /// #ftCo_800D730C
