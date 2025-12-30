4be0a24192b59ead3fed8966411b814dbf820a1f
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 00:29:54 -0800

    Match un_80312018_OnFrame (100%)
    Scratch: http://10.200.0.1/scratch/7Ijc6

--- src/melee/ty/toy.c
+++ src/melee/ty/toy.c
@@ -249,8 +249,13 @@ void un_803102C4(s8 arg0)
 
 /// #un_80311F5C
 
-/// #un_80312018_OnFrame
-
+void un_80312018_OnFrame(void)
+{
+    if (M2C_FIELD(un_804A284C, s8*, 4) != 0) {
+        un_80311F5C();
+        gm_801A4B60();
+    }
+}
 /// #un_80312050
 
 void un_803122D0_OnInit(void)
