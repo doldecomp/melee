f073dc93412f7fdb8c48d932896c6c768ef2fa44
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 00:43:39 -0800

    Match un_803182D4_OnFrame (100%)

--- src/melee/ty/tydisplay.c
+++ src/melee/ty/tydisplay.c
@@ -1,10 +1,65 @@
 #include "tydisplay.h"
 
+#include "gm/gm_1A3F.h"
 #include "if/textlib.h"
 #include "ty/toy.h"
 #include "ty/tylist.h"
 
 #include <baselib/random.h>
+
+/// #un_803181BC
+
+void un_803182D4_OnFrame(void)
+{
+    if (M2C_FIELD(un_804A284C, s8*, 4) != 0) {
+        un_803181BC();
+        gm_801A4B60();
+    }
+}
+
+/// #un_8031830C
+
+/// #un_80318714
+
+/// #un_80318B1C
+
+/// #un_80318CB4
+
+/// #un_80319540
+
+/// #un_80319994
+
+/// #un_80319EF0
+
+/// #fn_8031A4EC
+
+/// #fn_8031A94C
+
+/// #un_8031B1FC
+
+/// #un_8031B328
+
+/// #un_8031B460_OnEnter
+
+/// #un_8031B850
+
+/// #un_8031B9A4_OnFrame
+
+/// #un_8031B9DC
+
+/// #un_8031BA78
+
+/// #un_8031BB34
+
+/// #un_8031BB94
+
+/// #un_8031BBF4
+
+/// #un_8031BC54
+
+/// #un_8031BF34
+
+/// #un_8031C1D0
 
 void tyDisplay_8031C2CC(void)
 {
@@ -20,3 +75,11 @@ s32 tyDisplay_8031C2EC(void)
     }
     return un_80305058(2, 0, 1, 60.0f);
 }
+
+/// #un_8031C354
+
+/// #un_8031C454
+
+/// #un_8031C5E4
+
+/// #un_8031C8B8
