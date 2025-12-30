41616cc221742c6f6746f0e6a52390b09bfdd3fb
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 00:39:22 -0800

    Match un_803127D4 (100%)

--- src/melee/ty/tylist.c
+++ src/melee/ty/tylist.c
@@ -1,1 +1,68 @@
+#include "tylist.h"
 
+#include <string.h>
+
+extern u8 un_804A2AA8[];
+extern void* un_804D6ED0;
+extern void* un_804D6EC4;
+extern void* un_804D6EC0;
+extern void* un_804D6EBC;
+extern void* un_804D6EB8;
+extern void* un_804D6EB4;
+extern void* un_804D6EB0;
+extern void* un_804D6EAC;
+extern void* un_804D6ECC;
+extern void* un_804D6EA8;
+extern void* un_804D6EA4;
+extern void* un_804D6EC8;
+
+/// #un_803124BC
+
+/// #un_8031263C
+
+void un_803127D4(void)
+{
+    un_804D6ED0 = NULL;
+    un_804D6EC4 = NULL;
+    un_804D6EC0 = NULL;
+    un_804D6EBC = NULL;
+    un_804D6EB8 = NULL;
+    un_804D6EB4 = NULL;
+    un_804D6EB0 = NULL;
+    un_804D6EAC = NULL;
+    un_804D6ECC = NULL;
+    un_804D6EA8 = NULL;
+    un_804D6EA4 = NULL;
+    un_804D6EC8 = NULL;
+    memzero(un_804A2AA8, 0x14);
+}
+
+/// #un_80312834
+
+/// #un_80312904
+
+/// #un_80312BAC
+
+/// #un_80312E88
+
+/// #un_8031305C
+
+/// #un_80313358
+
+/// #un_80313464
+
+/// #un_80313508
+
+/// #un_80313774
+
+/// #fn_80313BD8
+
+/// #fn_8031438C
+
+/// #fn_80314504
+
+/// #un_8031457C
+
+/// #un_803147C4
+
+/// #un_803148E4
