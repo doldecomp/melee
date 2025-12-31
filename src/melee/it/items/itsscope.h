5ffd03405b6d42c570133bc672729949e6ca2d80
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 19:50:16 -0800

    Match fn_800D7938 (100%)
    Scratch: http://10.200.0.1/scratch/qkU2l

--- src/melee/it/items/itsscope.h
+++ src/melee/it/items/itsscope.h
@@ -16,7 +16,7 @@
 /* 291CF4 */ s32 it_80291CF4(Item_GObj*, s32);
 /* 291D38 */ UNK_RET it_80291D38(UNK_PARAMS);
 /* 291DAC */ s32 it_80291DAC(Item_GObj* gobj, s32 arg1);
-/* 291F14 */ UNK_RET it_80291F14(UNK_PARAMS);
+/* 291F14 */ void it_80291F14(Item_GObj* gobj, s32 arg1);
 /* 291FA8 */ UNK_RET it_80291FA8(UNK_PARAMS);
 /* 292030 */ void it_80292030(Item_GObj* gobj);
 /* 292080 */ bool itSscope_UnkMotion0_Anim(Item_GObj* gobj);
