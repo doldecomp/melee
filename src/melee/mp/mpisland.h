1219412137b4516299362d402bc222945d868baf
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 16:45:54 -0800

    mp/mpisland: update mpIsland_8005AC14 signature

--- src/melee/mp/mpisland.h
+++ src/melee/mp/mpisland.h
@@ -11,7 +11,7 @@
 /* 05A6F8 */ void mpIsland_8005A6F8(void);
 /* 05A728 */ void mpIsland_8005A728(void);
 /* 05AB54 */ mp_UnkStruct0* mpIsland_8005AB54(int surface_idx);
-/* 05AC14 */ mp_UnkStruct0* mpIsland_8005AC14(Vec3*, float);
+/* 05AC14 */ mp_UnkStruct0* mpIsland_8005AC14(Vec3* pos, float height);
 /* 05AC8C */ bool mpIsland_8005AC8C(mp_UnkStruct0* arg0);
 /* 05ACE8 */ void mpIsland_8005ACE8(mp_UnkStruct0*, Vec3*, Vec3*);
 /* 05AE1C */ void mpIsland_8005AE1C(mp_UnkStruct0** arg0, mp_UnkStruct0** arg1, int arg2, int arg3, bool arg4);
