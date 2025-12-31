5fdd13697eace792c21e66c80d02fa0542ab3319
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2025-12-30 19:00:41 -0800

    Match fn_80245028 (100%)
    Scratch: http://10.200.0.1/scratch/QbK2n

--- src/melee/mn/mndiagram2.c
+++ src/melee/mn/mndiagram2.c
@@ -1,1 +1,12 @@
+#include "mndiagram2.h"
 
+#include <sysdolphin/baselib/sislib.h>
+
+void HSD_Free(void*);
+
+void fn_80245028(void* arg0) {
+    if (*(void**)((u8*)arg0 + 0xC4) != NULL) {
+        HSD_SisLib_803A5CC4(*(void**)((u8*)arg0 + 0xC4));
+    }
+    HSD_Free(arg0);
+}
