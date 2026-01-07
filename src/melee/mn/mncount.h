c615cf234ee49f9d3573f11a31851ae074bbcb11
Robin Avery <robin@ribbanya.dev>
2026-01-06 20:08:13 -0500

    Clean up `mnncount.h`

--- src/melee/mn/mncount.h
+++ src/melee/mn/mncount.h
@@ -6,23 +6,17 @@
 
 #include "mn/forward.h"
 
-struct CountEntry;
-
 /* 2502CC */ UNK_RET mnCount_802502CC(UNK_PARAMS);
 /* 2502F0 */ UNK_RET fn_802502F0(UNK_PARAMS);
 /* 250314 */ UNK_RET fn_80250314(UNK_PARAMS);
 /* 250338 */ UNK_RET fn_80250338(UNK_PARAMS);
-
 /* 25035C */ int mnCount_8025035C(s32 skip_count, u32 (*get_val_func)(s8));
-
 /* 25069C */ UNK_RET fn_8025069C(UNK_PARAMS);
 /* 2506C0 */ UNK_RET fn_802506C0(UNK_PARAMS);
 /* 2506E4 */ UNK_RET fn_802506E4(UNK_PARAMS);
 /* 250708 */ UNK_RET fn_80250708(UNK_PARAMS);
-
 /* 25072C */ s32 mnCount_8025072C(CountEntry* entries, s32 start_idx,
                                   s32 mode);
-
 /* 25092C */ UNK_RET mnCount_8025092C(UNK_PARAMS);
 /* 250C7C */ UNK_RET mnCount_80250C7C(UNK_PARAMS);
 /* 250DE4 */ UNK_RET mnCount_80250DE4(UNK_PARAMS);
