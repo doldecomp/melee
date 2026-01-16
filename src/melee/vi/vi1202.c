7faf9d12460f08c92bcd844a5d56e0eeebed8997
Will <wcarter312@gmail.com>
2026-01-16 18:09:45 -0500

    Match vi1202 functions (#2098)
    * Match un_803224DC
    
    * Match un_80321EBC
    
    * Match un_8032201C
    
    * Fix copy-paste mistake in 1201v1
    
    * Match un_80321D30, get un_80321300_OnEnter to regswaps
    
    * Match un_80321300_OnEnter
    
    * Symbol cleanup

--- src/melee/vi/vi1202.c
+++ src/melee/vi/vi1202.c
@@ -2,19 +2,28 @@
 
 #include "vi.h"
 
-#include "ft/fighter.h"
+#include "baselib/forward.h"
+
+#include "ef/efasync.h"
+#include "ef/eflib.h"
+#include "ft/ft_0C31.h"
 #include "ft/ftlib.h"
 #include "gm/gm_1601.h"
 #include "gm/gm_unsplit.h"
+#include "lb/lb_00B0.h"
 #include "lb/lb_00F9.h"
+#include "lb/lbarchive.h"
 #include "lb/lbaudio_ax.h"
 #include "pl/player.h"
 #include "pl/plbonuslib.h"
 #include "sc/types.h"
+#include "ty/toy.h"
+#include "ty/tylist.h"
 
 #include <baselib/aobj.h>
 #include <baselib/cobj.h>
 #include <baselib/gobj.h>
+#include <baselib/gobjgxlink.h>
 #include <baselib/gobjobject.h>
 #include <baselib/gobjproc.h>
 #include <baselib/jobj.h>
@@ -23,7 +32,7 @@ struct vi1202_UnkStruct {
     /* 0x00 */ s32 x0;
     /* 0x04 */ s32 x4;
     /* 0x08 */ f32 x8;
-    /* 0x0C */ s32 xC;
+    /* 0x0C */ u32 xC;
     /* 0x10 */ s32 x10;
     /* 0x14 */ s32 x14;
     /* 0x18 */ s32 x18;
@@ -34,8 +43,10 @@ struct vi1202_UnkStruct {
     /* 0x2C */ s32 x2C;
 };
 
-/* 4D7040 */ extern DynamicModelDesc*** un_804D7040;
-/* 4D704C */ extern HSD_JObj* un_804D704C;
+static SceneDesc* un_804D7040;
+static SceneDesc* un_804D7044;
+static HSD_Joint* un_804D7048;
+static HSD_JObj* un_804D704C;
 
 void un_8032110C(HSD_GObj* gobj)
 {
@@ -59,12 +70,12 @@ void un_80321178(void)
     HSD_JObj* jobj;
 
     i = 0;
-    while ((*un_804D7040)[i] != NULL) {
+    while ((un_804D7040->models)[i] != NULL) {
         gobj = GObj_Create(0xE, 0xF, 0);
-        jobj = HSD_JObjLoadJoint((*un_804D7040)[i]->joint);
+        jobj = HSD_JObjLoadJoint((un_804D7040->models)[i]->joint);
         HSD_GObjObject_80390A70(gobj, HSD_GObj_804D7849, jobj);
         GObj_SetupGXLink(gobj, HSD_GObj_JObjCallback, 0xB, 0);
-        gm_8016895C(jobj, (*un_804D7040)[i], 0);
+        gm_8016895C(jobj, (un_804D7040->models)[i], 0);
         HSD_JObjReqAnimAll(jobj, 0.0f);
         HSD_JObjAnimAll(jobj);
         HSD_GObjProc_8038FD54(gobj, un_80321154, 0x17);
@@ -77,7 +88,7 @@ void un_80321178(void)
     lbAudioAx_80027648();
 }
 
-void un_80321294(HSD_GObj* gobj)
+void vi1202_RunFrame(HSD_GObj* gobj)
 {
     HSD_CObj* cobj = GET_COBJ(gobj);
     HSD_CObjAnim(cobj);
@@ -90,7 +101,154 @@ void un_80321294(HSD_GObj* gobj)
     }
 }
 
-void un_803218E0_OnFrame(void)
+static void HSD_JObjSetRotationY_2(HSD_JObj* jobj, f32 y)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 660, "jobj"));
+    ((!(jobj->flags & JOBJ_USE_QUATERNION))
+         ? ((void) 0)
+         : __assert("jobj.h", 661, "!(jobj->flags & JOBJ_USE_QUATERNION)"));
+    jobj->rotate.y = y;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetScaleX_2(HSD_JObj* jobj, f32 x)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 776, "jobj"));
+    jobj->scale.x = x;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetScaleY_2(HSD_JObj* jobj, f32 x)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 791, "jobj"));
+    jobj->scale.y = x;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetScaleZ_2(HSD_JObj* jobj, f32 x)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 806, "jobj"));
+    jobj->scale.z = x;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetTranslateX_2(HSD_JObj* jobj, f32 x)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 932, "jobj"));
+    jobj->translate.x = x;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetTranslateY_2(HSD_JObj* jobj, f32 y)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 947, "jobj"));
+    jobj->translate.y = y;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+static void HSD_JObjSetTranslateZ_2(HSD_JObj* jobj, f32 z)
+{
+    ((jobj) ? ((void) 0) : __assert("jobj.h", 962, "jobj"));
+    jobj->translate.z = z;
+    if (!(jobj->flags & JOBJ_MTX_INDEP_SRT)) {
+        ftCo_800C6AFC(jobj);
+    }
+}
+
+void vi1202_OnEnter(void* arg)
+{
+    f32 scale;
+    HSD_LObj* lobj;
+    HSD_CObj* cobj;
+    HSD_JObj* jobj;
+    HSD_JObj* child;
+    HSD_GObj *gobj, *cam_gobj;
+    PAD_STACK(24);
+
+    lbAudioAx_80023694();
+    lbAudioAx_800236DC();
+    efLib_8005B4B8();
+    efAsync_8006737C(0);
+    lbAudioAx_80023F28(0x5A);
+    lbAudioAx_80024E50(1);
+
+    lbArchive_LoadSymbols("Vi1202.dat", &un_804D7040, "visual1202Scene", NULL);
+    lbArchive_LoadSymbols("TyKoopaR.dat", &un_804D7048,
+                          "ToyKoopaRModel_TopN_joint", NULL);
+    lbArchive_LoadSymbols("GmRgStnd.dat", &un_804D7044, "standScene", NULL);
+    un_803124BC();
+
+    gobj = GObj_Create(0xB, 0x3, 0);
+    lobj = lb_80011AC4(un_804D7040->lights);
+    HSD_GObjObject_80390A70(gobj, HSD_GObj_804D784A, lobj);
+    GObj_SetupGXLink(gobj, HSD_GObj_LObjCallback, 0, 0);
+
+    cam_gobj = GObj_Create(0x13, 0x14, 0);
+    cobj =
+        lb_80013B14((HSD_CameraDescPerspective*) un_804D7040->cameras->desc);
+    HSD_GObjObject_80390A70(cam_gobj, HSD_GObj_804D784B, cobj);
+    GObj_SetupGXLinkMax(cam_gobj, HSD_GObj_803910D8, 0x8);
+    cam_gobj->gxlink_prios = 0x8A9;
+    HSD_CObjAddAnim(cobj, un_804D7040->cameras->anims[0]);
+    HSD_CObjReqAnim(cobj, 0.0f);
+    HSD_CObjAnim(cobj);
+    HSD_GObjProc_8038FD54(cam_gobj, vi1202_RunFrame, 0);
+    un_80321178();
+
+    gobj = GObj_Create(0xE, 0xF, 0);
+    jobj = HSD_JObjLoadJoint(un_804D7048);
+    HSD_GObjObject_80390A70(gobj, HSD_GObj_804D7849, jobj);
+    GObj_SetupGXLink(gobj, HSD_GObj_JObjCallback, 0xB, 0);
+
+    HSD_JObjSetScaleX(jobj, 0.49f);
+    HSD_JObjSetScaleY(jobj, 0.49f);
+    HSD_JObjSetScaleZ(jobj, 0.49f);
+
+    lb_8000C1C0(jobj, un_804D704C);
+    lb_8000C290(jobj, un_804D704C);
+    HSD_GObjProc_8038FD54(gobj, un_80321130, 0x17);
+
+    gobj = GObj_Create(0xE, 0xF, 0);
+    jobj = HSD_JObjLoadJoint(un_804D7044->models[0]->joint);
+    HSD_GObjObject_80390A70(gobj, HSD_GObj_804D7849, jobj);
+    GObj_SetupGXLink(gobj, HSD_GObj_JObjCallback, 0xB, 0);
+    HSD_GObjProc_8038FD54(gobj, un_8032110C, 0x17);
+
+    if (jobj == NULL) {
+        child = NULL;
+    } else {
+        child = jobj->child;
+    }
+
+    HSD_JObjSetTranslateX_2(child, -un_803060BC(0x1F, 0));
+    HSD_JObjSetTranslateY_2(child, -un_803060BC(0x1F, 1));
+    HSD_JObjSetTranslateZ_2(child, -un_803060BC(0x1F, 2));
+    HSD_JObjSetRotationY_2(child, -un_803060BC(0x1F, 5));
+
+    scale = 0.49f * (un_803060BC(0x1F, 4) * (1.0f / un_803060BC(0x1F, 3)));
+
+    HSD_JObjSetScaleX_2(child, scale);
+    HSD_JObjSetScaleY_2(child, scale);
+    HSD_JObjSetScaleZ_2(child, scale);
+
+    lb_8000C1C0(jobj, un_804D704C);
+    lb_8000C290(jobj, un_804D704C);
+    lbAudioAx_80024E50(0);
+}
+
+void vi1202_OnFrame(void)
 {
     vi_8031CAAC();
 }
@@ -195,7 +353,7 @@ void un_80321AF4(HSD_GObj* gobj)
                 {
                     data->x24 = data->x24 + 1;
                 } else {
-                    if ((u32) data->xC == ftLib_80087460(cur)) {
+                    if (data->xC == ftLib_80087460(cur)) {
                         flag = 1;
                     }
                 }
@@ -230,17 +388,31 @@ void un_80321C28(void)
     data->x2C = -1;
 }
 
-void un_80321C70(void)
+static void un_80321C70_inline(void)
 {
-    vi1202_UnkStruct* data = un_804D7050;
-    CrowdConfig* vdata = gCrowdConfig;
-    s32 x18;
+    vi1202_UnkStruct* data;
 
-    x18 = data->x18;
-    if (x18 >= vdata->max_gasp_count) {
+    data = un_804D7050;
+
+    if (data->x18 >= gCrowdConfig->max_gasp_count ||
+        data->x18 < gCrowdConfig->x24)
+    {
         return;
     }
-    if (x18 < vdata->x24) {
+    data->x1C = 1;
+}
+
+#pragma push
+#pragma dont_inline on
+void un_80321C70(void)
+{
+    vi1202_UnkStruct* data;
+
+    data = un_804D7050;
+
+    if (data->x18 >= gCrowdConfig->max_gasp_count ||
+        data->x18 < gCrowdConfig->x24)
+    {
         return;
     }
     data->x1C = 1;
@@ -252,6 +424,7 @@ void un_80321CA4(s32 arg)
     un_80321CE8();
     data->x28 = lbAudioAx_8002411C(arg);
 }
+#pragma pop
 
 void un_80321CE8(void)
 {
@@ -262,72 +435,61 @@ void un_80321CE8(void)
     data->x28 = -1;
 }
 
-void un_80321D30(int arg0, f32 arg1)
+static void un_80321CE8_caller(u32 arg0)
+{
+    vi1202_UnkStruct* data = un_804D7050;
+    if (lbAudioAx_80023710(data->x28) != 0) {
+        lbAudioAx_800236B8(data->x28);
+    }
+    data->x28 = -1;
+    data->x28 = lbAudioAx_8002411C(arg0);
+}
+
+void un_80321D30(u32 arg0, f32 arg1)
 {
     s32 cat;
-    vi1202_UnkStruct* data = un_804D7050;
-    cat = un_80322298(arg1);
+    vi1202_UnkStruct* data;
 
+    data = un_804D7050;
+    cat = un_80322298(arg1);
     if (cat >= 2) {
         if (un_80321EBC(arg0, arg1) != 0) {
-            vi1202_UnkStruct* data2 = un_804D7050;
-            if (lbAudioAx_80023710(data2->x28) != 0) {
-                lbAudioAx_800236B8(data2->x28);
-            }
-            data2->x28 = -1;
+            un_80321CE8();
             return;
         }
     }
 
     switch (cat) {
     case 3: {
-        vi1202_UnkStruct* data2 = un_804D7050;
-        if (lbAudioAx_80023710(data2->x28) != 0) {
-            lbAudioAx_800236B8(data2->x28);
-        }
-        data2->x28 = -1;
-        data2->x28 = lbAudioAx_8002411C(0x140);
+        un_80321CE8_caller(0x140);
         break;
     }
     case 2: {
-        vi1202_UnkStruct* data2 = un_804D7050;
-        if (lbAudioAx_80023710(data2->x28) != 0) {
-            lbAudioAx_800236B8(data2->x28);
-        }
-        data2->x28 = -1;
-        data2->x28 = lbAudioAx_8002411C(0x141);
+        un_80321CE8_caller(0x141);
         break;
     }
     case 1: {
-        vi1202_UnkStruct* data2 = un_804D7050;
-        if (lbAudioAx_80023710(data2->x28) != 0) {
-            lbAudioAx_800236B8(data2->x28);
-        }
-        data2->x28 = -1;
-        data2->x28 = lbAudioAx_8002411C(0x142);
+        un_80321CE8_caller(0x142);
         break;
     }
     }
 
-    if ((u32) arg0 != 0) {
-        if ((u32) data->xC == (u32) arg0) {
-            vi1202_UnkStruct* data3 = un_804D7050;
-            CrowdConfig* vdata = gCrowdConfig;
-            if (data3->x18 < vdata->max_gasp_count) {
-                if (data3->x18 >= vdata->x24) {
-                    data3->x1C = 1;
-                }
-            }
+    if (arg0 != 0) {
+        if (data->xC == arg0) {
+            un_80321C70_inline();
         }
     }
 }
+
 bool un_80321EBC(u32 arg0, f32 arg1)
 {
-    vi1202_UnkStruct* data = un_804D7050;
-    HSD_GObj* gobj;
     s32 port;
+    vi1202_UnkStruct* data;
+    HSD_GObj* gobj;
+    u32 arg0_copy = arg0;
 
-    gobj = ftLib_8008741C(arg0);
+    data = un_804D7050;
+    gobj = ftLib_8008741C(arg0_copy);
     if (gobj == NULL) {
         goto skip;
     }
@@ -349,7 +511,7 @@ bool un_80321EBC(u32 arg0, f32 arg1)
     }
 
     data->x14 = ftLib_8008746C(gobj);
-    if ((data->x14) == 0x83D60) {
+    if (data->x14 == 0x83D60) {
         return 0;
     }
 
@@ -365,22 +527,23 @@ bool un_80321EBC(u32 arg0, f32 arg1)
         } else {
             sfx = 0x141;
         }
-        un_804D7050->x2C = lbAudioAx_800240B4(sfx);
+        data2 = un_804D7050;
+        data2->x2C = lbAudioAx_800240B4(sfx);
     }
 
     data->xC = arg0;
     data->x18 = 0;
 
     port = ftLib_800874BC(gobj);
-    data->x10 = port;
     pl_8003FDA0(ftLib_80086BE0(gobj), port);
 
     return 1;
 }
 
-bool un_8032201C(int arg0, s32 cat)
+bool un_8032201C(u32 arg0, s32 cat)
 {
     vi1202_UnkStruct* data = un_804D7050;
+    HSD_GObj* gobj;
     char pad[16];
 
     switch (cat) {
@@ -409,25 +572,13 @@ bool un_8032201C(int arg0, s32 cat)
         return false;
     }
 
-    if ((u32) arg0 != 0) {
-        if ((u32) data->xC == (u32) arg0) {
-            vi1202_UnkStruct* data2;
-            CrowdConfig* vdata;
-
-            data2 = un_804D7050;
-            vdata = gCrowdConfig;
-            if (data2->x18 < vdata->max_gasp_count) {
-                if (data2->x18 >= vdata->x24) {
-                    data2->x1C = 1;
-                }
-            }
+    if (arg0 != 0) {
+        if (data->xC == arg0) {
+            un_80321C70_inline();
         }
 
-        {
-            HSD_GObj* gobj = ftLib_8008741C(arg0);
-            s32 port = ftLib_800874BC(gobj);
-            pl_8003FDC8(ftLib_80086BE0(gobj), port);
-        }
+        gobj = ftLib_8008741C(arg0);
+        pl_8003FDC8(ftLib_80086BE0(gobj), ftLib_800874BC(gobj));
     }
     return true;
 }
@@ -521,14 +672,15 @@ void un_80322314(void)
     data->x20 = 1;
 }
 
-void un_8032233C(int arg0, int arg1)
+void un_8032233C(u32 arg0, u32 arg1)
 {
-    vi1202_UnkStruct* data = un_804D7050;
-    HSD_GObj* gobj0;
-    f32 kb_mag;
     s32 cat;
+    vi1202_UnkStruct* data = un_804D7050;
+    HSD_GObj* gobj;
+    f32 kb_mag;
+    PAD_STACK(8);
 
-    gobj0 = ftLib_8008741C(arg0);
+    gobj = ftLib_8008741C(arg0);
     kb_mag = ftLib_80087454(ftLib_8008741C(arg1));
 
     if (kb_mag >= gCrowdConfig->kb_threshold_high) {
@@ -545,21 +697,17 @@ void un_8032233C(int arg0, int arg1)
         return;
     }
 
-    if (gobj0 != NULL) {
-        if (ftLib_80087454(gobj0) >= 3.0f) {
+    if (gobj != NULL) {
+        if (ftLib_80087454(gobj) >= 3.0f) {
             un_80321D30(arg0, kb_mag);
-            return;
+            goto end;
         }
     }
 
-    if ((u32) data->x0 == (u32) arg0) {
+    if (data->x0 == arg0) {
         if ((f32) data->x4 < gCrowdConfig->x18) {
-            f32 max = data->x8;
-            if (kb_mag > max) {
-                max = kb_mag;
-            }
-            un_80321D30(arg0, max);
-            return;
+            un_80321D30(arg0, kb_mag > data->x8 ? kb_mag : data->x8);
+            goto end;
         }
     }
 
@@ -575,9 +723,13 @@ void un_8032233C(int arg0, int arg1)
         break;
     }
 
-    if (cat == 3 || (cat == 2 && (u32) data->xC == (u32) arg1)) {
+    if (cat == 3 || (cat == 2 && data->xC == arg1)) {
         un_80321C70();
     }
+end:
+    data->x4 = 0;
+    data->x0 = arg0;
+    data->x8 = kb_mag;
 }
 
 bool un_803224DC(s32 spawn_id, f32 pos_x, f32 kb_mag)
--- src/melee/vi/vi1202.h
+++ src/melee/vi/vi1202.h
@@ -47,9 +47,9 @@ typedef struct CrowdConfig {
 /* 321130 */ void un_80321130(HSD_GObj*);
 /* 321154 */ void un_80321154(HSD_GObj*);
 /* 321178 */ void un_80321178(void);
-/* 321294 */ void un_80321294(HSD_GObj*);
-/* 321300 */ void un_80321300_OnEnter(UNK_T);
-/* 3218E0 */ void un_803218E0_OnFrame(void);
+/* 321294 */ void vi1202_RunFrame(HSD_GObj*);
+/* 321300 */ void vi1202_OnEnter(UNK_T);
+/* 3218E0 */ void vi1202_OnFrame(void);
 /* 321900 */ void un_80321900(void);
 /* 321950 */ void un_80321950(vi1202_UnkStruct*);
 /* 3219AC */ void fn_803219AC(HSD_GObj*);
@@ -60,15 +60,15 @@ typedef struct CrowdConfig {
 /* 321C70 */ void un_80321C70(void);
 /* 321CA4 */ void un_80321CA4(s32);
 /* 321CE8 */ void un_80321CE8(void);
-/* 321D30 */ void un_80321D30(int, f32);
+/* 321D30 */ void un_80321D30(u32, f32);
 /* 321EBC */ bool un_80321EBC(u32, f32);
-/* 32201C */ bool un_8032201C(int, s32);
+/* 32201C */ bool un_8032201C(u32, s32);
 /* 322178 */ void un_80322178(int);
 /* 322258 */ bool un_80322258(float);
 /* 322298 */ s32 un_80322298(float);
 /* 3222EC */ float un_803222EC(float mag, float angle);
 /* 322314 */ void un_80322314(void);
-/* 32233C */ void un_8032233C(int, int);
+/* 32233C */ void un_8032233C(u32, u32);
 /* 3224DC */ bool un_803224DC(s32 spawn_id, f32 pos_x, f32 kb_mag);
 /* 322598 */ int un_80322598(int, float);
 
