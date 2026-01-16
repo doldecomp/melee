a9e62c1521909229eb84394368397d27b9581691
William Carter <wcarter312@gmail.com>
2026-01-16 18:55:58 -0500

    Get un_8031D288_OnEnter matched up to regswaps and load

--- src/melee/vi/vi0401.h
+++ src/melee/vi/vi0401.h
@@ -6,8 +6,13 @@
 #include "ft/forward.h"
 #include <baselib/forward.h>
 
+typedef struct vi0401_SceneData {
+    u8 char_id;
+    u8 costume_id;
+} vi0401_SceneData;
+
 /* 31D030 */ void un_8031D030(CharacterKind, int);
-/* 31D288 */ void un_8031D288_OnEnter(UNK_T);
+/* 31D288 */ void un_8031D288_OnEnter(vi0401_SceneData*);
 /* 31D698 */ void un_8031D698_OnFrame(void);
 
 #endif
