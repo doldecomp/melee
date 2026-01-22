b1234f82a143a4a398503987b397fe31ad751407
Duglas560 <wichycrek@gmail.com>
2026-01-22 04:55:32 -0800

    update itfreezer 100% (#2114)
    * it_freezer update to 100%
    
    * freezer update
    
    * Add itfreezertAttributes and itfreezer_ItemVars structs
    
    * Add freezer item variable to Item struct
    
    * whitespaces fix itfreezer.c
    
    * fix commonItem whitespaces
    
    * Fix struct definition for itfreezer_ItemVars
    
    * Add include for gr/stage.h
    
    Added missing include for 'gr/stage.h' in itfreezer.c

--- src/melee/it/items/itfreezer.c
+++ src/melee/it/items/itfreezer.c
@@ -1,5 +1,6 @@
 #include "itfreezer.h"
 
+
 #include <placeholder.h>
 #include <platform.h>
 
@@ -9,8 +10,19 @@
 #include "it/it_26B1.h"
 #include "it/it_2725.h"
 #include "it/item.h"
+#include "gr/stage.h"
 
-/// #it_802CCF9C
+
+void it_802CCF9C(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    itfreezertAttributes* attrs = item->xC4_article_data->x4_specialAttributes;
+    item->facing_dir = 0.0f;
+    item->xDAC_itcmd_var0 = 0;
+    item->xDB0_itcmd_var1 = 0;
+    it_802CD2EC(gobj);
+    it_80279CDC(gobj, attrs->x0);
+}
 
 void it_802CCFFC(Item_GObj* gobj) {}
 
@@ -35,20 +47,92 @@ bool itFreezer_UnkMotion1_Coll(Item_GObj* gobj)
     return false;
 }
 
-/// #it_802CD090
+void it_802CD090(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    if(item->xDB0_itcmd_var1 != false)
+    {
+        it_8027ADEC(0x462, gobj, item->xBBC_dynamicBoneTable->bones[5], 1.4f);
+        item->xDB0_itcmd_var1 = false;
+    }
+    if(item->xDD4_itemVar.freezer.x60 <= 0)
+    {
+        HSD_JObj* jobj = HSD_GObjGetHSDObj(gobj);
+        itfreezertAttributes* attrs = item->xC4_article_data->x4_specialAttributes;
+        item->xDD4_itemVar.freezer.x60 = attrs->xC;
+        it_8027ADEC(0x461, gobj, jobj, 1.4f);
+        return;
+    }
+    item->xDD4_itemVar.freezer.x60 -= 1;
+}
 
-/// #it_802CD12C
+void it_802CD12C(HSD_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    Item_80268E5C(gobj, 2, ITEM_ANIM_UPDATE);
+    item->entered_hitlag = efLib_PauseAll;
+    item->exited_hitlag = efLib_ResumeAll;
+    item->on_accessory = it_802CD290;
+    item->xDB0_itcmd_var1 = 0;
+}
 
-/// #itFreezer_UnkMotion2_Anim
+static void itFreezer_UnkMotion2_Anim_Inline(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    Item_80268E5C(gobj, 2, ITEM_ANIM_UPDATE);
+    item->entered_hitlag = efLib_PauseAll;
+    item->exited_hitlag = efLib_ResumeAll;
+    item->on_accessory = it_802CD290;
+    item->xDB0_itcmd_var1 = 0;
+}
 
-/// #itFreezer_UnkMotion2_Phys
+bool itFreezer_UnkMotion2_Anim(Item_GObj* gobj)
+{
+    Item* item_temp = GET_ITEM(gobj);
+    if (it_80272C6C(gobj) == 0)
+    {
+        itFreezer_UnkMotion2_Anim_Inline(gobj);
+    }
+    if (item_temp->pos.y > Stage_GetBlastZoneTopOffset()) {
+        return 1;
+    }
+    return 0;
+}
+
+void itFreezer_UnkMotion2_Phys(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    if(item->xDAC_itcmd_var0 != false)
+    {
+        itfreezertAttributes* attrs = item->xC4_article_data->x4_specialAttributes;
+        item->x40_vel.y = attrs->x4;
+        item->xDAC_itcmd_var0 = 0;
+        item->xDB0_itcmd_var1 = 1;
+    }
+    if ((u32) item->xDB0_itcmd_var1 != false)
+    {
+        itfreezertAttributes* attrs = item->xC4_article_data->x4_specialAttributes;
+        item->x40_vel.y += attrs->x8;
+    }
+}
 
 bool itFreezer_UnkMotion2_Coll(Item_GObj* gobj)
 {
     return false;
 }
 
-/// #it_802CD290
+void it_802CD290(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    if (item->xDD4_itemVar.freezer.x60 <= 0) {
+        HSD_JObj* jobj = HSD_GObjGetHSDObj(gobj);
+        itfreezertAttributes* attrs = item->xC4_article_data->x4_specialAttributes;
+        item->xDD4_itemVar.freezer.x60 = attrs->xC;
+        it_8027ADEC(0x461, gobj, jobj, 1.4f);
+        return;
+    }
+    item->xDD4_itemVar.freezer.x60 -= 1;
+}
 
 void it_802CD2EC(Item_GObj* gobj)
 {
@@ -66,7 +150,31 @@ bool itFreezer_UnkMotion0_Anim(Item_GObj* gobj)
     return false;
 }
 
-/// #itFreezer_UnkMotion0_Phys
+static void itFreezer_UnkMotion0_Phys_inline2(Item_GObj* gobj)
+{
+    Item *item = GET_ITEM(gobj);
+
+    item->xDD4_itemVar.freezer.x60 = -1;
+    Item_80268E5C(gobj, 1, ITEM_ANIM_UPDATE);
+    item->entered_hitlag = efLib_PauseAll;
+    item->exited_hitlag = efLib_ResumeAll;
+    item->on_accessory = it_802CD090;
+}
+
+static void itFreezer_UnkMotion0_Phys_inline1(Item_GObj* gobj)
+{
+    Item* item = GET_ITEM(gobj);
+    it_80273454(gobj);
+    itFreezer_UnkMotion0_Phys_inline2(gobj);
+    item->xDD1_flag.b1 = true;
+}
+
+void itFreezer_UnkMotion0_Phys(Item_GObj* gobj)
+{
+    if (it_8027A09C(gobj) != 0) {
+        itFreezer_UnkMotion0_Phys_inline1(gobj);
+    }
+}
 
 bool itFreezer_UnkMotion0_Coll(Item_GObj* gobj)
 {
