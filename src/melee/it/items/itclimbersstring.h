03f0396342850ef30a70c6531399b32ad75a35ff
malvarezcastillo <malvarezcastillo@users.noreply.github.com>
2026-01-28 21:26:51 +0100

    Decompile it_2725_Logic70_PickedUp (99.7%, vacuum)

--- src/melee/it/items/itclimbersstring.h
+++ src/melee/it/items/itclimbersstring.h
@@ -8,7 +8,7 @@
 /* 2C2750 */ void it_802C2750(Item_GObj*);
 /* 2C27D4 */ Item_GObj* it_802C27D4(Fighter_GObj*, Vec3*, int msid, float dir);
 /* 2C28B8 */ void fn_802C28B8(Item_GObj* gobj);
-/* 2C37BC */ void it_2725_Logic70_PickedUp(Item_GObj*);
+/* 2C37BC */ void it_2725_Logic70_PickedUp(Item_GObj* gobj);
 /* 2C3AA4 */ void it_2725_Logic70_EvtUnk(Item_GObj*, Item_GObj*);
 /* 3F76B8 */ extern ItemStateTable it_803F76B8[];
 
