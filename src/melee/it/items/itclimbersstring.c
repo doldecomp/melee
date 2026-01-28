f9b5d5248ee2973ba38b48755204dcd3d8bf9bef
malvarezcastillo <malvarezcastillo@users.noreply.github.com>
2026-01-28 21:19:45 +0100

    Match it_2725_Logic70_EvtUnk (vacuum)

--- src/melee/it/items/itclimbersstring.c
+++ src/melee/it/items/itclimbersstring.c
@@ -8,3 +8,12 @@ void fn_802C28B8(Item_GObj* gobj)
 {
     it_802C33B8(GET_ITEM(gobj));
 }
+
+void it_2725_Logic70_EvtUnk(Item_GObj* arg0, Item_GObj* arg1)
+{
+    Item* ip = GET_ITEM((HSD_GObj*) arg0);
+    it_8026B894(arg0, (HSD_GObj*) arg1);
+    if ((HSD_GObj*) ip->xDD4_itemVar.bombhei.xDE0 == arg1) {
+        ip->xDD4_itemVar.bombhei.xDE0 = 0;
+    }
+}
