24c59b14472dc46ddb11893f85db62006e33930d
malvarezcastillo <malvarezcastillo@users.noreply.github.com>
2026-01-28 16:11:30 +0100

    Match it_2725_Logic3_Destroyed (vacuum)

--- src/melee/it/items/itwhitebea.c
+++ src/melee/it/items/itwhitebea.c
@@ -72,7 +72,15 @@ bool itOldottosea_UnkMotion10_Coll(Item_GObj* gobj)
     return it_8027C79C(gobj);
 }
 
-/// #it_2725_Logic3_Destroyed
+void it_2725_Logic3_Destroyed(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    if ((u32) ip->xDD4_itemVar.whitebea.x20 != 0U) {
+        it_8028ECE0((HSD_GObj*) ip->xDD4_itemVar.whitebea.x20);
+    }
+    it_802E37A4(gobj);
+    it_2725_Logic9_Destroyed(gobj);
+}
 
 void it_802E3528(Item_GObj* gobj)
 {
