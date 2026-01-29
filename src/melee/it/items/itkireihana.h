e7620d9534e777c5637554d68176e64800ecdf45
Duglas560 <wichycrek@gmail.com>
2026-01-29 01:26:12 -0800

    kireihana.h update

--- src/melee/it/items/itkireihana.h
+++ src/melee/it/items/itkireihana.h
@@ -9,16 +9,30 @@
 /* 2D0118 */ void it_2725_Logic15_EvtUnk(Item_GObj*, Item_GObj*);
 /* 2D0138 */ void it_802D0138(HSD_GObj* gobj);
 /* 2D01A0 */ void it_802D01A0(Item_GObj*);
+/* 2D0204 */ bool itKireihana_UnkMotion1_Anim(Item_GObj* gobj);
+/* 2D02BC */ void itKireihana_UnkMotion1_Phys(Item_GObj* gobj);
+/* 2D0390 */ bool itKireihana_UnkMotion1_Coll(Item_GObj* gobj);
+/* 2D03F8 */ void it_802D03F8(HSD_GObj* gobj);
+/* 2D0454 */ bool itKireihana_UnkMotion2_Anim(Item_GObj* gobj);
+/* 2D04F8 */ void itKireihana_UnkMotion2_Phys(Item_GObj* gobj);
 /* 2D05AC */ bool itKireihana_UnkMotion2_Coll(Item_GObj* gobj);
-/* 2D062C */ void it_802D062C(Item_GObj*);
+/* 2D05D8 */ void it_802D05D8(HSD_GObj* gobj);
+/* 2D062C */ void it_802D062C(Item_GObj* gobj);
+/* 2D0690 */ bool itKireihana_UnkMotion3_Anim(Item_GObj* gobj)
 /* 2D06EC */ void itKireihana_UnkMotion3_Phys(Item_GObj* gobj);
+/* 2D070C */ bool itKireihana_UnkMotion3_Coll(Item_GObj* gobj)
 /* 2D0774 */ void it_802D0774(Item_GObj* gobj);
 /* 2D07C0 */ void it_802D07C0(Item_GObj* gobj);
-/* 2D08F0 */ void it_802D08F0(Item_GObj* gobj);
+/* 2D080C */ bool itKireihana_UnkMotion4_Anim(Item_GObj* gobj);
+/* 2D0860 */ void itKireihana_UnkMotion4_Phys(Item_GObj* gobj);
 /* 2D08C4 */ bool itKireihana_UnkMotion4_Coll(Item_GObj* gobj);
+/* 2D08F0 */ void it_802D08F0(Item_GObj* gobj);
 /* 2D0964 */ bool itKireihana_UnkMotion0_Anim(Item_GObj* gobj);
 /* 2D0988 */ void itKireihana_UnkMotion0_Phys(Item_GObj* gobj);
 /* 2D09A8 */ void itKireihana_UnkMotion0_Coll(Item_GObj* gobj);
 /* 3F7E28 */ extern ItemStateTable it_803F7E28[];
+
+f32 it_804DD438 = 0.0f;
+f32 it_804DD43C = 2.6f;
 
 #endif
