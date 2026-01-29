34a4a5a6ab3d14900cde4ff58610698852e8faca
Martín Álvarez Castillo <martin.alvarez@merlinsoftware.es>
2026-01-29 18:15:05 +0100

    Update src/melee/gr/grvenom.c
    Co-authored-by: Robin Avery <robin@ribbanya.dev>

--- src/melee/gr/grvenom.c
+++ src/melee/gr/grvenom.c
@@ -149,8 +149,6 @@ void grVenom_802040AC(Ground_GObj* arg) {}
 
 void grVenom_802040B0(Ground_GObj* arg) {}
 
-// Decompilation of fn_802040B4
-
 enum_t Stage_80225194(void);
 
 void fn_802040B4(Ground_GObj* gobj)
