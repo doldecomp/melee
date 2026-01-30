1202dfbd123dd2ef4309c1e03a97d447892202b2
Duglas560 <wichycrek@gmail.com>
2026-01-29 16:51:46 -0800

    Add structures for Thunder item variables and attributes

--- src/melee/it/itCommonItems.h
+++ src/melee/it/itCommonItems.h
@@ -1172,4 +1172,20 @@ typedef struct itkireihanaAttributes {
     u32 xC;
 } itkireihanaAttributes;
 
+typedef struct itThunder_ItemVars
+{
+    /* xDD8 */ u8 pad[0x60 - 0x0];
+    /* xE34 */ u32 x60;
+    /* xE38 */ u32 x64;
+    /* xE3C */ f32 x68;
+} itThunder_ItemVars;
+
+typedef struct itThunderAttributes
+{
+    f32 x0;
+    u32 x4;
+    u32 x8;
+    u32 xC;
+} itThunderAttributes;
+
 #endif
