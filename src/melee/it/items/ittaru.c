8afd06c9cda0923236de9faa0a2dbf458047ec2d
sadkellz <forestruns@gmail.com>
2026-02-10 17:29:23 -0330

    clean up

--- src/melee/it/items/ittaru.c
+++ src/melee/it/items/ittaru.c
@@ -12,6 +12,7 @@
 #include "it/it_26B1.h"
 #include "it/it_2725.h"
 #include "it/itCommonItems.h"
+#include "it/itcoll.h"
 #include "it/item.h"
 #include "lb/lb_00B0.h"
 #include "lb/lb_00F9.h"
@@ -20,19 +21,31 @@
 void it_3F14_Logic2_Spawned(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
+    float temp;
     ip->facing_dir = -1.0f;
     ip->xD5C = 0;
     ip->xDCE_flag.b7 = 0;
     ip->xDAC_itcmd_var0 = 0;
     ip->xDB0_itcmd_var1 = 0;
     ip->xDD4_itemVar.taru.xDD4 = 0;
-    ip->xDD4_itemVar.taru.xDDC = 1.3089969f + (0.34906584f * HSD_Randf());
+    /// @todo float regswap. same as in it_3F14_Logic5_Spawned
+    temp = 0.34906584f * HSD_Randf();
+    ip->xDD4_itemVar.taru.xDDC = 1.3089969f + temp;
     ip->xDD4_itemVar.taru.xDE0 = 0.0f;
     ip->xDD4_itemVar.taru.xDE4 = 0.0f;
     ip->xDD4_itemVar.taru.xDE8.x = 0.0f;
     ip->xDD4_itemVar.taru.xDE8.y = 1.0f;
     ip->xDD4_itemVar.taru.xDE8.z = 0.0f;
     it_80287D0C(gobj);
+}
+
+static inline f32 inline_fabsf(f32 x)
+{
+    if (x < 0.0F) {
+        return -x;
+    } else {
+        return +x;
+    }
 }
 
 void it_802874F0(Item_GObj* gobj)
@@ -42,12 +55,18 @@ void it_802874F0(Item_GObj* gobj)
     f32 temp_f1;
     f32 temp_f2;
     f32 var_f0;
+    int new_var2;
     f32 var_f1;
+    f32 new_var;
     f32 var_f2;
 
     if (ip->ground_or_air != GA_Air) {
-        temp_f2 = ip->xDD4_itemVar.taru.xDE8.x;
-        if (temp_f2 < 0.0f) {
+        /// @todo this surely uses some type of fabs
+        /// but everything ive tried causes a reg swap
+        var_f1 = ip->xDD4_itemVar.taru.xDE8.x;
+        temp_f2 = var_f1;
+        new_var2 = temp_f2 < 0.0f;
+        if (new_var2) {
             var_f1 = -temp_f2;
         } else {
             var_f1 = temp_f2;
@@ -58,9 +77,8 @@ void it_802874F0(Item_GObj* gobj)
                 var_f1 = -var_f1;
             }
             if (var_f1 < attr->x2C) {
-                ip->xDD4_itemVar.taru.xDE4 =
-                    (ip->xDD4_itemVar.taru.xDE8.x * attr->x24) +
-                    ip->xDD4_itemVar.taru.xDE4;
+                ip->xDD4_itemVar.taru.xDE4 +=
+                    temp_f2 * attr->x24;
             }
         } else {
             temp_f1 = ip->xDD4_itemVar.taru.xDE4;
@@ -72,7 +90,7 @@ void it_802874F0(Item_GObj* gobj)
             temp_f2 = attr->x28;
             if (var_f0 < temp_f2) {
                 ip->xDD4_itemVar.taru.xDE4 = 0.0f;
-            } else if (temp_f1 > 0.0f) {
+            } else if (ip->xDD4_itemVar.taru.xDE4 > 0.0f) {
                 ip->xDD4_itemVar.taru.xDE4 -= temp_f2;
             } else {
                 ip->xDD4_itemVar.taru.xDE4 += temp_f2;
@@ -84,20 +102,14 @@ void it_802874F0(Item_GObj* gobj)
         {
             ip->facing_dir *= -1.0f;
         }
-        var_f1 = ip->xDD4_itemVar.taru.xDE4;
-        if (var_f1 < 0.0f) {
-            var_f1 = -var_f1;
-        }
+        var_f1 = inline_fabsf(ip->xDD4_itemVar.taru.xDE4);
         temp_f2 = attr->x20;
         if (var_f1 > temp_f2) {
             ip->xDD4_itemVar.taru.xDE4 = temp_f2 * ip->facing_dir;
         }
         ip->x40_vel.x =
             ip->xDD4_itemVar.taru.xDE4 * ip->xDD4_itemVar.taru.xDE8.y;
-        var_f2 = ip->xDD4_itemVar.taru.xDE4;
-        if (var_f2 < 0.0f) {
-            var_f2 = -var_f2;
-        }
+        var_f2 = inline_fabsf(ip->xDD4_itemVar.taru.xDE4);
         ip->xDD4_itemVar.taru.xDE0 =
             (0.12217305f * var_f2 * -ip->facing_dir) +
             ip->xDD4_itemVar.taru.xDE0;
@@ -160,9 +172,9 @@ void itTaru_UnkMotion0_Phys(Item_GObj* gobj) {}
 bool itTaru_UnkMotion0_Coll(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
+    itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
     HSD_JObj* jobj = GET_JOBJ(gobj);
     CollData* coll = &ip->x378_itemColl;
-    itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
     Vec3 up;
     f32 ground_rot;
 
@@ -300,32 +312,21 @@ void itTaru_UnkMotion3_Phys(Item_GObj* gobj)
 
 bool itTaru_UnkMotion3_Coll(Item_GObj* gobj)
 {
-    Item* ip = GET_ITEM(gobj);
+    Item* ip = gobj->user_data;
     ItemAttr* attrs = ip->xCC_item_attr;
     itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
     f32 var_f1;
-    f32 var_f2;
 
     if (it_8026DA08(gobj) != 0) {
         it_802762B0(ip);
         Item_8026AE84(ip, 0x104, 0x7F, 0x40);
-        var_f2 = ip->x40_vel.y;
-        if (var_f2 < 0.0f) {
-            var_f1 = -var_f2;
-        } else {
-            var_f1 = var_f2;
-        }
+        var_f1 = (ip->x40_vel.y < 0.0f) ? -ip->x40_vel.y : ip->x40_vel.y;
         if (var_f1 > attr->x34) {
             return it_3F14_Logic2_DmgDealt(gobj);
         }
-        if (var_f2 < 0.0f) {
-            var_f2 = -var_f2;
-        }
-        if (var_f2 < attrs->x5c) {
-            var_f1 = ip->x40_vel.x;
-            if (var_f1 < 0.0f) {
-                var_f1 = -var_f1;
-            }
+        var_f1 = (ip->x40_vel.y < 0.0f) ? -ip->x40_vel.y : ip->x40_vel.y;
+        if (var_f1 < attrs->x5c) {
+            var_f1 = (ip->x40_vel.x < 0.0f) ? -ip->x40_vel.x : ip->x40_vel.x;
             ip->xDD4_itemVar.taru.xDE4 = ip->facing_dir * var_f1;
             it_802881B4(gobj);
         } else {
@@ -396,8 +397,8 @@ void itTaru_UnkMotion5_Phys(Item_GObj* gobj)
 bool itTaru_UnkMotion5_Coll(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    CollData* coll = &ip->x378_itemColl;
     itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+    CollData* coll = &ip->x378_itemColl;
     f32 var_f1;
     s32 env_flags;
 
@@ -443,6 +444,16 @@ bool itTaru_UnkMotion4_Anim(Item_GObj* gobj)
     return false;
 }
 
+static inline void itTaru_UnkMotion4_Phys_inline(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    ip->x40_vel.x = ip->xDD4_itemVar.taru.xDE4;
+    ip->x40_vel.y = 0.0f;
+    it_8026B3A8(gobj);
+    Item_80268E5C(gobj, 5, ITEM_ANIM_UPDATE);
+    it_80275444(gobj);
+}
+
 void itTaru_UnkMotion4_Phys(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
@@ -456,12 +467,7 @@ void itTaru_UnkMotion4_Phys(Item_GObj* gobj)
         var_f1 = -var_f1;
     }
     if (var_f1 > attr->x1C) {
-        ip = GET_ITEM(gobj);
-        ip->x40_vel.x = ip->xDD4_itemVar.taru.xDE4;
-        ip->x40_vel.y = 0.0f;
-        it_8026B3A8(gobj);
-        Item_80268E5C(gobj, 5, ITEM_ANIM_UPDATE);
-        it_80275444(gobj);
+        itTaru_UnkMotion4_Phys_inline(gobj);
     }
     var_f1 = ip->xDD4_itemVar.taru.xDE4;
     if (var_f1 < 0.0f) {
@@ -515,23 +521,28 @@ bool itTaru_UnkMotion6_Coll(Item_GObj* gobj)
     return false;
 }
 
-void it_802886C4(Item_GObj* gobj)
+static inline void it_802886C4_inline(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    HSD_JObj* jobj = GET_JOBJ(gobj);
     Vec3 zero;
 
-    Item_8026AE84(ip, 0xFB, 0x7F, 0x40);
-    Camera_80030E44(2, &ip->pos);
-    ip = GET_ITEM(gobj);
-    zero.x = 0.0f;
-    zero.y = 0.0f;
-    zero.z = 0.0f;
+    /// @todo 4 byte gap for these accesses...
+    zero.x = zero.y = zero.z = 0.0f;
     if (!it_8026F8B4(gobj, &ip->pos, &zero, false) &&
         it_8026F6BC(gobj, &ip->pos, &zero, false) == NULL)
     {
         it_8026F3D4(gobj, NULL, 1, false);
     }
+}
+
+void it_802886C4(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    HSD_JObj* jobj = GET_JOBJ(gobj);
+
+    Item_8026AE84(ip, 0xFB, 0x7F, 0x40);
+    Camera_80030E44(2, &ip->pos);
+    it_802886C4_inline(gobj);
     HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
     it_802756D0(gobj);
     ip->x40_vel.x = 0.0f;
@@ -560,27 +571,34 @@ bool itTaru_UnkMotion7_Coll(Item_GObj* gobj)
     return false;
 }
 
+static inline bool itTaru_RandCheck(itTaruAttributes* attr)
+{
+    s32 x4 = attr->x4;
+    s32 x0 = attr->x0;
+    if (HSD_Randi(x0 + x4) < x0) {
+        return true;
+    }
+    return false;
+}
+
+static inline void it_3F14_Logic2_inline(Item_GObj* gobj, Item* ip, itTaruAttributes* attr)
+{
+    efSync_Spawn(0x427, gobj, &ip->pos);
+    if (itTaru_RandCheck(attr)) {
+        it_802886C4(gobj);
+    } else {
+        it_802885C8(gobj);
+    }
+}
+
 bool it_3F14_Logic2_DmgDealt(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    itTaruAttributes* attr;
-    s32 x0;
-    s32 var_r0;
+    PAD_STACK(24);
 
     if (ip->xDD4_itemVar.taru.xDD4 == 0) {
-        attr = ip->xC4_article_data->x4_specialAttributes;
-        efSync_Spawn(0x427, gobj, &ip->pos);
-        x0 = attr->x0;
-        if (HSD_Randi(x0 + attr->x4) < x0) {
-            var_r0 = 1;
-        } else {
-            var_r0 = 0;
-        }
-        if (var_r0 != 0) {
-            it_802886C4(gobj);
-        } else {
-            it_802885C8(gobj);
-        }
+        itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+        it_3F14_Logic2_inline(gobj, ip, attr);
     }
     return false;
 }
@@ -588,24 +606,11 @@ bool it_3F14_Logic2_DmgDealt(Item_GObj* gobj)
 bool it_3F14_Logic2_Clanked(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    itTaruAttributes* attr;
-    s32 x0;
-    s32 var_r0;
+    PAD_STACK(24);
 
     if (ip->xDD4_itemVar.taru.xDD4 == 0) {
-        attr = ip->xC4_article_data->x4_specialAttributes;
-        efSync_Spawn(0x427, gobj, &ip->pos);
-        x0 = attr->x0;
-        if (HSD_Randi(x0 + attr->x4) < x0) {
-            var_r0 = 1;
-        } else {
-            var_r0 = 0;
-        }
-        if (var_r0 != 0) {
-            it_802886C4(gobj);
-        } else {
-            it_802885C8(gobj);
-        }
+        itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+        it_3F14_Logic2_inline(gobj, ip, attr);
     }
     return false;
 }
@@ -613,24 +618,11 @@ bool it_3F14_Logic2_Clanked(Item_GObj* gobj)
 bool it_3F14_Logic2_HitShield(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    itTaruAttributes* attr;
-    s32 x0;
-    s32 var_r0;
+    PAD_STACK(24);
 
     if (ip->xDD4_itemVar.taru.xDD4 == 0) {
-        attr = ip->xC4_article_data->x4_specialAttributes;
-        efSync_Spawn(0x427, gobj, &ip->pos);
-        x0 = attr->x0;
-        if (HSD_Randi(x0 + attr->x4) < x0) {
-            var_r0 = 1;
-        } else {
-            var_r0 = 0;
-        }
-        if (var_r0 != 0) {
-            it_802886C4(gobj);
-        } else {
-            it_802885C8(gobj);
-        }
+        itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+        it_3F14_Logic2_inline(gobj, ip, attr);
     }
     return false;
 }
@@ -638,24 +630,11 @@ bool it_3F14_Logic2_HitShield(Item_GObj* gobj)
 bool it_3F14_Logic2_Reflected(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    itTaruAttributes* attr;
-    s32 x0;
-    s32 var_r0;
+    PAD_STACK(24);
 
     if (ip->xDD4_itemVar.taru.xDD4 == 0) {
-        attr = ip->xC4_article_data->x4_specialAttributes;
-        efSync_Spawn(0x427, gobj, &ip->pos);
-        x0 = attr->x0;
-        if (HSD_Randi(x0 + attr->x4) < x0) {
-            var_r0 = 1;
-        } else {
-            var_r0 = 0;
-        }
-        if (var_r0 != 0) {
-            it_802886C4(gobj);
-        } else {
-            it_802885C8(gobj);
-        }
+        itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+        it_3F14_Logic2_inline(gobj, ip, attr);
     }
     return false;
 }
@@ -666,22 +645,11 @@ bool it_3F14_Logic2_DmgReceived(Item_GObj* gobj)
     itTaruAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
     s32 x0;
     s32 var_r0;
-    PAD_STACK(0x20);
+    PAD_STACK(24);
 
     if (ip->xDD4_itemVar.taru.xDD4 == 0) {
         if (ip->xC9C >= attr->xC) {
-            efSync_Spawn(0x427, gobj, &ip->pos);
-            x0 = attr->x0;
-            if (HSD_Randi(x0 + attr->x4) < x0) {
-                var_r0 = 1;
-            } else {
-                var_r0 = 0;
-            }
-            if (var_r0 != 0) {
-                it_802886C4(gobj);
-            } else {
-                it_802885C8(gobj);
-            }
+            it_3F14_Logic2_inline(gobj, ip, attr);
         } else {
             if (ip->msid == 0) {
                 if (HSD_Randi(attr->x8) == 0) {
