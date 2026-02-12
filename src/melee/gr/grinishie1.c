25ef82df5057d538707e32181956a6d6125f3f83
William Carter <wcarter312@gmail.com>
2026-02-12 14:16:55 -0500

    Fix incorrect function call in grInishie1_801FBC4C

--- src/melee/gr/grinishie1.c
+++ src/melee/gr/grinishie1.c
@@ -97,6 +97,15 @@ Vec3 grI1_803B8268;
         }                                                                     \
     };
 
+#define DOBJ_CLEAR_LOOP(jobj)                                                 \
+    {                                                                         \
+        HSD_DObj* dobj = HSD_JObjGetDObj(jobj);                               \
+        while (dobj != NULL) {                                                \
+            HSD_DObjClearFlags(dobj, 1U);                                     \
+            dobj = (dobj != NULL) ? dobj->next : NULL;                        \
+        }                                                                     \
+    }
+
 StageCallbacks grI1_803E48F4[] = {
     {
         grInishie1_801FAAA0,
@@ -666,7 +675,7 @@ void grInishie1_801FBAA0(HSD_GObj* gobj, s32 index)
 void grInishie1_801FBC4C(HSD_GObj* gobj, u32 index)
 {
     Ground* gp = GET_GROUND(gobj);
-    DOBJ_LOOP(gp->gv.inishie1.blocks[index].jobj2);
+    DOBJ_CLEAR_LOOP(gp->gv.inishie1.blocks[index].jobj2);
     Ground_801C4A08(gp->gv.inishie1.blocks[index].hatena_gobj);
     gp->gv.inishie1.blocks[index].hatena_gobj = NULL;
 }
