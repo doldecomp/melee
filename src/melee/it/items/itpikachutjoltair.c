2a833f04617cc08296d0bc7f8d1cd5cf3c59d5c9
Farmerbilly27 <dmw2796@gmail.com>
2026-03-01 06:17:33 -0600

    Match it_2725_Logic107_DmgDealt in melee/it/items/itpikachutjoltair.c

--- src/melee/it/items/itpikachutjoltair.c
+++ src/melee/it/items/itpikachutjoltair.c
@@ -54,7 +54,16 @@ void itPikachutjoltair_UnkMotion0_Phys(Item_GObj* gobj)
 
 /// #itPikachutjoltair_UnkMotion0_Coll
 
-/// #it_2725_Logic107_DmgDealt
+bool it_2725_Logic107_DmgDealt(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    it_802725D4(gobj);
+    if (ip->xDD4_itemVar.pikachujoltair.xDD8 != 0) {
+        it_802B3544((Item_GObj*) ip->xDD4_itemVar.pikachujoltair.xDD8);
+        ip->xDD4_itemVar.pikachujoltair.xDD8 = 0;
+    }
+    return true;
+}
 
 /// #it_2725_Logic107_Clanked
 
