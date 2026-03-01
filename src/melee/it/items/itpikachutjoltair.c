831a1d4fe64cd169ee741f9a3a3553d1c8fe2a36
Farmerbilly27 <dmw2796@gmail.com>
2026-03-01 06:20:36 -0600

    Match it_2725_Logic107_Absorbed in melee/it/items/itpikachutjoltair.c

--- src/melee/it/items/itpikachutjoltair.c
+++ src/melee/it/items/itpikachutjoltair.c
@@ -76,7 +76,16 @@ bool it_2725_Logic107_Clanked(Item_GObj* gobj)
     return true;
 }
 
-/// #it_2725_Logic107_Absorbed
+bool it_2725_Logic107_Absorbed(Item_GObj* gobj)
+{
+    Item* ip = GET_ITEM(gobj);
+    it_802725D4(gobj);
+    if (ip->xDD4_itemVar.pikachujoltair.xDD8 != 0) {
+        it_802B3544((Item_GObj*) ip->xDD4_itemVar.pikachujoltair.xDD8);
+        ip->xDD4_itemVar.pikachujoltair.xDD8 = 0;
+    }
+    return true;
+}
 
 /// #it_2725_Logic107_Reflected
 
