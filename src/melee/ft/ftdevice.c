d0bb6a873593ea51bb88dd4a65d5ee0f8d5b6848
Farmerbilly27 <dmw2796@gmail.com>
2026-03-02 13:19:46 -0600

    Match ftCo_800C07F8 in melee/ft/ftdevice.c

--- src/melee/ft/ftdevice.c
+++ src/melee/ft/ftdevice.c
@@ -97,16 +97,21 @@ void ftCo_800C0764(Ground_GObj* arg0, u32 arg1, void* arg2)
 /// always of type ftDevice_Callback0.
 void ftCo_800C07F8(Ground_GObj* arg0, u32 arg1, void* arg2)
 {
-    struct ftDeviceUnk3* temp_r6;
-    /// @todo is supposed to load in ft_80459A8C, which is ft_80459A68[3]
-    if (ft_80459A68[3].ground == NULL) {
-        temp_r6 = &ft_80459A68[3];
-        temp_r6->ground = arg0;
-        temp_r6->type = arg1;
-        temp_r6->active_cb = arg2;
-        ft_804D6570++;
-    } else {
-        OSReport("fighter chk device coll func num over!\n", ft_80459A68);
-        __assert("ftdevice.c", 0x95U, "0");
+    struct ftDeviceUnk3* base = ft_80459A68;
+    struct ftDeviceUnk3* ptr;
+    int i;
+
+    ptr = base + 3;
+    for (i = 0; i < 1; i++) {
+        if (ptr->ground == NULL) {
+            base[i + 3].ground = arg0;
+            base[i + 3].type = arg1;
+            base[i + 3].active_cb = arg2;
+            ft_804D6570++;
+            return;
+        }
+        ptr++;
     }
+    OSReport("fighter chk device coll func num over!\n");
+    __assert("ftdevice.c", 0x95, "0");
 }
