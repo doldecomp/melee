ed8fe99b6b2f1ac4be4dd7fda56c48575b73e88b
JurreJelle <jurre@jilles.com>
2026-03-02 20:50:38 +0000

    tiny improvement on gm_801674C4

--- src/melee/gm/gm_1601.c
+++ src/melee/gm/gm_1601.c
@@ -2212,14 +2212,12 @@ void gm_80167470(s32 arg0, s32 arg1)
 void gm_801674C4(s8 arg0, u8 arg1, s8 arg2, s8 arg3, s32 arg4)
 {
     s8 temp_r31;
-    struct lbl_8046B488_t* temp_r3;
     struct lbl_8046B488_t* temp_ptr;
 
-    temp_r3 = fn_80169364();
-    temp_r3->x0 = arg0;
-    temp_r3->x1 = 0x21;
-    temp_r3->x2 = 0x21;
-    temp_ptr = temp_r3;
+    temp_ptr = fn_80169364();
+    temp_ptr->x0 = arg0;
+    temp_ptr->x1 = 0x21;
+    temp_ptr->x2 = 0x21;
     temp_ptr->x7 = arg1;
     temp_ptr->x8 = arg1;
     temp_ptr->x9 = arg2;
@@ -2229,7 +2227,7 @@ void gm_801674C4(s8 arg0, u8 arg1, s8 arg2, s8 arg3, s32 arg4)
     fn_80169574((s32) temp_ptr->x7, temp_ptr->x20);
     fn_80169900(temp_ptr->xD, temp_ptr, temp_ptr->xA2, temp_ptr->x20);
     temp_r31 = Player_GetCostumeId(0);
-    fn_8016989C((u8*) temp_ptr, (s32) Player_GetPlayerCharacter(0),
+    fn_8016989C((u8*) temp_ptr, (s8) Player_GetPlayerCharacter(0),
                 (s32) temp_r31, (u8*) temp_ptr->xA2, (u8*) temp_ptr->x20);
     fn_8016A09C();
     fn_80169434(arg4);
