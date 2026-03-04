e93b2fb465455dbe6d943e4a322dfead65b31827
Robin Avery <robin@ribbanya.dev>
2026-03-04 14:20:40 -0500

    Run `clang-format`

--- src/melee/ft/chara/ftCommon/ftCo_Attack100.c
+++ src/melee/ft/chara/ftCommon/ftCo_Attack100.c
@@ -1347,14 +1347,14 @@ void fn_800DC014(Fighter_GObj* gobj)
     }
 }
 
-s32 fn_800DC044(Fighter_GObj* gobj) {
+s32 fn_800DC044(Fighter_GObj* gobj)
+{
     Fighter* fp = GET_FIGHTER(gobj);
     if (fp->input.lstick.y >= p_ftCommonData->tap_jump_threshold) {
         return 1;
     }
     return 0;
 }
-
 
 /// #fn_800DC070
 
