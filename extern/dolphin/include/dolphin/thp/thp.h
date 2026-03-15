ed3a4aaaae296ad94a2d9186d96278c9ceac304d
William Carter <wcarter312@gmail.com>
2026-03-15 19:08:33 -0400

    Get __THPReadHuffmanTableSpecification closer to match

--- extern/dolphin/include/dolphin/thp/thp.h
+++ extern/dolphin/include/dolphin/thp/thp.h
@@ -21,7 +21,8 @@ typedef struct _THPHuffmanTab {
     u8* bits;
     u32 pad3;
     u32* codetab;
-    u32 pad2[2];
+    u32 pad2;
+    u16 num_Vij;
 } THPHuffmanTab;
 
 typedef struct _THPComponent {
@@ -55,13 +56,12 @@ typedef struct _THPFileInfo {
     THPComponent components[3];
 } THPFileInfo;
 
-typedef struct {    
+typedef struct {
     s32 val0;
     u16 val1;
     u16 _pad;
-    u8 val2;    
+    u8 val2;
 } THPDec_8032FD40_Data;
-
 
 static u8 __THPSetupBuffers(THPFileInfo*);
 static u8 __THPReadFrameHeader(void);
@@ -84,7 +84,8 @@ static void __THPHuffDecodeDCTCompY(THPFileInfo*, THPCoeff*);
 static void __THPHuffDecodeDCTCompU(THPFileInfo*, THPCoeff*);
 static void __THPHuffDecodeDCTCompV(THPFileInfo*, THPCoeff*);
 
-s32 THPVideoDecode(void* file, void* tileY, void* tileU, void* tileV, void* work);
+s32 THPVideoDecode(void* file, void* tileY, void* tileU, void* tileV,
+                   void* work);
 s32 THPDec_803302EC(u8** data);
 s32 THPDec_8032FD40(THPDec_8032FD40_Data* arg0, u16 arg1);
 void THPDec_803300E0(u32* data);
--- extern/dolphin/src/dolphin/thp/THPDec.c
+++ extern/dolphin/src/dolphin/thp/THPDec.c
@@ -240,7 +240,8 @@ s32 THPVideoDecode(void* file, void* tileY, void* tileU, void* tileV,
                 }
             }
 
-            else {
+            else
+            {
                 goto _err_unsupported_marker;
             }
         }
@@ -493,7 +494,7 @@ static u8 __THPReadHuffmanTableSpecification(THPFileInfo* info)
 
         info->huffmanTabs[tab_index].bits = huffmanBits;
         info->huffmanTabs[tab_index].Vij = info->file;
-        info->huffmanTabs[tab_index].pad2[1] = num_Vij;
+        info->huffmanTabs[tab_index].num_Vij = num_Vij;
         info->file += num_Vij;
         result =
             __THPHuffGenerateSizeTable(info, tab_index, (int) huffmanBits);
@@ -1760,8 +1761,7 @@ static void __THPHuffDecodeDCTCompY(register THPFileInfo* info,
         }
 #endif // clang-format on
 
-        for (k = 1; k < 64; k++)
-        {
+        for (k = 1; k < 64; k++) {
             register s32 ssss;
             register s32 rrrr;
 
@@ -2174,7 +2174,8 @@ static void __THPHuffDecodeDCTCompU(register THPFileInfo* info,
             block[__THPJpegNaturalOrder[k]] = (s16) rrrr;
         }
 
-        else {
+        else
+        {
             if (rrrr != 15) {
                 break;
             }
