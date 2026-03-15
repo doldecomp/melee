001e6483b0b573aa0bba9ebef988e6ce2eff6057
Will <wcarter312@gmail.com>
2026-03-14 22:28:25 -0400

    Match grShrineRoute_8020A104, cleanup callback function names (#2288)
    * Match grShrineRoute_8020A104
    
    * Cleanup function names

--- src/melee/gr/grshrineroute.h
+++ src/melee/gr/grshrineroute.h
@@ -3,13 +3,17 @@
 
 #include <placeholder.h>
 
+#include "forward.h"
+
 #include "it/inlines.h"
 
+#include <baselib/forward.h>
+
 /* 2087B0 */ float grShrineRoute_802087B0(void);
-/* 2087B8 */ void grShrineRoute_802087B8(bool);
-/* 2087BC */ void grShrineRoute_802087BC(void);
-/* 20882C */ void grShrineRoute_8020882C(void);
-/* 208850 */ void grShrineRoute_80208850(void);
+/* 2087B8 */ void grShrineRoute_OnDemoInit(bool);
+/* 2087BC */ void grShrineRoute_OnInit(void);
+/* 20882C */ void grShrineRoute_OnLoad(void);
+/* 208850 */ void grShrineRoute_OnStart(void);
 /* 2088B8 */ bool grShrineRoute_802088B8(void);
 /* 2088C0 */ HSD_GObj* grShrineRoute_802088C0(int);
 /* 2089AC */ void grShrineRoute_802089AC(Ground_GObj*);
@@ -39,16 +43,17 @@
 /* 20A89C */ void grShrineRoute_8020A89C(Ground_GObj*);
 /* 20A8A0 */ void grShrineRoute_8020A8A0(Ground_GObj*);
 /* 20A8A4 */ UNK_RET grShrineRoute_8020A8A4(UNK_PARAMS);
-/* 20AA40 */ UNK_RET grShrineRoute_8020AA40(UNK_PARAMS);
-/* 20AB58 */ UNK_RET grShrineRoute_8020AB58(UNK_PARAMS);
-/* 20AC44 */ UNK_RET grShrineRoute_8020AC44(UNK_PARAMS);
-/* 20AD24 */ bool fn_8020AD24(bool);
-/* 20AD58 */ UNK_RET fn_8020AD58(UNK_PARAMS);
-/* 20AE08 */ UNK_RET fn_8020AE08(UNK_PARAMS);
+/* 20AA40 */ void grShrineRoute_8020AA40(HSD_GObj*);
+/* 20AB58 */ HSD_LObj* grShrineRoute_8020AB58(Ground_GObj*);
+/* 20AC44 */ HSD_LObj* grShrineRoute_8020AC44(HSD_GObj*);
+/* 20AD24 */ bool grShrineRoute_8020AD24(bool);
+/* 20AD58 */ UNK_RET grShrineRoute_8020AD58(UNK_PARAMS);
+/* 20AE08 */ UNK_RET grShrineRoute_8020AE08(UNK_PARAMS);
 /* 20AF38 */ UNK_RET grShrineRoute_8020AF38(UNK_PARAMS);
 /* 20B020 */ void grShrineRoute_8020B020(HSD_GObj* gobj, int r4, bool hide);
 /* 20B0AC */ void grShrineRoute_8020B0AC(void);
-/* 20B104 */ DynamicsDesc* grShrineRoute_8020B104(enum_t);
-/* 20B10C */ bool grShrineRoute_8020B10C(Vec3*, int arg, HSD_JObj* jobj);
+/* 20B104 */ DynamicsDesc* grShrineRoute_OnTouchLine(enum_t);
+/* 20B10C */ bool grShrineRoute_OnCheckShadowRender(Vec3*, int arg,
+                                                    HSD_JObj* jobj);
 
 #endif
