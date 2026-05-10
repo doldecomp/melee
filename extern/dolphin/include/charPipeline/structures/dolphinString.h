55559d575f544e12c68780ef3994fe6987a09457
orbisai0security <mediratta01.pally@gmail.com>
2026-05-10 01:51:30 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by Orbis Security AI

--- extern/dolphin/include/charPipeline/structures/dolphinString.h
+++ extern/dolphin/include/charPipeline/structures/dolphinString.h
@@ -7,8 +7,8 @@
 extern "C" {
 #endif
 
-u8 Strcat(char *str1, char *str2, char *dst);
-void Strcpy(char *dst, char *src);
+u8 Strcat(char *str1, char *str2, char *dst, u32 dst_size);
+void Strcpy(char *dst, char *src, u32 dst_size);
 s8 Strcmp(char *str1, char *str2);
 u32 Strlen(char *str);
 
