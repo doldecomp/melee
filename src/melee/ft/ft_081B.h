f3b89a0bac2ea8ef3c477b944e9784f409409e75
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2026-05-09 20:27:51 -0700

    split & link ft_081B, ft_084E (#2475)

--- src/melee/ft/ft_081B.h
+++ src/melee/ft/ft_081B.h
@@ -9,6 +9,9 @@
 #include "ftCommon/forward.h"
 #include <baselib/forward.h>
 
+// TODO transitive include hack, should be included directly by dependers
+#include <melee/ft/ft_084E.h>
+
 /* 081B38 */ void ft_80081B38(Fighter_GObj* gobj);
 /* 081C88 */ void ft_80081C88(Fighter_GObj* dst_gobj, float scl_y);
 /* 081D0C */ GroundOrAir ft_80081D0C(Fighter_GObj* gobj);
@@ -81,20 +84,5 @@
 /* 084CB0 */ void ft_80084CB0(Fighter*, ftCollisionBox*);
 /* 084CE4 */ bool ft_80084CE4(Fighter* attacker, Fighter* victim);
 /* 084DB0 */ void ft_80084DB0(Fighter_GObj*);
-/* 084E1C */ void ft_80084E1C(Fighter_GObj* gobj, float, float, float);
-/* 084EEC */ void ft_80084EEC(Fighter_GObj* gobj);
-/* 084F3C */ void ft_80084F3C(Fighter_GObj* gobj);
-/* 084FA8 */ void ft_80084FA8(Fighter_GObj* gobj);
-/* 085004 */ void ft_80085004(Fighter_GObj* gobj);
-/* 085030 */ void ft_80085030(Fighter_GObj*, float gr_friction,
-                              float facing_dir);
-/* 085088 */ void ft_80085088(Fighter_GObj*);
-/* 0850B4 */ void ft_800850B4(Fighter_GObj*);
-/* 0850E0 */ void ft_800850E0(Fighter_GObj*, float, float);
-/* 085134 */ void ft_80085134(Fighter_GObj* gobj);
-/* 085154 */ void ft_80085154(Fighter_GObj* gobj);
-/* 0851C0 */ void ft_800851C0(Fighter_GObj*);
-/* 0851D0 */ void ft_800851D0(Fighter_GObj*);
-/* 085204 */ void ft_80085204(Fighter_GObj* gobj);
 
 #endif
