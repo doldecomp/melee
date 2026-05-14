705b21fceed3a7a16b79ce706cfe2073c87558b5
Robin Avery <robin@ribbanya.dev>
2026-05-14 05:01:15 -0400

    Clean up `hsd_3B34.h`

--- src/sysdolphin/baselib/hsd_3B34.h
+++ src/sysdolphin/baselib/hsd_3B34.h
@@ -3,16 +3,16 @@
 
 #include <platform.h>
 
-void hsd_803B3408(u8*, s32, s32, s32, s32);
-void hsd_803B3CD8(s32);
-void hsd_803B46D4(void);
-void hsd_803B4A2C(void);
-void hsd_803B4D64(u32, u32);
-s32 hsd_803B51C8(s32, s32, s32, char*, s32);
-void hsd_803B5C2C(s32);
-s32 hsd_803B5C4C(s32);
-s32 hsd_803B5D70(s32, s32);
-void hsd_803B5EA0(s32);
-s32 hsd_803B6BE4(char*, s32, void*);
+/* 3B3408 */ void hsd_803B3408(u8*, s32, s32, s32, s32);
+/* 3B3CD8 */ void hsd_803B3CD8(s32);
+/* 3B46D4 */ void hsd_803B46D4(void);
+/* 3B4A2C */ void hsd_803B4A2C(void);
+/* 3B4D64 */ void hsd_803B4D64(u32, u32);
+/* 3B51C8 */ s32 hsd_803B51C8(s32, s32, s32, char*, s32);
+/* 3B5C2C */ void hsd_803B5C2C(s32);
+/* 3B5C4C */ s32 hsd_803B5C4C(s32);
+/* 3B5D70 */ s32 hsd_803B5D70(s32, s32);
+/* 3B5EA0 */ void hsd_803B5EA0(s32);
+/* 3B6BE4 */ s32 hsd_803B6BE4(char*, s32, void*);
 
 #endif
