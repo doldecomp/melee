d7b3efeebc717b67ee9f252e4378ba0eea0be3a5
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2026-05-15 13:41:52 -0700

    lb: improve lbmthp matching

--- src/melee/lb/lb_01F8.c
+++ src/melee/lb/lb_01F8.c
@@ -109,7 +109,7 @@ void lbMthp8001FAA0(const char* filename, int width, int height)
     THPDec_8032F8D4(lbl_804335B8.unk94, context);
     decode_buf = HSD_MemAlloc(THPDec_8032FD40(context, header.h));
     decoded = THPVideoDecode(&header, &output, decode_buf,
-                             (s32) lbl_804335B8.unk94, context);
+                             (void*) lbl_804335B8.unk94, context);
     if ((u16) lbl_804335B8.x6C == 0x280) {
         THPDec_80331340(decoded, lbl_804335B8.x20, lbl_804335B8.x44,
                         lbl_804335B8.x68, lbl_804335B8.x6C);
