89f5eaffb74a14ff45102a9166e63a1e9b9e1ca4
Jurre Groenendijk <jurre@jilles.com>
2026-05-23 19:26:50 +0000

    Match fn_800C7DC4 (98%)

--- src/melee/ft/ft_0C31.c
+++ src/melee/ft/ft_0C31.c
@@ -30,6 +30,7 @@
 #include "pl/player.h"
 
 #include <dolphin/mtx.h>
+#include <trigf.h>
 #include <baselib/dobj.h>
 #include <baselib/jobj.h>
 
@@ -787,6 +788,62 @@ bool ftCo_800C7CA0(Fighter_GObj* gobj)
         return true;
     }
     return false;
+}
+
+void fn_800C7DC4(HSD_GObj* gobj, s32 motion_state, Vec3* normal, Vec3* offset)
+{
+    Fighter* fp = GET_FIGHTER(gobj);
+    CollData* coll = &fp->coll_data;
+    Vec3 vel;
+    Vec3 spawn_pos;
+    f32 angle;
+    f32 mag;
+
+    ftCommon_8007D5D4(fp);
+    spawn_pos.x = fp->cur_pos.x + offset->x;
+    spawn_pos.y = fp->cur_pos.y + offset->y;
+    spawn_pos.z = fp->cur_pos.z + offset->z;
+    angle = atan2f(-normal->x, normal->y);
+    efAsync_Spawn(gobj, &GET_FIGHTER(gobj)->x60C, 5, 0x406, NULL, &spawn_pos,
+                  &angle);
+    Camera_80030E44(2, &spawn_pos);
+    mag = fp->xF0_ground_kb_vel;
+    vel = *normal;
+    vel.x *= mag;
+    vel.y *= mag;
+    vel.z *= mag;
+    {
+        f32 mul = p_ftCommonData->x1BC;
+        vel.x *= mul;
+        vel.y *= mul;
+    }
+    fp->x8c_kb_vel = vel;
+    fp->self_vel.z = 0.0f;
+    fp->self_vel.y = 0.0f;
+    fp->self_vel.x = 0.0f;
+    {
+        f32 fdir;
+        if (fp->x8c_kb_vel.x < 0.0f) {
+            fdir = -1.0f;
+        } else {
+            fdir = 1.0f;
+        }
+        fp->facing_dir = fdir;
+    }
+    Fighter_ChangeMotionState(gobj, motion_state, 0x18040, 0.0f, 1.0f, 0.0f,
+                              NULL);
+    if (coll->env_flags & Collide_RightWallHug) {
+        fp->cur_pos.x = -(fp->x68C_transNPos.z * -fp->facing_dir -
+                          (fp->cur_pos.x + offset->x));
+    } else {
+        fp->cur_pos.y =
+            fp->x68C_transNPos.y + (fp->cur_pos.y + offset->y);
+    }
+    ftCo_80090574(gobj);
+    fp->dmg.x18A8 = mag;
+    ftCommon_8007EBAC(fp, 7, 0);
+    ftColl_8007B760(gobj, p_ftCommonData->x1B8);
+    PAD_STACK(4);
 }
 
 void ftCo_DownReflect_Anim(Fighter_GObj* gobj)
