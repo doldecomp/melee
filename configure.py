20bdb91c54dd98d5a4136abacdd2646f31b91efc
Robin Avery <robin@ribbanya.dev>
2026-06-07 06:33:31 -0400

    Link `ftkirbyspecialdonkey` (#2600)

--- configure.py
+++ configure.py
@@ -748,7 +748,7 @@ config.libs = [
             Object(Matching, "melee/ft/chara/ftKirby/ftKb_SpecialNFx.c"),
             Object(Matching, "melee/ft/chara/ftKirby/ftkirbyspecialfox.c"),
             Object(Matching, "melee/ft/chara/ftKirby/ftkirbyspecialness.c"),
-            Object(NonMatching, "melee/ft/chara/ftKirby/ftkirbyspecialdonkey.c"),
+            Object(Matching, "melee/ft/chara/ftKirby/ftkirbyspecialdonkey.c"),
             Object(Matching, "melee/ft/chara/ftKirby/ftkirbyspecialpurin.c"),
             Object(Matching, "melee/ft/chara/ftKirby/ftkirbyspecialzelda.c"),
             Object(NonMatching, "melee/ft/chara/ftKirby/ftkirbyspecialseak.c"),
