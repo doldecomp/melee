7d4ee0aa95136133505f725dad9ac1b305c15a62
Robin Avery <robin@ribbanya.dev>
2026-06-17 18:32:50 -0400

    Split and mostly link `gm_1B03`

---
+++ src/melee/gm/gm_1B14.h
@@ -0,0 +1,47 @@
+#ifndef GALE01_1B14A0
+#define GALE01_1B14A0
+
+#include "gm/types.h"
+
+/* 1B14A0 */ void gm_801B14A0(GameScene*);
+/* 1B14DC */ void gm_801B14DC(GameScene*);
+/* 1B1514 */ void gm_801B1514(GameScene*);
+/* 1B154C */ void gm_801B154C(GameScene*);
+/* 1B1588 */ void gm_801B1588(GameScene*);
+/* 1B15C8 */ void gm_801B15C8(GameScene*);
+/* 1B1648 */ void gm_801B1648(GameScene*);
+/* 1B1688 */ void gm_801B1688(GameScene*);
+/* 1B16A8 */ void gm_801B16A8(GameScene*);
+/* 1B16C8 */ void gm_801B16C8(GameScene*);
+/* 1B1724 */ void gm_801B1724(GameScene*);
+/* 1B174C */ void gm_801B174C(GameScene*);
+/* 1B1788 */ void gm_801B1788(GameScene*);
+/* 1B1810 */ void gm_801B1810(GameScene*);
+/* 1B1834 */ void gm_801B1834(GameScene*);
+/* 1B18D4 */ void gm_801B18D4(GameScene*);
+/* 1B1A2C */ void gm_801B1A2C(GameScene*);
+/* 1B1A84 */ void gm_801B1A84(GameScene*);
+/* 1B1AD4 */ void gm_801B1AD4(GameScene*);
+/* 1B1B74 */ void gm_801B1B74(GameScene*);
+/* 1B1C24 */ void gm_801B1C24(GameScene*);
+/* 1B1EB8 */ void gm_801B1EB8(GameScene*);
+/* 1B1EEC */ void gm_801B1EEC(GameScene*);
+/* 1B1F6C */ void fn_801B1F6C(int);
+/* 1B1F70 */ void gm_801B1F70(GameScene*);
+/* 1B2204 */ void gm_801B2204(GameScene*);
+/* 1B2298 */ void gm_801B2298_OnInit(void);
+/* 1B23C4 */ void gm_801B23C4_OnLoad(void);
+/* 1B23F0 */ void gm_801B23F0(void);
+/* 1B24B4 */ void gm_801B24B4(GameScene*);
+/* 1B2510 */ void gm_801B2510(GameScene*);
+/* 1B254C */ void gm_801B254C(GameScene*);
+/* 1B25D4 */ void gm_801B25D4(GameScene*);
+/* 1B26AC */ void gm_801B26AC(GameScene*);
+/* 1B2704 */ void gm_801B2704(GameScene*);
+/* 1B2790 */ void gm_801B2790(GameScene*);
+/* 1B2AF8 */ void gm_801B2AF8(GameScene*);
+/* 1B2B7C */ void gm_801B2B7C_OnInit(void);
+/* 3DD9A0 */ extern GameScene gm_803DD9A0_Scenes[9];
+/* 473814 */ extern struct TrainingMenuData gm_80473814;
+
+#endif
