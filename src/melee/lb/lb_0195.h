6137f9482c7471b45b8079ad74c352e3256f6097
Robin Avery <robin@ribbanya.dev>
2026-06-21 11:51:28 -0400

    Split and partially link `lb_0192`

---
+++ src/melee/lb/lb_0195.h
@@ -0,0 +1,18 @@
+#ifndef GALE01_01955C
+#define GALE01_01955C
+
+#include <platform.h>
+
+/* 01955C */ void lb_8001955C(void);
+/* 0195D0 */ void lb_800195D0(void);
+/* 0195FC */ void fn_800195FC(void);
+/* 019628 */ void lb_80019628(void);
+/* 019880 */ void lb_80019880(u64);
+/* 019894 */ u8 lb_80019894(void);
+/* 0198E0 */ void lb_800198E0(void);
+/* 019900 */ void lb_80019900(void);
+/* 019A30 */ int lb_80019A30(int);
+/* 019A48 */ void lb_80019A48(void);
+/* 019AAC */ void lb_80019AAC(Event);
+
+#endif
