e78d80e0ff502b333e8bfc6d54bf9bc0e4c232cc
JurreJelle <jurre@jilles.com>
2026-06-28 18:55:05 +0000

    bigblue + clang format

--- src/melee/gr/grbigblue.h
+++ src/melee/gr/grbigblue.h
@@ -79,4 +79,6 @@
 /* 1EFC0C */ DynamicsDesc* grBigBlue_801EFC0C(enum_t);
 /* 1EFC14 */ bool grBigBlue_801EFC14(Vec3*, int arg, HSD_JObj* jobj);
 
+f32 grBigBlue_801E8B84_but_inline1(f32 right, f32 left, f32 bottom, f32 top);
+f32 grBigBlue_801E8B84_but_inline2(f32 right, f32 left, f32 bottom, f32 top);
 #endif
