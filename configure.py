bce339a298bb1e22e51cb4988f4241e813503a3a
Ryan Burns <rtburns@protonmail.com>
2026-07-02 00:27:35 -0700

    Switch progress report to data-value diffing
    This is a stricter diffing type which additionally looks at the data the
    relocs are pointing to, rather than just the names of the relocs. Now
    that we're far along in progress, this should be the default as we focus
    more on matching the data. I usually set this locally, and have been
    encouraging others to do so as well, but setting it here will also
    benefit our CI and AI assistants.

--- configure.py
+++ configure.py
@@ -1817,7 +1817,7 @@ config.progress_each_module = args.verbose
 config.progress_report_args = [
     # Marks relocations as mismatching if the target value is different
     # Default is "functionRelocDiffs=none", which is most lenient
-    # "--config functionRelocDiffs=data_value",
+    "--config functionRelocDiffs=data_value",
 ]
 
 if args.mode == "configure":
