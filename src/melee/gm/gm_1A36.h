d0b30261becb10e042e6f78d28b49658c6b49fae
Jacob Barber <jacoby6000@gmail.com>
2026-07-11 15:09:43 -0500

    Remove unnecessary preprocessor directives in 1A36.dox and add missing address comments

--- src/melee/gm/gm_1A36.h
+++ src/melee/gm/gm_1A36.h
@@ -13,6 +13,6 @@
 /* 1A3714 */ void gm_801A3714(s32, u64, u64);
 /* 1A3A74 */ UNK_RET gm_EvaluateAllControllerInputs(UNK_PARAMS);
 /* 1A3E88 */ UNK_RET gm_801A3E88(UNK_PARAMS);
-void gm_801A3EF4(void);
+/* 1A3EF4 */ void gm_801A3EF4(void);
 
 #endif
