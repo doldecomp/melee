e9fff7bd36dc7f99e24ca76e1f51ea4f96741dfc
Ryan Burns <rtburns@protonmail.com>
2026-07-14 19:43:24 -0700

    Split, match, & link gmcamera + gm_1A33
    Cherry-picking some near matches from
    https://github.com/doldecomp/melee/pull/2782,
    and fixing up data/bss accesses to link them in
    
    Co-authored-by: Ford Lascari <44952366+fjooord@users.noreply.github.com>

---
+++ src/melee/gm/gm_1A33.c
@@ -0,0 +1,104 @@
+#include "gm_1A33.h"
+
+#include <dolphin/pad.h>
+#include <sysdolphin/baselib/sislib.h>
+#include <melee/gm/gm_1A36.h>
+#include <melee/gm/gmcamera.h>
+#include <melee/gm/types.h>
+
+static f32 gmCamera_803DA758[12] = {
+    0.7f,   0.7f,   168.0f, 160.0f, 0.7f, 0.7f,
+    168.0f, 192.0f, 0.7f,   0.7f,   0.0f, 256.0f,
+};
+
+static struct CameraVsData gmCamera_VsCameraTextLayout;
+
+void gmCamera_801A33BC(void)
+{
+    HSD_Text* text;
+    s32 sp10;
+    s32 spC;
+
+    gmCamera_801A25C8();
+    gmCamera_VsCameraTextLayout.slot_a =
+        gmCamera_801A2334(0, gmCamera_803DA758[2], gmCamera_803DA758[3],
+                          gmCamera_803DA758[0], gmCamera_803DA758[1]);
+    gmCamera_VsCameraTextLayout.slot_b =
+        gmCamera_801A2334(1, gmCamera_803DA758[6], gmCamera_803DA758[7],
+                          gmCamera_803DA758[4], gmCamera_803DA758[5]);
+    gmCamera_801A253C(&sp10, &spC);
+    text = HSD_SisLib_803A5ACC(3, gmCamera_801A2640(), gmCamera_803DA758[10],
+                               gmCamera_803DA758[11], 0.0f, 914.2857f, 64.0f);
+    gmCamera_VsCameraTextLayout.bottom_text = text;
+    text->default_alignment = 1;
+    text->default_kerning = 1;
+    text->default_fitting = 1;
+    {
+        f32 fy = gmCamera_803DA758[9];
+        f32 fx = gmCamera_803DA758[8];
+        text->font_size.x = fx;
+        text->font_size.y = fy;
+    }
+    HSD_SisLib_803A6368(text, 0x15);
+    switch (sp10) {
+    case 0:
+    case 1:
+        gmCamera_801A2224(HSD_SisLib_803A6530(3, 0x15, 0x11), spC);
+        HSD_SisLib_803A660C(3, 0x15, 0x12);
+        break;
+    case 2:
+        HSD_SisLib_803A6530(3, 0x15, 0x13);
+        break;
+    default:
+        HSD_SisLib_803A6530(3, 0x15, 0x14);
+    }
+}
+
+void gmCamera_801A34FC_OnFrame(void)
+{
+    CameraVsData* data;
+    s32 i;
+    u64 button;
+
+    data = &gmCamera_VsCameraTextLayout;
+    if ((lbSnap_8001D338(0) != 0) || (lbSnap_8001D338(1) != 0)) {
+        for (i = 0; i < 3; i++) {
+            if (data->text[i] != NULL) {
+                HSD_SisLib_803A5CC4(data->text[i]);
+                data->text[i] = NULL;
+            }
+        }
+        gmCamera_801A33BC();
+    } else if (mn_8022F218() != 0) {
+        lbAudioAx_80024030(0);
+        mn_8022F268();
+        *gmCamera_VsCameraTextLayout.x0 = 2;
+        gm_801A4B60();
+    } else if (button = gm_GetButtonsTriggered(PAD_ALL_CONTROLLERS),
+               (button & (HSD_PAD_START | HSD_PAD_A)) | (button & 0))
+    {
+        lbAudioAx_80024030(1);
+        *gmCamera_VsCameraTextLayout.x0 = 0;
+        gm_801A4B60();
+    } else if (button = gm_GetButtonsTriggered(PAD_ALL_CONTROLLERS),
+               (button & PAD_BUTTON_B) | (button & 0))
+    {
+        lbAudioAx_80024030(0);
+        *gmCamera_VsCameraTextLayout.x0 = 1;
+        gm_801A4B60();
+    }
+}
+
+void gmCamera_801A3634_OnEnter(UNK_T arg0)
+{
+    PAD_STACK(8);
+
+    gmCamera_VsCameraTextLayout.x0 = (u32*) arg0;
+    gmCamera_801A2650();
+    gmCamera_VsCameraTextLayout.slot_a = NULL;
+    gmCamera_VsCameraTextLayout.slot_b = NULL;
+    gmCamera_VsCameraTextLayout.bottom_text = NULL;
+    gmCamera_801A33BC();
+}
+
+void gmCamera_801A367C_OnLeave(UNK_T unused) {}
--- src/melee/gm/gmcamera.h
+++ src/melee/gm/gmcamera.h
@@ -7,22 +7,12 @@
 
 #include "dolphin/types.h"
 
-typedef struct _gmCameraUnkStruct3 {
-    /*0x00*/ u8 x0_padding[0x24 - 0x0];
-    /*0x24*/ s32 x24;
-    /*0x28*/ s32 x28;
-    /*0x2C*/ s32 x2C;
-    /*0x30*/ u8 x30_padding[0x49 - 0x30];
-    /*0x49*/ u8 x49;
-} gmCameraUnkStruct3;
-
-typedef struct _gmCameraUnkStruct2 {
-    /*0x00*/ u8 x0_padding[0x24 - 0x0];
-    /*0x24*/ u32 x24;
-    /*0x28*/ u32 x28;
-    /*0x2C*/ int x2C;
-    /*0x30*/ u32 x30;
-} gmCameraUnkStruct2;
+typedef struct _gmCameraUnkStruct4 {
+    /*0x00*/ bool x0;
+    /*0x04*/ int x4;
+    /*0x08*/ int x8;
+    /*0x0C*/ int xC;
+} gmCameraUnkStruct4;
 
 typedef struct _gmCameraUnkStruct {
     /*0x00*/ void* ifvscam; ///< interface dat file loaded from 801a3254
@@ -31,59 +21,23 @@ typedef struct _gmCameraUnkStruct {
     /*0x0C*/ s32 xC;
     /*0x10*/ s32 x10;
     /*0x14*/ s32 x14;
-    /*0x18*/ u32 x18;
+    /*0x18*/ s32 x18;
     /*0x1C*/ void* x1C;
-    /*0x20*/ u32 x20;
-    /*0x24*/ u32 x24;
-    /*0x28*/ s32 x28;
-    /*0x2C*/ u32 x2C;
-    /*0x30*/ s32 x30;
-    /*0x34*/ u8 pad_x34[0x38 - 0x34];
-    /*0x38*/ u32 x38;
-    /*0x3C*/ u8 pad_x3C[0x40 - 0x3C];
-    /*0x40*/ s32 x40;
+    /*0x20*/ int x20;
+    /*0x24*/ gmCameraUnkStruct4 x24[2];
     /*0x44*/ s32 x44;
     /*0x48*/ HSD_Text* x48[3];
-    /*0x54*/ u32 x54;
+    /*0x54*/ int x54;
 } gmCameraUnkStruct;
-
-typedef struct _gmCameraUnkStruct2Ptrs {
-    /*0x00*/ u8 x0_padding[0x58 - 0x0];
-} gmCameraUnkStruct2Ptrs;
-
-typedef struct _gmCameraUnkStruct4 {
-    /*0x00*/ bool x0;
-    /*0x04*/ u32 x4;
-    /*0x08*/ u32 x8;
-    /*0x0C*/ u32 xC;
-} gmCameraUnkStruct4;
-
-typedef struct _gmCameraUnkStruct4Ptrs {
-    /*0x00*/ u8 x0_padding[0x24 - 0x0];
-    /*0x24*/ gmCameraUnkStruct4 x24[2];
-} gmCameraUnkStruct4Ptrs;
-
-union _gmCameraUnkUnion {
-    gmCameraUnkStruct gcus;
-    // gmCameraUnkStruct2* gcus2_ptrs[0x58/4];
-    gmCameraUnkStruct2Ptrs gcus2_ptrs;
-    gmCameraUnkStruct4Ptrs gcus4;
-};
 
 typedef struct _gmCameraUnkFuncTable {
     /*0x00*/ struct {
         u8 x0;
-        u8 x1;
         u16 x2;
     } flags;
     /*0x04*/ void (*x4)(void);
     /*0x08*/ void (*x8)(void);
 } gmCameraUnkFuncTable;
-
-/* 479BC8 */ extern union _gmCameraUnkUnion gmCamera_80479BC8;
-/* 4DA9B8 */ extern float gmCamera_804DA9B8;
-/* 4DA9BC */ extern float gmCamera_804DA9BC;
-/* 4DA9C0 */ extern float gmCamera_804DA9C0;
 
 /* 1A2224 */ u8* gmCamera_801A2224(u8* arg0, u32 arg1);
 /* 1A2334 */ HSD_Text* gmCamera_801A2334(s32, f32, f32, f32, f32);
@@ -107,11 +61,5 @@ typedef struct _gmCameraUnkFuncTable {
 /* 1A30E4 */ UNK_RET gmCamera_801A30E4(UNK_PARAMS);
 /* 1A31D8 */ UNK_RET fn_801A31D8(HSD_GObj*);
 /* 1A31FC */ UNK_RET gmCamera_801A31FC(UNK_PARAMS);
-/* 1A33BC */ void gmCamera_801A33BC(void);
-/* 1A34FC */ void gmCamera_801A34FC_OnFrame(void);
-/* 1A3634 */ void gmCamera_801A3634_OnEnter(UNK_T);
-/* 1A367C */ void gmCamera_801A367C_OnLeave(UNK_T);
-
-/* 3DA6B4 */ extern gmCameraUnkFuncTable gmCamera_803DA6B4[];
 
 #endif
