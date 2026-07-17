29965a79e05d41c4eefd1a9b927a72206e29598a
Mark McCaskey <maoeurk@gmail.com>
2026-07-17 13:09:13 +0800

    Share attached SpawnItem initialization

--- src/melee/it/items/inlines.h
+++ src/melee/it/items/inlines.h
@@ -32,4 +32,20 @@ static inline void Item_InitSpawnOnPlane(SpawnItem* spawn, HSD_GObj* parent,
     spawn->x40 = 0;
 }
 
+static inline void Item_InitSpawnOnPlaneNoInitialCollision(SpawnItem* spawn,
+                                                           HSD_GObj* parent,
+                                                           Vec3* pos, f32 dir)
+{
+    spawn->prev_pos = *pos;
+    spawn->prev_pos.z = 0.0F;
+    spawn->pos = spawn->prev_pos;
+    spawn->facing_dir = dir;
+    spawn->x3C_damage = 0;
+    spawn->vel.x = spawn->vel.y = spawn->vel.z = 0.0F;
+    spawn->x0_parent_gobj = parent;
+    spawn->x4_parent_gobj2 = spawn->x0_parent_gobj;
+    spawn->x44_flag.b0 = false;
+    spawn->x40 = 0;
+}
+
 #endif
