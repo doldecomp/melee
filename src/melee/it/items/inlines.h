463fbfdb132c6b07c5bfc175434c73e3f376aabd
Mark McCaskey <maoeurk@gmail.com>
2026-07-17 10:46:40 +0800

    Share item angle normalization
    Move the two loop-order variants of signed angle normalization into item inlines and reuse them across Koopa Flame, Hinoarashi, and Light Gun Beam steering and collision code. Remove the duplicate per-file clamp helpers. All three complete objects retain their matched code.

--- src/melee/it/items/inlines.h
+++ src/melee/it/items/inlines.h
@@ -5,6 +5,7 @@
 #include "it/types.h"
 
 #include <baselib/jobj.h>
+#include <math.h>
 #include <MSL/math.h>
 
 static inline void itGamewatch_InitSpawnItem(SpawnItem* spawn,
@@ -29,6 +30,26 @@ static inline void Item_UpdateRollingShellRotation(Item_GObj* gobj, Item* ip,
     it_80276CB8(gobj);
     jobj = HSD_JObjGetChild(jobj);
     HSD_JObjAddRotationY(jobj, *rotation_rate * ABS(ip->x40_vel.x));
+}
+
+static inline void Item_ClampAngle(f32* angle)
+{
+    while (*angle < -M_PI) {
+        *angle += M_TAU;
+    }
+    while (*angle > M_PI) {
+        *angle -= M_TAU;
+    }
+}
+
+static inline void Item_ClampAngleReverse(f32* angle)
+{
+    while (*angle > M_PI) {
+        *angle -= M_TAU;
+    }
+    while (*angle < -M_PI) {
+        *angle += M_TAU;
+    }
 }
 
 #endif
