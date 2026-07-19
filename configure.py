79fd59ff623e859347cd568a5a3f55786c20753a
Robin Avery <robin@ribbanya.dev>
2026-07-19 09:10:58 -0400

    Link `ftCo_Guard` (#2909)

--- configure.py
+++ configure.py
@@ -585,7 +585,7 @@ config.libs = [
             Object(NonMatching, "melee/ft/chara/ftCommon/ftCo_Damage.c"),
             Object(Matching, "melee/ft/chara/ftCommon/ftCo_DamageFall.c"),
             Object(NonMatching, "melee/ft/chara/ftCommon/ftCo_DamageIce.c"),
-            Object(Testing, "melee/ft/chara/ftCommon/ftCo_Guard.c"),
+            Object(Matching, "melee/ft/chara/ftCommon/ftCo_Guard.c"),
             Object(Matching, "melee/ft/chara/ftCommon/ftpickupitem.c"),
             Object(NonMatching, "melee/ft/chara/ftCommon/ftCo_ItemThrow.c"),
             Object(Matching, "melee/ft/chara/ftCommon/ftCo_SpecialS.c"),
