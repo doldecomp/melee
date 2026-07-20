c578fbb6c78713d364e4747b1af4d52b38012695
Robin Avery <robin@ribbanya.dev>
2026-07-20 07:55:19 -0400

    Fix ctx

--- src/melee/lb/lbrefract.h
+++ src/melee/lb/lbrefract.h
@@ -1,24 +1,18 @@
 #ifndef GALE01_021CE8
 #define GALE01_021CE8
 
-#include <placeholder.h>
 #include <platform.h>
 
 #include <baselib/forward.h>
 
 /* 021CE8 */ void lbRefract_80021CE8(void*, s32);
-/* 0222A4 */ UNK_RET lbRefract_800222A4(UNK_PARAMS);
 /* 0222A4 */ void lbRefract_800222A4(void);
 /* 02247C */ void lbRefract_8002247C(HSD_CObj*);
-/* 022560 */ UNK_RET lbRefract_80022560(UNK_PARAMS);
 /* 022560 */ void lbRefract_80022560(void);
-/* 0225D4 */ UNK_RET lbRefract_800225D4(UNK_PARAMS);
 /* 0225D4 */ void lbRefract_800225D4(void);
 /* 0226A8 */ s32 lbRefract_PObjLoad(HSD_PObj* pobj, HSD_PObjDesc* desc);
 /* 022998 */ void lbRefract_80022998(HSD_MObj*, u32 rendermode, s32);
-/* 022BB8 */ UNK_RET lbRefract_80022BB8(UNK_PARAMS);
 /* 022BB8 */ void lbRefract_80022BB8(void);
-/* 022BD0 */ UNK_RET lbRefSetUnuse(UNK_PARAMS);
 /* 022BD0 */ void lbRefSetUnuse(void);
 /* 022C30 */ float atan2f(float y, float x);
 /* 022D1C */ float acosf(float);
