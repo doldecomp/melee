86f55b633e96ed67bdcc0b430c1a04c242c5f1d6
Robin Avery <robin@ribbanya.dev>
2026-08-13 17:00:09 -0400

    Link `itcoll`

--- configure.py
+++ configure.py
@@ -1234,7 +1234,7 @@ config.libs = [
             Object(Matching, "melee/it/itgroundcoll.c"),
             Object(Matching, "melee/it/itdraw.c"),
             Object(Matching, "melee/it/itdrop.c"),
-            Object(NonMatching, "melee/it/itcoll.c"),
+            Object(Matching, "melee/it/itcoll.c"),
             Object(Matching, "melee/it/it_3F14.c"),
             Object(Matching, "melee/it/it_2725.c"),
             Object(Matching, "melee/it/ithitbox.c"),
