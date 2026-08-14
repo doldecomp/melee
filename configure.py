e7914ea442df726f9e9a666c63f376d43b229e6f
Robin Avery <robin@ribbanya.dev>
2026-08-13 22:49:33 -0400

    Link `mnsoundtest` (#3071)

--- configure.py
+++ configure.py
@@ -1210,7 +1210,7 @@ config.libs = [
             Object(NonMatching, "melee/mn/mnvibration.c"),
             Object(NonMatching, "melee/mn/mnsound.c"),
             Object(Matching, "melee/mn/mndeflicker.c"),
-            Object(NonMatching, "melee/mn/mnsoundtest.c"),
+            Object(Matching, "melee/mn/mnsoundtest.c"),
             Object(Matching, "melee/mn/mnlanguage.c"),
             Object(Matching, "melee/mn/mnhyaku.c"),
             Object(NonMatching, "melee/mn/mnevent.c"),
