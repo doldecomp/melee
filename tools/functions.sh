a2f2001511fa0ae96521d9c15afda5a7b738fa24
Robin Avery <robin@ribbanya.dev>
2026-08-14 22:23:25 -0400

    Remove unused tools and build configurations

--- tools/functions.sh
+++ tools/functions.sh
@@ -4,7 +4,6 @@ function melee_sed {
     find \
         src \
         docs \
-        ldscript.lcf \
         config/GALE01/splits.txt \
         config/GALE01/symbols.txt \
         configure.py \
@@ -15,7 +14,7 @@ function melee_sd {
     {
         find src -type f \( -name '*.c' -o -name '*.h' \) -print0
         find docs -type f -name '*.md' -print0
-        find ldscript.lcf config/GALE01/splits.txt config/GALE01/symbols.txt configure.py -type f -print0
+        find config/GALE01/splits.txt config/GALE01/symbols.txt configure.py -type f -print0
     } | xargs -0 sd "${@}"
 }
 
