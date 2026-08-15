13e19401555b9605671ee7847541c9cbaac40839
Robin Avery <robin@ribbanya.dev>
2026-08-15 05:23:26 -0400

    Extract `HSD_SisLib_FontAtlas` as asset (#3094)
    * Extract `HSD_SisLib_FontAtlas` as asset
    
    * Remove reloc block for `HSD_SisLib_FontAtlas`
    
    * Hack symbol inclusion for now

--- configure.py
+++ configure.py
@@ -215,8 +215,8 @@ if not is_windows():
 if not args.asm:
     config.asm_dir = None
 
-# Handled internally
-config.generate_compile_commands = False
+config.generate_compile_commands = False  # Handled internally
+config.extra_clang_flags.extend([f"-isystembuild/{config.version}/include"])
 
 # Tool versions
 config.binutils_tag = "2.42-2"
@@ -336,6 +336,7 @@ includes_base = [
     "src/MSL",
     "src/Runtime",
     "extern/dolphin/include",
+    f"build/{config.version}/include",
 ]
 
 cflags_melee = [
@@ -417,6 +418,7 @@ def SysdolphinLib(lib_name: str, objects: Objects) -> Library:
         includes=[
             *includes_base,
             "src/sysdolphin",
+            f"build/{config.version}/GALE01/sysdolphin",
         ],
         category="hsd",
     )
@@ -1817,7 +1819,7 @@ config.libs = [
             ),
             Object(Matching, "sysdolphin/baselib/sobjlib.c"),
             Object(NonMatching, "sysdolphin/baselib/sislib.c"),
-            Object(Matching, "sysdolphin/baselib/hsd_40FF.c"),
+            Object(Matching, "sysdolphin/baselib/sislib_font.c"),
             Object(Matching, "sysdolphin/baselib/hsd_4D11.c"),
             Object(NonMatching, "sysdolphin/baselib/hsd_3A94.c"),
             Object(Matching, "sysdolphin/baselib/hsd_3B27.c"),
