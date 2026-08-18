0c4ceff23418dea22be694d580a60533ca88c7a7
Àlex Romàn <eirexe123@gmail.com>
2026-08-18 12:18:43 +0200

    Give the projection matrix all four rows instead of 4x3

--- src/sysdolphin/baselib/cobj.h
+++ src/sysdolphin/baselib/cobj.h
@@ -170,7 +170,7 @@ void HSD_CObjRemoveAnimByFlags(HSD_CObj* cobj, u32 flags);
 void HSD_CObjAddAnim(HSD_CObj* cobj, HSD_CameraAnim* canim);
 void HSD_CObjAnim(HSD_CObj* cobj);
 void HSD_CObjReqAnim(HSD_CObj* cobj, f32 startframe);
-GXProjectionType makeProjectionMtx(HSD_CObj* cobj, Mtx mtx);
+GXProjectionType makeProjectionMtx(HSD_CObj* cobj, Mtx44 mtx);
 void HSD_CObjSetupViewingMtx(HSD_CObj* cobj);
 f32 HSD_CObjGetEyeDistance(HSD_CObj* cobj);
 void HSD_CObjSetUpVector(HSD_CObj* cobj, Vec3* up);
