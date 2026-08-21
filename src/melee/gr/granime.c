5ac125226ab485ae0e8b4f1dd1a9821a19933d25
Ryan Burns <rtburns@protonmail.com>
2026-08-21 00:41:38 -0700

    Revert bad merge

--- src/melee/gr/granime.c
+++ src/melee/gr/granime.c
@@ -53,7 +53,7 @@
 /* 4D695C */ static float grAnime_804D695C;
 
 struct padded_jmp_buf {
-    jmp_buf buf;
+    __jmp_buf buf;
     u8 pad[0x118 - 0xF8];
 };
 
@@ -1215,7 +1215,7 @@ void grAnime_801C8138(HSD_GObj* gobj, enum_t arg1, bool arg2)
 void fn_801C82E8(int arg0, int* arg1)
 {
     *arg1 = arg0;
-    longjmp(grAnime_8049EE40.buf, 1);
+    longjmp(&grAnime_8049EE40.buf, 1);
 }
 
 HSD_AObj* grAnime_801C8318(HSD_GObj* gobj, int arg1, u32 arg2)
@@ -1236,7 +1236,7 @@ HSD_AObj* grAnime_801C8318(HSD_GObj* gobj, int arg1, u32 arg2)
     if (arg2 & 4) {
         var_r30 |= 0x100;
     }
-    if (setjmp(grAnime_8049EE40.buf) == 0) {
+    if (__setjmp(&grAnime_8049EE40.buf) == 0) {
         HSD_ForeachAnim(jobj, JOBJ_TYPE, var_r30, fn_801C82E8, AOBJ_ARG_AV,
                         &sp14);
     }
