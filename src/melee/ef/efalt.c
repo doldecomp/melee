494a6765343f49de21e0561b8cee8437382bcf1f
Ryan Burns <rtburns@protonmail.com>
2026-08-20 21:09:35 -0700

    ef: fix va_args usage

--- src/melee/ef/efalt.c
+++ src/melee/ef/efalt.c
@@ -9,7 +9,11 @@
 
 #include <math.h>
 
+#ifdef __MWERKS__
 #define EFALT_VA_ARG(t) (*((t*) __va_arg(vlist_arg, _var_arg_typeof(t))))
+#else
+#define EFALT_VA_ARG(t) va_arg(vlist, typeof(t))
+#endif
 
 extern volatile u32 efLib_LoadKind;
 extern volatile s32 efLib_AnimCount;
