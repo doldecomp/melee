78b720ba4923a4ea074aa5bacdb910715b81fd4c
Robin Avery <robin@ribbanya.dev>
2026-08-22 16:21:16 -0400

    WIP

--- src/sysdolphin/baselib/jobj.c
+++ src/sysdolphin/baselib/jobj.c
@@ -16,7 +16,6 @@
 #include <math.h>
 #include <string.h>
 #include <dolphin/mtx.h>
-#include <dolphin/os.h>
 
 void JObjInfoInit(void);
 HSD_JObjInfo hsdJObj = { JObjInfoInit };
@@ -673,15 +672,11 @@ HSD_JObj* HSD_JObjLoadJoint(HSD_Joint* arg0)
 }
 
 #ifdef MUST_MATCH
-#ifdef MUST_MATCH
 #pragma push
 #pragma force_active on
-#endif
 static char unused1[] = "jobj_root";
 static char unused2[] = "jobj_root == NULL";
-#ifdef MUST_MATCH
 #pragma pop
-#endif
 #endif
 
 void HSD_JObjResolveRefs(HSD_JObj* jobj, HSD_Joint* joint)
@@ -1544,10 +1539,8 @@ void JObjAmnesia(HSD_ClassInfo* info)
 }
 
 #ifdef MUST_MATCH
-#ifdef MUST_MATCH
 #pragma push
 #pragma force_active on
-#endif
 static char unused3[] = "jobj[%d,%d]";
 static char unused4[] = "SKELETON_ROOT ";
 static char unused5[] = "SKELETON ";
@@ -1559,9 +1552,7 @@ static char unused10[] = "  tra(L): ";
 static char unused11[] = "  rot(G): ";
 static char unused12[] = "  sca(G): ";
 static char unused13[] = "  tra(G): ";
-#ifdef MUST_MATCH
 #pragma pop
-#endif
 #endif
 
 void JObjInfoInit(void)
@@ -1580,12 +1571,8 @@ void JObjInfoInit(void)
 }
 
 #ifdef MUST_MATCH
-#ifdef MUST_MATCH
 #pragma push
 #pragma force_active on
-#endif
 static u32 unused14[6] = { 0 };
-#ifdef MUST_MATCH
 #pragma pop
-#endif
 #endif
