29955fc395eeaadb70a5e05f4c23828b52363bf8
Robin Avery <robin@ribbanya.dev>
2026-08-23 23:21:55 -0400

    WIP

--- src/melee/gr/grcorneria.c
+++ src/melee/gr/grcorneria.c
@@ -1,9 +1,6 @@
 #include "grcorneria.h"
 
 #include "granime.h"
-
-#include "grcorneria.static.h"
-
 #include "grdatfiles.h"
 #include "grdisplay.h"
 #include "grlib.h"
@@ -75,6 +72,24 @@ struct grCorneria_YakumonoParam {
     /* 0x84 */ s32 x84;
     /* 0x88 */ f32 x88;
 };
+
+/* 1DD654 */ static void grCorneria_801DD654(Ground_GObj*);
+/* 1DD658 */ static void grCorneria_801DD658(Ground_GObj*);
+/* 1DDAC0 */ static void grCorneria_801DDAC0(Ground_GObj*);
+/* 1DE4BC */ static void grCorneria_801DE4BC(Ground_GObj*);
+/* 1DE8E0 */ static void grCorneria_801DE8E0(Ground_GObj*);
+/* 1DF8CC */ static void grCorneria_801DF8CC(Ground_GObj*);
+/* 1DFBEC */ static void grCorneria_801DFBEC(Ground_GObj*);
+/* 1DFC24 */ static void grCorneria_801DFC24(Ground_GObj*);
+/* 1DFC28 */ static void grCorneria_801DFC28(Ground_GObj*);
+/* 1DFEB4 */ static void grCorneria_801DFEB4(Ground_GObj*);
+/* 1E013C */ static void grCorneria_801E013C(Ground_GObj*);
+/* 1E03C4 */ static void grCorneria_801E03C4(Ground_GObj*);
+/* 1E0DE0 */ static void grCorneria_801E0DE0(Ground_GObj*);
+/* 1E0E3C */ static void grCorneria_801E0E3C(Ground_GObj*);
+/* 1E0F30 */ static void grCorneria_801E0F30(Ground_GObj*);
+/* 1E1054 */ static void grCorneria_801E1054(Ground_GObj*);
+/* 1E12CC */ static void grCorneria_801E12CC(Ground_GObj*);
 
 static struct grCorneria_YakumonoParam* yakumono_param;
 static u32 grCn_804D69A4;
