c9ae63f6afa8708e13557d211afd72251a7c2496
Mark McCaskey <maoeurk@gmail.com>
2026-08-24 08:44:15 -0700

    Improve gmMainLib_8015F600 to 99.96%
    Split the fighter-data reset loop into its own inline so the (u8) index
    becomes a single hoisted web, and name the copied character in the name-tag
    copy loop.

--- src/melee/gm/gmmain_lib.c
+++ src/melee/gm/gmmain_lib.c
@@ -1099,6 +1099,22 @@ GetPersistentFighterDataBase(struct gmm_x1868* data)
     return data->x1F2C;
 }
 
+static inline void ResetAllPersistentFighterData(void)
+{
+    s32 i;
+
+    for (i = 0; i < 0x19; i++) {
+        int j = 0;
+        u8 k = i;
+        struct FighterData* base =
+            GetPersistentFighterDataBase(&gmMainLib_804D3EE0->thing);
+        for (; 0x19 > j; j++) {
+            base[k].fighter_kos[j] = 0;
+        }
+        gmMainLib_8015EF30((struct gmMainLib_8015EF30_s*) &base[k].sd_count);
+    }
+}
+
 static inline void ResetPersistentFighterData(s32 i)
 {
     int j = 0;
@@ -1227,12 +1243,7 @@ void gmMainLib_8015F600(int arg0, int arg1)
     PAD_STACK(88);
 
     if (arg0 == 1) {
-        {
-            s32 i;
-            for (i = 0; i < 0x19; i++) {
-                ResetPersistentFighterData(i);
-            }
-        }
+        ResetAllPersistentFighterData();
 
         memzero(&gmMainLib_804D3EE0->thing.trophy_count, 0x25C);
         Toy_80311960();
@@ -1298,8 +1309,9 @@ void gmMainLib_8015F600(int arg0, int arg1)
                 char* src = mnName_8023749C((s32) (u8) idx);
                 if (src != NULL) {
                     s32 k = 0;
-                    while (gmMainLib_804D3EE4[0] != (s8) (u8) *src) {
-                        data->namedata[k] = (u8) *src;
+                    u8 c;
+                    while (gmMainLib_804D3EE4[0] != (s8) (u8) (c = *src)) {
+                        data->namedata[k] = c;
                         k++;
                         src++;
                     }
