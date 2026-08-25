dd453138dd0899cc75918a80ad6126affa2ceaf2
Mark McCaskey <maoeurk@gmail.com>
2026-08-25 09:14:22 -0700

    Improve hsd_803A949C first-block check

--- src/sysdolphin/baselib/hsd_3A94.c
+++ src/sysdolphin/baselib/hsd_3A94.c
@@ -233,8 +233,7 @@ void hsd_803A949C(s32 chan, s32 arg1)
                 break;
             }
             block = (CardBlock*) (state->x0 + offset);
-            result = hsd_803A949C_FileId(block);
-            if (result != CMD_S32(0x20)) {
+            if (hsd_803A949C_FileId(block) != CMD_S32(0x20)) {
                 hsd_804D7988 = 2;
             } else if ((s32) block->x12 != CMD_S32(0x24)) {
                 hsd_804D7988 = 2;
