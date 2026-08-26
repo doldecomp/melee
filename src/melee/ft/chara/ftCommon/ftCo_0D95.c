b63ed766695dec6405d995bda49fcc7eea07af1f
ford-bubba-ai <ford.lascari@gmail.com>
2026-08-26 02:40:15 -0500

    worker-integration(job-6b55): main/melee/ft/chara/ftCommon/ftCo_0D95::fn_800D9558 [checkpoint b002c061]

--- src/melee/ft/chara/ftCommon/ftCo_0D95.c
+++ src/melee/ft/chara/ftCommon/ftCo_0D95.c
@@ -15,7 +15,8 @@ bool fn_800D9558(Fighter_GObj* gobj)
     Fighter* fp = GET_FIGHTER(gobj);
     ftSs_DatAttrs* attrs;
     Item_GObj* item;
-    Item* it;
+    s32 frame;
+    s32 i;
     itSamusGrappleAttributes* grappleAttrs;
     HSD_GObj* segGobj;
     HSD_JObj* jobj;
@@ -25,9 +26,7 @@ bool fn_800D9558(Fighter_GObj* gobj)
     f32 grav;
     f32 my;
     f32 r;
-    s32 i;
-    s32 frame;
-    PAD_STACK(0x14);
+    PAD_STACK(0x10);
     if (fp->kind == FTKIND_SAMUS) {
         attrs = fp->dat_attrs;
         fp->mv.ca.specials.grav += 1.0;
@@ -44,8 +43,9 @@ bool fn_800D9558(Fighter_GObj* gobj)
             fp->accessory3_cb = (void (*)(HSD_GObj*)) it_802BACC4;
         } else if (grav > (f32) attrs->x9C) {
             if (grav <= (f32) attrs->xA8) {
+                Item* it;
+                it = GET_ITEM(fp->u.ss.x223C);
                 item = fp->u.ss.x223C;
-                it = GET_ITEM(item);
                 grappleAttrs = it->xC4_article_data->x4_specialAttributes;
                 if (item != NULL) {
                     for (i = 0, frame = 0x14; i < 6; i++, frame += 3) {
@@ -56,7 +56,8 @@ bool fn_800D9558(Fighter_GObj* gobj)
                             effPos.x = jobj->mtx[0][3];
                             effPos.y = jobj->mtx[1][3];
                             effPos.z = jobj->mtx[2][3];
-                            effPos.x = 4.0 * (HSD_Randf() - 0.5f) + effPos.x;
+                            r = HSD_Randf();
+                            effPos.x = 4.0 * (r - 0.5f) + effPos.x;
                             effPos.y = 4.0 * (HSD_Randf() - 0.5f) + effPos.y;
                             r = HSD_Randf() - 0.5f;
                             effPos.z = 4.0 * r + effPos.z;
