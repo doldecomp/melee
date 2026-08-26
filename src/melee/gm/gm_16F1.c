177ee0ef7d9b7874c2c7ad209f85af7602085079
ford-bubba-ai <ford.lascari@gmail.com>
2026-08-26 15:53:03 -0500

    worker-integration(job-0c4b): main/melee/gm/gm_16F1::fn_801701C0 [checkpoint a50f517c]

--- src/melee/gm/gm_16F1.c
+++ src/melee/gm/gm_16F1.c
@@ -1050,6 +1050,8 @@ int fn_801701C0(void* arg0, int arg1, int arg2)
     }
 
     case 0xFC: {
+        struct lbl_8046B6A0_24C_58_t (*x58_array)[6] = &rules->x58;
+
         {
             struct lbl_8046B6A0_24C_58_t* p = x58;
             int i;
@@ -1071,7 +1073,7 @@ int fn_801701C0(void* arg0, int arg1, int arg2)
                     p++;
                 }
             }
-            if (!(x58[arg1].x3 & 1) && rankings[arg1] == 0 &&
+            if (!((*x58_array)[arg1].x3 & 1) && rankings[arg1] == 0 &&
                 x58[arg1].x20 == 0)
             {
                 return 1;
@@ -1094,13 +1096,13 @@ int fn_801701C0(void* arg0, int arg1, int arg2)
                         p++;
                     }
                 }
-                if (!(x58[arg1].x3 & 1) && x58[arg1].x5 == 0 &&
+                if (!((*x58_array)[arg1].x3 & 1) && x58[arg1].x5 == 0 &&
                     x58[arg1].x20 == 0)
                 {
                     return 1;
                 }
             } else {
-                if (!(x58[arg1].x3 & 1) && x58[arg1].x20 == 0) {
+                if (!((*x58_array)[arg1].x3 & 1) && x58[arg1].x20 == 0) {
                     return 1;
                 }
             }
