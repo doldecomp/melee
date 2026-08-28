c82e080ee4e8b68a067052dedac1ac5beefef968
ford-bubba-ai <ford.lascari@gmail.com>
2026-08-28 09:45:28 -0500

    worker-integration(job-f482): main/melee/gm/gm_16A2::fn_80169A84 [checkpoint 48845a4f]

--- src/melee/gm/gm_16A2.c
+++ src/melee/gm/gm_16A2.c
@@ -333,18 +333,19 @@ s32 fn_80169A84(u8 arg0, s8* arg1, s8* arg2)
         }
 
         {
+            s8* q;
             s32 idx = 0;
-            s8* dst2 = arg1;
-            s8* src2 = arg2;
-            while (*src2 != -2) {
-                while ((result = (p = &lbl_8046B488.x0 + idx)[0x1C0]) == -1) {
+            p = arg1;
+            arg1 = arg2;
+            while (*arg1 != -2) {
+                while ((result = (q = &lbl_8046B488.x0 + idx)[0x1C0]) == -1) {
                     idx = (idx + 1) % 27;
                 }
                 result = Player_800325C8(result, 0);
-                *dst2 = result;
+                *p = result;
                 idx += 1;
-                src2 += 1;
-                dst2 += 1;
+                arg1 += 1;
+                p += 1;
             }
         }
         break;
