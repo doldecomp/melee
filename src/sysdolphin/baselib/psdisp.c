d9972a27b9b35e8a3b3633132b6fc923f8c92085
Mark McCaskey <maoeurk@gmail.com>
2026-08-28 17:14:35 -0700

    psdisp: evaluate the AppSRT current transform x,y,z
    With psDispSubMakePolygon expanded the register pressure that previously
    punished this ordering is gone, so psDispSubAppSRT's cur_pos rows evaluate in
    the target's order and psDispSubAPPSRTPoint carries its z column instead of its
    x. Six real instruction-stream rows recovered (46 -> 40); 768 rows,
    98.7802%.

--- src/sysdolphin/baselib/psdisp.c
+++ src/sysdolphin/baselib/psdisp.c
@@ -1245,7 +1245,7 @@ static inline void psDispSubAPPSRTPoint(HSD_Particle* pp)
     {
         HSD_psAppSRT* appsrt = pp->appsrt;
         f32 pos_y;
-        f32 transformed_x;
+        f32 transformed_z;
 
         cur_pos.x = appsrt->ssy * (pos_y = pp->pos.y);
         cur_pos.y = appsrt->x78 * pos_y;
@@ -1253,10 +1253,10 @@ static inline void psDispSubAPPSRTPoint(HSD_Particle* pp)
         cur_pos.x = appsrt->ssx * pp->pos.x + cur_pos.x;
         cur_pos.z = (cur_pos.y = appsrt->x74 * pp->pos.x + cur_pos.y,
                      appsrt->x84 * pp->pos.x + cur_pos.z);
-        transformed_x = appsrt->x6C * pp->pos.z + cur_pos.x;
-        cur_pos.x = transformed_x;
+        cur_pos.x = appsrt->x6C * pp->pos.z + cur_pos.x;
         cur_pos.y = appsrt->x7C * pp->pos.z + cur_pos.y;
-        cur_pos.z = appsrt->x8C * pp->pos.z + cur_pos.z;
+        transformed_z = appsrt->x8C * pp->pos.z + cur_pos.z;
+        cur_pos.z = transformed_z;
         cur_pos.x = appsrt->x70 + cur_pos.x;
         cur_pos.y = appsrt->x80 + cur_pos.y;
         cur_pos.z = appsrt->x90 + cur_pos.z;
@@ -1419,18 +1419,18 @@ static inline void psDispSubAppSRT(HSD_Particle* pp, u8* texform)
         f32 app_cur_z;
 
         PSMTXCopy((MtxPtr) &pp->appsrt->ssx, draw_mtx);
-        app_cur_z =
-            draw_mtx[2][3] +
-            (draw_mtx[2][2] * pp->pos.z +
-             (draw_mtx[2][0] * pp->pos.x + draw_mtx[2][1] * pp->pos.y));
-        app_cur_y =
-            draw_mtx[1][3] +
-            (draw_mtx[1][2] * pp->pos.z +
-             (draw_mtx[1][0] * pp->pos.x + draw_mtx[1][1] * pp->pos.y));
         app_cur_x =
             draw_mtx[0][3] +
             (draw_mtx[0][2] * pp->pos.z +
              (draw_mtx[0][0] * pp->pos.x + draw_mtx[0][1] * pp->pos.y));
+        app_cur_y =
+            draw_mtx[1][3] +
+            (draw_mtx[1][2] * pp->pos.z +
+             (draw_mtx[1][0] * pp->pos.x + draw_mtx[1][1] * pp->pos.y));
+        app_cur_z =
+            draw_mtx[2][3] +
+            (draw_mtx[2][2] * pp->pos.z +
+             (draw_mtx[2][0] * pp->pos.x + draw_mtx[2][1] * pp->pos.y));
         cur_pos.x = app_cur_x;
         cur_pos.y = app_cur_y;
         cur_pos.z = app_cur_z;
@@ -1948,7 +1948,7 @@ void psDispParticles(u32 target_link, u32 sw)
     u32 prev_kind;
     HSD_Particle* pp;
     /// @todo Recover this stack space from the original inline hierarchy.
-    PAD_STACK(0xC);
+    PAD_STACK(0x8);
 
     alpha_compare_mode = 0;
     prev_tex_interp_near = 0;
