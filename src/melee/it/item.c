5ac4db8bbc8ab1de0f39c1fcd061fd3a9a190c26
sadkellz <forestruns@gmail.com>
2026-08-30 17:41:00 -0230

    run clang format

--- src/melee/it/item.c
+++ src/melee/it/item.c
@@ -454,8 +454,7 @@ static void Item_802676F4(HSD_GObj* gobj)
 
 static /// @remarks #Item_8026862C loads two integers into this,
        ///          but the second one goes _?
-    bool
-    Item_8026784C(enum_t dropItem, int _)
+    bool Item_8026784C(enum_t dropItem, int _)
 {
     bool result = false;
 
@@ -805,8 +804,7 @@ static void Item_8026814C(HSD_GObj* gobj)
 }
 
 static /// @todo Needs some serious cleaning.
-    bool
-    Item_802682F0(HSD_GObj* gobj)
+    bool Item_802682F0(HSD_GObj* gobj)
 {
     s32 var_r4;
     Item* item_data;
@@ -1494,8 +1492,7 @@ static void Item_80269B60(HSD_GObj* gobj)
 static /// @remarks Somewhat arbitrary. Does not run on Hook Shot / Grapple
        /// Beam,
     ///          rather items such as the Barrel Cannon.
-    void
-    Item_80269BE4(HSD_GObj* gobj)
+    void Item_80269BE4(HSD_GObj* gobj)
 {
     Item* item_data = gobj->user_data;
     if (item_data->xDD0_flag.b5) {
