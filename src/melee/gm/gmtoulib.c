45b4990789d178ceec35246507304fa463ebb7f6
ford-bubba-ai <ford.lascari@gmail.com>
2026-08-30 07:21:55 -0500

    worker-integration(job-1fea): main/melee/gm/gmtoulib::fn_8018AA74 [checkpoint 9991fb3e]

--- src/melee/gm/gmtoulib.c
+++ src/melee/gm/gmtoulib.c
@@ -259,7 +259,7 @@ void fn_8018AA74(HSD_JObj* jobj, s32 entry_idx, s32 slot_idx)
                 *p44 = xC;
                 *p34 = xC;
                 {
-                    x18 = entry->x18;
+                    x18 = *pX18;
                     x10 = *pX10;
                     val = x10 + x18 - slot_idx * x18;
                     *p40 = val;
