b3e6bd2016fa3b8ce9944e282e27f9efed1187da
Mark McCaskey <maoeurk@gmail.com>
2026-08-30 21:32:20 -0700

    Guard particle match-only array checks

--- src/sysdolphin/baselib/particle.c
+++ src/sysdolphin/baselib/particle.c
@@ -845,7 +845,12 @@ void* hsd_8039930C(void* pp_arg, void* prev_arg)
 
                     tga = psTexGroupArray[bank];
                     texGrp = tga[tgIdx];
-                    if (texGrp != NULL && texGrp->texTable != NULL) {
+                    if (texGrp != NULL
+#ifdef MUST_MATCH
+                        && texGrp->texTable != NULL
+#endif
+                    )
+                    {
                         if (texGrp->texTable[pp->poseNum] != NULL) {
                             pp->kind |= DispTexture;
                         }
@@ -2023,7 +2028,12 @@ void* hsd_8039930C(void* pp_arg, void* prev_arg)
 
                         tga = psTexGroupArray[bank];
                         texGrp = tga[tgIdx];
-                        if (texGrp != NULL && texGrp->texTable != NULL) {
+                        if (texGrp != NULL
+#ifdef MUST_MATCH
+                            && texGrp->texTable != NULL
+#endif
+                        )
+                        {
                             if (texGrp->texTable[pp->poseNum] != NULL) {
                                 pp->kind |= DispTexture;
                             }
