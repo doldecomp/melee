e392f478229867c3d2f7c651d13a74d4b9f616e0
Mark McCaskey <maoeurk@gmail.com>
2026-08-30 17:43:03 -0700

    Improve ifStock_802F98E8 allocation

--- src/melee/if/ifstock.c
+++ src/melee/if/ifstock.c
@@ -571,17 +571,44 @@ static inline HSD_JObj* ifStock_802F98E8_get_x3C(unsigned char player)
     return ifStock_804A1378.player[player].x3C;
 }
 
+struct IfStockMatchInfo {
+    u8 x4_b0 : 1;
+    u8 x4_b1 : 1;
+    u8 x4_b2 : 1;
+    u8 x4_b3 : 1;
+    u8 x4_b4 : 1;
+    u8 x4_b5 : 1;
+    u8 x4_b6 : 1;
+    u8 x4_b7 : 1;
+};
+
+static inline struct IfStockMatchInfo*
+ifStock_802F98E8_get_match_info(lbl_8046B6A0_t* data, int player)
+{
+    return (struct IfStockMatchInfo*) ((u8*) &data->FighterMatchInfo[player] +
+                                       4);
+}
+
+static inline unsigned char*
+ifStock_802F98E8_get_anim(unsigned char* user_data)
+{
+    return &user_data[5];
+}
+
 void ifStock_802F98E8(unsigned char player, int b)
 {
     struct ifStock_804A1378* stock = &ifStock_804A1378;
+    HSD_GObj* gobj;
     HSD_JObj* jobj;
     unsigned char* user_data;
+    int* stocks;
     int i;
-    HSD_GObj* gobj;
     lbl_8046B6A0_t* ae44;
+    HSD_JObj* icon_jobj;
     if (stock->x0 != NULL) {
         user_data = ((struct IfStockData*) stock)[player].x0;
-        *(user_data += sizeof(struct IfStockDataOffset)) = player;
+        *(user_data += sizeof(struct IfStockDataOffset) -
+                        sizeof(struct IfStockData)) = player;
         stock->x204[player].x0[1] = b;
         stock->x204[player].x0[2] = 1;
         if (stock->player[player].x0 != NULL) {
@@ -592,8 +619,6 @@ void ifStock_802F98E8(unsigned char player, int b)
         if (gobj != NULL) {
             jobj = HSD_JObjLoadJoint((*stock->x0)->joint);
             if (jobj != NULL) {
-                unsigned char* data;
-
                 HSD_GObjObject_80390A70(gobj, HSD_GObj_JObjKind, jobj);
                 GObj_SetupGXLink(gobj, fn_802F94E0, 11, 0);
                 gm_8016895C(jobj, *stock->x0, 0);
@@ -606,45 +631,43 @@ void ifStock_802F98E8(unsigned char player, int b)
                 stock->player[player].x0 = gobj;
                 ifStock_804A1378.player[player].coins =
                     Player_GetCoins(player);
-                ifStock_804A1378.player[player].stocks =
+                *(stocks = &ifStock_804A1378.player[player].stocks) =
                     Player_GetStocks(player);
-                if (ifStock_804A1378.player[player].stocks < 0) {
-                    ifStock_804A1378.player[player].stocks = 1;
+                if (*stocks < 0) {
+                    *stocks = 1;
                 }
-                data = user_data;
                 switch ((unsigned char) b) {
                 case 0:
                     HSD_JObjSetFlagsAll(ifStock_804A1378.player[player].x24,
                                         JOBJ_HIDDEN);
-                    {
-                        struct ifStock_804A1378_per_player* players =
-                            stock->player;
-                        if (players[player].stocks <= 5) {
+                    if (*stocks <= 5) {
                             HSD_JObjSetFlagsAll(
                                 ifStock_804A1378.player[player].x3C,
                                 JOBJ_HIDDEN);
-                            for (i = 0; i < 7; i++, data++) {
-                                jobj =
+                            for (i = 0; i < 7; i++, user_data++) {
+                                icon_jobj =
                                     ifStock_804A1378.player[player].x4[i + 1];
-                                if (i < ifStock_804A1378.player[player]
-                                            .stocks ||
+                                if (i < ifStock_804A1378.player[player].stocks ||
                                     i >= 5)
                                 {
-                                    data[5] = 0;
+                                    user_data[5] = 0;
                                     if (i >= 5) {
-                                        HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                                        HSD_JObjSetFlagsAll(icon_jobj,
+                                                            JOBJ_HIDDEN);
                                     }
                                 } else {
-                                    data[5] = 10;
-                                    HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                                    user_data[5] = 10;
+                                    HSD_JObjSetFlagsAll(icon_jobj, JOBJ_HIDDEN);
                                 }
-                                HSD_JObjReqAnimAll(jobj, data[5]);
-                                HSD_TObjReqAnimAll(jobj->u.dobj->mobj->tobj,
-                                                   gm_80168BF8(player));
-                                HSD_AObjSetRate(jobj->u.dobj->mobj->tobj->aobj,
-                                                0.0f);
+                                HSD_JObjReqAnimAll(
+                                    icon_jobj,
+                                    *ifStock_802F98E8_get_anim(user_data));
+                                HSD_TObjReqAnimAll(
+                                    icon_jobj->u.dobj->mobj->tobj,
+                                    gm_80168BF8(player));
+                                HSD_AObjSetRate(
+                                    icon_jobj->u.dobj->mobj->tobj->aobj, 0.0f);
                             }
-                        }
                     }
                     break;
                 case 1:
@@ -652,24 +675,27 @@ void ifStock_802F98E8(unsigned char player, int b)
                                         JOBJ_HIDDEN);
                     HSD_JObjSetFlagsAll(ifStock_802F98E8_get_x3C(player),
                                         JOBJ_HIDDEN);
-                    for (i = 0; i < 7; i++, data++) {
-                        jobj = ifStock_804A1378.player[player].x4[i + 1];
+                    for (i = 0; i < 7; i++, user_data++) {
+                        icon_jobj =
+                            ifStock_804A1378.player[player].x4[i + 1];
                         if (i == 0) {
-                            data[5] = 0;
+                            user_data[5] = 0;
                         } else {
-                            data[5] = 10;
+                            user_data[5] = 10;
                         }
                         if (i < 5) {
-                            HSD_JObjReqAnimAll(jobj, data[5]);
-                            HSD_TObjReqAnimAll(jobj->u.dobj->mobj->tobj,
+                            HSD_JObjReqAnimAll(
+                                icon_jobj,
+                                *ifStock_802F98E8_get_anim(user_data));
+                            HSD_TObjReqAnimAll(icon_jobj->u.dobj->mobj->tobj,
                                                gm_80168BF8(player));
-                            HSD_AObjSetRate(jobj->u.dobj->mobj->tobj->aobj,
-                                            0.0f);
+                            HSD_AObjSetRate(
+                                icon_jobj->u.dobj->mobj->tobj->aobj, 0.0f);
                             if (i != 0) {
-                                HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                                HSD_JObjSetFlagsAll(icon_jobj, JOBJ_HIDDEN);
                             }
                         } else {
-                            HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                            HSD_JObjSetFlagsAll(icon_jobj, JOBJ_HIDDEN);
                         }
                     }
                     break;
@@ -684,38 +710,45 @@ void ifStock_802F98E8(unsigned char player, int b)
                                         JOBJ_HIDDEN);
                     HSD_JObjSetFlagsAll(ifStock_804A1378.player[player].x3C,
                                         JOBJ_HIDDEN);
-                    for (i = 0; i < 7; i++, data++) {
-                        jobj = ifStock_804A1378.player[player].x4[i + 1];
+                    for (i = 0; i < 7; i++, user_data++) {
+                        icon_jobj =
+                            ifStock_804A1378.player[player].x4[i + 1];
                         if (i == 0) {
-                            data[5] = 0;
+                            user_data[5] = 0;
                         } else {
-                            data[5] = 10;
+                            user_data[5] = 10;
                         }
                         if (i < 5) {
-                            HSD_JObjReqAnimAll(jobj, data[5]);
-                            HSD_TObjReqAnimAll(jobj->u.dobj->mobj->tobj,
+                            HSD_JObjReqAnimAll(
+                                icon_jobj,
+                                *ifStock_802F98E8_get_anim(user_data));
+                            HSD_TObjReqAnimAll(icon_jobj->u.dobj->mobj->tobj,
                                                gm_80168BF8(player));
-                            HSD_AObjSetRate(jobj->u.dobj->mobj->tobj->aobj,
-                                            0.0f);
+                            HSD_AObjSetRate(
+                                icon_jobj->u.dobj->mobj->tobj->aobj, 0.0f);
                             if (i != 0) {
-                                HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                                HSD_JObjSetFlagsAll(icon_jobj, JOBJ_HIDDEN);
                             }
                         } else {
-                            HSD_JObjSetFlagsAll(jobj, JOBJ_HIDDEN);
+                            HSD_JObjSetFlagsAll(icon_jobj, JOBJ_HIDDEN);
                         }
                     }
                     break;
                 }
                 HSD_JObjAnimAll(jobj);
                 ae44 = gm_16AE_GetUnkData_1();
-                if (ae44->FighterMatchInfo[player].x4_b1) {
+                {
+                    struct IfStockMatchInfo* match_info =
+                        ifStock_802F98E8_get_match_info(ae44, player);
+                if (match_info->x4_b1) {
                     GXColor c = { 0x08, 0x08, 0x08, 0x80 };
                     GXColor* color = &c;
                     ifStock_802FB4EC(player, color);
-                } else if (ae44->FighterMatchInfo[player].x4_b0) {
+                } else if (match_info->x4_b0) {
                     GXColor c = { 0x3C, 0x3C, 0x46, 0x80 };
                     GXColor* color = &c;
                     ifStock_802FB4EC(player, color);
+                }
                 }
                 fn_802F9410(gobj); // inlined
             } else {
