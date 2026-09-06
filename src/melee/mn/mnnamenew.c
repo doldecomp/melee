0771b196951bb66a038da4cd84c9c2342c1295c1
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2026-09-06 05:55:13 -0700

    Remove redundant glyph-index cast for clang-tidy

--- src/melee/mn/mnnamenew.c
+++ src/melee/mn/mnnamenew.c
@@ -910,7 +910,7 @@ static inline void copyName(char* name_text, char* name_buffer)
 static inline s32 mnNameNew_CountVariants(GlyphRow* glyphs, u8 selected_key)
 {
     s32 count = 0;
-    char** ptrs = glyphs[(u8) selected_key];
+    char** ptrs = glyphs[selected_key];
     s8 terminator = (s8) *mnNameNew_NullCharacter;
 
     while (terminator != (s8) *ptrs[0]) {
