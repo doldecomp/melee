d4c482986142e3270e1faa260dfc828256c06f82
sadkellz <forestruns@gmail.com>
2026-09-06 14:41:41 -0230

    format

--- src/melee/ft/fighter.c
+++ src/melee/ft/fighter.c
@@ -1268,7 +1268,7 @@ void Fighter_ChangeMotionState(Fighter_GObj* gobj, FtMotionId msid,
                         ftAnim_8006EBE8(gobj, anim_start - anim_speed,
                                         anim_speed,
                                         (anim_blend == -1.0f) ? 0.0f
-                                        : anim_blend          ? anim_blend
+                                        : anim_blend ? anim_blend
                                                      : (*unk_byte_ptr)[0]);
                     }
                     ftAnim_8006E9B4(gobj);
@@ -1289,7 +1289,7 @@ void Fighter_ChangeMotionState(Fighter_GObj* gobj, FtMotionId msid,
                     if (fp->x590 != 0U) {
                         ftAnim_8006EBE8(gobj, anim_start, anim_speed,
                                         (anim_blend == -1.0f) ? 0.0f
-                                        : anim_blend          ? anim_blend
+                                        : anim_blend ? anim_blend
                                                      : (*unk_byte_ptr)[0]);
                     }
                     fp->x3E4_fighterCmdScript.timer = 0.0f;
