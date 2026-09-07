a6becbc1aacc8af7c5d794d86e7487637d5f216b
William Carter <wcarter312@gmail.com>
2026-09-07 12:45:52 -0400

    Fixup ftMaterial's tevdesc

--- src/melee/ft/ftmaterial.c
+++ src/melee/ft/ftmaterial.c
@@ -29,13 +29,17 @@ struct ft_MObjInfo {
 static HSD_TevDesc ftMaterial_803C69D0 = {
     NULL,
     TEVCONF_MODE,
-    0,
-    HSD_TE_UNDEF,
-    HSD_TE_UNDEF,
-    HSD_TE_UNDEF,
+    GX_TEVSTAGE0,
+    GX_TEXCOORD_NULL,
+    GX_TEXCOORD_NULL,
+    GX_COLOR_NULL,
     { {
-        0, 0, 15, 15,    15, 0, 0, true, 0, 0, 7, 7,
-        7, 0, 0,  false, 0,  0, 0, 0,    0, 0, 0,
+        GX_TEV_ADD,   GX_CC_CPREV,    GX_CC_ZERO,     GX_CC_ZERO,
+        GX_CC_ZERO,   GX_CS_SCALE_1,  GX_TB_ZERO,     GX_ENABLE,
+        GX_TEVPREV,   GX_TEV_ADD,     GX_CA_ZERO,     GX_CA_ZERO,
+        GX_CA_ZERO,   GX_CA_APREV,    GX_CS_SCALE_1,  GX_TB_ZERO,
+        GX_DISABLE,   GX_TEVPREV,     GX_TC_LINEAR,   GX_TEV_SWAP0,
+        GX_TEV_SWAP0, GX_TEV_KCSEL_1, GX_TEV_KASEL_1,
     } },
 };
 
