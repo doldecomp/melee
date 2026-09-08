05a1394faea2aac458e4bdd030621d8a5631ae62
Will <wcarter312@gmail.com>
2026-09-08 00:36:49 -0400

    Remove an outdated TODO, cleanup of some functions to address TODOs, more PS* fixup (#3420)
    * Remove outdated comment about a cast
    
    * Cleanup it_8026EB18
    
    * Cleanup of Item_802682F0
    
    * Cleanup gricemt stageGObj0_OnInit
    
    * Cleanup Item_8026814C
    
    * Fixup formatting

--- src/melee/pl/player.c
+++ src/melee/pl/player.c
@@ -236,7 +236,6 @@ void Player_80031AD0(int slot)
     first_struct.has_transformation = false;
     first_struct.x5 = -1;
 
-    /// @todo Eliminate cast.
     player->player_entity[0] = Fighter_Create(&first_struct);
     player->player_state = 2;
 
@@ -255,7 +254,6 @@ void Player_80031AD0(int slot)
         second_struct.has_transformation = has_transformation;
         second_struct.x5 = -1;
 
-        /// @todo Eliminate cast.
         player->player_entity[1] = Fighter_Create(&second_struct);
 
         if (player->player_state != 1) {
