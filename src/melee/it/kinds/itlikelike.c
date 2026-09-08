96894ef4c7034f73427d4992c3f5bec98f3c7061
William Carter <wcarter312@gmail.com>
2026-09-08 16:14:23 -0400

    Remove pointer hacks from itLikeLike

--- src/melee/it/kinds/itlikelike.c
+++ src/melee/it/kinds/itlikelike.c
@@ -1106,14 +1106,11 @@ void it_802DBAF0(Item_GObj* arg0, s32 arg1, s32 arg2)
                 fval = attr->x34;
             }
 
-            // TODO: fix pointer hacks
             if (arg2 != 0) {
-                ftCo_800C7B0C(ip->grab_victim, &sp18, &vec,
-                              (lbColl_80008D30_arg1*) ((u8*) attr + 0x64),
+                ftCo_800C7B0C(ip->grab_victim, &sp18, &vec, &attr->x40[1],
                               fval);
             } else {
-                ftCo_800C7B0C(ip->grab_victim, &sp18, &vec,
-                              (lbColl_80008D30_arg1*) ((u8*) attr + 0x40),
+                ftCo_800C7B0C(ip->grab_victim, &sp18, &vec, &attr->x40[0],
                               fval);
             }
             ip->xDD4_itemVar.likelike.x50 = NULL;
