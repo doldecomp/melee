480b0445408ddcd8db7b8ac64175c6d373053763
Will <wcarter312@gmail.com>
2026-09-10 19:37:52 -0400

    Cleanup lb_0219 with proper allocation, function, and variable naming (#3444)
    * Cleanup lb_0219 with proper allocation, function, and variable naming
    
    * Remove unnecessary pragma and update function name
    
    * Update alloc data naming

--- src/melee/lb/lb_0219.h
+++ src/melee/lb/lb_0219.h
@@ -10,10 +10,10 @@
 
 #include <sysdolphin/baselib/forward.h>
 
-/* 0219E4 */ void fn_800219E4(void*);
-/* 021A10 */ void lbBgFlash_80021A10(f32);
-/* 021A18 */ void lbBgFlash_80021A18(int);
-/* 021B04 */ void fn_80021B04(HSD_GObj*);
+/* 0219E4 */ void lbBgFlash_Free(void*);
+/* 021A10 */ void lbBgFlash_SetFlashScale(f32);
+/* 021A18 */ void lbBgFlash_Init(int);
+/* 021B04 */ void lbBgFlash_Proc(HSD_GObj*);
 /* 021C1C */ void fn_80021C1C(void);
 /* 021C48 */ void lbBgFlash_80021C48(u32, u32);
 /* 021C80 */ void fn_80021C80(HSD_GObj* gobj);
