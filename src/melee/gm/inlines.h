2093aa3111b0c2967acd2958655cabfa96b7058e
Mark McCaskey <maoeurk@gmail.com>
2026-09-13 17:51:44 -0700

    gm: share gmTournament_GetPlayerX/SetPlayerX between gmtou_1 and gmtou_2

--- src/melee/gm/inlines.h
+++ src/melee/gm/inlines.h
@@ -41,4 +41,26 @@ static inline s32 fn_801A7FB4_inline2(void)
     return count;
 }
 
+static inline f32 gmTournament_GetPlayerX(u8 player_count, s32 player)
+{
+    if ((s32) player_count == 4) {
+        return (13.0f * (f32) player) + -19.5f;
+    }
+    if ((s32) player_count == 3) {
+        return 6.5f + ((13.0f * (f32) player) - 19.5f);
+    }
+    return 6.5f + ((13.0f * (2.0f * (f32) player)) - 19.5f);
+}
+
+static inline void gmTournament_SetPlayerX(f32* x, u8 player_count, s32 player)
+{
+    if ((s32) player_count == 4) {
+        *x = (13.0f * (f32) player) + -19.5f;
+    } else if ((s32) player_count == 3) {
+        *x = 6.5f + ((13.0f * (f32) player) - 19.5f);
+    } else {
+        *x = 6.5f + ((13.0f * (2.0f * (f32) player)) - 19.5f);
+    }
+}
+
 #endif
