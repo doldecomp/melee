7a0d88978aaa5e5baf8bb6a76a6621b492f93083
William Carter <wcarter312@gmail.com>
2026-09-15 19:02:45 -0400

    Fix grGreens datatype and grGreens_802159B8 function definition

--- src/melee/gr/grgreens.h
+++ src/melee/gr/grgreens.h
@@ -52,7 +52,7 @@
 /* 2150C4 */ void grGreens_802150C4(Ground_GObj*, int, int);
 /* 215358 */ void grGreens_80215358(Ground_GObj*, int, int, int, int);
 /* 2159B4 */ void fn_802159B4(Item_GObj*, Ground*);
-/* 2159B8 */ void grGreens_802159B8(Ground*, int, int, int);
+/* 2159B8 */ void grGreens_802159B8(Ground*, int, int, HSD_GObj*);
 /* 215B84 */ void fn_80215B84(Item_GObj*, Ground*, Vec*, HSD_GObj*, float);
 /* 215D50 */ void fn_80215D50(Item_GObj*, Ground*, HSD_GObj*);
 /* 215D54 */ s32 grGreens_80215D54(Ground_GObj*, int, int);
