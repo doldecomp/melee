1c5220e64b06ccfdbb99fa5e79bd7b76c76dc895
William Carter <wcarter312@gmail.com>
2026-09-15 19:47:57 -0400

    Remove GObj -> u32 cast for NULL check in itwhitebea

--- src/melee/it/kinds/itwhitebea.c
+++ src/melee/it/kinds/itwhitebea.c
@@ -98,7 +98,7 @@ ItemStateTable it_803F8A88[] = {
 void it_802E31F8(Item_GObj* gobj)
 {
     Item* ip = gobj->user_data;
-    if ((u32) ip->xDD4_itemVar.whitebea.x20 != 0U) {
+    if (ip->xDD4_itemVar.whitebea.x20 != NULL) {
         it_8028ECE0((HSD_GObj*) ip->xDD4_itemVar.whitebea.x20);
         it_802E37A4(gobj);
     }
@@ -109,7 +109,7 @@ bool itOldottosea_UnkMotion8_Anim(Item_GObj* gobj)
 {
     if (!it_80272C6C(gobj)) {
         Item* ip = GET_ITEM(gobj);
-        if ((u32) ip->xDD4_itemVar.whitebea.x20 != 0U) {
+        if (ip->xDD4_itemVar.whitebea.x20 != NULL) {
             it_8028ECE0((HSD_GObj*) ip->xDD4_itemVar.whitebea.x20);
             it_802E37A4(gobj);
         }
@@ -188,7 +188,7 @@ bool itOldottosea_UnkMotion10_Coll(Item_GObj* gobj)
 void it_2725_Logic3_Destroyed(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    if ((u32) ip->xDD4_itemVar.whitebea.x20 != 0U) {
+    if (ip->xDD4_itemVar.whitebea.x20 != NULL) {
         it_8028ECE0((HSD_GObj*) ip->xDD4_itemVar.whitebea.x20);
     }
     it_802E37A4(gobj);
