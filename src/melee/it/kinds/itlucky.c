2769a44ea731d68045aa1dd521189c55c3321f95
Mark McCaskey <5770194+MarkMcCaskey@users.noreply.github.com>
2026-09-15 22:24:38 -0700

    Remove all uses of M2C_FIELD (#3523)
    * Replace raw M2C fields with typed game and item accesses
    
    * Use the trophy animation object directly
    
    * Represent snapshot slot joints as an array

--- src/melee/it/kinds/itlucky.c
+++ src/melee/it/kinds/itlucky.c
@@ -4,7 +4,6 @@
 
 #include <melee/it/forward.h>
 
-#include <m2c_macros.h>
 #include <placeholder.h>
 
 #include "itegg.h"
@@ -351,10 +350,10 @@ Item_GObj* it_802D5710(Item_GObj* gobj, Vec3* pos, Vec3* vel, f32 facing)
 void itLucky_Logic44_Spawned(Item_GObj* gobj)
 {
     Item* ip = GET_ITEM(gobj);
-    itLuckyAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
+    itLuckyEggAttributes* attr = ip->xC4_article_data->x4_specialAttributes;
     PAD_STACK(4);
     it_80275158(gobj, attr->x0);
-    ip->xDD4_itemVar.lucky.x60 = M2C_FIELD(attr, s32*, 4); ///< @todo
+    ip->xDD4_itemVar.lucky.x60 = attr->x4;
     it_802D58EC(gobj);
 }
 
