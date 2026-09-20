b87937aaa807ad6afcce2e7ab44d010c64ff3727
William Carter <wcarter312@gmail.com>
2026-09-19 20:17:33 -0400

    Use HSD_OBJECT_INFO for HSD_FogAlloc instead of `.parent.parent`

--- src/sysdolphin/baselib/fog.c
+++ src/sysdolphin/baselib/fog.c
@@ -85,7 +85,7 @@ void HSD_FogSet(HSD_Fog* fog)
 
 HSD_Fog* HSD_FogAlloc(void)
 {
-    HSD_Fog* fog = hsdNew(&hsdFog.parent.parent);
+    HSD_Fog* fog = hsdNew(HSD_OBJECT_INFO(&hsdFog));
     HSD_ASSERT(0x8C, fog);
     return fog;
 }
