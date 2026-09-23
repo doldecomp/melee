49f3b8443f15c61cc4219ad650d0a6c32afb9578
Mark McCaskey <maoeurk@gmail.com>
2026-09-23 10:00:51 -0700

    Remove dead inline_depth pragma in lb_020A
    It only restored the default after the inline_depth(1) removed earlier.

--- src/melee/lb/lb_020A.c
+++ src/melee/lb/lb_020A.c
@@ -110,9 +110,6 @@ void fn_80020AEC(HSD_JObj* jobj, Mtx out)
         cur = HSD_JObjGetParent(cur);
     }
 }
-#ifdef MUST_MATCH
-#pragma inline_depth(8)
-#endif
 void lbBgFlash_80020E38(HSD_JObj* jobj, Vec3* dir, f32 max_angle,
                         f32 min_angle, f32 unused)
 {
